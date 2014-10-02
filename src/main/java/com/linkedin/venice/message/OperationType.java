1d26744ed6238e33b6b72a325775dd414bd7c0fa
Clement Fung <clfung@linkedin.com>
2014-10-02 15:08:00 -0700

    Deleting original project venice.

--- src/main/java/com/linkedin/venice/message/OperationType.java
+++
@@ -1,8 +0,0 @@
-package com.linkedin.venice.message;
-
-/**
- * Enum which declares the possible types of operation supported by Venice.
- */
-public enum OperationType {
-  PUT, DELETE, PARTIAL_PUT
-}
--- src/main/java/com/linkedin/venice/storage/StorageType.java
+++
@@ -1,7 +0,0 @@
-package com.linkedin.venice.storage;
-
-/**
- * An enum that provides the available storage solutions in Venice.
- */
-public enum StorageType {
-  VOLDEMORT, BDB, MEMORY};
