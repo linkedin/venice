fd9c5768b1d94dd2958d4c563fd437249dcc636f
Clement Fung <clfung@linkedin.com>
2014-10-03 17:06:02 -0700

    Adding USAGE message to shell client

--- venice-client/bin/venice-client.sh
+++ venice-client/bin/venice-client.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+USAGE="USAGE: venice-client.sh [--command] --key [key] --value [value]"
+
 projectName="venice-client"
 
 base_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. && pwd )"
@@ -11,38 +13,41 @@ do
 
   case "$1" in
 
-     --put) operation="put"; shift 1;;
+    --put) operation="put"; shift 1;;
 
-     --delete) operation="delete"; shift 1;;     
+    --delete) operation="delete"; shift 1;;     
 
-     --get) operation="get"; shift 1;;
+    --get) operation="get"; shift 1;;
 
-     --key|-k) key=$2; shift 2;;
+    --key|-k) key=$2; shift 2;;
 
-     --value|-v) value=$2; shift 2;;
+    --value|-v) value=$2; shift 2;;
 
-   esac
+  esac
 done
 
 # Check that a valid operation has been given.
 if [[ $operation == "" ]]
 then
-   echo "No operation has been defined! Exiting..."
-   exit 1
+  echo "No operation has been defined! Exiting..."
+  echo $USAGE
+  exit 1
 fi
 
 # Check that a key exists
 if [[ $key == "" ]]
 then
-   echo "No key has been defined! Exiting...";
-   exit 1
+  echo "No key has been defined! Exiting...";
+  echo $USAGE
+  exit 1
 fi
 
 # Check that if a put is given, a value exists
 if [[ $operation == "put" && $value = "" ]]
 then
-   echo "Trying to execute a put, but no value given. Exiting...."
-   exit 1
+  echo "Trying to execute a put, but no value given. Exiting...."
+  echo $USAGE
+  exit 1
 fi
 
 args="$operation $key $value"
