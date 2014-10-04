cc4228d016f84a53a0efb9691b2bb77c751e8364
Clement Fung <clfung@linkedin.com>
2014-10-03 17:16:45 -0700

    Rerouting all traffic to use VeniceShellClient as the main class

--- venice-client/src/main/java/com/linkedin/venice/client/VeniceShellClient.java
+++ venice-client/src/main/java/com/linkedin/venice/client/VeniceShellClient.java
@@ -1,16 +1,63 @@
 package com.linkedin.venice.client;
 
-import com.linkedin.venice.Venice;
+import java.util.Scanner;
 
 /**
  * Class which acts as the primary interface when calling Venice from the run-client.sh script.
- * This class will become the primary main function for the client once the code base is divided between client/server.
  */
 public class VeniceShellClient {
 
+  static VeniceClient mainClient = new VeniceClient();
+
+  /*
+  * Main method for running the class in an IDE.
+  * The preferred method will be to use the VeniceShellClient via the client script.
+  * */
   public static void main(String[] args) {
 
-    Venice.execute(args);
+    // Running in IDE; use interactive shell
+    if (args.length < 2) {
+
+      Scanner reader = new Scanner(System.in);
+
+      while (true) {
+
+        System.out.println("Ready for input: ");
+        String input = reader.nextLine();
+        String[] commandArgs = input.split(" ");
+
+        execute(commandArgs);
+
+      }
+
+    } else {
+
+      // executed from the venice-client.sh script: simply pass the arguments onwards
+      execute(args);
+
+    }
+
+  }
+
+  /**
+   * A function used to send client inputs into the Venice Server.
+   * This method is used by both the Venice main class and the VeniceShellClient
+   * */
+  public static void execute(String[] commandArgs) {
+
+    if (commandArgs[0].equals("put")) {
+
+      mainClient.put(commandArgs[1], commandArgs[2]);
+
+    } else if (commandArgs[0].equals(("get"))) {
+
+      System.out.println("Got: " + mainClient.get(commandArgs[1]));
+
+    } else if (commandArgs[0].equals("delete")) {
+
+      mainClient.delete(commandArgs[1]);
+
+    }
 
   }
 
