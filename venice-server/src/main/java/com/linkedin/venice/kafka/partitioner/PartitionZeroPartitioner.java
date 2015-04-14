22c8728a860fdc0f2a2dddaea5a54f35e2268454
Felix GV <fgv@linkedin.com>
2015-04-14 15:36:48 -0400

    Fixed bug in PartitionZeroPartitioner (added the constructor Kafka expects).

--- venice-server/src/main/java/com/linkedin/venice/kafka/partitioner/PartitionZeroPartitioner.java
+++ venice-server/src/main/java/com/linkedin/venice/kafka/partitioner/PartitionZeroPartitioner.java
@@ -1,11 +1,15 @@
 package com.linkedin.venice.kafka.partitioner;
 
 import kafka.producer.Partitioner;
+import kafka.utils.VerifiableProperties;
 
 /**
  * Used for the ACK producer so we always route ACKs to the same partition...
  */
 public class PartitionZeroPartitioner implements Partitioner {
+  public PartitionZeroPartitioner(VerifiableProperties props) {
+  }
+
   @Override
   public int partition(Object key, int numPartitions) {
     return 0;
