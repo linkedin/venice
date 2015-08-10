926896c8eda457494ba151728fce70f3073a3a41
Akash Kapoor <aakapoor@linkedin.com>
2015-08-09 21:08:03 -0700

    Unit test from StateModel and KafkaConsumptionTask

---
+++ venice-server/src/test/java/com/linkedin/venice/helix/VenicePartitionStateModelTest.java
@@ -0,0 +1,113 @@
+package com.linkedin.venice.helix;
+
+import com.linkedin.venice.config.VeniceStoreConfig;
+import com.linkedin.venice.kafka.consumer.KafkaConsumerService;
+import com.linkedin.venice.server.StoreRepository;
+import com.linkedin.venice.store.AbstractStorageEngine;
+import org.apache.helix.NotificationContext;
+import org.apache.helix.model.Message;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+/**
+ * Unit tests to verify the State model takes the appropriate decisions on transitions.
+ */
+public class VenicePartitionStateModelTest {
+
+  private KafkaConsumerService mockKafkaConsumerService;
+  private StoreRepository mockStoreRepository;
+  private VeniceStoreConfig mockStoreConfig;
+  private AbstractStorageEngine mockAbstractStorageEngine;
+  private int testPartition = 0;
+
+  private Message mockMessage;
+  private NotificationContext mockContext;
+
+  private VenicePartitionStateModel testStateModel;
+
+  @BeforeSuite
+  public void setUp() throws Exception {
+    mockKafkaConsumerService = PowerMockito.mock(KafkaConsumerService.class);
+    mockStoreRepository = PowerMockito.mock(StoreRepository.class);
+    mockStoreConfig = PowerMockito.mock(VeniceStoreConfig.class);
+
+    mockAbstractStorageEngine = PowerMockito.mock(AbstractStorageEngine.class);
+    PowerMockito.when(mockStoreRepository.getLocalStorageEngine(Mockito.anyString()))
+        .thenReturn(mockAbstractStorageEngine);
+
+    mockMessage = PowerMockito.mock(Message.class);
+    mockContext = PowerMockito.mock(NotificationContext.class);
+
+    testStateModel = new VenicePartitionStateModel(mockKafkaConsumerService, mockStoreRepository, mockStoreConfig,
+        testPartition);
+  }
+
+  /**
+   * Verifies the following:
+   *  1. Kafka Partition consumption turned on.
+   *  2. Partition is added to local storage engine if required.
+   *    2.1 KafkaConsumption offset is reset if new local partition created.
+   */
+  @Test
+  public void testOnBecomeOnlineFromOffline() throws Exception {
+    // Check that only KafkaConsumption is started when corresponding partition information exists on local storage.
+    PowerMockito.when(mockAbstractStorageEngine.containsPartition(testPartition)).thenReturn(true);
+    testStateModel.onBecomeOnlineFromOffline(mockMessage, mockContext);
+    Mockito.verify(mockKafkaConsumerService, Mockito.atLeastOnce()).startConsumption(mockStoreConfig, testPartition);
+    Mockito.verify(mockAbstractStorageEngine, Mockito.never()).addStoragePartition(testPartition);
+
+    /*
+     * When partition information does not exists on local storage, Check that KafkaConsumption is started,
+     * local partition is created and kafka offset is reset.
+     */
+    PowerMockito.when(mockAbstractStorageEngine.containsPartition(testPartition)).thenReturn(false);
+    testStateModel.onBecomeOnlineFromOffline(mockMessage, mockContext);
+    Mockito.verify(mockKafkaConsumerService, Mockito.atLeastOnce()).startConsumption(mockStoreConfig, testPartition);
+    Mockito.verify(mockAbstractStorageEngine, Mockito.atLeastOnce()).addStoragePartition(testPartition);
+    Mockito.verify(mockKafkaConsumerService, Mockito.atLeastOnce()).resetConsumptionOffset(mockStoreConfig, testPartition);
+  }
+
+  /**
+   * Verifies the following:
+   *  1. Kafka Partition consumption is turned off.
+   */
+  @Test
+  public void testOnBecomeOfflineFromOnline() throws Exception {
+    testStateModel.onBecomeOfflineFromOnline(mockMessage, mockContext);
+    Mockito.verify(mockKafkaConsumerService, Mockito.atLeastOnce()).stopConsumption(mockStoreConfig, testPartition);
+  }
+
+  /**
+   * Verifies the following:
+   *  1. Information in the local engine is cleared for the dropped partition.
+   */
+  @Test
+  public void testOnBecomeDroppedFromOffline() throws Exception {
+    testStateModel.onBecomeDroppedFromOffline(mockMessage, mockContext);
+    Mockito.verify(mockAbstractStorageEngine, Mockito.atLeastOnce()).removePartition(testPartition);
+  }
+
+  /**
+   * Verifies the following:
+   *  1. Kafka Consumption is stopped.
+   */
+  @Test
+  public void testOnBecomeOfflineFromError() throws Exception {
+    testStateModel.onBecomeOfflineFromError(mockMessage, mockContext);
+    Mockito.verify(mockKafkaConsumerService, Mockito.atLeastOnce()).stopConsumption(mockStoreConfig, testPartition);
+  }
+
+  /**
+   * Verifies the following:
+   *  1. Kafka Consumption is stopped.
+   *  2. Information from the local storage engine is deleted for the dropped partition.
+   */
+  @Test
+  public void testOnBecomeDroppedFromError() throws Exception {
+    testStateModel.onBecomeDroppedFromError(mockMessage, mockContext);
+    Mockito.verify(mockKafkaConsumerService, Mockito.atLeastOnce()).stopConsumption(mockStoreConfig, testPartition);
+    Mockito.verify(mockAbstractStorageEngine, Mockito.atLeastOnce()).removePartition(testPartition);
+  }
+}
---
+++ venice-server/src/test/java/com/linkedin/venice/kafka/consumer/KafkaPerStoreConsumptionTaskTest.java
@@ -0,0 +1,167 @@
+package com.linkedin.venice.kafka.consumer;
+
+import com.linkedin.venice.message.KafkaKey;
+import com.linkedin.venice.message.KafkaValue;
+import com.linkedin.venice.message.OperationType;
+import com.linkedin.venice.serialization.Avro.AzkabanJobAvroAckRecordGenerator;
+import com.linkedin.venice.server.StoreRepository;
+import com.linkedin.venice.store.AbstractStorageEngine;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import org.apache.kafka.clients.consumer.CommitType;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.common.TopicPartition;
+import org.mockito.Mockito;
+
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.testng.PowerMockTestCase;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+/**
+ * Unit tests for the KafkaPerStoreConsumptionTask.
+ */
+@PrepareForTest({KafkaPerStoreConsumptionTask.class})
+public class KafkaPerStoreConsumptionTaskTest extends PowerMockTestCase {
+
+  public static final int TIMEOUT = 1000;
+  private KafkaConsumer mockKafkaConsumer;
+  private StoreRepository mockStoreRepository;
+  private Producer mockAckProducer;
+  private AzkabanJobAvroAckRecordGenerator mockAckRecordGenerator;
+  private Properties mockKafkaConsumerProperties;
+  private AbstractStorageEngine mockAbstractStorageEngine;
+
+  private ExecutorService taskPollingService;
+
+  private final int nodeId = 0;
+  private final String topic = "TestTopic";
+  private final int testPartition = 0;
+  private final TopicPartition testTopicPartition = new TopicPartition(topic, testPartition);
+
+  private final String putTestKey = "TestKeyPut";
+  private final String putTestValue = "TestValuePut";
+  private final String deleteTestKey = "TestKeyDelete";
+  private final String deleteTestValue = "TestValueDelete";
+
+  @BeforeSuite
+  public void setUp() throws Exception {
+    taskPollingService = Executors.newFixedThreadPool(1);
+  }
+
+  @AfterSuite
+  public void tearDown() throws Exception {
+    taskPollingService.shutdown();
+  }
+
+  private KafkaPerStoreConsumptionTask getKafkaPerStoreConsumptionTask() throws Exception {
+    mockKafkaConsumer = PowerMockito.mock(KafkaConsumer.class);
+    mockStoreRepository = PowerMockito.mock(StoreRepository.class);
+    mockAckProducer = PowerMockito.mock(Producer.class);
+    mockAckRecordGenerator = PowerMockito.mock(AzkabanJobAvroAckRecordGenerator.class);
+    mockKafkaConsumerProperties = PowerMockito.mock(Properties.class);
+    mockAbstractStorageEngine = PowerMockito.mock(AbstractStorageEngine.class);
+
+    PowerMockito.whenNew(KafkaConsumer.class).withParameterTypes(Properties.class)
+        .withArguments(mockKafkaConsumerProperties).thenReturn(mockKafkaConsumer);
+
+    return new KafkaPerStoreConsumptionTask(mockKafkaConsumerProperties, mockStoreRepository, mockAckProducer,
+        mockAckRecordGenerator, nodeId, topic);
+  }
+
+  /**
+   * Verifies that the KafkaTaskMessages are processed appropriately by invoking corresponding method on the
+   * KafkaConsumer.
+   */
+  @Test
+  public void testKafkaTaskMessagesProcessing() throws Exception {
+    // Get KafkaPerStoreConsumptionTask with fresh mocks to test & schedule it.
+    KafkaPerStoreConsumptionTask testSubscribeTask = getKafkaPerStoreConsumptionTask();
+    Future testSubscribeTaskFuture = taskPollingService.submit(testSubscribeTask);
+
+    // Verifies KafkaPerStoreConsumptionTask#subscribePartition invokes KafkaConsumer#subscribe with expected arguments.
+    testSubscribeTask.subscribePartition(topic, testPartition);
+    Mockito.verify(mockKafkaConsumer, Mockito.timeout(TIMEOUT).times(1)).subscribe(testTopicPartition);
+
+    // Prepare the Mocked KafkaConsumer to correctly reflect the subscribed partition.
+    Set<TopicPartition> mockKafkaConsumerSubscriptions = new HashSet<>();
+    mockKafkaConsumerSubscriptions.add(testTopicPartition);
+    PowerMockito.when(mockKafkaConsumer.subscriptions()).thenReturn(mockKafkaConsumerSubscriptions);
+
+    /*
+     * Verifies KafkaPerStoreConsumptionTask#resetPartitionConsumptionOffset invokes KafkaConsumer#seekToBeginning &
+     * KafkaConsumer#commit with expected arguments.
+     */
+    testSubscribeTask.resetPartitionConsumptionOffset(topic, testPartition);
+    Mockito.verify(mockKafkaConsumer, Mockito.timeout(TIMEOUT).times(1)).seekToBeginning(testTopicPartition);
+    Mockito.verify(mockKafkaConsumer, Mockito.timeout(TIMEOUT).times(1)).commit(Mockito.anyMap(),
+        Mockito.eq(CommitType.SYNC));
+
+    // Verifies KafkaPerStoreConsumptionTask#unsubscribePartition invokes KafkaConsumer#unsubscribe with expected arguments.
+    testSubscribeTask.unsubscribePartition(topic, testPartition);
+    Mockito.verify(mockKafkaConsumer, Mockito.timeout(TIMEOUT).times(1)).unsubscribe(testTopicPartition);
+
+    testSubscribeTask.stop();
+    testSubscribeTaskFuture.get();
+  }
+
+  /**
+   * Verifies that the VeniceMessages from KafkaConsumer are processed appropriately as follows:
+   *   1. A VeniceMessage with PUT requests leads to invoking of AbstractStorageEngine#put.
+   *   2. A VeniceMessage with DELETE requests leads to invoking of AbstractStorageEngine#put.
+   */
+  @Test
+  public void testVeniceMessagesProcessing() throws Exception {
+
+    // Get the KafkaPerStoreConsumptionTask with fresh mocks.
+    KafkaPerStoreConsumptionTask testSubscribeTask = getKafkaPerStoreConsumptionTask();
+    testSubscribeTask.subscribePartition(topic, testPartition);
+
+    // Prepare poll results.
+    Map<TopicPartition, List<ConsumerRecord>> mockPollResult = new HashMap<>();
+    List<ConsumerRecord> testVeniceMessages = new ArrayList<>();
+    ConsumerRecord<KafkaKey, KafkaValue> testPutRecord = new ConsumerRecord(topic, testPartition, 10, new KafkaKey(
+        OperationType.WRITE, putTestKey.getBytes()), new KafkaValue(OperationType.PUT, putTestValue.getBytes()));
+    ConsumerRecord<KafkaKey, KafkaValue> testDeleteRecord =
+        new ConsumerRecord(topic, testPartition, 11, new KafkaKey(OperationType.WRITE, deleteTestKey.getBytes()),
+            new KafkaValue(OperationType.DELETE, deleteTestValue.getBytes()));
+    testVeniceMessages.add(testPutRecord);
+    testVeniceMessages.add(testDeleteRecord);
+    mockPollResult.put(testTopicPartition, testVeniceMessages);
+    ConsumerRecords testPollConsumerRecords = new ConsumerRecords(mockPollResult);
+
+    // Prepare the mockKafkaConsumer to send the test poll results.
+    PowerMockito.when(mockKafkaConsumer.poll(Mockito.anyLong())).thenReturn(testPollConsumerRecords);
+    // Prepare mockStoreRepository to send a mock storage engine.
+    PowerMockito.when(mockStoreRepository.getLocalStorageEngine(topic)).thenReturn(mockAbstractStorageEngine);
+
+    // MockKafkaConsumer is prepared. Schedule for polling.
+    Future testSubscribeTaskFuture = taskPollingService.submit(testSubscribeTask);
+
+    // Verify KafkaConsumer#poll is invoked.
+    Mockito.verify(mockKafkaConsumer, Mockito.timeout(TIMEOUT).atLeastOnce()).poll(Mockito.anyLong());
+    // Verify StorageEngine#put is invoked with appropriate key & value.
+    Mockito.verify(mockAbstractStorageEngine, Mockito.timeout(TIMEOUT).atLeastOnce())
+        .put(testPartition, putTestKey.getBytes(), putTestValue.getBytes());
+    // Verify StorageEngine#put is invoked with appropriate key.
+    Mockito.verify(mockAbstractStorageEngine, Mockito.timeout(TIMEOUT).atLeastOnce()).delete(testPartition,
+        deleteTestKey.getBytes());
+
+    testSubscribeTask.stop();
+    testSubscribeTaskFuture.get();
+  }
+
+}
