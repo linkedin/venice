8129b5ee92ec09c6f19301c32d6672e619b7678a
Arunachalam Thirupathi <arunachalam.t@gmail.com>
2016-02-09 12:16:20 -0800

    Upgrade Kafka Client dependency
    After talking to Kafka team upgrading the Kafka dependency
    0.8.2.42 as 0.8.2.20 has known issues.
    
    This requires the Kafka Server to be 0.9 as well, otherwise
    it fails with following cryptic message.
    
    https://issues.apache.org/jira/browse/KAFKA-2750
    
    [2015-11-04 18:55:43,583] ERROR Uncaught error in kafka producer I/O
    thread: (org.apache.kafka.clients.producer.internals.Sender)
    org.apache.kafka.common.protocol.types.SchemaException: Error reading
    field 'throttle_time_ms': java.nio.BufferUnderflowException

--- venice-server/src/main/java/com/linkedin/venice/kafka/consumer/ApacheKafkaConsumer.java
+++ venice-server/src/main/java/com/linkedin/venice/kafka/consumer/ApacheKafkaConsumer.java
@@ -1,8 +1,11 @@
 package com.linkedin.venice.kafka.consumer;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
+import java.util.Set;
 import org.apache.kafka.clients.consumer.CommitType;
 import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
@@ -28,16 +31,25 @@ public class ApacheKafkaConsumer implements VeniceConsumer {
     @Override
     public void subscribe(String topic, int partition) {
         TopicPartition topicPartition = new TopicPartition(topic, partition);
-        if(!kafkaConsumer.subscriptions().contains(topicPartition)) {
-            kafkaConsumer.subscribe(topicPartition);
+
+        Set<TopicPartition> topicPartitionSet = kafkaConsumer.assignment();
+        if(!topicPartitionSet.contains(topicPartition)) {
+            List<TopicPartition> topicPartitionList = new ArrayList<>(topicPartitionSet);
+            topicPartitionList.add(topicPartition);
+            kafkaConsumer.assign(topicPartitionList);
         }
     }
 
     @Override
     public void unSubscribe(String topic, int partition) {
         TopicPartition topicPartition = new TopicPartition(topic, partition);
-        if(kafkaConsumer.subscriptions().contains(topicPartition)) {
-            kafkaConsumer.unsubscribe(topicPartition);
+
+        Set<TopicPartition> topicPartitionSet = kafkaConsumer.assignment();
+        if(topicPartitionSet.contains(topicPartition)) {
+            List<TopicPartition> topicPartitionList = new ArrayList<>(topicPartitionSet);
+            if( topicPartitionList.remove(topicPartition) ) {
+                kafkaConsumer.assign(topicPartitionList);
+            }
         }
     }
 
