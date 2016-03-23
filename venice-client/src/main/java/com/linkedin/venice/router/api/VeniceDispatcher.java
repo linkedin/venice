ed8f2572d816fe9c5cbab79a04ce00c2a6f0b3bc
Matthew Wise <mwise@linkedin.com>
2016-03-22 17:22:23 -0700

    add auto-retry logic for slow storagenodes
    RB=688021
    G=voldemort-reviewers
    R=acurtis
    A=yayan

--- venice-client/src/main/java/com/linkedin/venice/router/api/VeniceDispatcher.java
+++ venice-client/src/main/java/com/linkedin/venice/router/api/VeniceDispatcher.java
@@ -4,22 +4,20 @@ import com.linkedin.ddsstorage.base.concurrency.AsyncFuture;
 import com.linkedin.ddsstorage.base.concurrency.AsyncPromise;
 import com.linkedin.ddsstorage.netty3.misc.BasicHttpRequest;
 import com.linkedin.ddsstorage.router.api.PartitionDispatchHandler;
-import com.linkedin.ddsstorage.router.api.RouterException;
 import com.linkedin.ddsstorage.router.api.Scatter;
 import com.linkedin.ddsstorage.router.api.ScatterGatherRequest;
+import com.linkedin.venice.HttpConstants;
 import com.linkedin.venice.exceptions.VeniceException;
 import com.linkedin.venice.meta.Instance;
-import java.io.ByteArrayInputStream;
 import java.io.Closeable;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.Executor;
 import org.apache.commons.io.IOUtils;
 import org.apache.http.client.methods.HttpGet;
@@ -27,8 +25,7 @@ import org.apache.http.concurrent.FutureCallback;
 import org.apache.http.impl.DefaultConnectionReuseStrategy;
 import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
 import org.apache.http.impl.nio.client.HttpAsyncClients;
-import org.apache.http.nio.client.HttpAsyncClient;
-import org.apache.http.protocol.HTTP;
+import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.jboss.netty.buffer.ChannelBuffers;
 import org.jboss.netty.handler.codec.http.DefaultHttpResponse;
@@ -49,6 +46,9 @@ public class VeniceDispatcher implements PartitionDispatchHandler<Instance, Path
 
   // see: https://hc.apache.org/httpcomponents-asyncclient-dev/quickstart.html
   private final Map<Instance, CloseableHttpAsyncClient> clientPool;
+
+  // key is (resource + "_" + partition)
+  private final ConcurrentMap<String, Long> offsets = new ConcurrentHashMap<>();
 
   public VeniceDispatcher(){
     clientPool = new ConcurrentHashMap<>();
@@ -75,7 +75,7 @@ public class VeniceDispatcher implements PartitionDispatchHandler<Instance, Path
       hostSelected.setFailure(e);
       throw new VeniceException("Failed to route request to a host");
     }
-    logger.info("Routing request to host: " + host.getHost() + ":" + host.getHttpPort());
+    logger.debug("Routing request to host: " + host.getHost() + ":" + host.getHttpPort());
 
     CloseableHttpAsyncClient httpClient;
     if (clientPool.containsKey(host)){
@@ -90,7 +90,7 @@ public class VeniceDispatcher implements PartitionDispatchHandler<Instance, Path
     }
 
     String requestPath = path.getLocation();
-    logger.info("Using request path: " + requestPath);
+    logger.debug("Using request path: " + requestPath);
 
     //  http://host:port/path
     String address = HTTP + host.getHost() + ":" + host.getHttpPort() + "/" + requestPath;
@@ -99,6 +99,16 @@ public class VeniceDispatcher implements PartitionDispatchHandler<Instance, Path
 
       @Override
       public void completed(org.apache.http.HttpResponse result) {
+        long offset = Long.parseLong(result.getFirstHeader(HttpConstants.VENICE_OFFSET).getValue());
+        String offsetKey = path.getResourceName() + "_" + part.getPartitionsNames().iterator().next();
+        if ( offsets.containsKey(offsetKey) && offset < offsets.get(offsetKey) ) {
+          contextExecutor.execute(() -> {
+            // Triggers an immediate router retry excluding the host we selected.
+            retryFuture.setSuccess(HttpResponseStatus.SERVICE_UNAVAILABLE);
+          });
+          return;
+        }
+        offsets.put(offsetKey, offset);
         HttpResponse response = new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK);
         try (InputStream contentStream = result.getEntity().getContent()) {
           response.setContent(ChannelBuffers.wrappedBuffer(IOUtils.toByteArray(contentStream)));
@@ -107,7 +117,7 @@ public class VeniceDispatcher implements PartitionDispatchHandler<Instance, Path
           return;
         }
         HttpHeaders.setContentLength(response, response.getContent().readableBytes());
-        HttpHeaders.setHeader(response, HttpHeaders.Names.CONTENT_TYPE, org.apache.http.protocol.HTTP.OCTET_STREAM_TYPE);
+        HttpHeaders.setHeader(response, HttpHeaders.Names.CONTENT_TYPE, HttpConstants.APPLICATION_OCTET);
         contextExecutor.execute(() -> {
           responseFuture.setSuccess(Collections.singletonList(response));
         });
@@ -128,7 +138,7 @@ public class VeniceDispatcher implements PartitionDispatchHandler<Instance, Path
         HttpResponse response = new DefaultHttpResponse(HttpVersion.HTTP_1_1, status);
         response.setContent(ChannelBuffers.wrappedBuffer(e.getMessage().getBytes(StandardCharsets.UTF_8)));
         HttpHeaders.setContentLength(response, response.getContent().readableBytes());
-        HttpHeaders.setHeader(response, HttpHeaders.Names.CONTENT_TYPE, org.apache.http.protocol.HTTP.PLAIN_TEXT_TYPE);
+        HttpHeaders.setHeader(response, HttpHeaders.Names.CONTENT_TYPE, HttpConstants.TEXT_PLAIN);
         contextExecutor.execute(() -> {
           responseFuture.setSuccess(Collections.singletonList(response));
         });
