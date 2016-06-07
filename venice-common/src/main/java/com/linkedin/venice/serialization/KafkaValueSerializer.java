linkedin/venice

commit 0529beeda007676c8ae04d298759a3ca48603408

Transitioned the Venice Kafka wire format to the new Avro-based protocol.
The Kafka protocol is changed in the following way:

- Changed the magic byte at the beginning of Kafka values from 22 to 23.
  This ensures that Storage Node consumers will fail fast if you still
  have data formatted according to the old protocol in your Kafka topics.
  Since we are still in development mode, backwards compatibility is not
  supported for "magic byte 22"-encoded data.
- After the magic byte, there is a protocol version byte, which is set to
  '1' currently. This allows us to evolve the protocol gracefully in the
  future.
- The rest of the Kafka value beyond the first two bytes are a single
  binary-encoded Avro record, which is decoded using the writer schema
  specified by the protocol version byte.

In order to achieve the above, the following code changes were made:

- Deleted the OperationType enum. It was too messy to try to reconcile
  all message types in a single enum.
- Introduced two new enums: MessageType and ControlMessageType.
- Deleted KafkaValue, which is replaced by KafkaMessageEnvelope.
- Many unit test changes in order to cope with the API changes.

Other changes, only tangentially related to the protocol change:

- Deleted ControlFlagKafkaKey, as this whole part of the code will be
  made obsolete by the upcoming Data Ingest Validation work.
- Greatly simplified the KafkaKeySerializer's code, although the
  behavior is essentially the same as before.
- Renamed KafkaProducerWrapper's sendControlMessage() function to
  broadcastMessage(), which more accurately describes its function.

Build changes:

- The Gradle build for venice-common now has a test resources
  configuration, in order for tests involving the KafkaValueSerializer
  to run properly.
- The Gradle build for venice-thin-client now specifies the
  application plugin, which fixes an issue with `./gradlew idea`

RB=737069
BUG=VOLDENG-2452
G=voldemort-reviewers
R=mwise,goliu
A=mwise,athirupa

venice-common/build.gradle
@@ -16,6 +16,17 @@ jar {
   }
 }
 
+sourceSets {
+  // Tests work in IntelliJ regardless of whether this config is present or not,
+  // but it is necessary in order for Gradle's tests to find the main resources.
+  test {
+    // At the time of this writing, the only relevant resources under main which
+    // the code needs are the Avro schema definitions under: src/main/resources/avro/
+    output.resourcesDir = "build/resources/main"
+  }
+}
+
+
 dependencies {
 
   // Let's try to keep it clean! (i.e.: alphabetical)

venice-common/src/main/java/com/linkedin/venice/serialization/KafkaValueSerializer.java
@@ -2,180 +2,202 @@ package com.linkedin.venice.serialization;
 
 import com.linkedin.venice.exceptions.VeniceException;
 import com.linkedin.venice.exceptions.VeniceMessageException;
-import com.linkedin.venice.message.KafkaValue;
-import com.linkedin.venice.message.OperationType;
-import java.util.Map;
+import com.linkedin.venice.kafka.protocol.KafkaMessageEnvelope;
+import com.linkedin.venice.utils.ByteUtils;
+import org.apache.avro.Schema;
+import org.apache.avro.io.BinaryEncoder;
+import org.apache.avro.io.Decoder;
+import org.apache.avro.io.DecoderFactory;
+import org.apache.avro.io.Encoder;
+import org.apache.avro.specific.SpecificDatumReader;
+import org.apache.avro.specific.SpecificDatumWriter;
 import org.apache.log4j.Logger;
 
-import java.io.*;
-
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
- * VeniceSerializer to encode/decode KafkaValue for Venice customized kafka message
- * Used by Kafka to convert to/from byte arrays.
+ * Serializer for the Avro-based kafka protocol defined in:
  *
- * KafkaValue Schema (in order)
- * - Magic Byte
- * - Operation Type  - Can be one of the types - PUT, DELETE, PARTIAL_WRITE, BEGIN_OF_PUSH, END_OF_PUSH
- * - Schema Version
- * - Timestamp
- * - ZoneId
- * - Payload
+ * {@link com.linkedin.venice.kafka.protocol.KafkaMessageEnvelope}
  *
+ * The protocol is the following:
+ *
+ * 1st byte: The magic byte, should always equal '{@value #MAGIC_BYTE_VALUE}'.
+ * 2nd byte: The protocol version, currently, only '1' is supported.
+ * 3rd byte and onward: The payload (a single binary-encoded Avro record) encoded
+ *    with a writer schema determined by the protocol version specified in #2.
  */
-public class KafkaValueSerializer implements VeniceSerializer<KafkaValue> {
+public class KafkaValueSerializer implements VeniceSerializer<KafkaMessageEnvelope> {
 
-  static final Logger logger = Logger.getLogger(KafkaValueSerializer.class.getName()); // log4j logger
+  private static final Logger logger = Logger.getLogger(KafkaValueSerializer.class);
 
-  public KafkaValueSerializer() {
-    /* This constructor is not used, but is required for compilation */
-  }
+  // Constants related to the protocol definition:
 
-  @Override
+  // 1st byte: Magic Byte
+  private static final int MAGIC_BYTE_OFFSET = 0;
+  private static final int MAGIC_BYTE_LENGTH = 1;
+  private static final byte MAGIC_BYTE_VALUE = (byte) 23;
+
+  // 2nd byte: Protocol version
+  private static final int PROTOCOL_VERSION_OFFSET = MAGIC_BYTE_OFFSET + MAGIC_BYTE_LENGTH;
+  private static final int PROTOCOL_VERSION_LENGTH = 1;
+  private static final byte LATEST_PROTOCOL_VERSION_VALUE = (byte) 1;
+
+  // 3rd byte and onward: Payload (a single binary-encoded Avro record)
+  private static final int PAYLOAD_OFFSET = PROTOCOL_VERSION_OFFSET + PROTOCOL_VERSION_LENGTH;
+
+  // Re-usable Avro facilities. Can be shared across multiple threads, so we only need one per process.
+
+  /** Used to generate decoders. */
+  private static final DecoderFactory DECODER_FACTORY = new DecoderFactory();
+
+  /** Used to serialize objects into binary-encoded Avro according to the latest protocol version. */
+  private static final SpecificDatumWriter SPECIFIC_DATUM_WRITER = new SpecificDatumWriter(KafkaMessageEnvelope.SCHEMA$);
+
+  /** Holds the mapping of protocol version to {@link Schema} instance. */
+  private static final Map<Byte, Schema> PROTOCOLS = initializeProtocolSchemaMap();
+
+  public KafkaValueSerializer() {}
+
   /**
-   * Converts from a byte array to a VeniceMessage
-   * @param bytes - byte[] to be converted
-   * @return Converted Venice Message
-   * */
-  public KafkaValue deserialize(String topic, byte[] bytes) {
-
-    byte magicByte;
-    OperationType operationType = null;
-    short schemaVersionId;
-    long timestamp;
-    byte zoneId;
-    byte[] value;
-
-    ByteArrayInputStream bytesIn = null;
-    ObjectInputStream objectInputStream = null;
-
-    try {
-
-      bytesIn = new ByteArrayInputStream(bytes);
-      objectInputStream = new ObjectInputStream(bytesIn);
-
-      /* read magicByte and validate Venice message */
-      magicByte = objectInputStream.readByte();
-      if (magicByte != KafkaValue.DEFAULT_MAGIC_BYTE) {
-        // This means a non Venice kafka message was produced.
-        throw new VeniceMessageException("Illegal magic byte given: " + magicByte);
-      }
-
-      /* read operationType */
-      byte opTypeByte = objectInputStream.readByte();
-      operationType = OperationType.getOperationType(opTypeByte);
-
-      /* read schemaVersionId - TODO: currently unused */
-      schemaVersionId = objectInputStream.readShort();
-
-      /* read timestamp  - TODO: use current time or this timestamp for new venice message? */
-      timestamp = objectInputStream.readLong();
-
-      /* read zone Id - TODO: not used to create new venice message */
-      zoneId = objectInputStream.readByte();
-
-      /* read payload, one byte at a time */
-      int byteCount = objectInputStream.available();
-      value = new byte[byteCount];
-      for (int i = 0; i < byteCount; i++) {
-        value[i] = objectInputStream.readByte();
-      }
-    } catch (VeniceMessageException e) {
-      logger.error("Error occurred during deserialization of venice message", e);
-      throw e;
-    } catch (IOException e) {
-      logger.error("IOException while converting to VeniceMessage: ", e);
-      throw new VeniceMessageException("IOException while converting to VeniceMessage: ", e);
-    } finally {
-
-      // safely close the input/output streams
-      try {
-        objectInputStream.close();
-      } catch (IOException e) {
-        logger.error("IOException while closing the input stream", e);
-      }
-      try {
-        bytesIn.close();
-      } catch (IOException e) {
-        logger.error("IOException while closing the input stream", e);
-      }
-    }
-
-    return new KafkaValue(operationType, value, schemaVersionId);
-  }
-
-  @Override
-  /**
-   * Converts from a KafkaValue to a byte[]
-   * @param kafkaValue - KafkaValue to be converted
-   * @return Converted byte[]
-   * */
-  public byte[] serialize(String topic, KafkaValue kafkaValue) {
-
-    ByteArrayOutputStream bytesOut = null;
-    ObjectOutputStream objectOutputStream = null;
-    byte[] bytes = null;
-
-    try {
-
-      bytesOut = new ByteArrayOutputStream();
-      objectOutputStream = new ObjectOutputStream(bytesOut);
-
-      /* write Magic byte */
-      objectOutputStream.writeByte(kafkaValue.getMagicByte());
-
-      /* write operation type */
-      // serialize the operation type enum
-      byte opTypeByte = OperationType.getValue(kafkaValue.getOperationType());
-
-      objectOutputStream.writeByte(opTypeByte);
-
-      /* write schema version Id */
-      objectOutputStream.writeShort(kafkaValue.getSchemaVersionId());
-
-      /* write timestamp */
-      objectOutputStream.writeLong(kafkaValue.getTimestamp());
-
-      /* write zone Id */
-      objectOutputStream.writeByte(kafkaValue.getZoneId());
-
-      /* write payload */
-      objectOutputStream.write(kafkaValue.getValue());
-      objectOutputStream.flush();
-
-      bytes = bytesOut.toByteArray();
-    } catch (IOException e) {
-      logger.error("Could not serialize KafkaValue: " + kafkaValue.getValue());
-    } finally {
-
-      // safely close the input/output streams
-      try {
-        objectOutputStream.close();
-      } catch (IOException e) {}
-
-      try {
-        bytesOut.close();
-      } catch (IOException e) {}
-    }
-
-    return bytes;
-  }
-
-  @Override
-  /**
-   * Configure the Kafka Serializer.
-   * @param configMap Configuration for the serializer.
-   * @param isKey true if the serializer is going to be used for Keys.
-   * @throws VeniceException if the serializer is going to be used for Key data.
+   * Close this serializer.
+   * This method has to be idempotent if the serializer is used in KafkaProducer because it might be called
+   * multiple times.
    */
-  public void configure(Map<String, ?> configMap, boolean isKey) {
-    if(isKey == true) {
-      throw new VeniceException("Cannot use KafkaValueSerializer for Key.");
-    }
-  }
-
   @Override
   public void close() {
-    /* This function is not used, but is required for the interfaces. */
+
+  }
+
+  /**
+   * Configure this class.
+   *
+   * @param configMap configs in key/value pairs
+   * @param isKey     whether is for key or value
+   */
+  @Override
+  public void configure(Map<String, ?> configMap, boolean isKey) {
+    if (isKey) {
+      throw new VeniceException("Cannot use " + getClass().getSimpleName() + " for key data.");
+    }
+  }
+
+  /**
+   * Construct an array of bytes from the given object
+   *
+   * @param topic  Topic to which the object belongs.
+   * @param object A {@link KafkaMessageEnvelope} instance to be serialized.
+   * @return The Avro binary format bytes which represent the {@param object}
+   */
+  @Override
+  public byte[] serialize(String topic, KafkaMessageEnvelope object) {
+    try {
+      // If single-threaded, both the ByteArrayOutputStream and Encoder can be re-used. TODO: explore GC tuning later.
+      ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+      Encoder encoder = new BinaryEncoder(byteArrayOutputStream);
+
+      // We write according to the latest protocol version.
+      byteArrayOutputStream.write(MAGIC_BYTE_VALUE);
+      byteArrayOutputStream.write(LATEST_PROTOCOL_VERSION_VALUE);
+      SPECIFIC_DATUM_WRITER.write(object, encoder);
+
+      return byteArrayOutputStream.toByteArray();
+    } catch (IOException e) {
+      throw new VeniceMessageException("Failed to encode message from topic '" + topic + "': " + object.toString(), e);
+    }
+  }
+
+  /**
+   * Create an object from an array of bytes
+   *
+   * @param topic Topic to which the array of bytes belongs.
+   * @param bytes An array of bytes representing the object's data serialized in Avro binary format.
+   * @return A {@link KafkaMessageEnvelope} serialized from the bytes
+   */
+  @Override
+  public KafkaMessageEnvelope deserialize(String topic, byte[] bytes) {
+    try {
+      // Sanity check on the magic byte to make sure we understand the protocol itself
+      if (bytes[MAGIC_BYTE_OFFSET] != MAGIC_BYTE_VALUE) {
+        throw new VeniceMessageException("Received Magic Byte '" +
+            new String(bytes, MAGIC_BYTE_OFFSET, MAGIC_BYTE_LENGTH) +
+            "' which is not supported by " + this.getClass().getSimpleName() +
+            ". The only supported Magic Byte for this implementation is '" + MAGIC_BYTE_VALUE + "'.");
+      }
+
+      // Sanity check to make sure the writer's protocol (i.e.: Avro schema) version is known to us
+      if (!PROTOCOLS.containsKey(bytes[PROTOCOL_VERSION_OFFSET])) {
+        throw new VeniceMessageException("Received Protocol Version '" +
+            new String(bytes, PROTOCOL_VERSION_OFFSET, PROTOCOL_VERSION_LENGTH) +
+            "' which is not supported by " + this.getClass().getSimpleName() +
+            ". The only supported Protocol Versions are [" + LATEST_PROTOCOL_VERSION_VALUE + "].");
+      }
+
+      // If the data looks valid, then we deploy the Avro machinery to decode the payload
+
+      // If single-threaded, DatumReader can be re-used. TODO: explore GC tuning later.
+      SpecificDatumReader<KafkaMessageEnvelope> specificDatumReader = new SpecificDatumReader<>();
+
+      specificDatumReader.setSchema(PROTOCOLS.get(bytes[PROTOCOL_VERSION_OFFSET])); // Writer's schema
+      specificDatumReader.setExpected(KafkaMessageEnvelope.SCHEMA$);                // Reader's schema
+
+      Decoder decoder = DECODER_FACTORY.createBinaryDecoder(
+          bytes,                         // The bytes array we wish to decode
+          PAYLOAD_OFFSET,                // Where to start reading from in the bytes array
+          bytes.length - PAYLOAD_OFFSET, // The length to read in the bytes array
+          null                           // This param is to re-use a Decoder instance. TODO: explore GC tuning later.
+      );
+
+      KafkaMessageEnvelope kafkaMessageEnvelope = specificDatumReader.read(
+          null, // This param is to re-use a KafkaMessageEnvelope instance. TODO: explore GC tuning later.
+          decoder
+      );
+
+      return kafkaMessageEnvelope;
+    } catch (IOException e) {
+      throw new VeniceMessageException("Failed to decode message from topic '" + topic + "': " + ByteUtils.toHexString(bytes), e);
+    }
+  }
+
+  /**
+   * We initialize the protocols map once per process, the first time we construct an instance of this class.
+   */
+  private static Map<Byte, Schema> initializeProtocolSchemaMap() {
+    try {
+      Map protocolSchemaMap = new HashMap<>();
+      protocolSchemaMap.put((byte) 1, getSchemaFromResource("avro/v1/KafkaValueEnvelope.avsc"));
+
+      // TODO: If we add more versions to the protocol, they should be initialized here.
+
+      return protocolSchemaMap;
+    } catch (IOException e) {
+      throw new VeniceMessageException("Could not initialize " + KafkaValueSerializer.class.getSimpleName(), e);
+    }
+  }
+
+  /**
+   * Utility function to get schemas out of embedded resources.
+   *
+   * N.B.: For this to work in Gradle tests, a special sourceSets definition is included
+   *       in the venice-common module's build.gradle file. This does not affect IDEs.
+   *
+   * @param resourcePath The path of the file under the src/main/resources directory
+   * @return the {@link org.apache.avro.Schema} instance corresponding to the file at {@param resourcePath}
+   * @throws IOException
+   */
+  private static Schema getSchemaFromResource(String resourcePath) throws IOException {
+    ClassLoader thisClassLoader = KafkaValueSerializer.class.getClassLoader();
+    URL resourceURL = thisClassLoader.getResource(resourcePath);
+    if (resourceURL == null) {
+      throw new IOException("Resource path '" + resourcePath + "' does not exist!");
+    }
+    Schema schema = Schema.parse(new File(resourceURL.getFile()));
+    logger.debug("Loaded schema from resource path '" + resourcePath + "': " + schema.toString(true));
+    return schema;
   }
 }

commit 0dd9558714d71ab68ff02cf3fd726eb91299dbf3

Fixed regression introduced in 0529beeda007676c8ae04d298759a3ca48603408.
The Avro schema definition that we load as a resource was accessible in
unit tests, but was not accessible when running the built jars directly.
Using a different ClassLoader API fixes this.

Other related changes:
- The Gradle build tweak in venice-common related to resource loading
  is not needed anymore, so it was removed.
- Added more robust error handling in StoreConsumptionTask.run() by
  catching Throwables rather than just Expcetions. This was making the
  regression fail silently and caused the Storage Node's to corrupt its
  internal state and fail at a non-deterministic time in the future.

RB=742123
G=voldemort-reviewers
R=mwise,athirupa
A=athirupa

venice-common/build.gradle
@@ -16,17 +16,6 @@ jar {
   }
 }
 
-sourceSets {
-  // Tests work in IntelliJ regardless of whether this config is present or not,
-  // but it is necessary in order for Gradle's tests to find the main resources.
-  test {
-    // At the time of this writing, the only relevant resources under main which
-    // the code needs are the Avro schema definitions under: src/main/resources/avro/
-    output.resourcesDir = "build/resources/main"
-  }
-}
-
-
 dependencies {
 
   // Let's try to keep it clean! (i.e.: alphabetical)

venice-common/src/main/java/com/linkedin/venice/serialization/KafkaValueSerializer.java
@@ -11,12 +11,10 @@ import org.apache.avro.io.DecoderFactory;
 import org.apache.avro.io.Encoder;
 import org.apache.avro.specific.SpecificDatumReader;
 import org.apache.avro.specific.SpecificDatumWriter;
+import org.apache.commons.io.IOUtils;
 import org.apache.log4j.Logger;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
-import java.net.URL;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -183,21 +181,19 @@ public class KafkaValueSerializer implements VeniceSerializer<KafkaMessageEnvelo
   /**
    * Utility function to get schemas out of embedded resources.
    *
-   * N.B.: For this to work in Gradle tests, a special sourceSets definition is included
-   *       in the venice-common module's build.gradle file. This does not affect IDEs.
-   *
    * @param resourcePath The path of the file under the src/main/resources directory
    * @return the {@link org.apache.avro.Schema} instance corresponding to the file at {@param resourcePath}
    * @throws IOException
    */
   private static Schema getSchemaFromResource(String resourcePath) throws IOException {
-    ClassLoader thisClassLoader = KafkaValueSerializer.class.getClassLoader();
-    URL resourceURL = thisClassLoader.getResource(resourcePath);
-    if (resourceURL == null) {
+    ClassLoader classLoader = KafkaValueSerializer.class.getClassLoader();
+    InputStream inputStream = classLoader.getResourceAsStream(resourcePath);
+    if (inputStream == null) {
       throw new IOException("Resource path '" + resourcePath + "' does not exist!");
     }
-    Schema schema = Schema.parse(new File(resourceURL.getFile()));
-    logger.debug("Loaded schema from resource path '" + resourcePath + "': " + schema.toString(true));
+    String schemaString = IOUtils.toString(inputStream);
+    Schema schema = Schema.parse(schemaString);
+    logger.info("Loaded schema from resource path '" + resourcePath + "':\n" + schema.toString(true));
     return schema;
   }
 }

venice-server/src/main/java/com/linkedin/venice/kafka/consumer/StoreConsumptionTask.java
@@ -41,9 +41,9 @@ import org.apache.log4j.Logger;
  */
 public class StoreConsumptionTask implements Runnable, Closeable {
 
-  private static final Logger logger = Logger.getLogger(StoreConsumptionTask.class.getName());
+  private static final Logger logger = Logger.getLogger(StoreConsumptionTask.class);
 
-  private static final String CONSUMER_TASK_ID_FORMAT = "KafkaPerStoreConsumptionTask for " + "[ Node: %d, Topic: %s ]";
+  private static final String CONSUMER_TASK_ID_FORMAT = StoreConsumptionTask.class.getSimpleName() + " for [ Node: %d, Topic: %s ]";
 
   // Making it non final to shorten the time in testing.
   public static int READ_CYCLE_DELAY_MS = 1000;
@@ -219,8 +219,12 @@ public class StoreConsumptionTask implements Runnable, Closeable {
       // This will confused the controller.
       // FGV: Actually, the Controller doesn't seem to be aware at all that the consumer failed.
       //      It just keeps reporting "Push status: STARTED..." indefinitely to the H2V job. TODO: fix this
-      logger.error(consumerTaskId + " failed with Exception: ", e);
-      reportError(partitionToOffsetMap.keySet() , " Errors occurred during poll " , e );
+      logger.error(consumerTaskId + " failed with Exception.", e);
+      reportError(partitionToOffsetMap.keySet() , "Exception caught during poll." , e);
+    } catch (Throwable t) {
+      logger.error(consumerTaskId + " failed with Throwable!!!", t);
+      reportError(partitionToOffsetMap.keySet(), "Non-exception Throwable caught in " + getClass().getSimpleName() +
+          "'s run() function.", new VeniceException(t));
     } finally {
       internalClose();
     }

venice-server/src/main/java/com/linkedin/venice/notifier/VeniceNotifier.java
@@ -1,7 +1,5 @@
 package com.linkedin.venice.notifier;
 
-import java.util.Collection;
-
 
 /**
  * Interface for listening to Notifications for Store consumption.