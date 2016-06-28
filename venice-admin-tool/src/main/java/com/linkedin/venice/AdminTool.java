b24fd7b294adffbbd28f1de196b8a23c0c287c9d
Matthew Wise <mwise@linkedin.com>
2016-06-28 14:28:03 -0700

    Add store creation to the venice admin tool
    RB=758665
    G=voldemort-reviewers
    A=athirupa

--- venice-admin-tool/src/main/java/com/linkedin/venice/AdminTool.java
+++ venice-admin-tool/src/main/java/com/linkedin/venice/AdminTool.java
@@ -1,22 +1,30 @@
 package com.linkedin.venice;
 
 import com.linkedin.venice.controllerapi.ControllerClient;
+import com.linkedin.venice.controllerapi.ControllerResponse;
 import com.linkedin.venice.controllerapi.JobStatusQueryResponse;
 import com.linkedin.venice.controllerapi.MultiStoreResponse;
 import com.linkedin.venice.controllerapi.MultiVersionResponse;
+import com.linkedin.venice.controllerapi.NewStoreResponse;
+import com.linkedin.venice.controllerapi.SchemaResponse;
 import com.linkedin.venice.controllerapi.VersionResponse;
+import com.linkedin.venice.exceptions.VeniceException;
 import com.linkedin.venice.meta.Version;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Arrays;
-import java.util.StringJoiner;
+import java.util.List;
+import java.util.function.Supplier;
+import org.apache.avro.Schema;
 import org.apache.commons.cli.BasicParser;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.HelpFormatter;
-import org.apache.commons.cli.MissingArgumentException;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
-import static com.linkedin.venice.Arg.*;
 
 
 /**
@@ -31,101 +39,165 @@ public class AdminTool {
   public static final String JOB = "job"; /* job status (store and version ( */
   public static final String LIST = "list"; /* list all stores */
   public static final String DESCRIBE = "describe"; /* details about specified store or all stores */
+  public static final List<String> QUERY_OPTIONS = Arrays.asList(NEXT, AVAILABLE, CURRENT, JOB, LIST, DESCRIBE);
 
-  public static void main(String args[]) throws ParseException {
+  /* create options */
+  public static final String STORE = "store";
+  public static final List<String> CREATE_OPTIONS = Arrays.asList(STORE);
 
-    Option routerOpt = createOpt(ROUTER, true, "REQUIRED: Venice router url, eg. http://localhost:54333");
-    Option clusterOpt = createOpt(CLUSTER, true, "REQUIRED: Name of Venice cluster");
-    Option storeOpt = createOpt(STORE, true, "Name of Venice store");
-    Option versionOpt = createOpt(VERSION, true, "Venice store version number");
-    Option queryOpt = createOpt(QUERY, true, "REQUIRED: Query one of next, available, current, job, list, describe");
-    Options options = new Options()
-        .addOption(routerOpt)
-        .addOption(clusterOpt)
-        .addOption(storeOpt)
-        .addOption(versionOpt)
-        .addOption(queryOpt)
-        .addOption(createOpt(HELP, false, "Show usage"));
+  public static void main(String args[])
+      throws ParseException, IOException {
+
+    Options options = new Options();
+    createOpt(Arg.ROUTER, true, "REQUIRED: Venice router url, eg. http://localhost:54333", options);
+    createOpt(Arg.CLUSTER, true, "REQUIRED: Name of Venice cluster", options);
+    createOpt(Arg.STORE, true, "Name of Venice store", options);
+    createOpt(Arg.VERSION, true, "Venice store version number", options);
+    createOpt(Arg.QUERY, true, "OPTIONAL: Query one of: " + String.join(", ", QUERY_OPTIONS), options);
+    createOpt(Arg.NEW, true, "OPTIONAL: Create one of: " + String.join(", ", CREATE_OPTIONS), options);
+    createOpt(Arg.KEY_SCHEMA, true, "Path to text file with key schema", options);
+    createOpt(Arg.VALUE_SCHEMA, true, "Path to text file with value schema", options);
+    createOpt(Arg.OWNER, true, "Owner email for new store creation", options);
+    createOpt(Arg.HELP, false, "Show usage", options);
 
     CommandLineParser parser = new BasicParser();
     CommandLine cmd = parser.parse(options, args);
 
-    if (cmd.hasOption(HELP.first())){
-      printUsageAndExit(options);
-    }
-
-    validateCommand(cmd, ROUTER);
-    validateCommand(cmd, CLUSTER);
-    String routerHosts = cmd.getOptionValue(ROUTER.first());
-    String clusterName = cmd.getOptionValue(CLUSTER.first());
-
-    if (cmd.hasOption(QUERY.first())){
-      String queryAction = cmd.getOptionValue(QUERY.first());
-      String queryClause = " using --" + QUERY.toString();
-      String storeName;
-      switch (queryAction.toLowerCase()){
-        case NEXT:
-          validateCommand(cmd, STORE, queryClause);
-          storeName = cmd.getOptionValue(STORE.first());
-          VersionResponse nextResponse = ControllerClient.queryNextVersion(routerHosts, clusterName, storeName);
-          System.out.println(nextResponse.getVersion());
-          break;
-        case JOB:
-          validateCommand(cmd, STORE, queryClause);
-          storeName = cmd.getOptionValue(STORE.first());
-          validateCommand(cmd, VERSION, "querying " + JOB + " status");
-          int version = Integer.parseInt(cmd.getOptionValue(VERSION.first()));
-          String topicName = new Version(storeName, version).kafkaTopicName();
-          JobStatusQueryResponse jobStatus = ControllerClient.queryJobStatus(routerHosts, clusterName, topicName);
-          System.out.println(jobStatus.getStatus());
-          break;
-        case CURRENT:
-          validateCommand(cmd, STORE, queryClause);
-          storeName = cmd.getOptionValue(STORE.first());
-          VersionResponse currentResponse = ControllerClient.queryCurrentVersion(routerHosts, clusterName, storeName);
-          System.out.println(currentResponse.getVersion());
-          break;
-        case AVAILABLE:
-          validateCommand(cmd, STORE, queryClause);
-          storeName = cmd.getOptionValue(STORE.first());
-          MultiVersionResponse availableResponse = ControllerClient.queryActiveVersions(routerHosts, clusterName, storeName);
-          System.out.println(Arrays.toString(availableResponse.getVersions()));
-          break;
-        case DESCRIBE:
-          if (cmd.hasOption(STORE.first())) {
-            storeName = cmd.getOptionValue(STORE.first());
-            for (String store : storeName.split(",")) {
-              printStoreDescription(routerHosts, clusterName, store);
-            }
-          } else { /* describe all stores */
-            MultiStoreResponse storeResponse = ControllerClient.queryStoreList(routerHosts, clusterName);
-            for (String store : storeResponse.getStores()){
-              printStoreDescription(routerHosts, clusterName, store);
-            }
-          }
-          break;
-        case LIST:
-          MultiStoreResponse storeResponse = ControllerClient.queryStoreList(routerHosts, clusterName);
-          System.out.println(String.join("\n", storeResponse.getStores()));
-          break;
-        default:
-          System.err.println(queryAction + " NOT IMPLEMENTED");
-          System.exit(1);
+    try {
+      if (cmd.hasOption(Arg.HELP.first())) {
+        printUsageAndExit(options);
       }
-    } else { /* not --query */
-      validateCommand(cmd, QUERY); /* because right now we can only query */
+      if (cmd.hasOption(Arg.QUERY.first()) && cmd.hasOption(Arg.NEW.first())) {
+        throw new VeniceException("Must only specify one of --" + Arg.QUERY + " or --" + Arg.NEW);
+      }
+      String routerHosts = getRequiredArgument(cmd, Arg.ROUTER);
+      String clusterName = getRequiredArgument(cmd, Arg.CLUSTER);
+
+      if (cmd.hasOption(Arg.QUERY.first())) {
+        handleQueryAction(cmd, routerHosts, clusterName);
+      } else if (cmd.hasOption(Arg.NEW.first())) {
+        String newAction = cmd.getOptionValue(Arg.NEW.first());
+        switch (newAction.toLowerCase()) {
+          case STORE:
+            createNewStore(cmd, routerHosts, clusterName);
+            break;
+          default:
+            throw new VeniceException(newAction + " NOT IMPLEMENTED" + " for --" + Arg.NEW.toString());
+        }
+      } else { /* not --query or --new */
+        throw new VeniceException("--query or --new is a required argument");
+      }
+    } catch (VeniceException e){
+      printErrAndExit(e.getMessage());
     }
+  }
+
+  private static void handleQueryAction(CommandLine cmd, String routerHosts, String clusterName){
+    String queryAction = cmd.getOptionValue(Arg.QUERY.first());
+    String queryClause = " when using --" + Arg.QUERY.toString();
+    String storeName;
+    switch (queryAction.toLowerCase()) {
+      case NEXT:
+        storeName = getRequiredArgument(cmd, Arg.STORE, queryClause);
+        for (String store : storeName.split(",")) {
+          VersionResponse nextResponse = ControllerClient.queryNextVersion(routerHosts, clusterName, store);
+          System.out.println(nextResponse.getVersion());
+        }
+        break;
+      case JOB:
+        storeName = getRequiredArgument(cmd, Arg.STORE, queryClause);
+        getRequiredArgument(cmd, Arg.VERSION, "querying " + JOB + " status");
+        int version = Integer.parseInt(cmd.getOptionValue(Arg.VERSION.first()));
+        String topicName = new Version(storeName, version).kafkaTopicName();
+        JobStatusQueryResponse jobStatus = ControllerClient.queryJobStatus(routerHosts, clusterName, topicName);
+        System.out.println(jobStatus.getStatus());
+        break;
+      case CURRENT:
+        storeName = getRequiredArgument(cmd, Arg.STORE, queryClause);
+        for (String store : storeName.split(",")) {
+          VersionResponse currentResponse = ControllerClient.queryCurrentVersion(routerHosts, clusterName, store);
+          System.out.println(currentResponse.getVersion());
+        }
+        break;
+      case AVAILABLE:
+        storeName = getRequiredArgument(cmd, Arg.STORE, queryClause);
+        for (String store : storeName.split(",")) {
+          MultiVersionResponse availableResponse = ControllerClient.queryActiveVersions(routerHosts, clusterName, store);
+          System.out.println(Arrays.toString(availableResponse.getVersions()));
+        }
+        break;
+      case DESCRIBE:
+        if (cmd.hasOption(Arg.STORE.first())) {
+          storeName = cmd.getOptionValue(Arg.STORE.first());
+          for (String store : storeName.split(",")) {
+            printStoreDescription(routerHosts, clusterName, store);
+          }
+        } else { /* describe all stores */
+          MultiStoreResponse storeResponse = ControllerClient.queryStoreList(routerHosts, clusterName);
+          for (String store : storeResponse.getStores()) {
+            printStoreDescription(routerHosts, clusterName, store);
+          }
+        }
+        break;
+      case LIST:
+        MultiStoreResponse storeResponse = ControllerClient.queryStoreList(routerHosts, clusterName);
+        System.out.println(String.join("\n", storeResponse.getStores()));
+        break;
+      default:
+        throw new VeniceException(queryAction + " NOT IMPLEMENTED" + " for --" + Arg.QUERY.toString());
+    }
+  }
+
+
+  private static void createNewStore(CommandLine cmd, String routerHosts, String clusterName)
+      throws IOException {
+    String newClause = " when using --" + Arg.NEW.toString();
+    String store = getRequiredArgument(cmd, Arg.STORE, newClause);
+    String keySchemaFile = getRequiredArgument(cmd, Arg.KEY_SCHEMA, newClause);
+    String keySchema = readFile(keySchemaFile);
+    String valueSchemaFile = getRequiredArgument(cmd, Arg.VALUE_SCHEMA, newClause);
+    String valueSchema = readFile(valueSchemaFile);
+    String owner = getRequiredArgument(cmd, Arg.OWNER, newClause);
+    verifyValidSchema(keySchema);
+    verifyValidSchema(valueSchema);
+    verifyConnection(routerHosts, clusterName);
+    boolean storeExists = !ControllerClient.queryNextVersion(routerHosts, clusterName, store).isError();
+    if (storeExists) {
+      throw new VeniceException("Store " + store + " already exists, cannot create it");
+    }
+          /* TODO: createNewStore should be modified to require a key and value schema */
+    NewStoreResponse newStore = ControllerClient.createNewStore(routerHosts, clusterName, store, owner);
+    if (newStore.isError()) {
+      throw new VeniceException("Error creating store " + store + ": " + newStore.getError());
+    }
+    SchemaResponse keyResponse = ControllerClient.initKeySchema(routerHosts, clusterName, store, keySchema);
+    SchemaResponse valueResponse = ControllerClient.addValueSchema(routerHosts, clusterName, store, valueSchema);
+    for (SchemaResponse response : Arrays.asList(keyResponse, valueResponse)) {
+      if (response.isError()) {
+        System.err.println("Error initializing store with schema: " + response.getError());
+      }
+    }
+    System.out.println("Created Store: " + store);
   }
 
   private static void printStoreDescription(String routerHosts, String clusterName, String storeName){
     VersionResponse currentResponse = ControllerClient.queryCurrentVersion(routerHosts, clusterName, storeName);
     MultiVersionResponse availableResponse = ControllerClient.queryActiveVersions(routerHosts, clusterName, storeName);
     VersionResponse nextResponse = ControllerClient.queryNextVersion(routerHosts, clusterName, storeName);
-    StringBuilder output = new StringBuilder()
-        .append("Store: " + storeName + "\t")
-        .append("Available versions: " + Arrays.toString(availableResponse.getVersions()) + "\t")
-        .append("Current version: " + currentResponse.getVersion() + "\t")
-        .append("Next version: " + nextResponse.getVersion());
+    StringBuilder output = new StringBuilder().append("Store: " + storeName + "\t");
+    boolean isError = false;
+    for (ControllerResponse response : Arrays.asList(currentResponse, availableResponse, nextResponse)){
+      if (response.isError()){
+        output.append("Error: " + response.getError());
+        isError = true;
+        break;
+      }
+    }
+    if (!isError) {
+      output.append("Available versions: " + Arrays.toString(availableResponse.getVersions()) + "\t")
+          .append("Current version: " + currentResponse.getVersion() + "\t")
+          .append("Next version: " + nextResponse.getVersion());
+    }
     System.out.println(output.toString());
   }
 
@@ -139,14 +211,29 @@ public class AdminTool {
     System.exit(1);
   }
 
-  private static void validateCommand(CommandLine cmd, Arg arg){
+  private static String getRequiredArgument(CommandLine cmd, Arg arg){
+    return getRequiredArgument(cmd, arg, "");
+  }
+
+  private static String getRequiredArgument(CommandLine cmd, Arg arg, String errorClause){
     if (!cmd.hasOption(arg.first())){
-      printErrAndExit(arg.toString() + " is a required argument");
+      printErrAndExit(arg.toString() + " is a required argument " + errorClause);
+    }
+    return cmd.getOptionValue(arg.first());
+  }
+
+  private static void verifyConnection(String routerHosts, String cluster){
+    boolean canConnect = !ControllerClient.queryStoreList(routerHosts, cluster).isError();
+    if (!canConnect){
+      printErrAndExit("Cannot connect to cluster " + cluster + " with routers " + routerHosts);
     }
   }
-  private static void validateCommand(CommandLine cmd, Arg arg, String clause){
-    if (!cmd.hasOption(arg.first())){
-      printErrAndExit(arg.toString() + " is a required argument when " + clause);
+
+  private static void verifyValidSchema(String schema){
+    try {
+      Schema.parse(schema);
+    } catch (Exception e){
+      printErrAndExit("Invalid Schema: " + schema);
     }
   }
 
@@ -156,8 +243,14 @@ public class AdminTool {
     System.exit(1);
   }
 
-  private static Option createOpt(Arg name, boolean hasArg, String help){
-    return new Option(name.first(), name.toString(), hasArg, help);
+  private static void createOpt(Arg name, boolean hasArg, String help, Options options){
+    options.addOption(new Option(name.first(), name.toString(), hasArg, help));
+  }
+
+  static String readFile(String path) throws IOException {
+    String fullPath = path.replace("~", System.getProperty("user.home"));
+    byte[] encoded = Files.readAllBytes(Paths.get(fullPath));
+    return new String(encoded, StandardCharsets.UTF_8).trim();
   }
 
 }
