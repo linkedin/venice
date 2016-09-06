e57a5ffb8047489b49cb4e48653c9ab42c919339
Matthew Wise <mwise@linkedin.com>
2016-09-06 10:55:44 -0700

    Add non-deterministic retry for failing test
    RB=807312
    G=voldemort-reviewers
    A=goliu

--- venice-controller/src/test/java/com/linkedin/venice/controller/server/TestAdminSparkServerGetMaster.java
+++ venice-controller/src/test/java/com/linkedin/venice/controller/server/TestAdminSparkServerGetMaster.java
@@ -2,23 +2,29 @@ package com.linkedin.venice.controller.server;
 
 import com.linkedin.venice.controllerapi.ControllerApiConstants;
 import com.linkedin.venice.controllerapi.MasterControllerResponse;
+import com.linkedin.venice.exceptions.VeniceException;
 import com.linkedin.venice.integration.utils.KafkaBrokerWrapper;
 import com.linkedin.venice.integration.utils.ServiceFactory;
 import com.linkedin.venice.integration.utils.VeniceControllerWrapper;
+import com.linkedin.venice.utils.TestUtils;
 import com.linkedin.venice.utils.Utils;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.utils.URLEncodedUtils;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -59,17 +65,24 @@ public class TestAdminSparkServerGetMaster {
     queryParams.add(new BasicNameValuePair(ControllerApiConstants.HOSTNAME, "some-hostname"));
     String queryString = URLEncodedUtils.format(queryParams, StandardCharsets.UTF_8);
 
-    HttpGet get = new HttpGet(controllerUrl + MASTER_CONTROLLER.getPath() + "?" + queryString);
-    HttpResponse response = client.execute(get);
-    String jsonStr;
-    try (InputStream bodyStream = response.getEntity().getContent()) {
-      jsonStr = IOUtils.toString(bodyStream);
-    }
-    MasterControllerResponse responseObject = mapper.readValue(jsonStr, MasterControllerResponse.class);
+    TestUtils.waitForNonDeterministicAssertion(5, TimeUnit.SECONDS, () -> {
+      try {
+        HttpGet get = new HttpGet(controllerUrl + MASTER_CONTROLLER.getPath() + "?" + queryString);
+        HttpResponse response = client.execute(get);
+        String jsonStr;
+        try (InputStream bodyStream = response.getEntity().getContent()) {
+          jsonStr = IOUtils.toString(bodyStream);
+        }
 
-    String masterControllerUrl = responseObject.getUrl();
+        MasterControllerResponse responseObject = mapper.readValue(jsonStr, MasterControllerResponse.class);
 
-    Assert.assertEquals(masterControllerUrl, "http://" + Utils.getHostName() + ":" + veniceControllerWrapper.getPort(),
-        "Can not find correct master controller url.");
+        String masterControllerUrl = responseObject.getUrl();
+
+        Assert.assertEquals(masterControllerUrl, "http://" + Utils.getHostName() + ":" + veniceControllerWrapper.getPort(),
+            "Can not find correct master controller url.");
+      } catch (IOException e) {
+        throw new VeniceException(e);
+      }
+    });
   }
 }
