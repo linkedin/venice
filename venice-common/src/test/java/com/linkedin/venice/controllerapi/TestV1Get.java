f6e7d8556a9cfa48adcd433b9663db32731df92a
Matthew Wise <mwise@linkedin.com>
2016-09-30 15:35:03 -0700

    define routes for v1 controller api
    RB=830107
    G=voldemort-reviewers
    A=fvillega

---
+++ venice-common/src/test/java/com/linkedin/venice/controllerapi/TestV1Get.java
@@ -0,0 +1,27 @@
+package com.linkedin.venice.controllerapi;
+
+import com.linkedin.venice.controllerapi.routes.V1Get;
+import java.util.HashMap;
+import java.util.Map;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static com.linkedin.venice.controllerapi.ControllerApiConstants.CLUSTER;
+import static com.linkedin.venice.controllerapi.ControllerApiConstants.NAME;
+
+
+public class TestV1Get {
+  @Test
+  public void testGetStore(){
+    String expectedRaw = "/v1/cluster/:"+CLUSTER+"/store/:"+NAME;
+    Assert.assertEquals(V1Get.STORE.getRawPath(), expectedRaw);
+
+    Map<String, String> testParams = new HashMap<>();
+    testParams.put(NAME, "test-store");
+    testParams.put(CLUSTER, "test-cluster");
+
+    String expectedPath = "/v1/cluster/test-cluster/store/test-store";
+    Assert.assertEquals(V1Get.STORE.getPathWithParameters(testParams), expectedPath);
+  }
+
+}
---
+++ venice-common/src/test/java/com/linkedin/venice/controllerapi/TestV1Post.java
@@ -0,0 +1,30 @@
+package com.linkedin.venice.controllerapi;
+
+import com.linkedin.venice.controllerapi.routes.V1Post;
+import java.util.HashMap;
+import java.util.Map;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static com.linkedin.venice.controllerapi.ControllerApiConstants.CLUSTER;
+import static com.linkedin.venice.controllerapi.ControllerApiConstants.KEY_SCHEMA;
+import static com.linkedin.venice.controllerapi.ControllerApiConstants.NAME;
+import static com.linkedin.venice.controllerapi.ControllerApiConstants.OWNER;
+import static com.linkedin.venice.controllerapi.ControllerApiConstants.VALUE_SCHEMA;
+
+
+public class TestV1Post {
+  @Test
+  public void testCreateStore(){
+    String expectedRaw = "/v1/cluster/:"+CLUSTER+"/store";
+    Assert.assertEquals(V1Post.STORE.getRawPath(), expectedRaw);
+
+    Map<String, String> testParams = new HashMap<>();
+    testParams.put(CLUSTER, "test-cluster");
+
+    String expectedPath = "/v1/cluster/test-cluster/store";
+    Assert.assertEquals(V1Post.STORE.getPathWithParameters(testParams), expectedPath);
+
+    Assert.assertEquals(V1Post.STORE.getBodyParams(), new String[]{NAME, OWNER, KEY_SCHEMA, VALUE_SCHEMA});
+  }
+}
