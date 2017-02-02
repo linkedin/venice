972587a50dfc5deec28e502a12089dccd533bb6c
Matthew Wise <mwise@linkedin.com>
2017-02-02 13:05:08 -0800

    switch admin consumption task test to use an in memory offset manager for more reliable tests
    RB=910028
    G=voldemort-reviewers
    A=fvillega

---
+++ venice-test-common/src/main/java/com/linkedin/venice/offsets/InMemoryOffsetManager.java
@@ -0,0 +1,56 @@
+package com.linkedin.venice.offsets;
+
+import com.linkedin.venice.exceptions.VeniceException;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+
+/**
+ * In memory implementation of OffsetManager, should really only be used for tests
+ */
+public class InMemoryOffsetManager implements OffsetManager {
+  private ConcurrentMap<String, ConcurrentMap<Integer, OffsetRecord>> topicToPartitionToOffsetMap = new ConcurrentHashMap<>();
+
+  @Override
+  public void recordOffset(String topicName, int partitionId, OffsetRecord record) throws VeniceException {
+    topicToPartitionToOffsetMap.compute(topicName, (topic, map) -> {
+      if (null == map) {
+        ConcurrentMap<Integer, OffsetRecord> newMap = new ConcurrentHashMap<>();
+        newMap.put(partitionId, record);
+        return newMap;
+      } else {
+        map.compute(partitionId, (partition, oldRecord) -> {
+          if (null == oldRecord || oldRecord.getOffset() < record.getOffset()){
+            return record;
+          } else {
+            return oldRecord;
+          }
+        });
+        return map;
+      }
+    });
+  }
+
+  @Override
+  public void clearOffset(String topicName, int partitionId) {
+    topicToPartitionToOffsetMap.computeIfPresent(topicName, (topic, map) -> {
+      map.computeIfPresent(partitionId, (partition, record) -> null);
+      return map;
+    });
+  }
+
+  @Override
+  public OffsetRecord getLastOffset(String topicName, int partitionId) throws VeniceException {
+    OffsetRecord returnOffset = null;
+    ConcurrentMap<Integer, OffsetRecord> map = topicToPartitionToOffsetMap.get(topicName);
+    if (null != map) {
+      returnOffset = map.get(partitionId);
+    }
+    if (returnOffset != null){
+      return returnOffset;
+    } else {
+      return new OffsetRecord();
+    }
+  }
+}
