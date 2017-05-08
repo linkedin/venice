2f096f0292aadffb1fc8ebbd47dfabf81088cd51
Gaojie Liu <goliu@linkedin.com>
2017-05-08 15:56:25 -0700

    Bug fix: ControllerClient is throwing exception if the url contains spaces at the beginning
    Related ticket:
    https://jira01.corp.linkedin.com:8443/browse/EKG-153039
    
    RB=988822
    BUG=EKG-153039
    G=voldemort-reviewers
    A=fvillega

--- venice-common/src/test/java/com/linkedin/venice/controllerapi/TestControllerClient.java
+++ venice-common/src/test/java/com/linkedin/venice/controllerapi/TestControllerClient.java
@@ -1,16 +1,22 @@
 package com.linkedin.venice.controllerapi;
 
+import com.google.common.net.HttpHeaders;
+
+import com.linkedin.venice.integration.utils.MockHttpServerWrapper;
+import com.linkedin.venice.integration.utils.ServiceFactory;
 import com.linkedin.venice.utils.TestUtils;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.HttpVersion;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
 
-
-/**
- * Created by mwise on 6/1/16.
- */
 public class TestControllerClient {
 
   @Test
@@ -45,5 +51,29 @@ public class TestControllerClient {
     TestJsonObject jsonObject = objectMapper.readValue(jsonStr, TestJsonObject.class);
     Assert.assertEquals(jsonObject.getField1(), field1Value);
     Assert.assertNull(jsonObject.getField2());
+  }
+
+  @Test
+  public void testGetMasterControllerUrlWithUrlContainingSpace() throws IOException {
+    String clusterName = TestUtils.getUniqueString("test-cluster");
+    String fakeMasterControllerUrl = "http://fake.master.controller.url";
+    try (MockHttpServerWrapper mockController = ServiceFactory.getMockHttpServer("mock_controller")) {
+      MasterControllerResponse controllerResponse = new MasterControllerResponse();
+      controllerResponse.setCluster(clusterName);
+      controllerResponse.setUrl(fakeMasterControllerUrl);
+      ObjectMapper objectMapper = new ObjectMapper();
+      ByteBuf body = Unpooled.wrappedBuffer(objectMapper.writeValueAsBytes(controllerResponse));
+      FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, body);
+      response.headers().set(HttpHeaders.CONTENT_TYPE, "application/json");
+      // We must specify content_length header, otherwise netty will keep polling, since it
+      // doesn't know when to finish writing the response.
+      response.headers().set(HttpHeaders.CONTENT_LENGTH, response.content().readableBytes());
+      mockController.addResponseForUriPattern(ControllerRoute.MASTER_CONTROLLER.getPath() + ".*", response);
+
+      String controllerUrlWithSpaceAtBeginning = "   http://" + mockController.getAddress();
+      ControllerClient controllerClient = new ControllerClient(clusterName, controllerUrlWithSpaceAtBeginning);
+      String masterControllerUrl = controllerClient.getMasterControllerUrl(controllerUrlWithSpaceAtBeginning);
+      Assert.assertEquals(masterControllerUrl, fakeMasterControllerUrl);
+    }
   }
 }
