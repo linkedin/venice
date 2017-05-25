51ff1fd46fa294ac76c6ac1394104a82e6573662
Matthew Wise <mwise@linkedin.com>
2017-05-25 13:19:59 -0700

    Add kafka-monitor-topic and TrackingMonitoringEvent to ignored topics for our TopicMonitor
    These are the topics used by the LinkedIn Kakfa Monitor (https://engineering.linkedin.com/blog/2016/05/open-sourcing-kafka-monitor)
    We aren't explicitly ignoring them because they don't start with two underscores "__" like the proper kafka internal topics.
    This commit adds an additional list of topics to ignore and adds these to that list.
    
    RB=1003529
    G=voldemort-reviewers
    A=goliu

--- venice-controller/src/main/java/com/linkedin/venice/controller/kafka/AdminTopicUtils.java
+++ venice-controller/src/main/java/com/linkedin/venice/controller/kafka/AdminTopicUtils.java
@@ -1,14 +1,23 @@
 package com.linkedin.venice.controller.kafka;
 
 import com.linkedin.venice.exceptions.VeniceException;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class AdminTopicUtils {
   public static int PARTITION_NUM_FOR_ADMIN_TOPIC = 1;
   public static final int ADMIN_TOPIC_PARTITION_ID = 0;
   // TODO: Maybe we should make this prefix configurable so that different environments can share
   // the same Kafka cluster
-  public static String ADMIN_TOPIC_PREFIX = "venice_admin_";
-  private static String KAFKA_INTERNAL_TOPIC_PREFIX = "__";
+  public static final String ADMIN_TOPIC_PREFIX = "venice_admin_";
+  private static final String KAFKA_INTERNAL_TOPIC_PREFIX = "__";
+  private static final List<String> TOPICS_TO_IGNORE;
+  static {
+    TOPICS_TO_IGNORE = new ArrayList<>();
+    TOPICS_TO_IGNORE.add("kafka-monitor-topic");
+    TOPICS_TO_IGNORE.add("TrackingMonitoringEvent");
+  }
 
   public static String getTopicNameFromClusterName(String clusterName) {
     return ADMIN_TOPIC_PREFIX + clusterName;
@@ -26,6 +35,6 @@ public class AdminTopicUtils {
   }
 
   public static boolean isKafkaInternalTopic(String topicName) {
-    return topicName.startsWith(KAFKA_INTERNAL_TOPIC_PREFIX);
+    return topicName.startsWith(KAFKA_INTERNAL_TOPIC_PREFIX) || TOPICS_TO_IGNORE.contains(topicName);
   }
 }
