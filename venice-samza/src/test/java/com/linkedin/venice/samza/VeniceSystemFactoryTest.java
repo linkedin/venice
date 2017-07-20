d6769441c4b5fbf414b90d3170df63410ee671be
Matthew Wise <mwise@linkedin.com>
2017-07-20 10:46:37 -0700

    Add support for avro serialization of primitives in the Venice Samza Producer
    This way avro applications can directly do writes with longs, floats, booleans, ...
    
    RB=1048658
    G=voldemort-reviewers
    A=fvillega,goliu

--- venice-samza/src/test/java/com/linkedin/venice/samza/VeniceSystemFactoryTest.java
+++ venice-samza/src/test/java/com/linkedin/venice/samza/VeniceSystemFactoryTest.java
@@ -8,16 +8,21 @@ import com.linkedin.venice.controllerapi.ControllerClient;
 import com.linkedin.venice.integration.utils.ServiceFactory;
 import com.linkedin.venice.integration.utils.VeniceClusterWrapper;
 import com.linkedin.venice.utils.TestUtils;
+import java.nio.ByteBuffer;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericData;
 import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.util.Utf8;
 import org.apache.samza.config.MapConfig;
 import org.apache.samza.system.OutgoingMessageEnvelope;
 import org.apache.samza.system.SystemProducer;
 import org.apache.samza.system.SystemStream;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -31,6 +36,7 @@ public class VeniceSystemFactoryTest {
   //for a record like: {'string': 'somestring', 'number': 3.14}
   private static final String VALUE_SCHEMA = "{\"fields\":[{\"type\":[\"double\",\"null\"],\"name\":\"number\"},{\"type\":[\"string\",\"null\"],\"name\":\"string\"}],\"type\":\"record\",\"name\":\"testRecord\"}";
   private static final String KEY_SCHEMA = "\"string\"";
+  private static final String VENICE_SYSTEM_NAME = "venice"; //This is the Samza system name for use by the Samza API.
   private VeniceClusterWrapper venice;
 
   @BeforeClass
@@ -48,28 +54,20 @@ public class VeniceSystemFactoryTest {
    */
   @Test
   public void testGetProducer() throws Exception {
-    String veniceSystemName = "venice"; //This is the Samza system name for use by the Samza API.
 
     String storeName = TestUtils.getUniqueString("store");
     ControllerClient client = new ControllerClient(venice.getClusterName(), venice.getRandomRouterURL());
     client.createNewStore(storeName, "owner", KEY_SCHEMA, VALUE_SCHEMA);
 
     //Configure and create a SystemProducer for Venice
-    Map<String, String> samzaConfig = new HashMap<>();
-    String configPrefix = SYSTEMS_PREFIX + veniceSystemName + DOT;
-    samzaConfig.put(configPrefix + VENICE_PUSH_TYPE, ControllerApiConstants.PushType.BATCH.toString());
-    samzaConfig.put(configPrefix + VENICE_URL, venice.getRandomRouterURL());
-    samzaConfig.put(configPrefix + VENICE_CLUSTER, venice.getClusterName());
-    samzaConfig.put(JOB_ID, "i001");
-    VeniceSystemFactory factory = new VeniceSystemFactory();
-    SystemProducer veniceProducer = factory.getProducer("venice", new MapConfig(samzaConfig), null);
+    SystemProducer veniceProducer = getVeniceProducer();
 
     //Create an AVRO record
     GenericRecord record = new GenericData.Record(Schema.parse(VALUE_SCHEMA));
     record.put("string", "somestring");
     record.put("number", 3.14);
     OutgoingMessageEnvelope envelope = new OutgoingMessageEnvelope(
-        new SystemStream(veniceSystemName, storeName),
+        new SystemStream(VENICE_SYSTEM_NAME, storeName),
         "keystring",
         record);
 
@@ -88,5 +86,58 @@ public class VeniceSystemFactoryTest {
 
     client.close();
     veniceProducer.stop();
+  }
+
+  @Test
+  public void testAllSerialization() throws InterruptedException, ExecutionException, TimeoutException {
+    testSerialization(5, 10,"\"int\"");
+    testSerialization(new Utf8("one"), new Utf8("two"), "\"string\"");
+    testSerializationCast("three", "three", "four", new Utf8("four"), "\"string\"");
+    testSerialization(6L, 8L,"\"long\"");
+    testSerialization(9.12D, 12.45D,"\"double\"");
+    testSerialization(1.6F, 7.4F,"\"float\"");
+    testSerialization(ByteBuffer.wrap(new byte[] {0x1, 0x2, 0x3}), ByteBuffer.wrap(new byte[] {0xb, 0xc, 0xd}),"\"bytes\"");
+    testSerializationCast(new byte[] {0x3, 0x4, 0x5}, ByteBuffer.wrap(new byte[] {0x3, 0x4, 0x5}), new byte[] {0xd, 0xe, 0xf}, ByteBuffer.wrap(new byte[] {0xd, 0xe, 0xf}),"\"bytes\"");
+    testSerialization(true, false,"\"boolean\"");
+  }
+
+  /**
+   * Avro sometimes returns a different type when deserializing.  For example, a serialized String is deserialized
+   * into a Utf8 object.  This method can be used to get around the resulting assertion error when the returned object
+   * is equivalent but of a different class.
+   */
+  private <K1, K2, V1, V2> void testSerializationCast(K1 writeKey, K2 readKey, V1 value, V2 expectedValue, String schema)
+      throws InterruptedException, ExecutionException, TimeoutException {
+    VeniceSystemProducer producer = (VeniceSystemProducer) getVeniceProducer();
+    ControllerClient client = new ControllerClient(venice.getClusterName(), venice.getRandomRouterURL());
+    String storeName = TestUtils.getUniqueString("schema-test-store");
+    client.createNewStore(storeName, "owner", schema, schema);
+    producer.send(storeName, new OutgoingMessageEnvelope(
+        new SystemStream(VENICE_SYSTEM_NAME, storeName),
+        writeKey,value));
+    client.writeEndOfPush(storeName, 1);
+    TestUtils.waitForNonDeterministicCompletion(5, TimeUnit.SECONDS, () -> client.getStore(storeName).getStore().getCurrentVersion() == 1);
+
+    AvroGenericStoreClient<K2, V2> storeClient = AvroStoreClientFactory.getAndStartAvroGenericStoreClient(venice.getRandomRouterURL(), storeName);
+    V2 valueFromStore = storeClient.get(readKey).get(1, TimeUnit.SECONDS);
+    Assert.assertEquals(valueFromStore, expectedValue,
+        valueFromStore.toString() + " of type: " + valueFromStore.getClass()
+            + " but expected " + value + " of type: " + value.getClass());
+  }
+
+  private <T> void testSerialization(T key, T value, String schema) throws InterruptedException, ExecutionException, TimeoutException {
+    testSerializationCast(key, key, value, value, schema);
+  }
+
+  private SystemProducer getVeniceProducer(){
+    Map<String, String> samzaConfig = new HashMap<>();
+    String configPrefix = SYSTEMS_PREFIX + VENICE_SYSTEM_NAME + DOT;
+    samzaConfig.put(configPrefix + VENICE_PUSH_TYPE, ControllerApiConstants.PushType.BATCH.toString());
+    samzaConfig.put(configPrefix + VENICE_URL, venice.getRandomRouterURL());
+    samzaConfig.put(configPrefix + VENICE_CLUSTER, venice.getClusterName());
+    samzaConfig.put(JOB_ID, TestUtils.getUniqueString("samza-push-id"));
+    VeniceSystemFactory factory = new VeniceSystemFactory();
+    SystemProducer veniceProducer = factory.getProducer(VENICE_SYSTEM_NAME, new MapConfig(samzaConfig), null);
+    return veniceProducer;
   }
 }
