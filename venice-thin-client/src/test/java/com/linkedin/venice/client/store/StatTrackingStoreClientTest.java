77c4c1fa5b49e2252671087f8a73efae3e188e87
Sidian Wu <sdwu@linkedin.com>
2017-08-14 15:12:13 -0700

    Update client metrics name pattern
    The new pattern looks like
    ${service_name}.${store_name}--${metric_name}.${metric_type} where the
    default service_name is "venice-client"
    
    RB=1070514
    G=voldemort-reviewers
    A=goliu

--- venice-thin-client/src/test/java/com/linkedin/venice/client/store/StatTrackingStoreClientTest.java
+++ venice-thin-client/src/test/java/com/linkedin/venice/client/store/StatTrackingStoreClientTest.java
@@ -1,6 +1,7 @@
 package com.linkedin.venice.client.store;
 
 import com.linkedin.venice.client.exceptions.VeniceClientHttpException;
+import com.linkedin.venice.utils.TestUtils;
 import io.tehuti.Metric;
 import io.tehuti.metrics.MetricsRepository;
 import java.util.HashMap;
@@ -8,6 +9,7 @@ import java.util.HashSet;
 import java.util.Set;
 import org.eclipse.jetty.http.HttpStatus;
 import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.util.Map;
@@ -17,10 +19,21 @@ import java.util.concurrent.ExecutionException;
 import static org.mockito.Mockito.*;
 
 public class StatTrackingStoreClientTest {
+  private InternalAvroStoreClient<String, Object> mockStoreClient;
+  private String metricPrefix;
+
+  @BeforeTest
+  public void setUp() {
+    mockStoreClient = mock(InternalAvroStoreClient.class);
+
+    String storeName = TestUtils.getUniqueString("store");
+    doReturn(storeName).when(mockStoreClient).getStoreName();
+    metricPrefix = "." + storeName;
+  }
+
 
   @Test
   public void testGet() throws ExecutionException, InterruptedException {
-    InternalAvroStoreClient<String, Object> mockStoreClient = mock(InternalAvroStoreClient.class);
     CompletableFuture<Object> mockInnerFuture = new CompletableFuture();
     Object mockReturnObject = mock(Object.class);
     mockInnerFuture.complete(mockReturnObject);
@@ -33,17 +46,18 @@ public class StatTrackingStoreClientTest {
       return value;
 
     });
+
     doReturn(mockInnerFuture).when(mockStoreClient).get(any());
 
     MetricsRepository repository = new MetricsRepository();
 
     StatTrackingStoreClient<String, Object> statTrackingStoreClient = new StatTrackingStoreClient<>(mockStoreClient, repository);
-    statTrackingStoreClient.get(new String("123")).get();
+    statTrackingStoreClient.get(any()).get();
 
     Map<String, ? extends Metric> metrics = repository.metrics();
-    Metric requestMetric = metrics.get(".venice_client--request.OccurrenceRate");
-    Metric healthyRequestMetric = metrics.get(".venice_client--healthy_request.OccurrenceRate");
-    Metric unhealthyRequestMetric = metrics.get(".venice_client--unhealthy_request.OccurrenceRate");
+    Metric requestMetric = metrics.get(metricPrefix + "--request.OccurrenceRate");
+    Metric healthyRequestMetric = metrics.get(metricPrefix + "--healthy_request.OccurrenceRate");
+    Metric unhealthyRequestMetric = metrics.get(metricPrefix + "--unhealthy_request.OccurrenceRate");
 
     Assert.assertTrue(requestMetric.value() > 0.0);
     Assert.assertTrue(healthyRequestMetric.value() > 0.0);
@@ -52,7 +66,6 @@ public class StatTrackingStoreClientTest {
 
   @Test
   public void testMultiGet() throws ExecutionException, InterruptedException {
-    InternalAvroStoreClient<String, Object> mockStoreClient = mock(InternalAvroStoreClient.class);
     CompletableFuture<Object> mockInnerFuture = new CompletableFuture();
     Map<String, String> result = new HashMap<>();
     Set<String> keySet = new HashSet<>();
@@ -81,12 +94,12 @@ public class StatTrackingStoreClientTest {
     statTrackingStoreClient.batchGet(keySet).get();
 
     Map<String, ? extends Metric> metrics = repository.metrics();
-    Metric requestMetric = metrics.get(".venice_client--multiget_request.OccurrenceRate");
-    Metric healthyRequestMetric = metrics.get(".venice_client--multiget_healthy_request.OccurrenceRate");
-    Metric unhealthyRequestMetric = metrics.get(".venice_client--multiget_unhealthy_request.OccurrenceRate");
-    Metric keyCountMetric = metrics.get(".venice_client--multiget_request_key_count.Avg");
-    Metric successKeyCountMetric = metrics.get(".venice_client--multiget_success_request_key_count.Avg");
-    Metric successKeyRatioMetric = metrics.get(".venice_client--multiget_success_request_key_ratio.SimpleRatioStat");
+    Metric requestMetric = metrics.get(metricPrefix + "--multiget_request.OccurrenceRate");
+    Metric healthyRequestMetric = metrics.get(metricPrefix + "--multiget_healthy_request.OccurrenceRate");
+    Metric unhealthyRequestMetric = metrics.get(metricPrefix + "--multiget_unhealthy_request.OccurrenceRate");
+    Metric keyCountMetric = metrics.get(metricPrefix + "--multiget_request_key_count.Avg");
+    Metric successKeyCountMetric = metrics.get(metricPrefix + "--multiget_success_request_key_count.Avg");
+    Metric successKeyRatioMetric = metrics.get(metricPrefix + "--multiget_success_request_key_ratio.SimpleRatioStat");
 
     Assert.assertTrue(requestMetric.value() > 0.0);
     Assert.assertTrue(healthyRequestMetric.value() > 0.0);
@@ -98,7 +111,6 @@ public class StatTrackingStoreClientTest {
 
   @Test
   public void testGetWithException() throws ExecutionException, InterruptedException {
-    InternalAvroStoreClient<String, Object> mockStoreClient = mock(InternalAvroStoreClient.class);
     CompletableFuture<Object> mockInnerFuture = new CompletableFuture();
     mockInnerFuture.completeExceptionally(new VeniceClientHttpException("Inner mock exception", HttpStatus.BAD_REQUEST_400));
     mockInnerFuture = mockInnerFuture.handle((value, throwable) -> {
@@ -117,7 +129,7 @@ public class StatTrackingStoreClientTest {
 
     StatTrackingStoreClient<String, Object> statTrackingStoreClient = new StatTrackingStoreClient<>(mockStoreClient, repository);
     try {
-      statTrackingStoreClient.get(new String("123")).get();
+      statTrackingStoreClient.get(any()).get();
       Assert.fail("ExecutionException should be thrown");
     } catch (ExecutionException e) {
       System.out.println(e);
@@ -125,10 +137,10 @@ public class StatTrackingStoreClientTest {
     }
 
     Map<String, ? extends Metric> metrics = repository.metrics();
-    Metric requestMetric = metrics.get(".venice_client--request.OccurrenceRate");
-    Metric healthyRequestMetric = metrics.get(".venice_client--healthy_request.OccurrenceRate");
-    Metric unhealthyRequestMetric = metrics.get(".venice_client--unhealthy_request.OccurrenceRate");
-    Metric http400RequestMetric = metrics.get(".venice_client--http_400_request.OccurrenceRate");
+    Metric requestMetric = metrics.get(metricPrefix + "--request.OccurrenceRate");
+    Metric healthyRequestMetric = metrics.get(metricPrefix + "--healthy_request.OccurrenceRate");
+    Metric unhealthyRequestMetric = metrics.get(metricPrefix + "--unhealthy_request.OccurrenceRate");
+    Metric http400RequestMetric = metrics.get(metricPrefix + "--http_400_request.OccurrenceRate");
 
     Assert.assertTrue(requestMetric.value() > 0.0);
     Assert.assertEquals(healthyRequestMetric.value(), 0.0);
