1573de38195f533f73dfe5747817d065ac74ab73
Yan Yan <yayan@linkedin.com>
2017-10-24 12:35:12 -0700

    Let venice shell client to support query from vson store.
    RB=1136182
    G=voldemort-reviewers
    R=sdwu,goli
    A=goliu

--- venice-thin-client/bin/query_key.sh
+++ venice-thin-client/bin/query_key.sh
@@ -4,8 +4,8 @@
 # then execute the query command.
 BASE_DIR="`dirname ${BASH_SOURCE[0]}`/.."
 
-if [ $# -gt 3 ] || [ $# -lt 3 ]; then
-	echo "Usage: $0 <fabric> <store_name> <key_string>"
+if [ $# -gt 4 ] || [ $# -lt 3 ]; then
+	echo "Usage: $0 <fabric> <store_name> <key_string> [is_vson_store]"
 	echo "Do not include any space in key_string, and replace all \" by \\\"."
 	exit 1
 else
@@ -13,6 +13,10 @@ else
 	FABRIC=$1
 	STORE_NAME=$2
 	KEY_STRING=$3
+	IS_VSON_STORE="false"
+	if [ $# -gt 3 ]; then
+	  IS_VSON_STORE=$4
+	fi
 	D2_CLUSTER="VeniceRouter"
 fi
 
@@ -29,4 +33,4 @@ ROUTER_URL=`echo $FIND_ROUTER_RESULT | jq '.uris[0].URI'`
 
 echo "Send a request to $ROUTER_URL, Store $STORE_NAME, Key string: $KEY_STRING..."
 
-java -jar $BASE_DIR/build/libs/venice-thin-client-0.1.jar $STORE_NAME $KEY_STRING $ROUTER_URL
+java -jar $BASE_DIR/build/libs/venice-thin-client-0.1.jar $STORE_NAME $KEY_STRING $ROUTER_URL $IS_VSON_STORE
