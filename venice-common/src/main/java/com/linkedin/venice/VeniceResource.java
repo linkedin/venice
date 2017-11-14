2b506f36d30b442aaddfd7b717139b8280c4cc28
Gaojie Liu <goliu@linkedin.com>
2017-11-13 17:39:53 -0800

    Fixed a race condition in HelixReadOnlySchemaRepository
    Preivously, a race condition issue could happen in this sequence:
    1. HelixReadOnlySchemaRepository#getKeySchema gets triggered;
    2. The schema doesn't exist, so it will trigger
    HelixReadOnlySchemaRepository#fetchStoreSchemaIfNotInCache;
    3. Then a zk reconnect issue happens, so
    HelixReadOnlySchemaRepository#refresh will clear all the schema data;
    4. HelixReadOnlySchemaRepository#getKeySchema will throw
    VeniceNoStoreException;
    
    With this change, we moved
    HelixReadOnlySchemaRepository#fetchStoreSchemaIfNotInCache inside of the
    scope of schemaLock.readLock(), and use
    ConcurrentHashMap#computeIfAbsent in
    HelixReadOnlySchemaRepository#fetchStoreSchemaIfNotInCache
    to avoid the complication of
    readlock/writelock switching/degrading.
    
    RB=1156759
    G=voldemort-reviewers
    R=fvillega,yayan
    A=fvillega

--- venice-common/src/main/java/com/linkedin/venice/VeniceResource.java
+++ venice-common/src/main/java/com/linkedin/venice/VeniceResource.java
@@ -4,7 +4,11 @@ package com.linkedin.venice;
  * Venice resource that could be refreshed to retrieve the newest data or clear the current data in memory.
  */
 public interface VeniceResource {
-  public void refresh();
+  void refresh();
 
-  public void clear();
+  /**
+   * TODO: we may need to rename this function to be 'close' since this resource should not used any more
+   * after calling this function.
+   */
+  void clear();
 }
