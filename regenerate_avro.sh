151e4527a5beb25f63bb63694d999a11826e8036
Felix GV <fgv@linkedin.com>
2017-11-20 18:14:20 -0800

    Added a couple of new schemas to enable value-chunking.
    - ChunkedValueManifest is intended to be stored in the value, and distinguished
      from regular values by tagging it as schema ID -1. It contains various pieces
      of information needed to reconstruct the multi-chunk value.
    - ChunkId is intended to prepend the key for record chunks. Whether to use
      this ChunkId as a key prefix or not is going to be decided at the granularity
      of store-version. i.e.: for a store-version that enables this feature, all
      of its keys are going to be prepended. The record itself is optional, which
      means the overhead should be minimal for non-chunked keys.
    
    The two new schemas are placed inside venice-common, rather than venice-server
    because we intend to re-use the same GUID class which is part of the Kafka
    protocol schemas, and it is easier to achieve this within a single module. They
    will also be leveraged from within the VenicePushJob.
    
    In addition to the above new schemas, the following old schemas have also been
    updated:
    
    - KafkaMessageEnvelope, v4: Includes a new "chunked" boolean field in the
      StartOfPush control message.
    - StoreVersionState, v2: Includes a new "chunked" boolean field as well.
    
    Both of these new fields default to false, if absent.
    
    RB=1163273
    G=voldemort-reviewers
    R=goliu
    A=goliu

--- regenerate_avro.sh
+++ regenerate_avro.sh
@@ -6,12 +6,16 @@ AVRO_SCHEMAS_PATH=(
   "venice-common/src/main/resources/avro/KafkaMessageEnvelope/v3/*"
   "venice-common/src/main/resources/avro/PartitionState/v3/*"
   "venice-common/src/main/resources/avro/StoreVersionState/v1/*"
+  "venice-common/src/main/resources/avro/ChunkedValueManifest/v-1/*"
+  "venice-common/src/main/resources/avro/ChunkId/*"
   "venice-controller/src/main/resources/avro/AdminOperation/v10/*"
   "venice-schema-common/src/main/resources/avro/MultiGetResponseRecord/*"
   "venice-schema-common/src/main/resources/avro/MultiGetClientRequestKey/*"
   "venice-schema-common/src/main/resources/avro/MultiGetRouterRequestKey/*"
 )
 CODE_GEN_PATH=(
+  "venice-common/src/main/java"
+  "venice-common/src/main/java"
   "venice-common/src/main/java"
   "venice-common/src/main/java"
   "venice-common/src/main/java"
@@ -24,10 +28,12 @@ FULL_CODE_GEN_PATH=(
   "${CODE_GEN_PATH[0]}/com/linkedin/venice/kafka/protocol/*.java"
   "${CODE_GEN_PATH[1]}/com/linkedin/venice/kafka/protocol/state/*.java"
   "${CODE_GEN_PATH[2]}/com/linkedin/venice/kafka/protocol/state/*.java"
-  "${CODE_GEN_PATH[3]}/com/linkedin/venice/controller/kafka/protocol/admin/*.java"
-  "${CODE_GEN_PATH[4]}/com/linkedin/venice/read/protocol/response/*.java"
-  "${CODE_GEN_PATH[5]}/com/linkedin/venice/read/protocol/request/client/*.java"
-  "${CODE_GEN_PATH[6]}/com/linkedin/venice/read/protocol/request/router/*.java"
+  "${CODE_GEN_PATH[3]}/com/linkedin/venice/storage/protocol/*.java"
+  "${CODE_GEN_PATH[4]}/com/linkedin/venice/storage/protocol/*.java"
+  "${CODE_GEN_PATH[5]}/com/linkedin/venice/controller/kafka/protocol/admin/*.java"
+  "${CODE_GEN_PATH[6]}/com/linkedin/venice/read/protocol/response/*.java"
+  "${CODE_GEN_PATH[7]}/com/linkedin/venice/read/protocol/request/client/*.java"
+  "${CODE_GEN_PATH[8]}/com/linkedin/venice/read/protocol/request/router/*.java"
 )
 
 if [[ $# < 1 ]]; then
