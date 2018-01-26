9c129d70390de6c40a9f9437130b045184ff6419
yayan <yayan@linkedin.com>
2018-01-25 18:59:21 -0800

    Add the filter to prevent logging redundant exception during the specifiy duration.
    RB=1205068
    G=voldemort-reviewers
    A=fvillega

---
+++ venice-common/src/main/java/com/linkedin/venice/utils/RedundantExceptionFilter.java
@@ -0,0 +1,101 @@
+package com.linkedin.venice.utils;
+
+import com.linkedin.ddsstorage.router.api.RouterException;
+import com.linkedin.venice.exceptions.VeniceException;
+import java.util.BitSet;
+import java.util.concurrent.TimeUnit;
+import org.apache.log4j.Logger;
+
+
+public class RedundantExceptionFilter {
+  public static final int DEFAULT_BITSET_SIZE = 8 * 1024 * 1024 * 16; //16MB
+  public static final long DEFAULT_NO_REDUNDANT_EXCEPTION_DURATION_MS = TimeUnit.SECONDS.toMillis(60); // 60s
+  public static final Logger logger = Logger.getLogger(RedundantExceptionFilter.class);
+  private static RedundantExceptionFilter singleton;
+  private BitSet activeBitset;
+  private BitSet oldBitSet;
+
+  private final long noRedundantExceptionDurationMs;
+  private final int bitSetSize;
+
+  private final Thread cleanerThread;
+
+  protected RedundantExceptionFilter(int bitSetSize, long noRedundantExceptionDurationMs) {
+    this.noRedundantExceptionDurationMs = noRedundantExceptionDurationMs;
+    this.bitSetSize = bitSetSize;
+    activeBitset = new BitSet(bitSetSize);
+    oldBitSet = new BitSet(bitSetSize);
+    // Start a cleaner thread to clear bit sets every noRedundantExceptionDurationMs
+    cleanerThread = new Thread(() -> {
+      while (true) {
+        try {
+          Utils.sleep(this.noRedundantExceptionDurationMs);
+          clearBitSet();
+        } catch (Throwable e) {
+          logger.error("Get an exception in cleaner thread.", e);
+        }
+      }
+    });
+    cleanerThread.start();
+  }
+
+  public synchronized static RedundantExceptionFilter getRedundantExceptionFilter(){
+    return getRedundantExceptionFilter(DEFAULT_BITSET_SIZE, DEFAULT_NO_REDUNDANT_EXCEPTION_DURATION_MS);
+  }
+
+  public synchronized static RedundantExceptionFilter getDailyRedundantExceptioFilter(){
+    // clean up the bitset every day for the use case do not need to clean up frequently.
+    return getRedundantExceptionFilter(DEFAULT_BITSET_SIZE, Time.MS_PER_DAY);
+  }
+
+  protected synchronized static RedundantExceptionFilter getRedundantExceptionFilter(int bitSetSize,
+      long noRedundantExceptionDurationMs) {
+    if (singleton == null) {
+      singleton = new RedundantExceptionFilter(bitSetSize, noRedundantExceptionDurationMs);
+    }
+    return singleton;
+  }
+
+  private int getIndex(String key) {
+    return Math.abs((key).hashCode() % bitSetSize);
+  }
+
+
+  public boolean isRedundantException(String exceptionMessage){
+    int index = getIndex(exceptionMessage);
+    return isRedundant(index);
+  }
+
+  public boolean isRedundantException(String storeName, Exception e) {
+    // By default use exception's class as the type. For VeniceException and RouterException use http status code instead.
+    String exceptionType = e.getClass().getName();
+    if (e instanceof RouterException) {
+      exceptionType = String.valueOf(((RouterException) e).code());
+    } else if (e instanceof VeniceException) {
+      exceptionType = String.valueOf(((VeniceException) e).getHttpStatusCode());
+    }
+    int index = getIndex(storeName + exceptionType);
+    return isRedundant(index);
+  }
+
+  private boolean isRedundant(int index){
+    if (!activeBitset.get(index)) {
+      // It's possible that we found the bit was not set, then activeBitset is changed, and we set the bit in the new
+      // set. But it doesn't matter.
+      activeBitset.set(index);
+      return false;
+    }
+    return true;
+  }
+
+  public void clearBitSet() {
+    synchronized (this) {
+      // Swap bit sets so we are not blocked by clear operation.
+      BitSet temp = oldBitSet;
+      oldBitSet = activeBitset;
+      activeBitset = temp;
+    }
+    oldBitSet.clear();
+  }
+
+}
---
+++ venice-common/src/test/java/com/linkedin/venice/utils/TestRedundantExceptionFilter.java
@@ -0,0 +1,57 @@
+package com.linkedin.venice.utils;
+
+import com.linkedin.ddsstorage.router.api.RouterException;
+import com.linkedin.venice.utils.RedundantExceptionFilter;
+import com.linkedin.venice.utils.TestUtils;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import java.util.concurrent.TimeUnit;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class TestRedundantExceptionFilter {
+
+  @Test
+  public void testIsRedundantException() {
+    long duration = 1000;
+    RedundantExceptionFilter filter =
+        new RedundantExceptionFilter(RedundantExceptionFilter.DEFAULT_BITSET_SIZE, duration);
+    String store = "testLogException";
+    String store1 = "testLogException1";
+    HttpResponseStatus status = HttpResponseStatus.NOT_FOUND;
+    HttpResponseStatus status1 = HttpResponseStatus.TOO_MANY_REQUESTS;
+    Exception e = new RouterException(HttpResponseStatus.class, status, status.code(), "test", false);
+    Exception e1 = new RouterException(HttpResponseStatus.class, status1, status1.code(), "test", false);
+    Assert.assertFalse(filter.isRedundantException(store, e), "This is the first time we see this exception.");
+    Assert.assertFalse(filter.isRedundantException(store1, e), "This is the first time we see this exception.");
+    Assert.assertFalse(filter.isRedundantException(store, e1), "This is the first time we see this exception.");
+    Assert.assertFalse(filter.isRedundantException(store1, e1), "This is the first time we see this exception.");
+    Assert.assertTrue(filter.isRedundantException(store, e), "This is the second time we see this exception.");
+    // after duration, the filter's bitset will be cleaned up.
+    TestUtils.waitForNonDeterministicCompletion(duration, TimeUnit.MILLISECONDS,
+        () -> !filter.isRedundantException(store, e));
+  }
+
+  @Test
+  public void testClear() {
+    long duration = 10000000;
+    RedundantExceptionFilter filter =
+        new RedundantExceptionFilter(RedundantExceptionFilter.DEFAULT_BITSET_SIZE, duration);
+    String store = "testClear";
+    HttpResponseStatus status = HttpResponseStatus.NOT_FOUND;
+    Exception e = new RouterException(HttpResponseStatus.class, status, status.code(), "test", false);
+    filter.isRedundantException(store, e);
+    filter.clearBitSet();
+    Assert.assertFalse(filter.isRedundantException(store, e));
+  }
+
+  @Test
+  public void testGetFilter() {
+    long duration = 1000;
+    RedundantExceptionFilter filter =
+        RedundantExceptionFilter.getRedundantExceptionFilter(RedundantExceptionFilter.DEFAULT_BITSET_SIZE, duration);
+    RedundantExceptionFilter filter1 =
+        RedundantExceptionFilter.getRedundantExceptionFilter(RedundantExceptionFilter.DEFAULT_BITSET_SIZE, duration * 10);
+    Assert.assertTrue(filter == filter1);
+  }
+}
