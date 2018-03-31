6b23f87fdaeb70c99c165a2581ce9c82d3fa468b
Charles Gao <chgao@linkedin.com>
2018-03-30 17:21:23 -0700

    Validate User Certificate Before Sending A Request
    As more and more users switching to Venice, we have started receiving
    questions about venice-thin-client caused by multiple reasons. I'm
    adding the following checks to improve the user experience and to
    provide more meaningful error messages for both clients and the Venice
    team:
    
    * keystore.password must exist in config file
    * keystore.path must exist in config file
    * keystore.path must exist in file system
    * keystore.path cannot be relative path
    * User certificate must not be expired
    
    RB=1268170
    G=voldemort-reviewers
    A=fvillega

--- venice-thin-client/bin/query_key.sh
+++ venice-thin-client/bin/query_key.sh
@@ -20,6 +20,39 @@ else
 	D2_CLUSTER="VeniceRouter"
 fi
 
+SSL_CONFIG="$BASE_DIR/config/sslconfig.properties"
+
+# Validate certificate
+if ! grep -q "keystore.password=" $SSL_CONFIG; then
+  echo "ERROR: $SSL_CONFIG does not contain keystore.password"
+  exit 1
+elif ! grep -q "keystore.path=" $SSL_CONFIG; then
+  echo "ERROR: $SSL_CONFIG does not contain keystore.path"
+  exit 1
+fi
+
+keystorePassword=`grep "keystore.password=" $SSL_CONFIG | cut -d= -f2-`
+keystorePath=`grep "keystore.path=" $SSL_CONFIG | cut -d= -f2-`
+keystoreRealPath=`python -c "import os; print os.path.realpath('$keystorePath')"`
+
+if [[ "$keystorePath" != "$keystoreRealPath" ]]; then
+  echo "ERROR: Please use absolute path for $keystorePath"
+  exit 1
+elif [[ ! -e $keystorePath ]]; then
+  echo "ERROR: $keystorePath does not exist"
+  exit 1
+else
+  notAfter=$(2>/dev/null openssl pkcs12 -in $keystorePath -clcerts -nodes -passin pass:$keystorePassword | openssl x509 -noout -enddate | cut -d= -f2)
+  expirationTime=$(python -c "from datetime import datetime; print datetime.strptime('$notAfter', '%b %d %X %Y %Z').strftime('%s')")
+  currentTime=`date +%s`
+  remainingTime=$(($expirationTime - $currentTime))
+
+  if (( $remainingTime < 1000 )); then
+    echo "Your certificate $keystorePath has expired. Please generate a new certificate."
+    exit 1
+  fi
+fi
+
 # discover cluster for store
 echo "Find the real d2 cluster for store: $STORE_NAME through in fabric: $FABRIC through d2..."
 FIND_CLUSTER_RESULT=`curli --fabric $FABRIC d2://$D2_CLUSTER/discover_cluster/$STORE_NAME`
@@ -33,5 +66,4 @@ ROUTER_URL=`echo $FIND_ROUTER_RESULT | jq '[.uris[]| select(.URI|contains("https
 echo ""
 echo "Will send a request to $ROUTER_URL, Store $STORE_NAME, Key string: $KEY_STRING..."
 
-SSL_CONFIG="$BASE_DIR/config/sslconfig.properties"
 java -jar $BASE_DIR/build/libs/venice-thin-client-0.1.jar $STORE_NAME $KEY_STRING $ROUTER_URL $IS_VSON_STORE $SSL_CONFIG
