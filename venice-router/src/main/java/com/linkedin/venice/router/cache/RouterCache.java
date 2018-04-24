f03a0b4dc30978ee26c590991876f902eddb5d88
Matthew Wise <mwise@linkedin.com>
2018-04-23 17:40:38 -0700

    Pulled message digest object into thread local for reusability
    In performance testing I found that if the storage node is fast (by
    using an in-memory storage engine) then under heavy load the router
    netty threads spend a lot of time blocked trying to get a message
    digest object from the java security packages.  This object can be
    reused, but has some state so I persist that object as a ThreadLocal
    so it can be reused.  With 100 concurrent batchget requests of 2000
    keys each and an in-memory storage engine this improves peformance
    (in local unit testing) from an average of 73ms per request to an
    average of 46ms per request.
    
    RB=1289907
    G=voldemort-reviewers
    A=goliu

--- venice-router/src/main/java/com/linkedin/venice/router/cache/RouterCache.java
+++ venice-router/src/main/java/com/linkedin/venice/router/cache/RouterCache.java
@@ -66,7 +66,7 @@ public class RouterCache implements RoutingDataRepository.RoutingDataChangedList
       try {
         return createMD5MessageDigest();
       } catch (NoSuchAlgorithmException e) {
-        return null;
+        throw new VeniceException("Failed to create MD5 MessageDigest");
       }
     });
 
