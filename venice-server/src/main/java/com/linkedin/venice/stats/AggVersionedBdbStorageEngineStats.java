214a2d9c3d6bfcc55176cf08585a7add8936c4b3
Sidian Wu <sidian.wu@gmail.com>
2018-07-26 11:10:00 -0700

    Fix the SN bounce failure due to storage engine stats
    After we moved BdbStorageEngineStats as Versioned stats, it started to
    listen to ZK store metadata repository when creating/removing new stats.
    If a store is removed during SN is in offline, there is an edge case
    when SN rebounces and scans the disk to restore exisiting storage
    engine. Since the store is removed in ZK, SN will throw exception and
    fail to rebounce.
    
    Catch the exception in the SN to prevent it.
    
    Jira: VOLDENG-4056
    
    RB=1371882
    G=voldemort-reviewers
    R=mwise,yayan,chgao,goliu
    A=mwise

--- venice-server/src/main/java/com/linkedin/venice/stats/AggVersionedBdbStorageEngineStats.java
+++ venice-server/src/main/java/com/linkedin/venice/stats/AggVersionedBdbStorageEngineStats.java
@@ -1,14 +1,18 @@
 package com.linkedin.venice.stats;
 
+import com.linkedin.venice.exceptions.VeniceException;
 import com.linkedin.venice.meta.ReadOnlyStoreRepository;
 import com.linkedin.venice.meta.Version;
 import com.sleepycat.je.Environment;
 import io.tehuti.metrics.MetricsRepository;
 import javafx.util.Pair;
+import org.apache.log4j.Logger;
 
 
 public class AggVersionedBdbStorageEngineStats
     extends AbstractVeniceAggVersionedStats<BdbStorageEngineStats, BdbStorageEngineStatsReporter> {
+  private static final Logger logger = Logger.getLogger(AggVersionedBdbStorageEngineStats.class);
+
   public AggVersionedBdbStorageEngineStats(MetricsRepository metricsRepository, ReadOnlyStoreRepository metadataRepository) {
     super(metricsRepository, metadataRepository, () -> new BdbStorageEngineStats(),
         (mr, name) -> new BdbStorageEngineStatsReporter(mr, name));
@@ -17,14 +21,22 @@ public class AggVersionedBdbStorageEngineStats
   public void setBdbEnvironment(String topicName, Environment environment) {
     Pair<String, Integer> storeInfo = getStoreNameAndVersionFromTopic(topicName);
     if (storeInfo != null) {
-      getStats(storeInfo.getKey(), storeInfo.getValue()).setBdbEnvironment(environment);
+      try {
+        getStats(storeInfo.getKey(), storeInfo.getValue()).setBdbEnvironment(environment);
+      } catch (VeniceException e) {
+        logger.warn("Could not create BDB storage engine stats for store: " + topicName, e);
+      }
     }
   }
 
   public void removeBdbEnvironment(String topicName) {
     Pair<String, Integer> storeInfo = getStoreNameAndVersionFromTopic(topicName);
     if (storeInfo != null) {
-      getStats(storeInfo.getKey(), storeInfo.getValue()).removeBdbEnvironment();
+      try {
+        getStats(storeInfo.getKey(), storeInfo.getValue()).removeBdbEnvironment();
+      } catch (VeniceException e) {
+        logger.warn("Could not remove BDB storage engine stats for store: " + topicName, e);
+      }
     }
   }
 
