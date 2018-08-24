324509a6a0e233a035769897678018f6abcee736
Min Huang(Data Infrastructure) <mnhuang@linkedin.com>
2018-08-24 12:07:29 -0700

    [controller] failed_admin_message: emit 0 instead of nothing when successfully process admin message
    RB=1402069
    G=voldemort-reviewers
    R=kchung
    A=goliu

--- venice-controller/src/main/java/com/linkedin/venice/controller/stats/AdminConsumptionStats.java
+++ venice-controller/src/main/java/com/linkedin/venice/controller/stats/AdminConsumptionStats.java
@@ -4,7 +4,6 @@ import com.linkedin.venice.stats.AbstractVeniceStats;
 import io.tehuti.metrics.MetricsRepository;
 import io.tehuti.metrics.Sensor;
 import io.tehuti.metrics.stats.Count;
-import io.tehuti.metrics.stats.Max;
 
 
 public class AdminConsumptionStats extends AbstractVeniceStats {
@@ -14,15 +13,21 @@ public class AdminConsumptionStats extends AbstractVeniceStats {
   public AdminConsumptionStats(MetricsRepository metricsRepository, String name) {
     super(metricsRepository, name);
 
-    adminConsumeFailCountSensor = registerSensor("failed_admin_messages", new Max());
+    adminConsumeFailCountSensor = registerSensor("failed_admin_messages", new Count());
     adminTopicDIVErrorReportCountSensor = registerSensor("admin_message_div_error_report_count", new Count());
   }
 
   /**
    * @param retryCount the number of times that a failure has consecutively triggered a retry
    */
-  public void recordFailedAdminConsumption(double retryCount) {
-    adminConsumeFailCountSensor.record(retryCount);
+
+  /**
+   * Record the number of failed admin messages in the past one minute;
+   * if controller keeps retrying the admin messages, this metric will keep growing;
+   * this metric will be reset to 0 once the blocked admin message is processed.
+   */
+  public void recordFailedAdminConsumption() {
+    adminConsumeFailCountSensor.record();
   }
 
   public void recordAdminTopicDIVErrorReportCount() {
