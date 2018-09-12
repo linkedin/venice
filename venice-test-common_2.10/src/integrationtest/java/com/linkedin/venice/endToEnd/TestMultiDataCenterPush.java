e30932dffd3a8847de076f09762e2396eb50cfbf
Charles Gao <chgao@linkedin.com>
2018-09-12 16:40:49 -0700

    [test-common] Refactoring TestMultiDataCenterPush Integration Test
    Removed cluster creation logic and replaced with
    VeniceTwoLayerMultiColoMultiClusterWrapper
    
    RB=1419144
    G=voldemort-reviewers
    R=fvillega,mnhuang
    A=fvillega

--- venice-test-common_2.10/src/integrationtest/java/com/linkedin/venice/endToEnd/TestMultiDataCenterPush.java
+++ venice-test-common_2.10/src/integrationtest/java/com/linkedin/venice/endToEnd/TestMultiDataCenterPush.java
@@ -6,98 +6,95 @@ import com.linkedin.venice.client.store.ClientFactory;
 import com.linkedin.venice.controllerapi.ControllerClient;
 import com.linkedin.venice.controllerapi.JobStatusQueryResponse;
 import com.linkedin.venice.hadoop.KafkaPushJob;
-import com.linkedin.venice.integration.utils.KafkaBrokerWrapper;
-import com.linkedin.venice.integration.utils.MirrorMakerWrapper;
 import com.linkedin.venice.integration.utils.ServiceFactory;
-import com.linkedin.venice.integration.utils.VeniceClusterWrapper;
 import com.linkedin.venice.integration.utils.VeniceControllerWrapper;
+import com.linkedin.venice.integration.utils.VeniceMultiClusterWrapper;
+import com.linkedin.venice.integration.utils.VeniceTwoLayerMultiColoMultiClusterWrapper;
 import com.linkedin.venice.kafka.TopicManager;
 import com.linkedin.venice.meta.Version;
 import com.linkedin.venice.pushmonitor.ExecutionStatus;
 import com.linkedin.venice.utils.TestUtils;
 import com.linkedin.venice.utils.Time;
 import java.io.File;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import org.apache.avro.Schema;
-import org.apache.commons.io.IOUtils;
 import org.apache.log4j.Logger;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import static com.linkedin.venice.hadoop.KafkaPushJob.*;
 import static com.linkedin.venice.utils.TestPushUtils.*;
-import static com.linkedin.venice.hadoop.KafkaPushJob.INCREMENTAL_PUSH;
 
 
 public class TestMultiDataCenterPush {
   private static final Logger LOGGER = Logger.getLogger(TestMultiDataCenterPush.class);
   private static final int TEST_TIMEOUT = 360 * Time.MS_PER_SECOND;
-  private static final String CLUSTER_NAME = "multi-dc-test-cluster";
   private static final int NUMBER_OF_CHILD_DATACENTERS = 2;
-  private final List<VeniceClusterWrapper> childClusters = new ArrayList<>(NUMBER_OF_CHILD_DATACENTERS);
-  private final List<MirrorMakerWrapper> mirrorMakers = new ArrayList<>(NUMBER_OF_CHILD_DATACENTERS);
-  private KafkaBrokerWrapper parentKafka;
-  private VeniceControllerWrapper parentController;
-  private VeniceControllerWrapper[] childControllers;
+  private static final int NUMBER_OF_CLUSTERS = 2;
+  private static final String[] CLUSTER_NAMES =
+      IntStream.range(0, NUMBER_OF_CLUSTERS).mapToObj(i -> "venice-cluster" + i).toArray(String[]::new); // ["venice-cluster0", "venice-cluster1", ...];
+
+  private List<VeniceMultiClusterWrapper> childClusters;
+  private List<List<VeniceControllerWrapper>> childControllers;
+  private List<VeniceControllerWrapper> parentControllers;
+  private VeniceTwoLayerMultiColoMultiClusterWrapper multiColoMultiClusterWrapper;
 
   @BeforeClass
   public void setUp() {
-    parentKafka = ServiceFactory.getKafkaBroker();
+    multiColoMultiClusterWrapper = ServiceFactory.getVeniceTwoLayerMultiColoMultiClusterWrapper(NUMBER_OF_CHILD_DATACENTERS, 2, 1, 1, 1, 1);
 
-    childControllers = new VeniceControllerWrapper[NUMBER_OF_CHILD_DATACENTERS];
-    for (int dataCenterIndex = 0; dataCenterIndex < NUMBER_OF_CHILD_DATACENTERS; dataCenterIndex++) {
-      VeniceClusterWrapper childCluster = ServiceFactory.getVeniceCluster(CLUSTER_NAME);
-      childClusters.add(childCluster);
-      childControllers[dataCenterIndex] = childCluster.getMasterVeniceController();
+    childClusters = multiColoMultiClusterWrapper.getClusters();
+    childControllers = childClusters.stream()
+        .map(veniceClusterWrapper -> veniceClusterWrapper.getControllers()
+            .values()
+            .stream()
+            .collect(Collectors.toList()))
+        .collect(Collectors.toList());
+    parentControllers = multiColoMultiClusterWrapper.getParentControllers();
 
-      MirrorMakerWrapper mirrorMakerWrapper = ServiceFactory.getKafkaMirrorMaker(parentKafka, childCluster.getKafka());
-      mirrorMakers.add(mirrorMakerWrapper);
-    }
+    LOGGER.info("parentControllers: " + parentControllers.stream()
+        .map(c -> c.getControllerUrl())
+        .collect(Collectors.joining(", ")));
 
-    parentController = ServiceFactory.getVeniceParentController(
-        CLUSTER_NAME,
-        parentKafka.getZkAddress(),
-        parentKafka,
-        childControllers,
-        false);
-
-    LOGGER.info("parentController.getControllerUrl(): " + parentController.getControllerUrl());
-    for (int dataCenterIndex = 0; dataCenterIndex < NUMBER_OF_CHILD_DATACENTERS; dataCenterIndex++) {
-      LOGGER.info("childControllers[" + dataCenterIndex + "].getControllerUrl(): " + childControllers[dataCenterIndex].getControllerUrl());
+    int i = 0;
+    for (VeniceMultiClusterWrapper multiClusterWrapper : childClusters) {
+      LOGGER.info("childCluster" + i++ + " controllers: " + multiClusterWrapper.getControllers()
+          .values()
+          .stream()
+          .map(c -> c.getControllerUrl())
+          .collect(Collectors.joining(", ")));
     }
   }
 
   @AfterClass
   public void cleanUp() {
-    for (int dataCenterIndex = 0; dataCenterIndex < NUMBER_OF_CHILD_DATACENTERS; dataCenterIndex++) {
-      MirrorMakerWrapper mirrorMakerWrapper = mirrorMakers.get(dataCenterIndex);
-      IOUtils.closeQuietly(mirrorMakerWrapper);
-      VeniceClusterWrapper childCluster = childClusters.get(dataCenterIndex);
-      IOUtils.closeQuietly(childCluster);
-    }
-    IOUtils.closeQuietly(parentController);
-    IOUtils.closeQuietly(parentKafka);
+    multiColoMultiClusterWrapper.close();
   }
 
   @Test(timeOut = TEST_TIMEOUT)
   public void testMultiDataCenterPush() throws Exception {
+    String clusterName = CLUSTER_NAMES[0];
     File inputDir = getTempDataDirectory();
     Schema recordSchema = writeSimpleAvroFileWithUserSchema(inputDir);
     String inputDirPath = "file:" + inputDir.getAbsolutePath();
     String storeName = TestUtils.getUniqueString("store");
+    VeniceControllerWrapper parentController =
+        parentControllers.stream().filter(c -> c.isMasterController(clusterName)).findAny().get();
     Properties props = defaultH2VProps(parentController.getControllerUrl(), inputDirPath, storeName);
-    createStoreForJob(CLUSTER_NAME, recordSchema, props);
+    createStoreForJob(clusterName, recordSchema, props);
 
     KafkaPushJob job = new KafkaPushJob("Test push job", props);
     job.run();
 
     // Verify job properties
     Assert.assertEquals(job.getKafkaTopic(), Version.composeKafkaTopic(storeName, 1));
-    for (int version : parentController.getVeniceAdmin().getCurrentVersionsForMultiColos(CLUSTER_NAME, storeName).values())  {
+    for (int version : parentController.getVeniceAdmin().getCurrentVersionsForMultiColos(clusterName, storeName).values())  {
       Assert.assertEquals(version, 1);
     }
     Assert.assertEquals(job.getInputDirectory(), inputDirPath);
@@ -109,8 +106,8 @@ public class TestMultiDataCenterPush {
 
     // Verify the data in Venice Store
     for (int dataCenterIndex = 0; dataCenterIndex < NUMBER_OF_CHILD_DATACENTERS; dataCenterIndex++) {
-      VeniceClusterWrapper veniceCluster = childClusters.get(dataCenterIndex);
-      String routerUrl = veniceCluster.getRandomRouterURL();
+      VeniceMultiClusterWrapper veniceCluster = childClusters.get(dataCenterIndex);
+      String routerUrl = veniceCluster.getClusters().get(clusterName).getRandomRouterURL();
       try(AvroGenericStoreClient<String, Object> client =
           ClientFactory.getAndStartGenericAvroClient(ClientConfig.defaultGenericClientConfig(storeName).setVeniceURL(routerUrl))) {
         for (int i = 1; i <= 100; ++i) {
@@ -119,7 +116,7 @@ public class TestMultiDataCenterPush {
           Assert.assertEquals(actual, expected);
         }
 
-        ControllerClient controllerClient = new ControllerClient(veniceCluster.getClusterName(), routerUrl);
+        ControllerClient controllerClient = new ControllerClient(clusterName, routerUrl);
         JobStatusQueryResponse jobStatus = controllerClient.queryJobStatus(job.getKafkaTopic());
         Assert.assertEquals(jobStatus.getStatus(), ExecutionStatus.COMPLETED.toString(),
             "After job is complete, status should reflect that");
@@ -152,7 +149,7 @@ public class TestMultiDataCenterPush {
     });
 
     // Verify the topics in child controller
-    TopicManager childTopicManager = childControllers[0].getVeniceAdmin().getTopicManager();
+    TopicManager childTopicManager = childControllers.get(0).get(0).getVeniceAdmin().getTopicManager();
     TestUtils.waitForNonDeterministicAssertion(10, TimeUnit.SECONDS,() -> {
       Assert.assertFalse(childTopicManager.containsTopic(v1Topic), "Topic: " + v1Topic + " should be deleted after 3 pushes");
     });
@@ -162,16 +159,17 @@ public class TestMultiDataCenterPush {
 
   @Test(timeOut = TEST_TIMEOUT)
   public void testMultiDataCenterIncrementalPush() throws Exception {
+    String clusterName = CLUSTER_NAMES[1];
     //create a batch push job
     File inputDir = getTempDataDirectory();
     Schema recordSchema = writeSimpleAvroFileWithUserSchema(inputDir);
     String inputDirPath = "file:" + inputDir.getAbsolutePath();
     String storeName = TestUtils.getUniqueString("store");
-    Properties props = defaultH2VProps(parentController.getControllerUrl(), inputDirPath, storeName);
+    Properties props = defaultH2VProps(parentControllers.get(0).getControllerUrl(), inputDirPath, storeName);
     String keySchemaStr = recordSchema.getField(props.getProperty(KafkaPushJob.KEY_FIELD_PROP)).schema().toString();
     String valueSchemaStr = recordSchema.getField(props.getProperty(KafkaPushJob.VALUE_FIELD_PROP)).schema().toString();
 
-    createStoreForJob(CLUSTER_NAME, keySchemaStr, valueSchemaStr, props, false, false, true);
+    createStoreForJob(clusterName, keySchemaStr, valueSchemaStr, props, false, false, true);
 
     KafkaPushJob job = new KafkaPushJob("Test batch push job", props);
     job.run();
@@ -184,8 +182,8 @@ public class TestMultiDataCenterPush {
 
     //validate the client can read data
     for (int dataCenterIndex = 0; dataCenterIndex < NUMBER_OF_CHILD_DATACENTERS; dataCenterIndex++) {
-      VeniceClusterWrapper veniceCluster = childClusters.get(dataCenterIndex);
-      String routerUrl = veniceCluster.getRandomRouterURL();
+      VeniceMultiClusterWrapper veniceCluster = childClusters.get(dataCenterIndex);
+      String routerUrl = veniceCluster.getClusters().get(clusterName).getRandomRouterURL();
       try(AvroGenericStoreClient<String, Object> client =
           ClientFactory.getAndStartGenericAvroClient(ClientConfig.defaultGenericClientConfig(storeName).setVeniceURL(routerUrl))) {
         for (int i = 1; i <= 50; ++i) {
