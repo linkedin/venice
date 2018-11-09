49ed6a0051b66cc0b4c92c54361f986734166fa0
Matthew Wise <mwise@linkedin.com>
2018-11-09 11:02:14 -0800

    Fix Per-Store Token Bucket Metrics
    The gauge-type metric used for monitoring token buckets specifies how to query the metric but not
    which stores to query.  This change initializes the metric for each store and registers the metric
    for store change events in order to guaranee that we emit per store token bucket metrics.
    
    RB=1480610
    G=voldemort-reviewers
    R=sdwu
    A=sdwu,fvillega

--- venice-server/src/main/java/com/linkedin/venice/stats/AggServerQuotaTokenBucketStats.java
+++ venice-server/src/main/java/com/linkedin/venice/stats/AggServerQuotaTokenBucketStats.java
@@ -1,14 +1,37 @@
 package com.linkedin.venice.stats;
 
 import com.linkedin.venice.listener.StorageQuotaEnforcementHandler;
+import com.linkedin.venice.meta.Store;
+import com.linkedin.venice.meta.StoreDataChangedListener;
 import io.tehuti.metrics.MetricsRepository;
 
 
-public class AggServerQuotaTokenBucketStats extends AbstractVeniceAggStats<ServerQuotaTokenBucketStats>{
+public class AggServerQuotaTokenBucketStats extends AbstractVeniceAggStats<ServerQuotaTokenBucketStats> implements StoreDataChangedListener {
   public AggServerQuotaTokenBucketStats(MetricsRepository metricsRepository, StorageQuotaEnforcementHandler quotaEnforcer) {
     super(metricsRepository,
         (metrics, storeName) -> new ServerQuotaTokenBucketStats(metrics, storeName,
             () -> quotaEnforcer.getBucketForStore(storeName))
     );
+  }
+
+  @Override
+  public void handleStoreCreated(Store store) {
+    handleStoreChanged(store);
+  }
+
+  @Override
+  public void handleStoreDeleted(String storeName) {
+    //no-op
+  }
+
+  @Override
+  public void handleStoreChanged(Store store) {
+    initializeStatsForStore(store.getName());
+  }
+
+  public void initializeStatsForStore(String storeName){
+    //The side-effect of this call is to create the stat if it does not
+    //already exist.  That side-effect is idempotent.
+    getStoreStats(storeName);
   }
 }
