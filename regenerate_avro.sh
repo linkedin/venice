34466ddfa64e2ef38fd118db9aa00bb039820e19
Gaojie Liu <goliu@linkedin.com>
2018-11-09 10:49:41 -0800

    [VOLDENG-4357][venice-schema-common] Read computation protocol
    Client request is composed by the following two parts:
    1. ComputeRequestV1
    2. ComputeClientRequestkeyV1
    Router request is composed by the following two parts:
    1. ComputeRequestV1
    2. ComputeClientRouterKeyV1
    Response format will be shared by Router and SN:
    1. ComputeResponseRecordV1
    
    To support the effcient serialization, we need to modify the
    existing "RecordSerializer" to add the following interfaces:
    byte[] serializeObjects(ByteBuffer prefix, Iterable<T> objects);
    With this interface, the serialized bytes could be copied to the output
    stream before serializing the second part: keys.
    
    To support efficient deserialization, we need to modify the existing
    "RecordDeserializer" to add the following interface:
    T deserialize(BinaryDecoder binaryDecoder);
    Since "BinaryDecoder" could maintain the offset after the first object
    de-serialization.
    
    Right now, 'GUID.java' generated by 'regenerate_avro.sh' could not be
    used since 'GUID.java' generated by using 'avro-tools-1.4.0.jar' is
    incomptabile with avro-1.7.7, and we could not switch to avro-tools-1.7.7.jar
    since some generated avro classes are shared by both client and Venice backend.
    The next step is to have two avro generation script, and one for
    schemas, which are only being used by backend, and one for the schemas
    shared by both client and backend.
    So far, Client has to support both 1.4 and 1.7 and Backend only supports
    1.7.7.
    
    RB=1480851
    BUG=VOLDENG-4357
    G=voldemort-reviewers
    R=fvillega,mnhuang
    A=fvillega,mnhuang

--- regenerate_avro.sh
+++ regenerate_avro.sh
@@ -1,37 +1,43 @@
 #!/bin/bash
 
-mkdir -p avro
-avro_tools=`ls avro | sort -rV | head -n 1`
+mkdir -p avro_tools
+avro_tools=`ls avro_tools | sort -rV | head -n 1`
 
 if [ -z $avro_tools ]; then
-  wget "https://artifactory.corp.linkedin.com:8083/artifactory/ext-libraries/org/apache/avro/avro-tools/1.4.0/avro-tools-1.4.0.jar" -P avro
-  avro_tools=`ls avro | sort -rV | head -n 1`
+  wget "https://artifactory.corp.linkedin.com:8083/artifactory/ext-libraries/org/apache/avro/avro-tools/1.4.0/avro-tools-1.4.0.jar" -P avro_tools
+  avro_tools=`ls avro_tools | sort -rV | head -n 1`
 fi
 
 DEFAULT_AVRO_TOOLS_JAR="avro/$avro_tools"
 
 AVRO_SCHEMAS_PATH=(
-  "venice-common/src/main/resources/avro/KafkaMessageEnvelope/v6/*"
-  "venice-common/src/main/resources/avro/PushJobStatusRecord/PushJobStatusRecordKey/v1/*"
-  "venice-common/src/main/resources/avro/PushJobStatusRecord/PushJobStatusRecordValue/v1/*"
-  "venice-common/src/main/resources/avro/PartitionState/v5/*"
-  "venice-common/src/main/resources/avro/StoreVersionState/v3/*"
-  "venice-common/src/main/resources/avro/ChunkedValueManifest/v-20/*"
-  "venice-common/src/main/resources/avro/ChunkedKeySuffix/*"
+  "venice-common_2.10/src/main/resources/avro/KafkaMessageEnvelope/v6/*"
+  "venice-common_2.10/src/main/resources/avro/PushJobStatusRecord/PushJobStatusRecordKey/v1/*"
+  "venice-common_2.10/src/main/resources/avro/PushJobStatusRecord/PushJobStatusRecordValue/v1/*"
+  "venice-common_2.10/src/main/resources/avro/PartitionState/v5/*"
+  "venice-common_2.10/src/main/resources/avro/StoreVersionState/v3/*"
+  "venice-common_2.10/src/main/resources/avro/ChunkedValueManifest/v-20/*"
+  "venice-common_2.10/src/main/resources/avro/ChunkedKeySuffix/*"
   "venice-controller/src/main/resources/avro/AdminOperation/v20/*"
   "venice-schema-common/src/main/resources/avro/MultiGetResponseRecord/*"
   "venice-schema-common/src/main/resources/avro/MultiGetClientRequestKey/*"
   "venice-schema-common/src/main/resources/avro/MultiGetRouterRequestKey/*"
+  "venice-schema-common/src/main/resources/avro/ComputeRequest/*"
+  "venice-schema-common/src/main/resources/avro/ComputeResponseRecord/*"
+  "venice-schema-common/src/main/resources/avro/ComputeRouterRequestKey/*"
 )
 CODE_GEN_PATH=(
-  "venice-common/src/main/java"
-  "venice-common/src/main/java"
-  "venice-common/src/main/java"
-  "venice-common/src/main/java"
-  "venice-common/src/main/java"
-  "venice-common/src/main/java"
-  "venice-common/src/main/java"
+  "venice-common_2.10/src/main/java"
+  "venice-common_2.10/src/main/java"
+  "venice-common_2.10/src/main/java"
+  "venice-common_2.10/src/main/java"
+  "venice-common_2.10/src/main/java"
+  "venice-common_2.10/src/main/java"
+  "venice-common_2.10/src/main/java"
   "venice-controller/src/main/java"
+  "venice-schema-common/src/main/java"
+  "venice-schema-common/src/main/java"
+  "venice-schema-common/src/main/java"
   "venice-schema-common/src/main/java"
   "venice-schema-common/src/main/java"
   "venice-schema-common/src/main/java"
@@ -48,6 +54,9 @@ FULL_CODE_GEN_PATH=(
   "${CODE_GEN_PATH[8]}/com/linkedin/venice/read/protocol/response/*.java"
   "${CODE_GEN_PATH[9]}/com/linkedin/venice/read/protocol/request/client/*.java"
   "${CODE_GEN_PATH[10]}/com/linkedin/venice/read/protocol/request/router/*.java"
+  "${CODE_GEN_PATH[11]}/com/linkedin/venice/compute/protocol/request/*.java"
+  "${CODE_GEN_PATH[12]}/com/linkedin/venice/compute/protocol/response/*.java"
+  "${CODE_GEN_PATH[13]}/com/linkedin/venice/compute/protocol/request/router/*.java"
 )
 
 if [[ $# < 1 ]]; then
