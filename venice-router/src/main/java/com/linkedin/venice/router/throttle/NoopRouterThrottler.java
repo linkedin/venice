43d3c3e716d654dd609d392ce25df37872795576
Min Huang(Data Infrastructure) <mnhuang@linkedin.com>
2019-07-15 10:59:53 -0700

    [router] Report store read quota per router even w/o enabling router throttling
    Previously routers would stop emitting per router read quota for store
    if router throttling is disabled, which breaks the health dash board
    which compares the aggregated KPS in routers with the aggregated per
    router store quota to fire quota breaching alerts. This RB adds the
    ability to keep reporting per router store quota even though router
    throttling is disabled in router.
    
    Besides, an integration test is added to ensure the stats could report
    new metrics when the store read quota is updated.
    
    Other changes:
     - Rename TestThrottleReadRequestPerStore to TestRouterReadQuotaThrottler;
    
    RB=1728447
    BUG=VOLDENG-4834
    G=voldemort-reviewers
    R=fvillega,sdwu,smaji,xyin
    A=fvillega

--- venice-router/src/main/java/com/linkedin/venice/router/throttle/NoopRouterThrottler.java
+++ venice-router/src/main/java/com/linkedin/venice/router/throttle/NoopRouterThrottler.java
@@ -1,10 +1,28 @@
 package com.linkedin.venice.router.throttle;
 
+import com.linkedin.venice.helix.ZkRoutersClusterManager;
+import com.linkedin.venice.meta.ReadOnlyStoreRepository;
+import com.linkedin.venice.meta.RoutersClusterManager;
+import com.linkedin.venice.meta.Store;
+import com.linkedin.venice.meta.StoreDataChangedListener;
+import com.linkedin.venice.router.stats.AggRouterHttpRequestStats;
+import java.util.List;
 import java.util.Optional;
 
 
-public class NoopRouterThrottler implements RouterThrottler{
-  public NoopRouterThrottler(){}
+public class NoopRouterThrottler implements RouterThrottler, RoutersClusterManager.RouterCountChangedListener, StoreDataChangedListener {
+  private final ZkRoutersClusterManager zkRoutersManager;
+  private final ReadOnlyStoreRepository storeRepository;
+  private final AggRouterHttpRequestStats stats;
+  public NoopRouterThrottler(ZkRoutersClusterManager zkRoutersManager, ReadOnlyStoreRepository storeRepository,
+      AggRouterHttpRequestStats stats) {
+    this.zkRoutersManager = zkRoutersManager;
+    this.storeRepository = storeRepository;
+    this.stats = stats;
+    this.zkRoutersManager.subscribeRouterCountChangedEvent(this);
+    this.storeRepository.registerStoreDataChangedListener(this);
+    buildAllStoreReadQuotaStats();
+  }
 
   @Override
   public void mayThrottleRead(String storeName, double readCapacityUnit, Optional<String> storageNodeId) {
@@ -14,5 +32,44 @@ public class NoopRouterThrottler implements RouterThrottler{
   @Override
   public int getReadCapacity() {
     return 1;
+  }
+
+  private void buildAllStoreReadQuotaStats() {
+    List<Store> allStores = storeRepository.getAllStores();
+    for (Store store : allStores) {
+      buildStoreReadQuotaStats(store.getName(), store.getReadQuotaInCU());
+    }
+  }
+
+  private void buildStoreReadQuotaStats(String storeName, long storeReadQuota) {
+    int routerCount = zkRoutersManager.isQuotaRebalanceEnabled()
+        ? zkRoutersManager.getLiveRoutersCount()
+        : zkRoutersManager.getExpectedRoutersCount();
+
+    long storeQuotaPerRouter = routerCount > 0
+        ? storeReadQuota / routerCount
+        : 0;
+
+    stats.recordQuota(storeName, storeQuotaPerRouter);
+  }
+
+  @Override
+  public void handleStoreCreated(Store store) {
+    buildStoreReadQuotaStats(store.getName(), store.getReadQuotaInCU());
+  }
+
+  @Override
+  public void handleStoreChanged(Store store) {
+    buildStoreReadQuotaStats(store.getName(), store.getReadQuotaInCU());
+  }
+
+  @Override
+  public void handleStoreDeleted(String storeName) {
+    stats.recordQuota(storeName, 0);
+  }
+
+  @Override
+  public void handleRouterCountChanged(int newRouterCount) {
+    buildAllStoreReadQuotaStats();
   }
 }
