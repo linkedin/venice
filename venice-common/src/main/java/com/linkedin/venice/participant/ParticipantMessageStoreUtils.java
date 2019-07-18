d9936375a5d5310eadcd7d5f9a6eabdadd1fb11a
Xun Yin <xyin@linkedin.com>
2019-07-18 15:21:39 -0700

    [common] change Venice system store name padding
    Use the prefix "venice_system_store_" to indicate system stores in venice instead of
    the "__" prefix and suffix padding. This is because the "_" prefix is also used by
    other services such as KMM to filter out kafka system stores.
    
    RB=1732718
    BUG=VOLDENG-4846
    G=voldemort-reviewers
    R=fvillega
    A=fvillega

--- venice-common/src/main/java/com/linkedin/venice/participant/ParticipantMessageStoreUtils.java
+++ venice-common/src/main/java/com/linkedin/venice/participant/ParticipantMessageStoreUtils.java
@@ -1,9 +1,13 @@
 package com.linkedin.venice.participant;
 
+import com.linkedin.venice.meta.Store;
+
+
 public class ParticipantMessageStoreUtils {
-  private static final String STORE_NAME_FORMAT = "__participant_message_store_cluster_%s__";
+  private static final String PARTICIPANT_STORE = "participant_store";
+  private static final String PARTICIPANT_STORE_FORMAT = Store.SYSTEM_STORE_FORMAT + "_cluster_%s";
 
   public static String getStoreNameForCluster(String clusterName) {
-    return String.format(STORE_NAME_FORMAT, clusterName);
+    return String.format(PARTICIPANT_STORE_FORMAT, PARTICIPANT_STORE, clusterName);
   }
 }
