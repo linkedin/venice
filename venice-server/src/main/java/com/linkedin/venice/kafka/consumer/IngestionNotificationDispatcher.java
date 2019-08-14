a9c1a5742a297549c6d703f980b8f11c20a9bdb5
Gaojie Liu <goliu@linkedin.com>
2019-08-13 18:07:05 -0700

    [server] fixed hybrid ingestion failure
    In storage node, we shouldn't report error after end_of_push since
    the DIV error could happen because of messages in RT topic.
    
    RB=1763915
    BUG=VOLDENG-4918
    R=anaberez,yurzhou,mnhuang,smaji,xyin,fvillega,sdwu
    A=fvillega

--- venice-server/src/main/java/com/linkedin/venice/kafka/consumer/IngestionNotificationDispatcher.java
+++ venice-server/src/main/java/com/linkedin/venice/kafka/consumer/IngestionNotificationDispatcher.java
@@ -135,7 +135,7 @@ class IngestionNotificationDispatcher {
 
           if (!isCurrentVersion.getAsBoolean() && // The currently-active version should always report progress.
               (!pcs.isStarted() ||
-                  pcs.isComplete() ||
+                  pcs.isEndOfPushReceived() ||
                   pcs.isErrorReported())) {
             if (logger.isDebugEnabled()) {
               logger.debug("Can not report progress for topic '" + topic +
@@ -183,7 +183,7 @@ class IngestionNotificationDispatcher {
             String logMessage = "Partition: " + pcs.getPartition() + " has already been ";
             boolean report = true;
 
-            if (pcs.isComplete()) {
+            if (pcs.isEndOfPushReceived()) {
               logMessage += "marked as completed so an error will not be reported.";
               report = false;
             }
