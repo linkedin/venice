99015fe916cccbd32d38e3332702c67bbb40d5aa
Gaojie Liu <goliu@linkedin.com>
2019-08-20 22:09:28 -0700

    [test] added unit test to avoid regression in IngestionNotificationDispatcher for error reporting
    RB=1772711
    BUG=VOLDENG-4924
    R=smaji,mnhuang,fvillega,sdwu,xyin,yurzhou,anaberez
    A=fvillega

--- venice-server/src/test/java/com/linkedin/venice/kafka/consumer/IngestionNotificationDispatcherTest.java
+++ venice-server/src/test/java/com/linkedin/venice/kafka/consumer/IngestionNotificationDispatcherTest.java
@@ -3,12 +3,14 @@ package com.linkedin.venice.kafka.consumer;
 import com.linkedin.venice.exceptions.VeniceException;
 import com.linkedin.venice.exceptions.VeniceIngestionTaskKilledException;
 import com.linkedin.venice.notifier.VeniceNotifier;
+import com.linkedin.venice.utils.TestUtils;
 import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Queue;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.testng.annotations.Test;
+
+import static org.mockito.Mockito.*;
 
 
 public class IngestionNotificationDispatcherTest {
@@ -20,11 +22,11 @@ public class IngestionNotificationDispatcherTest {
     String topic = "test_v1";
     int partitionId = 1;
     VeniceIngestionTaskKilledException error = new VeniceIngestionTaskKilledException("test");
-    VeniceNotifier mockNotifier = Mockito.mock(VeniceNotifier.class);
+    VeniceNotifier mockNotifier = mock(VeniceNotifier.class);
     Queue<VeniceNotifier> notifiers = new ArrayDeque<>();
     notifiers.add(mockNotifier);
     IngestionNotificationDispatcher dispatcher = new IngestionNotificationDispatcher(notifiers, topic, () -> true);
-    PartitionConsumptionState psc = Mockito.mock(PartitionConsumptionState.class);
+    PartitionConsumptionState psc = mock(PartitionConsumptionState.class);
 
     // Mock a hybrid partition already received the end of push.
     Mockito.doReturn(partitionId).when(psc).getPartition();
@@ -48,5 +50,23 @@ public class IngestionNotificationDispatcherTest {
     Mockito.doReturn(true).when(psc).isErrorReported();
     dispatcher.reportKilled(Arrays.asList(new PartitionConsumptionState[]{psc}), error);
     Mockito.verify(mockNotifier, Mockito.never()).error(topic, partitionId, error.getMessage(), error);
+  }
+
+  @Test
+  public void testReportError() {
+    String topic = TestUtils.getUniqueString("test_v1");
+    int partitionId = 1;
+    VeniceNotifier mockNotifier = mock(VeniceNotifier.class);
+    Queue<VeniceNotifier> notifiers = new ArrayDeque<>();
+    notifiers.add(mockNotifier);
+    IngestionNotificationDispatcher dispatcher = new IngestionNotificationDispatcher(notifiers, topic, () -> true);
+    PartitionConsumptionState pcs = mock(PartitionConsumptionState.class);
+    Mockito.doReturn(partitionId).when(pcs).getPartition();
+    Mockito.doReturn(true).when(pcs).isHybrid();
+    Mockito.doReturn(true).when(pcs).isEndOfPushReceived();
+    Mockito.doReturn(false).when(pcs).isErrorReported();
+    Mockito.doReturn(false).when(pcs).isComplete();
+    dispatcher.reportError(Arrays.asList(new PartitionConsumptionState[]{pcs}), "fake ingestion error", mock(VeniceException.class));
+    verify(mockNotifier, never()).error(eq(topic), eq(partitionId), any(), any());
   }
 }
