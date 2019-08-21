linkedin/venice

commit 6395f6303721bf1d46520a1efc79a151c3012390

[server] Only reset offset when consumer has subscription to the partition
Currently in the RESET_OFFSET consumer action processing function, we blindly
reset offset for a partition and add a brand new PartitionConsumptionState to
the state map without checking whether the consumer has already unsubscribe
from that topic already. This RB adds the extra check inside RESET_OFFSET
action to ensure we only make changes when the consumer is still subscribed
to the corresponding topic/partition.

RB=1772028
BUG=VOLDENG-4942
G=voldemort-reviewers
R=fvillega,sdwu,goliu,smaji,yurzhou,anaberez,xyin
A=goliu,smaji

venice-server/src/main/java/com/linkedin/venice/kafka/consumer/StoreIngestionTask.java
@@ -692,16 +692,31 @@ public abstract class StoreIngestionTask implements Runnable, Closeable {
         );
         break;
       case RESET_OFFSET:
-        try {
-          consumer.resetOffset(topic, partition);
-          logger.info(consumerTaskId + " Reset OffSet : Topic " + topic + " Partition Id " + partition);
-        } catch (UnsubscribedTopicPartitionException e) {
+        /**
+         * After auditing all the calls that can result in the RESET_OFFSET action, it turns out we always unsubscribe
+         * from the topic/partition before resetting offset, which is unnecessary; but we decided to keep this action
+         * for now in case that in future, we do want to reset the consumer without unsubscription.
+         */
+        if (consumer.hasSubscription(topic, partition)) {
+          /**
+           * Only update the consumer and partitionConsumptionStateMap when consumer actually has
+           * subscription to this topic/partition; otherwise, we would blindly update the StateMap
+           * and mess up other operations on the StateMap.
+           */
+          try {
+            consumer.resetOffset(topic, partition);
+            logger.info(consumerTaskId + " Reset OffSet : Topic " + topic + " Partition Id " + partition);
+          } catch (UnsubscribedTopicPartitionException e) {
+            logger.error(consumerTaskId + " Kafka consumer should have subscribed to the partition already but it fails "
+                + "on resetting offset for Topic: " + topic + " Partition Id: " + partition);
+          }
+          partitionConsumptionStateMap.put(
+              partition,
+              new PartitionConsumptionState(partition, new OffsetRecord(), hybridStoreConfig.isPresent(), isIncrementalPushEnabled));
+        } else {
           logger.info(consumerTaskId + " No need to reset offset by Kafka consumer, since the consumer is not " +
               "subscribing Topic: " + topic + " Partition Id: " + partition);
         }
-        partitionConsumptionStateMap.put(
-            partition,
-            new PartitionConsumptionState(partition, new OffsetRecord(), hybridStoreConfig.isPresent(), isIncrementalPushEnabled));
         producerTrackerMap.values().stream().forEach(
             producerTracker -> producerTracker.clearPartition(partition)
         );

venice-test-common/src/main/java/com/linkedin/venice/unit/kafka/consumer/MockInMemoryConsumer.java
@@ -75,6 +75,11 @@ public class MockInMemoryConsumer implements KafkaConsumerWrapper {
   }
 
   @Override
+  public boolean hasSubscription(String topic, int partition) {
+    return offsets.containsKey(new TopicPartition(topic, partition));
+  }
+
+  @Override
   public Map<String, List<PartitionInfo>> listTopics() {
     return delegate.listTopics();
   }

venice-common/src/main/java/com/linkedin/venice/kafka/consumer/ApacheKafkaConsumer.java
@@ -107,12 +107,10 @@ public class ApacheKafkaConsumer implements KafkaConsumerWrapper {
 
   @Override
   public void resetOffset(String topic, int partition) {
-    TopicPartition topicPartition = new TopicPartition(topic, partition);
-    Set<TopicPartition> topicPartitionSet = kafkaConsumer.assignment();
-
-    if (!topicPartitionSet.contains(topicPartition)) {
+    if (!hasSubscription(topic, partition)) {
       throw new UnsubscribedTopicPartitionException(topic, partition);
     }
+    TopicPartition topicPartition = new TopicPartition(topic, partition);
     kafkaConsumer.seekToBeginning(Arrays.asList(topicPartition));
   }
 
@@ -152,6 +150,12 @@ public class ApacheKafkaConsumer implements KafkaConsumerWrapper {
   @Override
   public boolean hasSubscription() {
     return !kafkaConsumer.assignment().isEmpty();
+  }
+
+  @Override
+  public boolean hasSubscription(String topic, int partition) {
+    TopicPartition tp = new TopicPartition(topic, partition);
+    return kafkaConsumer.assignment().contains(tp);
   }
 
   @Override

venice-common/src/main/java/com/linkedin/venice/kafka/consumer/KafkaConsumerWrapper.java
@@ -23,6 +23,8 @@ public interface KafkaConsumerWrapper extends AutoCloseable {
 
   boolean hasSubscription();
 
+  boolean hasSubscription(String topic, int partition);
+
   Map<String, List<PartitionInfo>> listTopics();
 
   Map<TopicPartition,Long> beginningOffsets(List<TopicPartition> topicPartitions);

venice-server/src/main/java/com/linkedin/venice/helix/AbstractParticipantModel.java
@@ -162,10 +162,9 @@ public abstract class AbstractParticipantModel extends StateModel {
     }
     /**
      * TODO: After checking all the calls to function, I noticed that we have already unsubscribe from
-     * this topic/partition before calling this reset function; previously we remove this partition from
-     * the partitionConsumptionStateMap during unsubscription, but we add it back to the map when we reset
-     * offset. Consider removing the RESET_OFFSET action completely and move one special logic
-     * "storageMetadataService.clearOffset(topic, partition)" from RESET_OFFSET handling branch to UNSUBSCRIBE branch.
+     * this topic/partition before calling this reset function. Consider removing the RESET_OFFSET action
+     * completely and move one special logic "storageMetadataService.clearOffset(topic, partition)"
+     * from RESET_OFFSET handling branch to UNSUBSCRIBE branch.
      */
     getStoreIngestionService().resetConsumptionOffset(getStoreConfig(), getPartition());
   }

commit 08a8592e4be866bb0bd281d8674511f04d0daeff

[server] OffsetRecord can have multiple OffsetRecordTransformer for different producer
This RB fixes a bug that restarting server during Samza Grandfathering (GF)
would fail the GF job: GF job can have multiple producer producing to the
same partition and the OffsetRecordTransformer (transformer) we build during
consumption would only update the state of one producer; therefore, different
producers' transformers override each other and only one of them will be
persisted into disk.

This RB enables OffsetRecord to keep track of multiple transformer from
different producer; before we sync OffsetRecord to disk, apply all the
transformer function on the OffsetRecord.

Other changes:
1. Fix a test regression introduced by commit 6395f630

RB=1773263
BUG=VOLDENG-4755
G=voldemort-reviewers
R=goliu,fvillega,yurzhou,sdwu,smaji,xyin,anaberez
A=goliu,fvillega

venice-common/src/main/java/com/linkedin/venice/offsets/OffsetRecord.java
@@ -8,6 +8,7 @@ import com.linkedin.venice.kafka.protocol.state.ProducerPartitionState;
 import com.linkedin.venice.kafka.validation.OffsetRecordTransformer;
 import com.linkedin.venice.serialization.avro.AvroProtocolDefinition;
 import com.linkedin.venice.serialization.avro.InternalAvroSpecificSerializer;
+import com.linkedin.venice.utils.concurrent.VeniceConcurrentHashMap;
 import org.apache.avro.Schema;
 import org.apache.avro.util.Utf8;
 
@@ -26,7 +27,7 @@ public class OffsetRecord {
 
   private final PartitionState partitionState;
 
-  private Optional<OffsetRecordTransformer> offsetRecordTransformer = Optional.empty();
+  private Map<GUID, OffsetRecordTransformer> offsetRecordTransformers = new VeniceConcurrentHashMap<>();
 
   public OffsetRecord(PartitionState partitionState) {
     this.partitionState = partitionState;
@@ -59,13 +60,11 @@ public class OffsetRecord {
   }
 
   /**
-   * This function will keep a reference of passed {@link OffsetRecordTransformer},
-   * which will be used in {@link #transform()}.
-   *
-   * @param transformer
+   * This function will keep a map of reference to all the {@link OffsetRecordTransformer}
+   * from different producers.
    */
-  public void setOffsetRecordTransformer(OffsetRecordTransformer transformer) {
-    offsetRecordTransformer = Optional.of(transformer);
+  public void addOffsetRecordTransformer(GUID producerGUID, OffsetRecordTransformer transformer) {
+    offsetRecordTransformers.put(producerGUID, transformer);
   }
 
   /**
@@ -73,9 +72,10 @@ public class OffsetRecord {
    * so this function should be only invoked when necessary.
    */
   public void transform() {
-    if (offsetRecordTransformer.isPresent()) {
-      offsetRecordTransformer.get().transform(this);
+    for (OffsetRecordTransformer transformer : offsetRecordTransformers.values()) {
+      transformer.transform(this);
     }
+    offsetRecordTransformers.clear();
   }
 
   public long getOffset() {

venice-server/src/main/java/com/linkedin/venice/kafka/consumer/StoreIngestionTask.java
@@ -1207,7 +1207,7 @@ public abstract class StoreIngestionTask implements Runnable, Closeable {
          * See {@link ProducerTracker#addMessage(int, KafkaKey, KafkaMessageEnvelope, boolean, Optional)}
          * to find more details.
          */
-        offsetRecord.setOffsetRecordTransformer(offsetRecordTransformer.get());
+        offsetRecord.addOffsetRecordTransformer(kafkaValue.producerMetadata.producerGUID, offsetRecordTransformer.get());
       } /** else, {@link #validateMessage(int, KafkaKey, KafkaMessageEnvelope)} threw a {@link DuplicateDataException} */
 
       // Potentially update the offset metadata in the OffsetRecord

venice-test-common/src/main/java/com/linkedin/venice/unit/kafka/consumer/MockInMemoryConsumer.java
@@ -1,5 +1,6 @@
 package com.linkedin.venice.unit.kafka.consumer;
 
+import com.linkedin.venice.exceptions.UnsubscribedTopicPartitionException;
 import com.linkedin.venice.kafka.consumer.KafkaConsumerWrapper;
 import com.linkedin.venice.offsets.OffsetRecord;
 import com.linkedin.venice.unit.kafka.InMemoryKafkaBroker;
@@ -50,6 +51,9 @@ public class MockInMemoryConsumer implements KafkaConsumerWrapper {
 
   @Override
   public void resetOffset(String topic, int partition) {
+    if (!hasSubscription(topic, partition)) {
+      throw new UnsubscribedTopicPartitionException(topic, partition);
+    }
     delegate.resetOffset(topic, partition);
     offsets.put(new TopicPartition(topic, partition), OffsetRecord.LOWEST_OFFSET);
   }

venice-server/src/test/java/com/linkedin/venice/kafka/consumer/StoreIngestionTaskTest.java
@@ -642,7 +642,8 @@ public class StoreIngestionTaskTest {
       // Reset should be able to handle the scenario, when the topic partition has been unsubscribed.
       storeIngestionTaskUnderTest.resetPartitionConsumptionOffset(topic, PARTITION_FOO);
       verify(mockKafkaConsumer, timeout(TEST_TIMEOUT)).unSubscribe(topic, PARTITION_FOO);
-      verify(mockKafkaConsumer, timeout(TEST_TIMEOUT)).resetOffset(topic, PARTITION_FOO);
+      // StoreIngestionTask won't invoke consumer.resetOffset() if it already unsubscribe from that topic/partition
+      verify(mockKafkaConsumer, timeout(TEST_TIMEOUT).times(0)).resetOffset(topic, PARTITION_FOO);
       verify(mockStorageMetadataService, timeout(TEST_TIMEOUT)).clearOffset(topic, PARTITION_FOO);
     }, isLeaderFollowerModelEnabled);
   }