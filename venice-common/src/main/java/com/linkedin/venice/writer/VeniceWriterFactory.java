linkedin/venice

commit d4bce2bd07cc8cc7b4ec5888e36e87aeec0c48ee

[server][samza][h2v][common] L/F hybrid store chunking support
The Leader Storage Node is now capable of writing chunked messages to the
store-version topic. The RT topic is still expected to be non-chunked,
but if a store is configured to enable chunking, then the leader SN will
honor that setting. Supporting chunking in the RT topic is out of scope
as it significantly increases complexity (it would likely require a
protocol upgrade to support specifying chunking on a per-segment basis
rather than a per-push basis).

This commit should not actually change the behavior of the VeniceWriter
in H2V or Samza, but since the VeniceWriter code has changed, it is not
impossible that there could be unintended side-effects to these other
components.

Main code changes:

1. Introduced a new ChunkAwareCallback interface, which is implemented by
   the LeaderProducerMessageCallback.
2. The VeniceWriter checks if the passed in callback is of this type, and
   if so, sets some state in it.
3. The LeaderProducerMessageCallback can persist chunks and manifests in
   addition to small values, thanks to the state provided by the
   VeniceWriter via the ChunkAwareCallback interface, and thanks to a
   change to the WriteToStorageEngine functional interface, which now
   takes the key as input to its apply function.
4. The VeniceWriter inside of the StoreIngestionTask is initialized even
   more lazily than before, since we need to have received the SoP prior
   to initialization, in order to find out whether the version is chunked
   or not. It is accessible via a new protected getVeniceWriter function.
5. The StoreIngestionTask's GC-friendly code to write to the storage has
   been moved to a new protected putInStorageEngine function.
6. The VeniceWriterFactory has a new function for specifying whether the
   chunking feature is to be enabled or not.

Unrelated code changes:

1. Moved some utility code from RocksDBStoragePartition to ByteUtils.

venice-common/src/main/java/com/linkedin/venice/writer/VeniceWriterFactory.java
@@ -36,31 +36,36 @@ public class VeniceWriterFactory {
    * Create a basic venice writer with default serializer.
    */
   public VeniceWriter<byte[], byte[]> getBasicVeniceWriter(String topicName, Time time) {
-    return getVeniceWriter(topicName, new DefaultSerializer(), new DefaultSerializer(), time);
+    return getVeniceWriter(topicName, new DefaultSerializer(), new DefaultSerializer(), false, time);
   }
 
   public VeniceWriter<byte[], byte[]> getBasicVeniceWriter(String topicName) {
     return getBasicVeniceWriter(topicName, SystemTime.INSTANCE);
   }
 
+  public VeniceWriter<byte[], byte[]> getBasicVeniceWriter(String topicName, boolean chunkingEnabled) {
+    return getVeniceWriter(topicName, new DefaultSerializer(), new DefaultSerializer(), chunkingEnabled, SystemTime.INSTANCE);
+  }
+
   public <K, V> VeniceWriter<K, V> getVeniceWriter(String topicName, VeniceKafkaSerializer<K> keySerializer,
       VeniceKafkaSerializer<V> valueSerializer) {
-    return getVeniceWriter(topicName, keySerializer, valueSerializer, SystemTime.INSTANCE);
+    return getVeniceWriter(topicName, keySerializer, valueSerializer, false, SystemTime.INSTANCE);
   }
 
   /**
    * Create a venice writer which is used to communicated with the topic contains real data.
    */
   protected <K, V> VeniceWriter<K, V> getVeniceWriter(String topic, VeniceKafkaSerializer<K> keySerializer,
-      VeniceKafkaSerializer<V> valueSerializer, Time time) {
+      VeniceKafkaSerializer<V> valueSerializer, boolean chunkingEnabled, Time time) {
     Properties writerProperties = new Properties();
     writerProperties.putAll(this.properties);
     writerProperties.put(ConfigKeys.KAFKA_BOOTSTRAP_SERVERS, kafkaBootstrapServers);
+    writerProperties.put(VeniceWriter.ENABLE_CHUNKING, chunkingEnabled);
     return new VeniceWriter<>(new VeniceProperties(writerProperties), topic, keySerializer, valueSerializer, time);
   }
 
   public VeniceWriter<KafkaKey, byte[]> getVeniceWriter(String topic) {
-    return getVeniceWriter(topic, new KafkaKeySerializer(), new DefaultSerializer(), SystemTime.INSTANCE);
+    return getVeniceWriter(topic, new KafkaKeySerializer(), new DefaultSerializer(), false, SystemTime.INSTANCE);
   }
 
   private void checkProperty(String key) {

venice-test-common/src/main/java/com/linkedin/venice/utils/TestUtils.java
@@ -204,7 +204,7 @@ public class TestUtils {
     }
 
     public <K, V> VeniceWriter<K, V> getVeniceWriter(String topic, VeniceKafkaSerializer<K> keySer,  VeniceKafkaSerializer<V>  valSer) {
-      return getVeniceWriter(topic, keySer, valSer, SystemTime.INSTANCE);
+      return getVeniceWriter(topic, keySer, valSer, false, SystemTime.INSTANCE);
     }
   }
 

hadoop-to-venice-bridge/src/main/java/com/linkedin/venice/hadoop/VeniceReducer.java
@@ -18,6 +18,7 @@ import com.linkedin.venice.utils.VeniceProperties;
 import com.linkedin.venice.utils.concurrent.VeniceConcurrentHashMap;
 import com.linkedin.venice.writer.AbstractVeniceWriter;
 
+import com.linkedin.venice.writer.VeniceWriter;
 import com.linkedin.venice.writer.VeniceWriterFactory;
 import java.io.PrintWriter;
 import java.io.StringWriter;
@@ -159,7 +160,8 @@ public class VeniceReducer implements Reducer<BytesWritable, BytesWritable, Null
       }
       writerProps.put(ConfigKeys.PUSH_JOB_MAP_REDUCE_JOB_ID, mapReduceJobId.getId());
       VeniceWriterFactory factory = new VeniceWriterFactory(writerProps);
-      veniceWriter = factory.getBasicVeniceWriter(props.getString(TOPIC_PROP));
+      boolean chunkingEnabled = props.getBoolean(VeniceWriter.ENABLE_CHUNKING);
+      veniceWriter = factory.getBasicVeniceWriter(props.getString(TOPIC_PROP), chunkingEnabled);
     }
     if (null == previousReporter || !previousReporter.equals(reporter)) {
       callback = new KafkaMessageCallback(reporter);

venice-common/src/main/java/com/linkedin/venice/meta/Partition.java
@@ -109,4 +109,8 @@ public class Partition {
   public int getId() {
     return id;
   }
+
+  public String toString() {
+    return this.getClass().getSimpleName() + " {id: " + id + ", stateToInstancesMap: " + stateToInstancesMap + "}";
+  }
 }

venice-common/src/main/java/com/linkedin/venice/meta/PartitionAssignment.java
@@ -60,11 +60,19 @@ public class PartitionAssignment {
     return idToPartitionMap.size();
   }
 
-  public boolean hasEnoughAssignedPartitions() {
-    return getAssignedNumberOfPartitions() >= expectedNumberOfPartitions;
+  public boolean isMissingAssignedPartitions() {
+    return getAssignedNumberOfPartitions() < expectedNumberOfPartitions;
   }
 
   public String getTopic() {
     return topic;
+  }
+
+  public String toString() {
+    return this.getClass().getSimpleName() + " {"
+        + "\n\ttopic: " + topic + ", "
+        + "\n\texpectedNumberOfPartitions: " + expectedNumberOfPartitions + ", "
+        + "\n\tidToPartitionMap: " + idToPartitionMap.toString()
+        + "\n}";
   }
 }

venice-common/src/main/java/com/linkedin/venice/pushmonitor/PushStatusDecider.java
@@ -44,7 +44,7 @@ public abstract class PushStatusDecider {
     boolean isAllPartitionCompleted = true;
     // If there are not enough partitions assigned, push could not be completed.
     // Continue to decide whether push is failed or not.
-    if (!partitionAssignment.hasEnoughAssignedPartitions()) {
+    if (partitionAssignment.isMissingAssignedPartitions()) {
       logger.warn("There no not enough partitions assigned to resource: " + pushStatus.getKafkaTopic());
       isAllPartitionCompleted = false;
     }
@@ -90,15 +90,23 @@ public abstract class PushStatusDecider {
    */
   public Pair<ExecutionStatus, Optional<String>> checkPushStatusAndDetailsByPartitionsStatus(OfflinePushStatus pushStatus,
       PartitionAssignment partitionAssignment) {
-    if (partitionAssignment == null) {
+    // Sanity check
+    if (partitionAssignment == null || partitionAssignment.isMissingAssignedPartitions()) {
+      logger.warn("partitionAssignment not ready: " + partitionAssignment);
       return new Pair<>(NOT_CREATED, Optional.empty());
     }
 
     boolean isAllPartitionCompleted = true;
 
     for (PartitionStatus partitionStatus : pushStatus.getPartitionStatuses()) {
+      int partitionId = partitionStatus.getPartitionId();
+      Partition partition = partitionAssignment.getPartition(partitionId);
+      if (null == partition) {
+        // Defensive coding. Should never happen if the sanity check above works.
+        throw new IllegalStateException("partition " + partitionId + " is null.");
+      }
       ExecutionStatus executionStatus = getPartitionStatus(partitionStatus, pushStatus.getReplicationFactor(),
-          partitionAssignment.getPartition(partitionStatus.getPartitionId()).getInstanceToStateMap());
+          partition.getInstanceToStateMap());
 
       if (executionStatus == ERROR) {
         return new Pair<>(executionStatus, Optional.of("too many ERROR replicas in partition: " + partitionStatus.getPartitionId()
@@ -129,7 +137,7 @@ public abstract class PushStatusDecider {
     }
     PartitionAssignment partitionAssignment =
         resourceAssignment.getPartitionAssignment(kafkaTopic);
-    if (!partitionAssignment.hasEnoughAssignedPartitions()) {
+    if (partitionAssignment.isMissingAssignedPartitions()) {
       String reason = "not enough partitions in EXTERNALVIEW";
       logger.info("There are " + reason + " assigned to resource: " + kafkaTopic);
       return Optional.of(reason);

venice-common/src/main/java/com/linkedin/venice/serialization/avro/ChunkedValueManifestSerializer.java
@@ -6,10 +6,10 @@ import com.linkedin.venice.utils.ByteUtils;
 
 public class ChunkedValueManifestSerializer extends InternalAvroSpecificSerializer<ChunkedValueManifest> {
   /**
-   * @param writePath When used from the write path (true), this will encode only the raw data, with no header
-   *                  When used from the read path (false), this will deserialize data assuming the presence of a four bytes header
+   * @param withoutHeader true: this will encode only the raw data, with no header
+   *                      false: this will deserialize data assuming the presence of a 4 bytes header and serialize it with 4 bytes of padding
    */
-  public ChunkedValueManifestSerializer(boolean writePath) {
-    super(AvroProtocolDefinition.CHUNKED_VALUE_MANIFEST, writePath ? null : ByteUtils.SIZE_OF_INT);
+  public ChunkedValueManifestSerializer(boolean withoutHeader) {
+    super(AvroProtocolDefinition.CHUNKED_VALUE_MANIFEST, withoutHeader ? null : ByteUtils.SIZE_OF_INT);
   }
 }

venice-common/src/main/java/com/linkedin/venice/serialization/avro/InternalAvroSpecificSerializer.java
@@ -116,7 +116,8 @@ public class InternalAvroSpecificSerializer<SPECIFIC_RECORD extends SpecificReco
       this.PAYLOAD_OFFSET = PROTOCOL_VERSION_OFFSET + PROTOCOL_VERSION_LENGTH;
     } else {
       if (protocolDef.magicByte.isPresent() || protocolDef.protocolVersionStoredInHeader) {
-        throw new VeniceMessageException("The payload offset override is not intended to be used for protocols which have explicitly defined magic bytes or which protocol versions stored in their header.");
+        throw new VeniceMessageException("The payload offset override is not intended to be used for protocols "
+            + "which have explicitly defined magic bytes or which have protocol versions stored in their header.");
       }
       this.PAYLOAD_OFFSET = payloadOffsetOverride;
     }
@@ -175,6 +176,12 @@ public class InternalAvroSpecificSerializer<SPECIFIC_RECORD extends SpecificReco
       }
       if (PROTOCOL_VERSION_LENGTH == 1) {
         byteArrayOutputStream.write(currentProtocolVersion);
+      }
+      if (byteArrayOutputStream.size() < PAYLOAD_OFFSET) {
+        // In some code paths, we override the payload offset so that we can have some padding at the beginning.
+        for (int i = 0; i < PAYLOAD_OFFSET - byteArrayOutputStream.size(); i++) {
+          byteArrayOutputStream.write(0);
+        }
       }
       writer.write(object, encoder);
       encoder.flush();

commit 16dc3f2ba02f77a7275b392442495ab297ccba33

[test] Fixed regression in d4bce2bd07cc8cc7b4ec5888e36e87aeec0c48ee
The VeniceWriterFactory's new parameter was not used consistently.

RB=1787552
BUG=VOLDENG-4957
G=voldemort-reviewers
R=mnhuang,smaji,goliu,xyin,anaberez,sdwu,yurzhou
A=goliu

venice-common/src/main/java/com/linkedin/venice/writer/VeniceWriterFactory.java
@@ -10,6 +10,7 @@ import com.linkedin.venice.utils.SystemTime;
 import com.linkedin.venice.utils.Time;
 import com.linkedin.venice.utils.VeniceProperties;
 
+import java.util.Optional;
 import java.util.Properties;
 
 
@@ -36,36 +37,51 @@ public class VeniceWriterFactory {
    * Create a basic venice writer with default serializer.
    */
   public VeniceWriter<byte[], byte[]> getBasicVeniceWriter(String topicName, Time time) {
-    return getVeniceWriter(topicName, new DefaultSerializer(), new DefaultSerializer(), false, time);
+    return getVeniceWriter(topicName, new DefaultSerializer(), new DefaultSerializer(), Optional.empty(), time);
   }
 
   public VeniceWriter<byte[], byte[]> getBasicVeniceWriter(String topicName) {
     return getBasicVeniceWriter(topicName, SystemTime.INSTANCE);
   }
 
+  /**
+   * @param chunkingEnabled override the factory's default chunking setting.
+   */
   public VeniceWriter<byte[], byte[]> getBasicVeniceWriter(String topicName, boolean chunkingEnabled) {
-    return getVeniceWriter(topicName, new DefaultSerializer(), new DefaultSerializer(), chunkingEnabled, SystemTime.INSTANCE);
+    return getVeniceWriter(topicName, new DefaultSerializer(), new DefaultSerializer(), Optional.of(chunkingEnabled), SystemTime.INSTANCE);
   }
 
   public <K, V> VeniceWriter<K, V> getVeniceWriter(String topicName, VeniceKafkaSerializer<K> keySerializer,
       VeniceKafkaSerializer<V> valueSerializer) {
-    return getVeniceWriter(topicName, keySerializer, valueSerializer, false, SystemTime.INSTANCE);
+    return getVeniceWriter(topicName, keySerializer, valueSerializer, Optional.empty(), SystemTime.INSTANCE);
   }
 
   /**
-   * Create a venice writer which is used to communicated with the topic contains real data.
+   * @param chunkingEnabled override the factory's default chunking setting.
+   */
+  public <K, V> VeniceWriter<K, V> getVeniceWriter(String topicName, VeniceKafkaSerializer<K> keySerializer,
+      VeniceKafkaSerializer<V> valueSerializer, boolean chunkingEnabled) {
+    return getVeniceWriter(topicName, keySerializer, valueSerializer, Optional.of(chunkingEnabled), SystemTime.INSTANCE);
+  }
+
+  /**
+   * Create a {@link VeniceWriter} which is used to communicate with the real-time topic.
+   *
+   * @param chunkingEnabled override the factory's default chunking setting.
    */
   protected <K, V> VeniceWriter<K, V> getVeniceWriter(String topic, VeniceKafkaSerializer<K> keySerializer,
-      VeniceKafkaSerializer<V> valueSerializer, boolean chunkingEnabled, Time time) {
+      VeniceKafkaSerializer<V> valueSerializer, Optional<Boolean> chunkingEnabled, Time time) {
     Properties writerProperties = new Properties();
     writerProperties.putAll(this.properties);
     writerProperties.put(ConfigKeys.KAFKA_BOOTSTRAP_SERVERS, kafkaBootstrapServers);
-    writerProperties.put(VeniceWriter.ENABLE_CHUNKING, chunkingEnabled);
+    if (chunkingEnabled.isPresent()) {
+      writerProperties.put(VeniceWriter.ENABLE_CHUNKING, chunkingEnabled.get());
+    }
     return new VeniceWriter<>(new VeniceProperties(writerProperties), topic, keySerializer, valueSerializer, time);
   }
 
   public VeniceWriter<KafkaKey, byte[]> getVeniceWriter(String topic) {
-    return getVeniceWriter(topic, new KafkaKeySerializer(), new DefaultSerializer(), false, SystemTime.INSTANCE);
+    return getVeniceWriter(topic, new KafkaKeySerializer(), new DefaultSerializer(), Optional.empty(), SystemTime.INSTANCE);
   }
 
   private void checkProperty(String key) {

venice-test-common/src/main/java/com/linkedin/venice/utils/TestUtils.java
@@ -187,16 +187,10 @@ public class TestUtils {
   }
 
   public static VeniceTestWriterFactory getVeniceTestWriterFactory(String kafkaBootstrapServer){
-    return getVeniceTestWriterFactory(kafkaBootstrapServer, false);
-  }
-
-    public static VeniceTestWriterFactory getVeniceTestWriterFactory(String kafkaBootstrapServer, boolean enableChunking) {
     Properties properties = new Properties();
-    properties.put(VeniceWriter.ENABLE_CHUNKING, enableChunking);
     properties.put(ConfigKeys.KAFKA_BOOTSTRAP_SERVERS, kafkaBootstrapServer);
     return new VeniceTestWriterFactory(properties);
   }
-
 
   public static class VeniceTestWriterFactory extends VeniceWriterFactory {
     public VeniceTestWriterFactory(Properties properties) {
@@ -204,7 +198,7 @@ public class TestUtils {
     }
 
     public <K, V> VeniceWriter<K, V> getVeniceWriter(String topic, VeniceKafkaSerializer<K> keySer,  VeniceKafkaSerializer<V>  valSer) {
-      return getVeniceWriter(topic, keySer, valSer, false, SystemTime.INSTANCE);
+      return getVeniceWriter(topic, keySer, valSer, Optional.empty(), SystemTime.INSTANCE);
     }
   }
 

venice-test-common/src/integrationtest/java/com/linkedin/venice/storagenode/StorageNodeComputeTest.java
@@ -156,8 +156,10 @@ public class StorageNodeComputeTest {
     final int pushVersion = newVersion.getVersion();
     String topic = newVersion.getKafkaTopic();
 
-    VeniceWriterFactory vwFactory = TestUtils.getVeniceTestWriterFactory(veniceCluster.getKafka().getAddress(), valueLargerThan1MB);
-    try (VeniceWriter<Object, byte[]> veniceWriter = vwFactory.getVeniceWriter(topic, keySerializer, new DefaultSerializer());
+    VeniceWriterFactory vwFactory =
+        TestUtils.getVeniceTestWriterFactory(veniceCluster.getKafka().getAddress());
+    try (VeniceWriter<Object, byte[]> veniceWriter =
+        vwFactory.getVeniceWriter(topic, keySerializer, new DefaultSerializer(), valueLargerThan1MB);
         AvroGenericStoreClient<String, Object> storeClient = ClientFactory.getAndStartGenericAvroClient(
             ClientConfig.defaultGenericClientConfig(storeName)
                 .setVeniceURL(routerAddr)