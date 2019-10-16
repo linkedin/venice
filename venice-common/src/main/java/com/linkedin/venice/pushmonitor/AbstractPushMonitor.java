linkedin/venice

commit 2f974b4f31c5aa2b47c36fc3e824727604b69b14

[common] Fix the potential case where push monitor could purge a successful push before the corresponding version gets retired
Push monitor maintains at most "MAX_PUSH_TO_KEEP" pushes for debugging
purpose. Previously, when it reaches the limit, it always deleted the
oldest version regardless it's a successful push or a failed push. This
causes issue in L/F model if the oldest version is the serving version.
(When all later pushes for a store are failed pushes)

We add constraint in this RB to only remove failed pushes. The
successful push can be only removed if the version is retired.

RB=1838194
BUG=VOLDENG-5149
G=voldemort-reviewers
R=mnhuang
A=goliu

venice-common/src/main/java/com/linkedin/venice/pushmonitor/AbstractPushMonitor.java
@@ -43,7 +43,7 @@ import org.apache.log4j.Logger;
 public abstract class AbstractPushMonitor
     implements PushMonitor, OfflinePushAccessor.PartitionStatusListener, RoutingDataRepository.RoutingDataChangedListener {
   protected final Logger logger = Logger.getLogger(getClass().getSimpleName());
-  public static final int MAX_ERROR_PUSH_TO_KEEP = 5;
+  public static final int MAX_PUSH_TO_KEEP = 5;
   private final OfflinePushAccessor offlinePushAccessor;
   private final String clusterName;
   private final ReadWriteStoreRepository metadataRepository;
@@ -123,7 +123,7 @@ public abstract class AbstractPushMonitor
               storeName -> new ArrayList<>()).add(Version.parseVersionFromKafkaTopicName(topic))
       );
 
-      storeToVersionNumsMap.entrySet().forEach(entry -> retireOldErrorPushes(entry.getKey(), entry.getValue()));
+      storeToVersionNumsMap.forEach(this::retireOldErrorPushes);
     }
   }
 
@@ -276,21 +276,24 @@ public abstract class AbstractPushMonitor
   }
 
   /**
-   * Once offline push failed, we want to keep some latest offline push in ZK for debug.
+   * We'd like to keep at most {@link #MAX_PUSH_TO_KEEP} push status for debugging purpose.
+   * If it's a successful push, it will be cleaned up when the version is retired. If it's
+   * error push, we'll leave it until the store reaches the push status limit.
    */
   protected void retireOldErrorPushes(String storeName, List<Integer> versionNums) {
-    while (versionNums.size() > MAX_ERROR_PUSH_TO_KEEP) {
-      int oldestVersionNumber = Collections.min(versionNums);
-      versionNums.remove(Integer.valueOf(oldestVersionNumber));
+    List<OfflinePushStatus> errorPushStatusList = versionNums.stream().sorted()
+        .map(version -> topicToPushMap.get(Version.composeKafkaTopic(storeName, version)))
+        .filter(offlinePushStatus -> offlinePushStatus.getCurrentStatus().equals(ExecutionStatus.ERROR))
+        .collect(Collectors.toList());
 
-      String topicName = Version.composeKafkaTopic(storeName, oldestVersionNumber);
-
-      try {
-        offlinePushAccessor.deleteOfflinePushStatusAndItsPartitionStatuses(topicToPushMap.get(topicName));
-        topicToPushMap.remove(topicName);
-      } catch (Exception e) {
-        logger.warn("Could not retire push status: " + topicName, e);
+    for (OfflinePushStatus errorPushStatus : errorPushStatusList) {
+      if (versionNums.size() <= MAX_PUSH_TO_KEEP) {
+        break;
       }
+
+      versionNums.remove(Version.parseVersionFromKafkaTopicName(errorPushStatus.getKafkaTopic()));
+
+      cleanupPushStatus(errorPushStatus);
     }
   }
 

venice-common/src/test/java/com/linkedin/venice/pushmonitor/AbstractPushMonitorTest.java
@@ -135,7 +135,7 @@ public abstract class AbstractPushMonitorTest {
   @Test
   public void testStopMonitorErrorOfflinePush() {
     String store = getStoreName();
-    for (int i = 0; i < OfflinePushMonitor.MAX_ERROR_PUSH_TO_KEEP; i++) {
+    for (int i = 0; i < OfflinePushMonitor.MAX_PUSH_TO_KEEP; i++) {
       String topic = Version.composeKafkaTopic(store, i);
       monitor.startMonitorOfflinePush(topic, numberOfPartition, replicationFactor,
           OfflinePushStrategy.WAIT_N_MINUS_ONE_REPLCIA_PER_PARTITION);
@@ -144,11 +144,11 @@ public abstract class AbstractPushMonitorTest {
       monitor.stopMonitorOfflinePush(topic);
     }
     // We should keep MAX_ERROR_PUSH_TO_KEEP error push for debug.
-    for (int i = 0; i < OfflinePushMonitor.MAX_ERROR_PUSH_TO_KEEP; i++) {
+    for (int i = 0; i < OfflinePushMonitor.MAX_PUSH_TO_KEEP; i++) {
       Assert.assertNotNull(monitor.getOfflinePush(Version.composeKafkaTopic(store, i)));
     }
     // Add a new error push, the oldest one should be collected.
-    String topic = Version.composeKafkaTopic(store, OfflinePushMonitor.MAX_ERROR_PUSH_TO_KEEP + 1);
+    String topic = Version.composeKafkaTopic(store, OfflinePushMonitor.MAX_PUSH_TO_KEEP + 1);
     monitor.startMonitorOfflinePush(topic, numberOfPartition, replicationFactor,
         OfflinePushStrategy.WAIT_N_MINUS_ONE_REPLCIA_PER_PARTITION);
     OfflinePushStatus pushStatus = monitor.getOfflinePush(topic);
@@ -184,22 +184,33 @@ public abstract class AbstractPushMonitorTest {
 
   @Test
   public void testClearOldErrorVersion() {
-    int statusCount = OfflinePushMonitor.MAX_ERROR_PUSH_TO_KEEP * 2;
+    //creating MAX_PUSH_TO_KEEP * 2 pushes. The first is successful and the rest of them are failed.
+    int statusCount = OfflinePushMonitor.MAX_PUSH_TO_KEEP * 2;
     List<OfflinePushStatus> statusList = new ArrayList<>(statusCount);
     for (int i = 0; i < statusCount; i++) {
       OfflinePushStatus pushStatus =
           new OfflinePushStatus("testLoadAllPushes_v" + i, numberOfPartition, replicationFactor,
               OfflinePushStrategy.WAIT_N_MINUS_ONE_REPLCIA_PER_PARTITION);
-      pushStatus.setCurrentStatus(ExecutionStatus.ERROR);
+
+      //set all push statuses except the first to error
+      if (i == 0) {
+        pushStatus.setCurrentStatus(ExecutionStatus.COMPLETED);
+      } else {
+        pushStatus.setCurrentStatus(ExecutionStatus.ERROR);
+      }
       statusList.add(pushStatus);
     }
     doReturn(statusList).when(mockAccessor).loadOfflinePushStatusesAndPartitionStatuses();
     monitor.loadAllPushes();
     // Make sure we delete old error pushes from accessor.
-    verify(mockAccessor, times(statusCount - OfflinePushMonitor.MAX_ERROR_PUSH_TO_KEEP))
+    verify(mockAccessor, times(statusCount - OfflinePushMonitor.MAX_PUSH_TO_KEEP))
         .deleteOfflinePushStatusAndItsPartitionStatuses(any());
+
+    //the first push should be persisted since it succeeded. But the next 5 pushes should be purged.
     int i = 0;
-    for (; i < statusCount - OfflinePushMonitor.MAX_ERROR_PUSH_TO_KEEP; i++) {
+    Assert.assertEquals(monitor.getPushStatus("testLoadAllPushes_v" + i), ExecutionStatus.COMPLETED);
+
+    for (i = 1; i <= OfflinePushMonitor.MAX_PUSH_TO_KEEP; i++) {
       try {
         monitor.getOfflinePush("testLoadAllPushes_v" + i);
         Assert.fail("Old error pushes should be collected after loading.");
@@ -207,6 +218,7 @@ public abstract class AbstractPushMonitorTest {
         //expected
       }
     }
+
     for (; i < statusCount; i++) {
       Assert.assertEquals(monitor.getPushStatus("testLoadAllPushes_v" + i), ExecutionStatus.ERROR);
     }

commit 91d980cd2df0696aee551c47919d1e46f03d9608

[common] Fix a regression in AbstractPushMonitor
This RB fix a regression introduced in 2f974b4f
List.remove(primitive integer) treats the primitive int as index,
which is not the expected behavior and could trigger
IndexOutOfBoundsExcepton:

2019/10/16 06:44:12.876 ERROR [HelixStateTransitionHandler] [Inactive ST thread.] [venice-controller-war] [] Exception while executing a state transition task test-cluster_0
......
Caused by: java.lang.IndexOutOfBoundsException: Index: 41, Size: 6
at java.util.ArrayList.rangeCheck (ArrayList.java:657)
at java.util.ArrayList.remove (ArrayList.java:496)
at com.linkedin.venice.pushmonitor.AbstractPushMonitor.retireOldErrorPushes (AbstractPushMonitor.java:294)
at java.util.HashMap.forEach (HashMap.java:1289)
at com.linkedin.venice.pushmonitor.AbstractPushMonitor.loadAllPushes (AbstractPushMonitor.java:126)
at com.linkedin.venice.pushmonitor.PushMonitorDelegator.loadAllPushes (PushMonitorDelegator.java:124)
at com.linkedin.venice.controller.VeniceHelixResources.refresh (VeniceHelixResources.java:114)
at com.linkedin.venice.controller.VeniceDistClusterControllerStateModel.lambda$onBecomeLeaderFromStandby$0 (VeniceDistClusterControllerStateModel.java:107)
at com.linkedin.venice.controller.VeniceDistClusterControllerStateModel.executeStateTransition (VeniceDistClusterControllerStateModel.java:80)
at com.linkedin.venice.controller.VeniceDistClusterControllerStateModel.onBecomeLeaderFromStandby (VeniceDistClusterControllerStateModel.java:95)
... 12 more

This RB does boxing on the version number before removing
it from the list.

RB=1843482
BUG=VOLDENG-5149
G=voldemort-reviewers
R=sdwu
A=smaji,goliu

venice-common/src/main/java/com/linkedin/venice/pushmonitor/AbstractPushMonitor.java
@@ -291,7 +291,9 @@ public abstract class AbstractPushMonitor
         break;
       }
 
-      versionNums.remove(Version.parseVersionFromKafkaTopicName(errorPushStatus.getKafkaTopic()));
+      int errorVersion = Version.parseVersionFromKafkaTopicName(errorPushStatus.getKafkaTopic());
+      // Make sure we do boxing; List.remove(primitive int) treats the primitive int as index
+      versionNums.remove(Integer.valueOf(errorVersion));
 
       cleanupPushStatus(errorPushStatus);
     }