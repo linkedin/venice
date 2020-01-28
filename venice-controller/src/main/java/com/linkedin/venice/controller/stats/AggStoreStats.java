39ce5f7b4bbba7e782c906a92d6e7aaf20a6c1d0
Alexei Naberezhnov <anaberezhnov@linkedin.com>
2020-01-27 17:41:04 -0800

    [controller] add store data age metric
    RB=1946798
    BUG=VENG-5324
    G=voldemort-reviewers
    R=sdwu,fvillega,smaji,xyin,mnhuang,zpolicze,goliu,yurzhou
    A=yurzhou

---
+++ venice-controller/src/main/java/com/linkedin/venice/controller/stats/AggStoreStats.java
@@ -0,0 +1,30 @@
+package com.linkedin.venice.stats;
+
+import com.linkedin.venice.meta.StoreDataChangedListener;
+import  com.linkedin.venice.stats.StoreStats;
+
+import com.linkedin.venice.meta.ReadOnlyStoreRepository;
+import com.linkedin.venice.meta.Store;
+
+import io.tehuti.metrics.MetricsRepository;
+
+public class AggStoreStats {
+  public AggStoreStats(MetricsRepository metricsRepository, ReadOnlyStoreRepository storeRepository) {
+    for (Store store : storeRepository.getAllStores()) {
+      new StoreStats(store.getName(), metricsRepository, storeRepository);
+    }
+
+    storeRepository.registerStoreDataChangedListener(new StoreDataChangedListener() {
+      @Override
+      public void handleStoreCreated(Store store) {
+        new StoreStats(store.getName(), metricsRepository, storeRepository);
+      }
+
+      @Override
+      public void handleStoreDeleted(String storeName) {}
+
+      @Override
+      public void handleStoreChanged(Store store) {}
+    });
+  }
+}
