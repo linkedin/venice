90801a315d9c8b52433323627fb1f3eab5784994
Sidian Wu <sdwu@linkedin.com>
2020-03-17 15:53:28 -0700

    [h2v][samza][thin-client]Introduce a new AvroGenericSerialize that sorts the map before serializing it
    By default, Avro serializer calls Map#entrySet to iterate the Map. Since
    the iteration order is not guaranteed (e.g. when 2 keys A and B have the
    same has code; map collision happens and iteration order will depends on
    the order that the entries get inserted. This causes problem if users
    have Map in their key and the way they construct the keys differ between
    write path and read path.
    
    This RB brings a new Avro serializer that sorts the Map entry set by key before serializing.
    
    RB=2012723
    BUG=VENG-5431
    G=voldemort-reviewers
    R=anaberez,fvillega,jlliu,zpolicze,smaji,xyin,nithakka,yurzhou,zfan,mnhuang,shuhliu,goliu,xnma
    A=goliu

---
+++ venice-schema-common/src/test/java/org/apache/avro/generic/TestMapAwareGenericDatumWriter.java
@@ -0,0 +1,77 @@
+package org.apache.avro.generic;
+
+import com.linkedin.avro.compatibility.AvroCompatibilityHelper;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.avro.Schema;
+import org.apache.avro.io.Encoder;
+import org.apache.avro.util.Utf8;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class TestMapAwareGenericDatumWriter {
+
+  /**
+   * a helper String class that can config hash code manually
+   */
+  private static class TestString extends Utf8 {
+    private int i;
+
+    TestString(int i, String s) {
+      super(s);
+      this.i = i;
+    }
+
+    @Override
+    public int hashCode() {
+      return i;
+    }
+  }
+
+  @Test
+  public void testWriterCanWriteMapInOrder() throws IOException {
+    //construct 2 elements that have the same hash code
+    TestString string1 = new TestString(1, "a");
+    TestString string2 = new TestString(1, "b");
+
+    Schema mapSchema = Schema.createMap(Schema.create(Schema.Type.INT));
+
+    /*construct 2 maps that have 2 entries with the same key hash code.
+    This guarantee that the collision will happen when inserting the
+    entries. That being said, if we insert the entries in different
+    order, it will be persisted in different order in the map. (By
+    default, Java HashMap uses linked list to persist collided entries
+    if the number of collided entries is less than 8)*/
+    Map<TestString, Integer> map1 = new HashMap<>();
+    map1.put(string1, 1);
+    map1.put(string2, 2);
+    Map<TestString, Integer> map2 = new HashMap<>();
+    map2.put(string2, 2);
+    map2.put(string1, 1);
+
+    //Since GenericDatumWriter doesn't sort the entries before serializing,
+    //the output byte arrays will look differently between map1 and map2.
+    GenericDatumWriter genericDatumWriter = new GenericDatumWriter(mapSchema);
+    Assert.assertFalse(Arrays.equals(serialize(genericDatumWriter, map1),
+        serialize(genericDatumWriter, map2)));
+
+    MapAwareGenericDatumWriter mapAwareGenericDatumWriter = new MapAwareGenericDatumWriter(mapSchema);
+    Assert.assertTrue(Arrays.equals(serialize(mapAwareGenericDatumWriter, map1),
+        serialize(mapAwareGenericDatumWriter, map2)));
+  }
+
+  //helper method that serializes the object
+  private byte[] serialize(GenericDatumWriter writer, Map map) throws IOException {
+    ByteArrayOutputStream output = new ByteArrayOutputStream();
+    Encoder encoder = AvroCompatibilityHelper.newBufferedBinaryEncoder(output);
+
+    writer.write(map, encoder);
+    encoder.flush();
+
+    return output.toByteArray();
+  }
+}
