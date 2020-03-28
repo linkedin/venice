47238c5599a2e56d4f30583659c86e964a5d03ba
Xun Yin <xyin@linkedin.com>
2020-03-27 18:16:28 -0700

    [server] Add configurable FixedLengthPrefixExtractor for enabling RocksDB PlainTable
    Added config validation and integration test for PlainTable.
    
    RB=2028909
    BUG=VENG-5686
    G=voldemort-reviewers
    R=fvillega,smaji,goliu,anaberez
    A=zpolicze

---
+++ venice-test-common/src/integrationtest/java/com/linkedin/venice/endToEnd/RocksDBPlainTableTest.java
@@ -0,0 +1,64 @@
+package com.linkedin.venice.endToEnd;
+
+import com.linkedin.venice.client.store.AvroGenericStoreClient;
+import com.linkedin.venice.client.store.ClientConfig;
+import com.linkedin.venice.client.store.ClientFactory;
+import com.linkedin.venice.exceptions.VeniceException;
+import com.linkedin.venice.integration.utils.ServiceFactory;
+import com.linkedin.venice.integration.utils.VeniceClusterWrapper;
+import com.linkedin.venice.meta.PersistenceType;
+import java.util.Properties;
+import org.apache.commons.io.IOUtils;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static com.linkedin.venice.ConfigKeys.*;
+import static com.linkedin.venice.store.rocksdb.RocksDBServerConfig.*;
+
+
+public class RocksDBPlainTableTest {
+  private VeniceClusterWrapper veniceCluster;
+
+  @BeforeClass
+  public void setup() {
+    veniceCluster = ServiceFactory.getVeniceCluster(1, 0, 1);
+  }
+
+  @AfterClass
+  public void cleanup() {
+    IOUtils.closeQuietly(veniceCluster);
+  }
+
+  @Test
+  public void testInvalidPlainTableConfig() {
+    Properties serverProperties = getRocksDBPlainTableEnabledProperties();
+    serverProperties.put(ROCKSDB_OPTIONS_USE_DIRECT_READS, true);
+    Assert.assertThrows(VeniceException.class, () -> {
+      veniceCluster.addVeniceServer(new Properties(), serverProperties);
+    });
+  }
+
+  @Test
+  public void testPlainTableEndToEnd() throws Exception {
+    veniceCluster.addVeniceServer(new Properties(), getRocksDBPlainTableEnabledProperties());
+    final int keyCount = 100;
+    String storeName = veniceCluster.createStore(keyCount);
+    try (AvroGenericStoreClient<Integer, Integer> client = ClientFactory.getAndStartGenericAvroClient(
+        ClientConfig.defaultGenericClientConfig(storeName).setVeniceURL(veniceCluster.getRandomRouterURL()))) {
+      for (int i = 0; i < keyCount; ++i) {
+        Integer value = client.get(i).get();
+        Assert.assertNotNull(value);
+      }
+    }
+  }
+
+  private Properties getRocksDBPlainTableEnabledProperties() {
+    Properties serverProperties = new Properties();
+    serverProperties.put(PERSISTENCE_TYPE, PersistenceType.ROCKS_DB);
+    serverProperties.put(ROCKSDB_PLAIN_TABLE_FORMAT_ENABLED, true);
+    serverProperties.put(ROCKSDB_OPTIONS_USE_DIRECT_READS, false);
+    return serverProperties;
+  }
+}
