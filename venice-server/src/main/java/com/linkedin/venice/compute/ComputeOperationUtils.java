bb9bf14e9eefc10f3a6fd56448719676ad97552b
Sourav Maji <smaji@linkedin.com>
2020-04-06 13:07:35 -0700

    [server] Replace PrimitiFloatList with float[] for compute hadamardoperation
    RB=2041951
    G=voldemort-reviewers
    R=fvillega,nithakka,zfan,jlliu,sdwu,xnma,zpolicze,goliu,yurzhou,anaberez,xyin,shuhliu,mnhuang
    A=goliu,fvillega

--- venice-server/src/main/java/com/linkedin/venice/compute/ComputeOperationUtils.java
+++ venice-server/src/main/java/com/linkedin/venice/compute/ComputeOperationUtils.java
@@ -5,6 +5,8 @@ import com.linkedin.venice.exceptions.VeniceException;
 import com.linkedin.venice.serializer.ComputablePrimitiveFloatList;
 import java.util.List;
 
+import static com.linkedin.venice.utils.Utils.*;
+
 
 /**
  * This class provides utilities for float-vector operations, and it also handles {@link ComputablePrimitiveFloatList}
@@ -21,13 +23,14 @@ public class ComputeOperationUtils {
     if (list1 instanceof ComputablePrimitiveFloatList && list2 instanceof ComputablePrimitiveFloatList) {
       ComputablePrimitiveFloatList computablePrimitiveFloatList1 = (ComputablePrimitiveFloatList)list1;
       ComputablePrimitiveFloatList computablePrimitiveFloatList2 = (ComputablePrimitiveFloatList)list2;
-      return dotProduct(list1.size(), (i) -> computablePrimitiveFloatList1.getPrimitive(i), (i) -> computablePrimitiveFloatList2.getPrimitive(i));
+      return dotProduct(list1.size(), computablePrimitiveFloatList1::getPrimitive,
+          computablePrimitiveFloatList2::getPrimitive);
     } else if (list1 instanceof PrimitiveFloatList && list2 instanceof PrimitiveFloatList) {
       PrimitiveFloatList primitiveFloatList1 = (PrimitiveFloatList)list1;
       PrimitiveFloatList primitiveFloatList2 = (PrimitiveFloatList)list2;
-      return dotProduct(list1.size(), (i) -> primitiveFloatList1.getPrimitive(i), (i) -> primitiveFloatList2.getPrimitive(i));
+      return dotProduct(list1.size(), primitiveFloatList1::getPrimitive, primitiveFloatList2::getPrimitive);
     } else {
-      return dotProduct(list1.size(), (i) -> list1.get(i), (i) -> list2.get(i));
+      return dotProduct(list1.size(), list1::get, list2::get);
     }
   }
 
@@ -39,13 +42,14 @@ public class ComputeOperationUtils {
     if (list1 instanceof ComputablePrimitiveFloatList && list2 instanceof ComputablePrimitiveFloatList) {
       ComputablePrimitiveFloatList computablePrimitiveFloatList1 = (ComputablePrimitiveFloatList)list1;
       ComputablePrimitiveFloatList computablePrimitiveFloatList2 = (ComputablePrimitiveFloatList)list2;
-      return hadamardProduct(list1.size(), (i) -> computablePrimitiveFloatList1.getPrimitive(i), (i) -> computablePrimitiveFloatList2.getPrimitive(i));
+      return hadamardProduct(list1.size(), computablePrimitiveFloatList1::getPrimitive,
+          computablePrimitiveFloatList2::getPrimitive);
     } else if (list1 instanceof PrimitiveFloatList && list2 instanceof PrimitiveFloatList) {
       PrimitiveFloatList primitiveFloatList1 = (PrimitiveFloatList)list1;
       PrimitiveFloatList primitiveFloatList2 = (PrimitiveFloatList)list2;
-      return hadamardProduct(list1.size(), (i) -> primitiveFloatList1.getPrimitive(i), (i) -> primitiveFloatList2.getPrimitive(i));
+      return hadamardProduct(list1.size(), primitiveFloatList1::getPrimitive, primitiveFloatList2::getPrimitive);
     } else {
-      return hadamardProduct(list1.size(), (i) -> list1.get(i), (i) -> list2.get(i));
+      return hadamardProduct(list1.size(), list1::get, list2::get);
     }
   }
 
@@ -79,7 +83,7 @@ public class ComputeOperationUtils {
   }
 
   private static List<Float> hadamardProduct(int size, FloatSupplierByIndex floatSupplier1, FloatSupplierByIndex floatSupplier2) {
-    PrimitiveFloatList hadamardProductResult = new PrimitiveFloatList(size);
+    float[] floats = new float[size];
 
     // round up size to the largest multiple of 4
     int i = 0;
@@ -88,21 +92,17 @@ public class ComputeOperationUtils {
     // Unrolling mult-add into blocks of 4 multiply op and assign to 4 different variables so that CPU can take
     // advantage of out of order execution, making the operation faster (on a single thread ~2x improvement)
     for (; i < limit; i += 4) {
-      float s0 = floatSupplier1.get(i) * floatSupplier2.get(i);
-      float s1 = floatSupplier1.get(i + 1) * floatSupplier2.get(i + 1);
-      float s2 = floatSupplier1.get(i + 2) * floatSupplier2.get(i + 2);
-      float s3 = floatSupplier1.get(i + 3) * floatSupplier2.get(i + 3);
-      hadamardProductResult.addPrimitive(s0);
-      hadamardProductResult.addPrimitive(s1);
-      hadamardProductResult.addPrimitive(s2);
-      hadamardProductResult.addPrimitive(s3);
+      floats[i] = floatSupplier1.get(i) * floatSupplier2.get(i);
+      floats[i + 1] = floatSupplier1.get(i + 1) * floatSupplier2.get(i + 1);
+      floats[i + 2] = floatSupplier1.get(i + 2) * floatSupplier2.get(i + 2);
+      floats[i + 3] = floatSupplier1.get(i + 3) * floatSupplier2.get(i + 3);
     }
 
     // Multiply the remaining elements
     for (; i < size; i++) {
-      hadamardProductResult.addPrimitive(floatSupplier1.get(i) * floatSupplier2.get(i));
+      floats[i] = floatSupplier1.get(i) * floatSupplier2.get(i);
     }
-    return hadamardProductResult;
+    return asUnmodifiableList(floats);
   }
 
   public static float squaredL2Norm(List<Float> list) {
@@ -111,11 +111,11 @@ public class ComputeOperationUtils {
     } else if (list instanceof PrimitiveFloatList) {
       PrimitiveFloatList primitiveFloatList = (PrimitiveFloatList)list;
       int size = primitiveFloatList.size();
-      FloatSupplierByIndex floatSupplierByIndex = (i) -> primitiveFloatList.getPrimitive(i);
+      FloatSupplierByIndex floatSupplierByIndex = primitiveFloatList::getPrimitive;
       return dotProduct(size, floatSupplierByIndex, floatSupplierByIndex);
     } else {
       int size = list.size();
-      FloatSupplierByIndex floatSupplierByIndex = (i) -> list.get(i);
+      FloatSupplierByIndex floatSupplierByIndex = list::get;
       return dotProduct(size, floatSupplierByIndex, floatSupplierByIndex);
     }
   }
