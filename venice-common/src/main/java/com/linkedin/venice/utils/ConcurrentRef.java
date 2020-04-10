bedf3e46398a1fb34a467f242e2b76836962d5e7
Alexei Naberezhnov <anaberezhnov@linkedin.com>
2020-04-10 04:18:56 -0700

    [davinci] automatically re-subscribe to new versions
    RB=2050266
    
    RB=2050266
    BUG=VENG-5496
    G=voldemort-reviewers
    R=yurzhou,xyin,jlliu,shuhliu,smaji,goliu,mnhuang,zpolicze,nithakka,fvillega,sdwu,xnma,zfan
    A=jlliu,zfan,smaji

--- venice-common/src/main/java/com/linkedin/venice/utils/ConcurrentRef.java
+++ venice-common/src/main/java/com/linkedin/venice/utils/ConcurrentRef.java
@@ -1,39 +1,26 @@
 package com.linkedin.venice.utils;
 
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Consumer;
 
 
-public class ConcurrentRef<T> implements AutoCloseable {
-  private final AtomicReference<ConcurrentRef> holder;
-  private final AtomicReference<T> object;
+public class ConcurrentRef<T> {
+  private final AtomicReference<ReferenceCounted<T>> holder;
   private final Consumer<T> deleter;
-  private final AtomicInteger refCount = new AtomicInteger(1);
 
   public ConcurrentRef(Consumer<T> deleter) {
     this(null, deleter);
   }
 
   public ConcurrentRef(T object, Consumer<T> deleter) {
-    this.holder = new AtomicReference(this);
-    this.object = new AtomicReference(object);
+    this.holder = new AtomicReference<>(new ReferenceCounted<>(object, deleter));
     this.deleter = deleter;
   }
 
-  public T get() {
-    return object.get();
-  }
-
-  @Override
-  public void close() {
-    release();
-  }
-
-  public ConcurrentRef acquire() {
+  public ReferenceCounted<T> get() {
     for (;;) {
-      ConcurrentRef ref = holder.get();
-      ref.refCount.incrementAndGet();
+      ReferenceCounted<T> ref = holder.get();
+      ref.retain();
       if (ref == holder.get()) {
         return ref;
       }
@@ -41,21 +28,12 @@ public class ConcurrentRef<T> implements AutoCloseable {
     }
   }
 
-  public void release() {
-    if (refCount.decrementAndGet() == 0) {
-      T obj = object.getAndSet(null);
-      if (obj != null) {
-        deleter.accept(obj);
-      }
-    }
-  }
-
-  public void reset(T object) {
-    ConcurrentRef ref = holder.getAndSet(new ConcurrentRef(object, deleter));
+  public void set(T object) {
+    ReferenceCounted<T> ref = holder.getAndSet(new ReferenceCounted<>(object, deleter));
     ref.release();
   }
 
   public void clear() {
-    holder.set(new ConcurrentRef(null, deleter));
+    holder.set(new ReferenceCounted<>(null, null));
   }
 }
---
+++ venice-common/src/main/java/com/linkedin/venice/utils/ReferenceCounted.java
@@ -0,0 +1,47 @@
+package com.linkedin.venice.utils;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Consumer;
+
+
+public class ReferenceCounted<T> implements AutoCloseable {
+  private final T object;
+  private final AtomicInteger refCount;
+  private final AtomicReference<Consumer<T>> deleterRef;
+
+  public ReferenceCounted(T object, Consumer<T> deleter) {
+    this.object = object;
+    this.refCount = new AtomicInteger(1);
+    this.deleterRef = new AtomicReference<>(deleter);
+  }
+
+  public T get() {
+    return object;
+  }
+
+  @Override
+  public void close() {
+    release();
+  }
+
+  public void retain() {
+    int count = refCount.incrementAndGet();
+    if (count <= 0) {
+      throw new ArithmeticException("Reference counter overflow, refCount=" + count);
+    }
+  }
+
+  public void release() {
+    int count = refCount.decrementAndGet();
+    if (count < 0) {
+      throw new ArithmeticException("Reference counter underflow, refCount=" + count);
+    }
+    if (count == 0) {
+      Consumer<T> deleter = deleterRef.getAndSet(null);
+      if (deleter != null && object != null) {
+        deleter.accept(object);
+      }
+    }
+  }
+}
