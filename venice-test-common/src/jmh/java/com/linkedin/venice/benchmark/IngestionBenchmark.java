111869a7d7404eb50eb2f7e72b2a66296f1ec230
Ziqi Fan <zfan@linkedin.com>
2020-05-29 16:47:19 -0700

    [test] use Measurement annotations to control test iterations for IngestionBenchmark
    RB=2125929
    BUG=VENG-6059
    G=voldemort-reviewers
    R=xnma,anaberez,fvillega,jlliu
    A=anaberez

--- venice-test-common/src/jmh/java/com/linkedin/venice/benchmark/IngestionBenchmark.java
+++ venice-test-common/src/jmh/java/com/linkedin/venice/benchmark/IngestionBenchmark.java
@@ -8,7 +8,6 @@ import com.linkedin.venice.utils.TestUtils;
 import com.linkedin.venice.utils.Utils;
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
@@ -23,7 +22,6 @@ import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
-import org.openjdk.jmh.annotations.OperationsPerInvocation;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
@@ -31,7 +29,6 @@ import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.TearDown;
 import org.openjdk.jmh.annotations.Warmup;
-import org.openjdk.jmh.infra.Blackhole;
 import org.openjdk.jmh.profile.GCProfiler;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
@@ -44,10 +41,9 @@ import static com.linkedin.venice.integration.utils.ServiceFactory.*;
 @State(Scope.Benchmark)
 @Fork(value = 1)
 @Warmup(iterations = 1)
-@Measurement(iterations = 1)
+@Measurement(iterations = 10)
 public class IngestionBenchmark {
   private static final int numRecords = 100_000;
-  private static final int numIngestions = 10;
   private static final String FLOAT_VECTOR_VALUE_SCHEMA = "{" +
       "  \"namespace\" : \"example.avro\",  " +
       "  \"type\": \"record\",   " +
@@ -84,25 +80,19 @@ public class IngestionBenchmark {
   }
 
   @Benchmark
-  @OperationsPerInvocation(numIngestions)
-  public void ingestionBenchmarkTest(Blackhole bh) {
+  public void ingestionBenchmarkTest() {
     File dataBasePath = TestUtils.getTempDataDirectory();
-    for (int i = 0; i < numIngestions; i++) {
-      try {
-        // Delete and recreate data base folder.
-        FileUtils.deleteDirectory(dataBasePath);
-        Files.createDirectories(dataBasePath.toPath());
-        DaVinciClient<Long, GenericRecord> client =
-            getGenericAvroDaVinciClient(storeName, cluster, dataBasePath.toString());
-        // Ingest data to local folder.
-        client.subscribeToAllPartitions().get(60, TimeUnit.SECONDS);
-        client.close();
-      } catch (IOException | InterruptedException | ExecutionException | TimeoutException e) {
-        throw new VeniceException(e);
-      }
+    try {
+      // Delete and recreate data base folder.
+      FileUtils.deleteDirectory(dataBasePath);
+      DaVinciClient<Long, GenericRecord> client = getGenericAvroDaVinciClient(storeName, cluster, dataBasePath.toString());
+      // Ingest data to local folder.
+      client.subscribeToAllPartitions().get(60, TimeUnit.SECONDS);
+      client.close();
+    } catch (IOException | InterruptedException | ExecutionException | TimeoutException e) {
+      throw new VeniceException(e);
     }
   }
-
 
   public static void main(String[] args) throws RunnerException {
     org.openjdk.jmh.runner.options.Options opt = new OptionsBuilder()
