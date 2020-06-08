a2426c7de016b84237cffbcd9acaa70720a5b8fc
Ziqi Fan <zfan@linkedin.com>
2020-06-08 14:21:50 -0700

    [davinci] add more clarification to DV Client Javadocs
    RB=2141706
    BUG=VENG-6105
    G=voldemort-reviewers
    R=xnma,anaberez,jlliu,fvillega
    A=kkao,fvillega

--- da-vinci-client/src/main/java/com/linkedin/davinci/client/DaVinciClient.java
+++ da-vinci-client/src/main/java/com/linkedin/davinci/client/DaVinciClient.java
@@ -13,7 +13,7 @@ import com.linkedin.venice.client.store.AvroGenericStoreClient;
 public interface DaVinciClient<K, V> extends AvroGenericStoreClient<K, V> {
 
   /**
-   * Ingest the entire data locally.
+   * Ingest the entire data (i.e. all partitions) locally.
    *
    * @return a future which completes when the data is ready to serve
    * @throws a VeniceException if subscription failed for any of the partitions
@@ -21,7 +21,7 @@ public interface DaVinciClient<K, V> extends AvroGenericStoreClient<K, V> {
   CompletableFuture<Void> subscribeToAllPartitions();
 
   /**
-   * Ingest a partition locally.
+   * Ingest specific partition/partitions locally.
    *
    * @param partitions the set of partition IDs to subscribe to
    * @return a future which completes when the partitions are ready to serve
@@ -30,14 +30,20 @@ public interface DaVinciClient<K, V> extends AvroGenericStoreClient<K, V> {
   CompletableFuture<Void> subscribe(Set<Integer> partitions);
 
   /**
-   * Stop ingesting all subscribed partition locally, and drop their associated local states.
+   * Stop ingesting all subscribed partition locally, and drop their associated local states/data.
+   *
+   * If applications intend to keep the states/data for future use, no need to invoke this function before
+   * calling {@link com.linkedin.venice.client.store.AvroGenericStoreClient#close()}.
    *
    * @throws a VeniceException if cleanup failed for any of the partitions
    */
   void unsubscribeFromAllPartitions();
 
   /**
-   * Stop ingesting a partition locally, and drop its associated local state.
+   * Stop ingesting a partition locally, and drop its associated local states/data.
+   *
+   * If applications intend to keep the states/data for future use, no need to invoke this function before
+   * calling {@link com.linkedin.venice.client.store.AvroGenericStoreClient#close()}.
    *
    * @param partitions the set of partition IDs to unsubscribe from
    * @throws a VeniceException if cleanup failed for any of the partitions
