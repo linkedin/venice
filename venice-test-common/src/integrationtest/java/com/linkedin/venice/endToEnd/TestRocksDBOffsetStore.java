9f1f9428afbcc9061ace31f331a0ab146cd6e8a6
Sourav Maji <smaji@linkedin.com>
2020-06-09 11:11:07 -0700

    [test] Added offset metadata store integration test
    RB=2143410
    G=voldemort-reviewers
    R=goliu,mnhuang,jlliu,nithakka,yurzhou,xnma,shuhliu,xyin,zfan,sdwu,fvillega,anaberez,zpolicze
    A=xyin

---
+++ venice-test-common/src/integrationtest/java/com/linkedin/venice/endToEnd/TestRocksDBOffsetStore.java
@@ -0,0 +1,64 @@
+package com.linkedin.venice.endToEnd;
+
+import com.linkedin.venice.client.store.AvroGenericStoreClient;
+import com.linkedin.venice.client.store.ClientConfig;
+import com.linkedin.venice.client.store.ClientFactory;
+import com.linkedin.venice.integration.utils.ServiceFactory;
+import com.linkedin.venice.integration.utils.VeniceClusterWrapper;
+import com.linkedin.venice.integration.utils.VeniceServerWrapper;
+import com.linkedin.venice.meta.PersistenceType;
+import com.linkedin.venice.storage.StorageMetadataService;
+import com.linkedin.venice.utils.TestUtils;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+import org.apache.commons.io.IOUtils;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static com.linkedin.venice.ConfigKeys.*;
+
+
+public class TestRocksDBOffsetStore {
+  private VeniceClusterWrapper veniceCluster;
+  
+  @BeforeClass
+  public void setup() {
+    veniceCluster = ServiceFactory.getVeniceCluster(1, 0, 1);
+  }
+
+  @AfterClass
+  public void cleanup() {
+    IOUtils.closeQuietly(veniceCluster);
+  }
+
+  @Test
+  public void testStorageMetadataServiceOffsets() throws Exception {
+    VeniceServerWrapper serverWrapper = veniceCluster.addVeniceServer(new Properties(), getRocksDBOffsetStoreEnabledProperties());
+    final int keyCount = 100;
+    String storeName = veniceCluster.createStore(keyCount);
+    String storeTopicName = storeName + "_v1";
+    StorageMetadataService storageMetadataService = serverWrapper.getVeniceServer().getStorageMetadataService();
+    Assert.assertTrue(storageMetadataService.getLastOffset(storeTopicName, 0).getOffset() != -1);
+    veniceCluster.stopVeniceServer(serverWrapper.getPort());
+    TestUtils.waitForNonDeterministicCompletion(30, TimeUnit.SECONDS, () -> veniceCluster.getRandomVeniceRouter()
+        .getRoutingDataRepository().getPartitionAssignments(storeTopicName).getAssignedNumberOfPartitions() == 0);    veniceCluster.restartVeniceServer(serverWrapper.getPort());
+    storageMetadataService = veniceCluster.getVeniceServers().get(0).getVeniceServer().getStorageMetadataService();
+    Assert.assertTrue(storageMetadataService.getLastOffset(storeTopicName, 0).getOffset() != -1);
+    try (AvroGenericStoreClient<Integer, Integer> client = ClientFactory.getAndStartGenericAvroClient(
+        ClientConfig.defaultGenericClientConfig(storeName).setVeniceURL(veniceCluster.getRandomRouterURL()))) {
+      for (int i = 0; i < keyCount; ++i) {
+        Integer value = client.get(i).get();
+        Assert.assertNotNull(value);
+      }
+    }
+  }
+
+  private Properties getRocksDBOffsetStoreEnabledProperties() {
+    Properties serverProperties = new Properties();
+    serverProperties.put(PERSISTENCE_TYPE, PersistenceType.ROCKS_DB);
+    serverProperties.put(SERVER_ENABLE_ROCKSDB_METADATA, true);
+    return serverProperties;
+  }
+}
