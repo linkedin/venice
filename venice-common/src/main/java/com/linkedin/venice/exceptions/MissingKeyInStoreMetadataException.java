a819c5c958019f667f708ac3c12de8a4c1b738f9
Jialin Liu <jlliu@linkedin.com>
2020-06-12 10:29:08 -0700

    [common][test-common] Implement ReadOnlySchemaRepository interface in ZK-free store repository
    RB=2185560
    BUG=VENG-6224
    G=voldemort-reviewers
    R=zfan,zpolicze,fvillega,xnma,anaberez,xyin
    A=xyin

---
+++ venice-common/src/main/java/com/linkedin/venice/exceptions/MissingKeyInStoreMetadataException.java
@@ -0,0 +1,24 @@
+package com.linkedin.venice.exceptions;
+
+public class MissingKeyInStoreMetadataException extends VeniceException {
+    private final String keyName;
+    private final String metadataClassName;
+    public MissingKeyInStoreMetadataException(String keyName, String metadataClassName) {
+        super("Get null " + metadataClassName + " value for key " + keyName + " in metadata store.");
+        this.keyName = keyName;
+        this.metadataClassName = metadataClassName;
+    }
+    public MissingKeyInStoreMetadataException(String keyName, String metadataClassName, Throwable t) {
+        super("Get null " + metadataClassName + " value for key " + keyName + " in metadata store.", t);
+        this.keyName = keyName;
+        this.metadataClassName = metadataClassName;
+    }
+    public String getKeyName(){
+        return keyName;
+    }
+
+    public String getMetadataClassName() {
+        return metadataClassName;
+    }
+
+}
