1e3d94d2fe1c99e406fcb8e0307809ad5afec6cc
Jialin Liu <jlliu@linkedin.com>
2020-06-18 00:18:24 -0700

    [da-vinci-client] Separate getClient and getAndStartClient into two APIs
    RB=2158694
    G=voldemort-reviewers
    R=xnma,fvillega,zfan,anaberez
    A=zfan,anaberez

--- da-vinci-client/src/main/java/com/linkedin/davinci/client/factory/DaVinciClientFactory.java
+++ da-vinci-client/src/main/java/com/linkedin/davinci/client/factory/DaVinciClientFactory.java
@@ -7,6 +7,8 @@ import org.apache.avro.specific.SpecificRecord;
 
 public interface DaVinciClientFactory extends AutoCloseable {
   <K, V> DaVinciClient<K, V> getGenericAvroClient(String storeName, DaVinciConfig config);
+  <K, V> DaVinciClient<K, V> getAndStartGenericAvroClient(String storeName, DaVinciConfig config);
   <K, V extends SpecificRecord> DaVinciClient<K, V> getSpecificAvroClient(String storeName, DaVinciConfig config, Class<V> valueClass);
+  <K, V extends SpecificRecord> DaVinciClient<K, V> getAndStartSpecificAvroClient(String storeName, DaVinciConfig config, Class<V> valueClass);
 }
 
