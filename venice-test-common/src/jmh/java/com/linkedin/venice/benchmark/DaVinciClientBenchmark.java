5923f770e70c41a70d51cb7e268f5c649390ae7e
Alexei Naberezhnov <anaberezhnov@linkedin.com>
2020-06-27 16:25:25 -0700

    [da-vinci] DaVinciClientBenchmark bugfix and improvements
    RB=2173297
    G=voldemort-reviewers
    R=xnma,sdwu,goliu,smaji,jlliu,zpolicze,zfan,nithakka,fvillega,shuhliu,yurzhou,mnhuang,xyin
    A=jlliu

--- venice-test-common/src/jmh/java/com/linkedin/venice/benchmark/DaVinciClientBenchmark.java
+++ venice-test-common/src/jmh/java/com/linkedin/venice/benchmark/DaVinciClientBenchmark.java
@@ -1,18 +1,17 @@
 package com.linkedin.venice.benchmark;
 
-import com.linkedin.davinci.client.DaVinciClient;
 import com.linkedin.venice.exceptions.VeniceException;
-import com.linkedin.venice.integration.utils.ServiceFactory;
 import com.linkedin.venice.integration.utils.VeniceClusterWrapper;
+import com.linkedin.venice.integration.utils.VeniceControllerWrapper;
+import com.linkedin.venice.integration.utils.VeniceRouterWrapper;
+import com.linkedin.venice.integration.utils.VeniceServerWrapper;
 import com.linkedin.venice.utils.Utils;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
+
+import com.linkedin.davinci.client.DaVinciClient;
+
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericData;
 import org.apache.avro.generic.GenericRecord;
-import org.apache.log4j.Logger;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Fork;
@@ -25,73 +24,61 @@ import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.TearDown;
+import org.openjdk.jmh.annotations.Threads;
 import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.infra.Blackhole;
 import org.openjdk.jmh.profile.GCProfiler;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
-import org.testng.Assert;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 import static com.linkedin.venice.integration.utils.ServiceFactory.*;
 
 
-@BenchmarkMode(Mode.AverageTime)
-@OutputTimeUnit(TimeUnit.NANOSECONDS)
-@State(Scope.Benchmark)
-@Fork(value = 2)
+@Fork(value = 2, jvmArgs = {"-Xms4G", "-Xmx4G"})
 @Warmup(iterations = 10)
 @Measurement(iterations = 10)
+@State(Scope.Benchmark)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
 public class DaVinciClientBenchmark {
-  private static final Logger logger = Logger.getLogger(DaVinciClientBenchmark.class);
-  private static final int recordCount = 100_000;
-  private static final int queryCount = 1000_000;
-  private static final String FLOAT_VECTOR_VALUE_SCHEMA = "{" +
-      "  \"namespace\" : \"example.avro\",  " +
-      "  \"type\": \"record\",   " +
-      "  \"name\": \"FloatVector\",     " +
-      "  \"fields\": [           " +
-      "       { \"name\": \"value\", \"type\": {\"type\": \"array\", \"items\": \"float\"} }  " +
-      "  ] " +
-      " } ";
+  protected static final int RECORD_COUNT = 100_000;
+  protected static final int KEY_COUNT = 1000_000;
+  protected static final String VALUE_FIELD_NAME = "value";
 
-  private static final String SPARSE_VECTOR_VALUE_SCHEMA = "{" +
-      "  \"namespace\" : \"example.avro\",  " +
-      "  \"type\": \"record\",   " +
-      "  \"name\": \"SparseVector\",     " +
-      "  \"fields\": [           " +
-      "       { \"name\": \"index\", \"type\": {\"type\": \"array\", \"items\": \"int\"} },  " +
-      "       { \"name\": \"value\", \"type\": {\"type\": \"array\", \"items\": \"float\"} }  " +
-
-      "  ] " +
-      " } ";
-
-  private VeniceClusterWrapper cluster;
-  private DaVinciClient<Long, GenericRecord> client;
-  private GenericRecord record;
-  private long[] keys = new long[queryCount];
-
-  @Param({"1", "10", "100"})
-//  @Param({"2500"})
-  protected String valueLength;
-
-  @Param({"FLOAT_VECTOR", "SPARSE_VECTOR"})
-//  @Param({"FLOAT_VECTOR"})
+  @Param({"DENSE_VECTOR"})
   protected String valueType;
 
-  @Param({"true"})
-//  @Param({"true", "false"})
-  protected boolean assertions;
+  @Param({"2500"})
+  protected int valueLength;
+
+  protected VeniceClusterWrapper cluster;
+  protected DaVinciClient<Integer, GenericRecord> client;
+  protected int[] keys = new int[KEY_COUNT];
+
+  public static void main(String[] args) throws Exception {
+    Options opt = new OptionsBuilder()
+                      .include(DaVinciClientBenchmark.class.getSimpleName())
+                      .addProfiler(GCProfiler.class)
+                      .build();
+    new Runner(opt).run();
+  }
 
   @Setup
   public void setup() throws Exception {
     Utils.thisIsLocalhost();
-    cluster = ServiceFactory.getVeniceCluster(1, 1, 1);
+    cluster = getVeniceCluster(1, 1, 1);
 
     String storeName;
-    if (valueType.equals("FLOAT_VECTOR")) {
-      storeName = buildFloatVectorStore(cluster, Integer.parseInt(valueLength));
+    if (valueType.equals("DENSE_VECTOR")) {
+      storeName = buildDenseVectorStore(cluster);
     } else if (valueType.equals("SPARSE_VECTOR")) {
-      storeName = buildSparseVectorStore(cluster, Integer.parseInt(valueLength));
+      storeName = buildSparseVectorStore(cluster);
     } else {
       throw new VeniceException("Value type " + valueType + " is not supported in benchmark.");
     }
@@ -99,70 +86,85 @@ public class DaVinciClientBenchmark {
     client = getGenericAvroDaVinciClient(storeName, cluster);
     client.subscribeToAllPartitions().get(60, TimeUnit.SECONDS);
 
-    // Trying to close as much as possible of the stuff we don't need to minimize interference...
-    cluster.getVeniceServers().forEach(veniceServerWrapper -> veniceServerWrapper.close());
-    cluster.getVeniceRouters().forEach(veniceRouterWrapper -> veniceRouterWrapper.close());
-    cluster.getVeniceControllers().forEach(veniceControllerWrapper -> veniceControllerWrapper.close());
+    // Close as much as possible of the stuff we don't need, to minimize interference.
+    cluster.getVeniceServers().forEach(VeniceServerWrapper::close);
+    cluster.getVeniceRouters().forEach(VeniceRouterWrapper::close);
+    cluster.getVeniceControllers().forEach(VeniceControllerWrapper::close);
 
-    long key = 11;
-    for (int i = 0; i < queryCount; ++i) {
-      keys[i] = (key * 31) % recordCount;
+    Random random = new Random(11);
+    for (int i = 0; i < KEY_COUNT; ++i) {
+      keys[i] = random.nextInt(RECORD_COUNT);
     }
   }
 
   @TearDown
-  public void teardown() throws InterruptedException {
+  public void cleanup() {
     client.close();
     cluster.close();
   }
 
   @Benchmark
-  @OperationsPerInvocation(queryCount)
-  public void randomReadQueries() throws ExecutionException, InterruptedException {
-    GenericRecord value = null;
-    int intValueLength = Integer.parseInt(valueLength);
-    String fieldName = "value";
-    for (int i = 0; i < queryCount; ++i) {
-      value = client.get(keys[i], value).get();
-      if (assertions) {
-        Assert.assertEquals(((List<Float>)value.get(fieldName)).size(), intValueLength);
-      }
+  @Threads(1)
+  @OperationsPerInvocation(KEY_COUNT)
+  public void testSingleGetHitT1(Blackhole blackhole) throws Exception {
+    runSingleGetHitWorkload(blackhole);
+  }
+
+  @Benchmark
+  @Threads(8)
+  @OperationsPerInvocation(KEY_COUNT)
+  public void testSingleGetHitT8(Blackhole blackhole) throws Exception {
+    runSingleGetHitWorkload(blackhole);
+  }
+
+  protected void runSingleGetHitWorkload(Blackhole blackhole) throws Exception {
+    GenericRecord record = null;
+    for (int i = 0; i < KEY_COUNT; ++i) {
+      record = client.get(keys[i], record).get();
+      blackhole.consume(record);
     }
   }
 
-  public static void main(String[] args) throws Exception {
-    Options opt = new OptionsBuilder()
-        .include(DaVinciClientBenchmark.class.getSimpleName())
-        .addProfiler(GCProfiler.class)
-        .build();
-    new Runner(opt).run();
+  protected String buildDenseVectorStore(VeniceClusterWrapper cluster) throws Exception {
+    Schema schema = Schema.parse(
+            "{" +
+            "  \"namespace\" : \"example.avro\"," +
+            "  \"type\": \"record\"," +
+            "  \"name\": \"DenseVector\"," +
+            "  \"fields\": [" +
+            "     { \"name\": \"value\", \"type\": {\"type\": \"array\", \"items\": \"float\"} }" +
+            "   ]" +
+            "}");
+    GenericRecord record = new GenericData.Record(schema);
+    List<Float> values = new ArrayList<>();
+    for (int i = 0; i < valueLength; i++) {
+      values.add(1.0f * i);
+    }
+    record.put(VALUE_FIELD_NAME, values);
+    return cluster.createStore(RECORD_COUNT, record);
   }
 
-  private String buildFloatVectorStore(VeniceClusterWrapper cluster, int valueSize) throws Exception {
-    Schema schema = Schema.parse(FLOAT_VECTOR_VALUE_SCHEMA);
-    record = new GenericData.Record(schema);
-    List<Float> floatVector = new ArrayList<>();
-    for (int i = 0; i < valueSize; i++) {
-      floatVector.add((float)(i * 1.0));
+
+  protected String buildSparseVectorStore(VeniceClusterWrapper cluster) throws Exception {
+    Schema schema = Schema.parse(
+            "{" +
+            "  \"namespace\" : \"example.avro\"," +
+            "  \"type\": \"record\"," +
+            "  \"name\": \"SparseVector\"," +
+            "  \"fields\": [" +
+            "     { \"name\": \"index\", \"type\": {\"type\": \"array\", \"items\": \"int\"} }," +
+            "     { \"name\": \"value\", \"type\": {\"type\": \"array\", \"items\": \"float\"} }" +
+            "   ]" +
+            "}");
+    GenericRecord record = new GenericData.Record(schema);
+    List<Integer> indices = new ArrayList<>();
+    List<Float> values = new ArrayList<>();
+    for (int i = 0; i < valueLength; i++) {
+      indices.add(i);
+      values.add(1.0f * i);
     }
-    record.put("value", floatVector);
-
-    return cluster.createStore(recordCount, record);
-  }
-
-
-  private String buildSparseVectorStore(VeniceClusterWrapper cluster, int valueSize) throws Exception {
-    Schema schema = Schema.parse(SPARSE_VECTOR_VALUE_SCHEMA);
-    record = new GenericData.Record(schema);
-    List<Integer> sparseVectorIndies = new ArrayList<>();
-    List<Float> sparseVectorValues = new ArrayList<>();
-    for (int i = 0; i < valueSize; i++) {
-      sparseVectorIndies.add(i);
-      sparseVectorValues.add((float)(i * 1.0));
-    }
-    record.put("index", sparseVectorIndies);
-    record.put("value", sparseVectorValues);
-
-    return cluster.createStore(recordCount, record);
+    record.put("index", indices);
+    record.put(VALUE_FIELD_NAME, values);
+    return cluster.createStore(RECORD_COUNT, record);
   }
 }
