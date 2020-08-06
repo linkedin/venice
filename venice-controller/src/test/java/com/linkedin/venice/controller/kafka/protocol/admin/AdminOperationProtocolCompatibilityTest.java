b6a437cb379c4f4236ace7f42aaba1c92f79ffbe
Gaojie Liu <goliu@linkedin.com>
2020-08-06 13:43:15 -0700

    Fixed the bad latest schema of AdminOperations
    RB=2222822
    BUG=VENG-6372
    R=zpolicze,smaji,xnma,yurzhou,mnhuang,xyin,anaberez,nithakka,fvillega,jlliu,zfan,shuhliu,sdwu
    A=yurzhou,mnhuang

--- venice-controller/src/test/java/com/linkedin/venice/controller/kafka/protocol/admin/AdminOperationProtocolCompatibilityTest.java
+++ venice-controller/src/test/java/com/linkedin/venice/controller/kafka/protocol/admin/AdminOperationProtocolCompatibilityTest.java
@@ -2,8 +2,12 @@ package com.linkedin.venice.controller.kafka.protocol.admin;
 
 import com.linkedin.venice.controller.kafka.protocol.serializer.AdminOperationSerializer;
 import com.linkedin.venice.schema.avro.SchemaCompatibility;
+import java.util.Collections;
 import java.util.Map;
 import org.apache.avro.Schema;
+import org.apache.avro.SchemaValidationException;
+import org.apache.avro.SchemaValidator;
+import org.apache.avro.SchemaValidatorBuilder;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -16,6 +20,9 @@ public class AdminOperationProtocolCompatibilityTest {
 
     Assert.assertNotNull(schemaMap.containsKey(latestSchemaId), "The latest schema should exist!");
 
+    SchemaValidatorBuilder schemaValidatorBuilder = new SchemaValidatorBuilder();
+    SchemaValidator schemaValidator = schemaValidatorBuilder.canReadStrategy().validateAll();
+
     Schema latestSchema = schemaMap.get(latestSchemaId);
     schemaMap.forEach( (schemaId, schema) -> {
       if (schemaId == latestSchemaId) {
@@ -23,10 +30,26 @@ public class AdminOperationProtocolCompatibilityTest {
       }
       SchemaCompatibility.SchemaPairCompatibility backwardCompatibility =
           SchemaCompatibility.checkReaderWriterCompatibility(latestSchema, schema);
+      String failMessage = "Older admin operation protocol with schema id: " + schemaId + ", schema: " + schema.toString(true)
+          + " is not compatible with the latest admin operation protocol with schema id: " + latestSchemaId
+          + ", schema: " + latestSchema.toString(true);
       Assert.assertEquals(backwardCompatibility.getType(), SchemaCompatibility.SchemaCompatibilityType.COMPATIBLE,
-          "Older admin operation protocol with schema id: " + schemaId + ", schema: " + schema.toString(true)
-              + " is not compatible with the latest admin operation protocol with schema id: " + latestSchemaId
-              + ", schema: " + latestSchema.toString(true));
+          failMessage);
+
+      /**
+       * Validate the compatibility between the latest schema and each historical schema by {@link SchemaValidator} since
+       * it could tell whether the schema is a valid schema or not.
+       * For example, if we specify a wrong default value: "-1" (string type) to a field with "Long" type, the above
+       * compatibility check will still pass with a warning message, but the {@link SchemaValidator} will fail when encountering
+       * such kind of invalid schema
+       */
+      try {
+        schemaValidator.validate(latestSchema, Collections.singletonList(schema));
+      } catch (SchemaValidationException e) {
+        Assert.fail(failMessage);
+      } catch (Exception e) {
+        Assert.fail("Received schema validation exception, and please check the content of schema with ids: " + latestSchemaId + " or " + schemaId, e);
+      }
     });
   }
 }
