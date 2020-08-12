e571b31be76da70c74ef4524b7e611f150ec2ba1
Shuhui Liu <shuhliu@linkedin.com>
2020-08-12 09:31:28 -0700

    [common] Add the right number of padding bytes in front of the serialization
    Today for L/F hybrid store whose value needs to be chunked, when leader producer
    writes the chunked value manifest to storage engine, it overrides the first 4
    bytes with schema id. However, the serializer didn't add enough padding at the
    beginning so that 2 bytes of actual manifest is overriden.
    
    RB=2231639
    G=voldemort-reviewers
    R=xyin,zfan,jlliu,mnhuang,goliu,fvillega,sdwu,zpolicze,xnma,anaberez,nithakka,yurzhou,smaji
    A=zpolicze,fvillega

---
+++ venice-common/src/test/java/com/linkedin/venice/serialization/avro/ChunkedValueManifestSerializerTest.java
@@ -0,0 +1,37 @@
+package com.linkedin.venice.serialization.avro;
+
+import com.linkedin.venice.storage.protocol.ChunkedValueManifest;
+import com.linkedin.venice.utils.ByteUtils;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class ChunkedValueManifestSerializerTest {
+  private static final ChunkedValueManifestSerializer CHUNKED_VALUE_MANIFEST_SERIALIZER = new ChunkedValueManifestSerializer(false);
+
+  @Test
+  public void testSerializerWithHeader() {
+    byte[] keyWithChunkIdSuffix = "keyChunkedKeySuffix".getBytes();
+
+    ChunkedValueManifest chunkedValueManifest = new ChunkedValueManifest();
+    chunkedValueManifest.keysWithChunkIdSuffix = new ArrayList<>();
+    chunkedValueManifest.keysWithChunkIdSuffix.add(ByteBuffer.wrap(keyWithChunkIdSuffix));
+    chunkedValueManifest.schemaId = 1;
+    chunkedValueManifest.size = 1;
+    byte[] serialized = CHUNKED_VALUE_MANIFEST_SERIALIZER.serialize("", chunkedValueManifest);
+    ChunkedValueManifest deserialized = CHUNKED_VALUE_MANIFEST_SERIALIZER.deserialize(serialized,
+        AvroProtocolDefinition.CHUNKED_VALUE_MANIFEST.getCurrentProtocolVersion());
+
+    Assert.assertEquals(deserialized.keysWithChunkIdSuffix.size(), 1);
+    Assert.assertTrue(Arrays.equals(deserialized.keysWithChunkIdSuffix.get(0).array(), keyWithChunkIdSuffix));
+    Assert.assertEquals(deserialized.schemaId, 1);
+    Assert.assertEquals(deserialized.size, 1);
+    // Assert that serializer with header adds desired padding at the beginning
+    for (int i = 0; i < ByteUtils.SIZE_OF_INT; i++) {
+      Assert.assertEquals(serialized[i], 0);
+    }
+  }
+}
