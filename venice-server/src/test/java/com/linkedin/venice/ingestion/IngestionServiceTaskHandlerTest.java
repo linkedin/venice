504cd63903775defd9b5e00de08745bcfee427c3
Alexei Naberezhnov <anaberezhnov@linkedin.com>
2020-09-22 03:51:16 -0700

    [test] fix broken test IngestionServiceTaskHandlerTest
    RB=2294241
    BUG=VENG-6528
    G=voldemort-reviewers
    R=xhao,ssen,jlliu,xyin,mnhuang,smaji,xnma,sdwu,goliu,yurzhou,zpolicze,fvillega,shuhliu,nithakka
    A=jlliu,goliu

--- venice-server/src/test/java/com/linkedin/venice/ingestion/IngestionServiceTaskHandlerTest.java
+++ venice-server/src/test/java/com/linkedin/venice/ingestion/IngestionServiceTaskHandlerTest.java
@@ -2,7 +2,11 @@ package com.linkedin.venice.ingestion;
 
 import com.linkedin.venice.ingestion.handler.IngestionServiceTaskHandler;
 import com.linkedin.venice.ingestion.protocol.InitializationConfigs;
+import com.linkedin.venice.integration.utils.ServiceFactory;
+import com.linkedin.venice.integration.utils.ZkServerWrapper;
 import com.linkedin.venice.meta.IngestionAction;
+import com.linkedin.venice.utils.TestUtils;
+
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.http.DefaultFullHttpRequest;
@@ -11,31 +15,39 @@ import io.netty.handler.codec.http.FullHttpResponse;
 import io.netty.handler.codec.http.HttpMethod;
 import io.netty.handler.codec.http.HttpResponseStatus;
 import io.netty.handler.codec.http.HttpVersion;
-import java.util.HashMap;
+
 import org.mockito.ArgumentCaptor;
-import org.mockito.Mockito;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.HashMap;
+
+import static com.linkedin.venice.ConfigKeys.*;
 import static org.mockito.Mockito.*;
 
 
 public class IngestionServiceTaskHandlerTest {
-
   @Test
   public void testInitializationConfig() throws Exception {
-    String path = "/" + IngestionAction.INIT;
-    InitializationConfigs initializationConfigs = new InitializationConfigs();
-    initializationConfigs.aggregatedConfigs = new HashMap<>();
-    byte[] content = IngestionServiceTaskHandler.initializationConfigSerializer.serialize(null, initializationConfigs);
-    ChannelHandlerContext mockContext = Mockito.mock(ChannelHandlerContext.class);
-    FullHttpRequest msg = new DefaultFullHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.POST, path, Unpooled.wrappedBuffer(content));
-    ArgumentCaptor<FullHttpResponse> argumentCaptor = ArgumentCaptor.forClass(FullHttpResponse.class);
+    try (ZkServerWrapper zk = ServiceFactory.getZkServer()) {
+      String path = "/" + IngestionAction.INIT;
+      InitializationConfigs initializationConfigs = new InitializationConfigs();
+      initializationConfigs.aggregatedConfigs = new HashMap<>();
+      initializationConfigs.aggregatedConfigs.put(CLUSTER_NAME, TestUtils.getUniqueString());
+      initializationConfigs.aggregatedConfigs.put(ZOOKEEPER_ADDRESS, zk.getAddress());
+      initializationConfigs.aggregatedConfigs.put(KAFKA_BOOTSTRAP_SERVERS, TestUtils.getUniqueString());
+      initializationConfigs.aggregatedConfigs.put(KAFKA_ZK_ADDRESS, TestUtils.getUniqueString());
+      byte[] content = IngestionUtils.initializationConfigSerializer.serialize(null, initializationConfigs);
+      ChannelHandlerContext mockContext = mock(ChannelHandlerContext.class);
+      FullHttpRequest msg = new DefaultFullHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.POST, path, Unpooled.wrappedBuffer(content));
+      ArgumentCaptor<FullHttpResponse> argumentCaptor = ArgumentCaptor.forClass(FullHttpResponse.class);
 
-    IngestionServiceTaskHandler ingestionServiceTaskHandler = new IngestionServiceTaskHandler(null);
-    ingestionServiceTaskHandler.channelRead(mockContext, msg);
-    verify(mockContext).writeAndFlush(argumentCaptor.capture());
-    FullHttpResponse response = argumentCaptor.getValue();
-    Assert.assertEquals(response.status(), HttpResponseStatus.OK);
+      IngestionService ingestionService = mock(IngestionService.class);
+      IngestionServiceTaskHandler ingestionServiceTaskHandler = new IngestionServiceTaskHandler(ingestionService);
+      ingestionServiceTaskHandler.channelRead(mockContext, msg);
+      verify(mockContext).writeAndFlush(argumentCaptor.capture());
+      FullHttpResponse response = argumentCaptor.getValue();
+      Assert.assertEquals(response.status(), HttpResponseStatus.OK);
+    }
   }
 }
