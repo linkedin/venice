11220a43b231a9d3630b6248517befb8944d9b80
Min Huang <mnhuang@linkedin.com>
2020-11-09 11:29:54 -0800

    [Server] Added unit tests for building state model
    Added unit tests for both L/F state model and O/O state model to ensure
    that building state model should remain as simple as possible; we shouldn't
    bind state model creation with the healthiness of other components like ZK.
    
    RB=2361109
    BUG=VENG-6643
    G=voldemort-reviewers
    R=xyin,shuhliu,nithakka,xhao,xnma,goliu,jlliu,smaji,zpolicze,sdwu,ssen,yurzhou,anaberez,fvillega
    A=xnma

---
+++ venice-server/src/test/java/com/linkedin/venice/helix/LeaderFollowerParticipantModelFactoryTest.java
@@ -0,0 +1,87 @@
+package com.linkedin.venice.helix;
+
+import com.linkedin.venice.config.VeniceServerConfig;
+import com.linkedin.venice.config.VeniceStoreConfig;
+import com.linkedin.venice.kafka.consumer.StoreIngestionService;
+import com.linkedin.venice.meta.ReadOnlyStoreRepository;
+import com.linkedin.venice.meta.Store;
+import com.linkedin.venice.meta.Version;
+import com.linkedin.venice.server.VeniceConfigLoader;
+import com.linkedin.venice.storage.StorageService;
+import com.linkedin.venice.utils.DaemonThreadFactory;
+import com.linkedin.venice.utils.HelixUtils;
+import java.util.Optional;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import org.apache.helix.NotificationContext;
+import org.apache.helix.model.Message;
+import org.apache.helix.participant.statemachine.StateModel;
+import org.mockito.Mockito;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+
+public class LeaderFollowerParticipantModelFactoryTest {
+  private StoreIngestionService mockStoreIngestionService;
+  private StorageService mockStorageService;
+  private VeniceConfigLoader mockConfigLoader;
+  private VeniceServerConfig mockServerConfig;
+  private VeniceStoreConfig mockStoreConfig;
+  private ReadOnlyStoreRepository mockReadOnlyStoreRepository;
+  private Store mockStore;
+  private int testPartition = 0;
+  private String resourceName = Version.composeKafkaTopic("testStore", 1);
+
+  private Message mockMessage;
+
+  private LeaderFollowerParticipantModelFactory factory;
+  private ExecutorService executorService;
+
+  @BeforeClass
+  void setup() {
+    executorService = Executors.newCachedThreadPool(new DaemonThreadFactory("venice-unittest"));
+  }
+
+  @AfterClass
+  void cleanup() {
+    executorService.shutdownNow();
+  }
+
+  @BeforeMethod
+  public void setupTestCase() {
+    mockStoreIngestionService = Mockito.mock(StoreIngestionService.class);
+    mockStorageService = Mockito.mock(StorageService.class);
+    mockConfigLoader = Mockito.mock(VeniceConfigLoader.class);
+    mockServerConfig = Mockito.mock(VeniceServerConfig.class);
+    mockStoreConfig = Mockito.mock(VeniceStoreConfig.class);
+    mockReadOnlyStoreRepository = Mockito.mock(ReadOnlyStoreRepository.class);
+    mockStore = Mockito.mock(Store.class);
+    Mockito.when(mockConfigLoader.getVeniceServerConfig()).thenReturn(mockServerConfig);
+    Mockito.when(mockConfigLoader.getStoreConfig(resourceName)).thenReturn(mockStoreConfig);
+    Mockito.when(mockReadOnlyStoreRepository.getStore(Version.parseStoreFromKafkaTopicName(resourceName)))
+        .thenReturn(mockStore);
+    Mockito.when(mockStore.getBootstrapToOnlineTimeoutInHours()).thenReturn(Store.BOOTSTRAP_TO_ONLINE_TIMEOUT_IN_HOURS);
+
+    mockMessage = Mockito.mock(Message.class);
+    Mockito.when(mockMessage.getResourceName()).thenReturn(resourceName);
+
+    factory = new LeaderFollowerParticipantModelFactory(
+        mockStoreIngestionService,
+        mockStorageService,
+        mockConfigLoader,
+        this.executorService,
+        mockReadOnlyStoreRepository, Optional.empty(), null);
+  }
+
+  @Test
+  public void testLeaderFollowerStateModelCanBeBuiltWithoutErrors() {
+    /**
+     * No exception is expected from building a simple state model; building a state model should remain as simple
+     * as possible, we shouldn't bind state model creation with the healthiness of other components like ZK.
+     */
+    String partitionName = HelixUtils.getPartitionName(resourceName, testPartition);
+    StateModel stateModel = factory.createNewStateModel(resourceName, partitionName);
+  }
+}
