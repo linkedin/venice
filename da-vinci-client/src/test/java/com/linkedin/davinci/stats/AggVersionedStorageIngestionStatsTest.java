1da76d375d7f5912e938fdbc58457310aae07e25
Xun Yin <xyin@linkedin.com>
2020-12-18 16:20:57 -0800

    [da-vinci][common] Fix versioned ingestion metrics for zk shared system stores
    1. Unlike regular Venice stores, the zk shared system stores don't have individual change events triggered
    for the actual ingestion. Therefore, versioned metrics never got a chance to set the versioned stats
    for current, backend and future.
    
    2. This is a temporary solution to set the versioned stats during start consumption when also providing the
    IngestionTask to set the AggVersionedStorageIngestionStats.
    
    RB=2408860
    BUG=VENG-6766
    G=voldemort-reviewers
    R=nithakka,jlliu,goliu,fvillega,sdwu,shuhliu,xhao,xnma,yurzhou,ssen,mnhuang,zpolicze,smaji,anaberez
    A=sdwu

---
+++ da-vinci-client/src/test/java/com/linkedin/davinci/stats/AggVersionedStorageIngestionStatsTest.java
@@ -0,0 +1,65 @@
+package com.linkedin.davinci.stats;
+
+import com.linkedin.davinci.kafka.consumer.StoreIngestionTask;
+import com.linkedin.venice.common.VeniceSystemStoreUtils;
+import com.linkedin.venice.meta.OfflinePushStrategy;
+import com.linkedin.venice.meta.PersistenceType;
+import com.linkedin.venice.meta.ReadOnlyStoreRepository;
+import com.linkedin.venice.meta.ReadStrategy;
+import com.linkedin.venice.meta.RoutingStrategy;
+import com.linkedin.venice.meta.Store;
+import com.linkedin.venice.meta.Version;
+import com.linkedin.venice.meta.ZKStore;
+import com.linkedin.venice.tehuti.MockTehutiReporter;
+import io.tehuti.metrics.MetricsRepository;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import static org.mockito.Mockito.*;
+
+
+public class AggVersionedStorageIngestionStatsTest {
+
+  private AggVersionedStorageIngestionStats versionedIngestionStats;
+  private MetricsRepository metricsRepository;
+  private MockTehutiReporter reporter;
+
+  private ReadOnlyStoreRepository mockStoreRepository;
+
+  @BeforeTest
+  public void setup() {
+    metricsRepository = new MetricsRepository();
+    reporter = new MockTehutiReporter();
+    metricsRepository.addReporter(reporter);
+
+    mockStoreRepository = mock(ReadOnlyStoreRepository.class);
+    versionedIngestionStats = new AggVersionedStorageIngestionStats(metricsRepository, mockStoreRepository);
+  }
+
+  @Test
+  public void testWithMetadataStoreIngestion() {
+    String regularStoreName = "test-store";
+    String metadataStoreName = VeniceSystemStoreUtils.getMetadataStoreName(regularStoreName);
+    String zkSharedStoreName = VeniceSystemStoreUtils.getSharedZkNameForMetadataStore("test-cluster");
+    Store zkSharedStore = getMockStore(zkSharedStoreName);
+    zkSharedStore.addVersion(new Version(zkSharedStoreName, 1, ""));
+    zkSharedStore.setCurrentVersion(1);
+    doReturn(zkSharedStore).when(mockStoreRepository).getStore(zkSharedStoreName);
+    doReturn(zkSharedStore).when(mockStoreRepository).getStore(metadataStoreName);
+    StoreIngestionTask mockMetadataStoreIngestionTask = mock(StoreIngestionTask.class);
+    doReturn(zkSharedStore).when(mockMetadataStoreIngestionTask).getIngestionStore();
+    doReturn(true).when(mockMetadataStoreIngestionTask).isHybridMode();
+    doReturn(100L).when(mockMetadataStoreIngestionTask).getRealTimeBufferOffsetLag();
+    doReturn(0L).when(mockMetadataStoreIngestionTask).getOffsetLagThreshold();
+    versionedIngestionStats.setIngestionTask(Version.composeKafkaTopic(metadataStoreName, 1), mockMetadataStoreIngestionTask);
+    // Expected to see v1's records consumed on future reporter
+    Assert.assertEquals(reporter.query("." + metadataStoreName + "_current--rt_topic_offset_lag.IngestionStatsGauge").value(),
+        100d);
+  }
+
+  private Store getMockStore(String storeName) {
+    return new ZKStore(storeName, "test", 0, PersistenceType.ROCKS_DB,
+        RoutingStrategy.CONSISTENT_HASH, ReadStrategy.ANY_OF_ONLINE, OfflinePushStrategy.WAIT_ALL_REPLICAS);
+  }
+}
