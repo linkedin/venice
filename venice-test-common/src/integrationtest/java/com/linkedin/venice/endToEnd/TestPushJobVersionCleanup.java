87d86c5a4857e2f1980dd0956be55fcdce078946
Subhadeep Sen <ssen@linkedin.com>
2021-02-15 22:44:20 -0800

    [server] Fix false ingestion_failure reporting
    With "participant.meesage.store" being enanbled, controller writes a KILL_PUSH_JOB
    message to the participant store when a store version needs to be retired/killed.
    In the server the ParticipantStoreConsumptionTask runs every 60 seconds to cleanup
    StoreIngestionTasks for all killed versions. Because of this batch processing logic
    it does not cleanup/close the ingestion tasks in SNs immediately. In the meantime
    controller also deletes the related version info in ZK and kafka VT topic. With dedicated
    kafka consumer in each consumerPoll() we check if the version is present or not and throw
    VeniceException which gets reported as ingestion_failure metric later on and
    the ingestion taks gets closed. This is not really an ingestion_failure
    and should not be reported as such as it may create false alarm in
    monitoring dashboards.
    
    Since this is a normal flow of operation we can throw a more appropriate
    excetption which will get handled correctly and not falesly report the metric.
    
    Also sdding a new integration test to make sure in such cases we properly
    cleanup/close the ingestion task and don't report ingestion_failure metrics.
    
    RB=2470191
    BUG=VENG-7101
    G=voldemort-reviewers
    R=xhao,nithakka,xnma,fvillega,yurzhou,mnhuang,anaberez,shuhliu,jlliu,zpolicze,goliu,xyin,smaji,sdwu
    A=xnma

---
+++ venice-test-common/src/integrationtest/java/com/linkedin/venice/endToEnd/TestPushJobVersionCleanup.java
@@ -0,0 +1,136 @@
+package com.linkedin.venice.endToEnd;
+
+import com.linkedin.venice.controllerapi.UpdateStoreQueryParams;
+import com.linkedin.venice.hadoop.KafkaPushJob;
+import com.linkedin.venice.integration.utils.MirrorMakerWrapper;
+import com.linkedin.venice.integration.utils.ServiceFactory;
+import com.linkedin.venice.integration.utils.VeniceControllerWrapper;
+import com.linkedin.venice.integration.utils.VeniceMultiClusterWrapper;
+import com.linkedin.venice.integration.utils.VeniceTwoLayerMultiColoMultiClusterWrapper;
+import com.linkedin.venice.meta.Store;
+import com.linkedin.venice.server.VeniceServer;
+import com.linkedin.venice.utils.TestPushUtils;
+import com.linkedin.venice.utils.TestUtils;
+import com.linkedin.venice.utils.VeniceProperties;
+import java.io.File;
+import java.util.List;
+import java.util.Optional;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
+import org.apache.avro.Schema;
+import org.apache.log4j.Logger;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static com.linkedin.davinci.store.rocksdb.RocksDBServerConfig.*;
+import static com.linkedin.venice.ConfigKeys.*;
+import static com.linkedin.venice.hadoop.KafkaPushJob.*;
+import static com.linkedin.venice.utils.TestPushUtils.*;
+
+
+public class TestPushJobVersionCleanup {
+  private static final Logger logger = Logger.getLogger(TestPushJobWithNativeReplication.class);
+  private static final int TEST_TIMEOUT = 120_000; // ms
+
+  private static final int NUMBER_OF_CHILD_DATACENTERS = 1;
+  private static final int NUMBER_OF_CLUSTERS = 1;
+  private static final String[] CLUSTER_NAMES =
+      IntStream.range(0, NUMBER_OF_CLUSTERS).mapToObj(i -> "venice-cluster" + i).toArray(String[]::new);
+      // ["venice-cluster0", "venice-cluster1", ...];
+
+  private List<VeniceMultiClusterWrapper> childDatacenters;
+  private List<VeniceControllerWrapper> parentControllers;
+  private VeniceTwoLayerMultiColoMultiClusterWrapper multiColoMultiClusterWrapper;
+
+  @BeforeClass(alwaysRun = true)
+  public void setUp() {
+    Properties serverProperties = new Properties();
+    serverProperties.put(SERVER_SHARED_CONSUMER_POOL_ENABLED, "false");
+    serverProperties.setProperty(PARTICIPANT_MESSAGE_CONSUMPTION_DELAY_MS, "60000");
+
+    Properties controllerProps = new Properties();
+    controllerProps.put(ADMIN_HELIX_MESSAGING_CHANNEL_ENABLED, false);
+    controllerProps.put(PARTICIPANT_MESSAGE_STORE_ENABLED, true);
+
+    multiColoMultiClusterWrapper =
+        ServiceFactory.getVeniceTwoLayerMultiColoMultiClusterWrapper(NUMBER_OF_CHILD_DATACENTERS, NUMBER_OF_CLUSTERS, 1,
+            1, 1, 1, 1, Optional.of(new VeniceProperties(controllerProps)), Optional.of(controllerProps),
+            Optional.of(new VeniceProperties(serverProperties)), false, MirrorMakerWrapper.DEFAULT_TOPIC_WHITELIST);
+    childDatacenters = multiColoMultiClusterWrapper.getClusters();
+    parentControllers = multiColoMultiClusterWrapper.getParentControllers();
+  }
+
+  @AfterClass(alwaysRun = true)
+  public void cleanUp() {
+    multiColoMultiClusterWrapper.close();
+  }
+
+  @Test(timeOut = TEST_TIMEOUT)
+  public void testMultipleBatchPushWithVersionCleanup() throws Exception {
+    String clusterName = CLUSTER_NAMES[0];
+    File inputDir = getTempDataDirectory();
+    Schema recordSchema = TestPushUtils.writeSimpleAvroFileWithUserSchema(inputDir, true, 50);
+    String inputDirPath = "file:" + inputDir.getAbsolutePath();
+    String storeName = TestUtils.getUniqueString("store");
+    VeniceControllerWrapper parentController =
+        parentControllers.stream().filter(c -> c.isMasterController(clusterName)).findAny().get();
+    Properties props = defaultH2VProps(parentController.getControllerUrl(), inputDirPath, storeName);
+    props.put(SEND_CONTROL_MESSAGES_DIRECTLY, true);
+    String keySchemaStr = recordSchema.getField(props.getProperty(KafkaPushJob.KEY_FIELD_PROP)).schema().toString();
+    String valueSchemaStr = recordSchema.getField(props.getProperty(KafkaPushJob.VALUE_FIELD_PROP)).schema().toString();
+
+    UpdateStoreQueryParams updateStoreParams =
+        new UpdateStoreQueryParams().setStorageQuotaInByte(Store.UNLIMITED_STORAGE_QUOTA).setPartitionCount(2);
+    createStoreForJob(clusterName, keySchemaStr, valueSchemaStr, props, updateStoreParams).close();
+
+    VeniceMultiClusterWrapper childDataCenter = childDatacenters.get(NUMBER_OF_CHILD_DATACENTERS - 1);
+    VeniceServer server = childDataCenter.getClusters().get(clusterName).getVeniceServers().get(0).getVeniceServer();
+
+    /**
+     * Run 3 push jobs sequentially and at the end verify the first version is cleaned up properly without any ingestion_failure metrics being reported.
+     */
+    try (KafkaPushJob job = new KafkaPushJob("Test push job 1", props)) {
+      job.run();
+    }
+    TestUtils.waitForNonDeterministicAssertion(30, TimeUnit.SECONDS, () -> {
+      // Current version should become 1
+      for (int version : parentController.getVeniceAdmin()
+          .getCurrentVersionsForMultiColos(clusterName, storeName)
+          .values()) {
+        Assert.assertEquals(version, 1);
+      }
+    });
+
+    try (KafkaPushJob job = new KafkaPushJob("Test push job 2", props)) {
+      job.run();
+    }
+    TestUtils.waitForNonDeterministicAssertion(30, TimeUnit.SECONDS, () -> {
+      // Current version should become 2
+      for (int version : parentController.getVeniceAdmin()
+          .getCurrentVersionsForMultiColos(clusterName, storeName)
+          .values()) {
+        Assert.assertEquals(version, 2);
+      }
+    });
+
+    try (KafkaPushJob job = new KafkaPushJob("Test push job 3", props)) {
+      job.run();
+    }
+    TestUtils.waitForNonDeterministicAssertion(30, TimeUnit.SECONDS, () -> {
+      // Current version should become 3
+      for (int version : parentController.getVeniceAdmin()
+          .getCurrentVersionsForMultiColos(clusterName, storeName)
+          .values()) {
+        Assert.assertEquals(version, 3);
+      }
+    });
+
+    //There should not be any ingestion_failure.
+    Assert.assertEquals(server.getMetricsRepository().getMetric("." + storeName + "--ingestion_failure.Count").value(),
+        0.0);
+  }
+}
