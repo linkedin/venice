linkedin/venice

commit 7ec4673e89f09a1e5523f167336f5339e9790ae6

[server] Add offset lag measurement for Leader replicas.
In native replication offset lag measurement for leader replica is
very useful to measure. It may provide insights into how fast/slow remote
consumption is working. Made following changes
1. Measuring offset lag from remote kafka broker needs a java based admin
   client. We can't use scala based admin client as it needs ZK address
   which is not available in SN ingestion task and even if we make it we
   shouldn't use because it will create cross-colo ZK connection.
2. Also enabling java based admin client in server by default.
3. Introducing a seperate offsetCache for remote kafka broker in CachedKafkaMetadataGetter
   to keep this cache seperate from local kafka offset cache.

RB=2493769
BUG=VENG-7245
G=voldemort-reviewers
R=lcli,mnhuang,nithakka,shuhliu,goliu,xhao,fvillega,jlliu,anaberez,xnma,xyin,zpolicze,smaji,sdwu,yurzhou
A=mnhuang

da-vinci-client/src/main/java/com/linkedin/davinci/kafka/consumer/LeaderFollowerStoreIngestionTask.java
@@ -19,6 +19,7 @@ import com.linkedin.venice.exceptions.validation.FatalDataValidationException;
 import com.linkedin.venice.guid.GuidUtils;
 import com.linkedin.venice.kafka.KafkaClientFactory;
 import com.linkedin.venice.kafka.TopicManagerRepository;
+import com.linkedin.venice.kafka.admin.ScalaAdminUtils;
 import com.linkedin.venice.kafka.consumer.KafkaConsumerWrapper;
 import com.linkedin.venice.kafka.protocol.ControlMessage;
 import com.linkedin.venice.kafka.protocol.KafkaMessageEnvelope;
@@ -1391,6 +1392,56 @@ public class LeaderFollowerStoreIngestionTask extends StoreIngestionTask {
   }
 
   @Override
+  public long getLeaderOffsetLag() {
+    /**
+     * If the admin client is scala based then getting offset from remote kafka is difficult as it needs the
+     * ZK address of the kafka broker. So this metric will not work in this case.
+     */
+    if (serverConfig.getKafkaAdminClass().contains(ScalaAdminUtils.class.getName())) {
+      return 0;
+    }
+
+    Optional<StoreVersionState> svs = storageMetadataService.getStoreVersionState(kafkaVersionTopic);
+    if (!svs.isPresent()) {
+      /**
+       * Store version metadata is created for the first time when the first START_OF_PUSH message is processed;
+       * however, the ingestion stat is created the moment an ingestion task is created, so there is a short time
+       * window where there is no version metadata, which is not an error.
+       */
+      return 0;
+    }
+
+    if (partitionConsumptionStateMap.isEmpty()) {
+      /**
+       * Partition subscription happens after the ingestion task and stat are created, it's not an error.
+       */
+      return 0;
+    }
+
+    long offsetLag = partitionConsumptionStateMap.values()
+        .stream()
+        .filter(pcs -> pcs.getLeaderState().equals(LEADER))
+        //the lag is (latest VT offset - consumed VT offset)
+        .mapToLong((pcs) -> {
+          String currentLeaderTopic = pcs.getOffsetRecord().getLeaderTopic();
+          if (currentLeaderTopic == null || currentLeaderTopic.isEmpty()) {
+            currentLeaderTopic = kafkaVersionTopic;
+          }
+          if (!pcs.consumeRemotely()) {
+            return (cachedKafkaMetadataGetter.getOffset(currentLeaderTopic, pcs.getPartition()) - 1)
+                - pcs.getOffsetRecord().getOffset();
+          } else {
+            return
+                (cachedKafkaMetadataGetter.getOffsetFromRemoteKafka(nativeReplicationSourceAddress, currentLeaderTopic,
+                    pcs.getPartition()) - 1) - pcs.getOffsetRecord().getOffset();
+          }
+        })
+        .sum();
+
+    return minZeroLag(offsetLag);
+  }
+
+  @Override
   public long getFollowerOffsetLag() {
     Optional<StoreVersionState> svs = storageMetadataService.getStoreVersionState(kafkaVersionTopic);
     if (!svs.isPresent()) {
@@ -1421,6 +1472,8 @@ public class LeaderFollowerStoreIngestionTask extends StoreIngestionTask {
 
     return minZeroLag(offsetLag);
   }
+
+
 
   /**
    * Unsubscribe from all the topics being consumed for the partition in partitionConsumptionState

da-vinci-client/src/main/java/com/linkedin/davinci/config/VeniceServerConfig.java
@@ -3,6 +3,7 @@ package com.linkedin.davinci.config;
 import com.linkedin.davinci.store.rocksdb.RocksDBServerConfig;
 import com.linkedin.venice.exceptions.ConfigurationException;
 import com.linkedin.venice.exceptions.VeniceException;
+import com.linkedin.venice.kafka.admin.KafkaAdminClient;
 import com.linkedin.venice.kafka.admin.ScalaAdminUtils;
 import com.linkedin.venice.meta.IngestionMode;
 import com.linkedin.venice.utils.VeniceProperties;
@@ -299,7 +300,7 @@ public class VeniceServerConfig extends VeniceClusterConfig {
     }
 
     restServiceEpollEnabled = serverProperties.getBoolean(SERVER_REST_SERVICE_EPOLL_ENABLED, false);
-    kafkaAdminClass = serverProperties.getString(KAFKA_ADMIN_CLASS, ScalaAdminUtils.class.getName());
+    kafkaAdminClass = serverProperties.getString(KAFKA_ADMIN_CLASS, KafkaAdminClient.class.getName());
     // Disable it by default, and when router connection warming is enabled, we need to adjust this config.
     routerConnectionWarmingDelayMs = serverProperties.getLong(SERVER_ROUTER_CONNECTION_WARMING_DELAY_MS, 0);
     sharedConsumerPoolEnabled = serverProperties.getBoolean(SERVER_SHARED_CONSUMER_POOL_ENABLED, false);

da-vinci-client/src/main/java/com/linkedin/davinci/kafka/consumer/OnlineOfflineStoreIngestionTask.java
@@ -263,6 +263,11 @@ public class OnlineOfflineStoreIngestionTask extends StoreIngestionTask {
   }
 
   @Override
+  public long getLeaderOffsetLag() {
+    return StatsErrorCode.METRIC_ONLY_AVAILABLE_FOR_LEADER_FOLLOWER_STORES.code;
+  }
+
+  @Override
   public long getFollowerOffsetLag() {
     return StatsErrorCode.METRIC_ONLY_AVAILABLE_FOR_LEADER_FOLLOWER_STORES.code;
   }

da-vinci-client/src/main/java/com/linkedin/davinci/kafka/consumer/StoreIngestionTask.java
@@ -1719,6 +1719,8 @@ public abstract class StoreIngestionTask implements Runnable, Closeable {
     return minZeroLag(offsetLag);
   }
 
+  public abstract long getLeaderOffsetLag();
+
   /**
    * Measure the offset lag between follower and leader
    */
@@ -2740,6 +2742,7 @@ public abstract class StoreIngestionTask implements Runnable, Closeable {
     private final Map<String, Pair<Long, Boolean>> topicExistenceCache = new VeniceConcurrentHashMap<>();
     private final Map<Pair<String, Integer>, Pair<Long, Long>> offsetCache = new VeniceConcurrentHashMap<>();
     private final Map<Pair<String, Integer>, Pair<Long, Long>> lastProducerTimestampCache = new VeniceConcurrentHashMap<>();
+    private final Map<Pair<String, Integer>, Pair<Long, Long>> remoteOffsetCache = new VeniceConcurrentHashMap<>();
 
     CachedKafkaMetadataGetter(TopicManagerRepository topicManagerRepository, long timeToLiveMs) {
       this.ttl = MILLISECONDS.toNanos(timeToLiveMs);
@@ -2763,6 +2766,21 @@ public abstract class StoreIngestionTask implements Runnable, Closeable {
       entry = offsetCache.compute(key, (k, oldValue) ->
           (oldValue != null && oldValue.getFirst() > now) ?
               oldValue : new Pair<>(now + ttl, topicManagerRepository.getTopicManager().getLatestOffsetAndRetry(topicName, partitionId, 10)));
+      return entry.getSecond();
+    }
+
+    long getOffsetFromRemoteKafka(String remoteKafkaServer, String topicName, int partitionId) {
+      long now = System.nanoTime();
+
+      Pair key = new Pair<>(topicName, partitionId);
+      Pair<Long, Long> entry = remoteOffsetCache.get(key);
+      if (entry != null && entry.getFirst() > now) {
+        return entry.getSecond();
+      }
+
+      entry = remoteOffsetCache.compute(key, (k, oldValue) ->
+          (oldValue != null && oldValue.getFirst() > now) ?
+              oldValue : new Pair<>(now + ttl, topicManagerRepository.getTopicManager(remoteKafkaServer).getLatestOffsetAndRetry(topicName, partitionId, 10)));
       return entry.getSecond();
     }
 

da-vinci-client/src/main/java/com/linkedin/davinci/stats/AggVersionedStorageIngestionStats.java
@@ -228,6 +228,13 @@ public class AggVersionedStorageIngestionStats extends AbstractVeniceAggVersione
       return ingestionTaskErroredGauge;
     }
 
+    public long getLeaderOffsetLag() {
+      if (ingestionTask == null) {
+        return 0;
+      }
+      return ingestionTask.getLeaderOffsetLag();
+    }
+
     /**
      * @return This stats is usually aggregated across the nodes so that
      * we can see the overall lags between leaders and followers.
@@ -327,6 +334,8 @@ public class AggVersionedStorageIngestionStats extends AbstractVeniceAggVersione
       registerSensor("ingestion_task_errored_gauge", new IngestionStatsGauge(this,
           () -> (double) getStats().getIngestionTaskErroredGauge()));
 
+      registerSensor("leader_offset_lag", new IngestionStatsGauge(this, () ->
+          (double) getStats().getLeaderOffsetLag(), 0));
       registerSensor("follower_offset_lag", new IngestionStatsGauge(this, () ->
           (double) getStats().getFollowerOffsetLag(), 0));
       registerSensor("write_compute_operation_failure", new IngestionStatsGauge(this,

venice-common/src/main/java/com/linkedin/venice/kafka/TopicManagerRepository.java
@@ -1,6 +1,7 @@
 package com.linkedin.venice.kafka;
 
 import com.linkedin.venice.exceptions.VeniceException;
+import com.linkedin.venice.kafka.admin.ScalaAdminUtils;
 import com.linkedin.venice.utils.Pair;
 import com.linkedin.venice.utils.concurrent.VeniceConcurrentHashMap;
 import java.io.Closeable;
@@ -88,6 +89,15 @@ public class TopicManagerRepository implements Closeable {
 
   public TopicManager getTopicManager(Pair<String, String> kafkaServerAndZk) {
     return topicManagersMap.computeIfAbsent(kafkaServerAndZk.getFirst(), k -> topicManagerCreator.apply(kafkaServerAndZk));
+  }
+
+  public TopicManager getTopicManager(String kafkaBootstrapServers) {
+    // Creating remote topic manager by remote Kafka server requires Java-based Kafka admin client
+    if (kafkaClientFactory.getKafkaAdminClient() instanceof ScalaAdminUtils) {
+      throw new VeniceException("Kafka ZK address is required by Scala Kafka admin client.");
+    }
+    return topicManagersMap.computeIfAbsent(kafkaBootstrapServers,
+        k -> topicManagerCreator.apply(Pair.create(kafkaBootstrapServers, "")));
   }
 
   @Override

commit 912c0ea2feff39217e19718c30cd58c387a7334d

[server] Fix a potential with ScalaAdminUtil class loading.
This is to correct a potential issue caused by commit
7ec4673e89f09a1e5523f167336f5339e9790ae6. The da-vinci client does
not laod the ScalaAdminUtil class, so at runtime the function
getLeaderLag() may throw exception. Ideally this function should not
be called in da-vinci as this is only needed for Leaders, but this will
be major refactoring. For now using hard coded classname here.

RB=2493913
BUG=VENG-7245
G=voldemort-reviewers
R=goliu,mnhuang,zpolicze,nithakka,anaberez,smaji,sdwu,xhao,yurzhou,xyin,xnma,jlliu,shuhliu,lcli,fvillega
A=fvillega

da-vinci-client/src/main/java/com/linkedin/davinci/kafka/consumer/LeaderFollowerStoreIngestionTask.java
@@ -69,6 +69,7 @@ import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.log4j.Logger;
 
 import static com.linkedin.davinci.kafka.consumer.LeaderFollowerStateType.*;
+import static com.linkedin.venice.VeniceConstants.*;
 import static com.linkedin.venice.writer.VeniceWriter.*;
 
 
@@ -1397,7 +1398,7 @@ public class LeaderFollowerStoreIngestionTask extends StoreIngestionTask {
      * If the admin client is scala based then getting offset from remote kafka is difficult as it needs the
      * ZK address of the kafka broker. So this metric will not work in this case.
      */
-    if (serverConfig.getKafkaAdminClass().contains(ScalaAdminUtils.class.getName())) {
+    if (serverConfig.getKafkaAdminClass().contains(SCALA_BASED_KAFKA_ADMIN_CLIENT_CLASS_NAME)) {
       return 0;
     }
 

venice-schema-common/src/main/java/com/linkedin/venice/VeniceConstants.java
@@ -61,4 +61,6 @@ public class VeniceConstants {
   // End of router request path
 
   public static final String NATIVE_REPLICATION_DEFAULT_SOURCE_FABRIC = "prod-lva1";
+
+  public static final String SCALA_BASED_KAFKA_ADMIN_CLIENT_CLASS_NAME = "ScalaAdminUtils";
 }