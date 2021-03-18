619953a3bc5bfcd331264d42212b8bc07d8dceb2
xnma <xnma@linkedin.com>
2021-03-18 16:24:38 -0700

    [test-common] fix testIncrementalPush flakiness
    RB=2517514
    G=voldemort-reviewers
    R=smaji,shuhliu,xyin,xhao,zpolicze,goliu,anaberez,lcli,jlliu,mnhuang,nithakka,fvillega,sdwu,ssen,yurzhou
    A=jlliu

--- venice-test-common/src/integrationtest/java/com/linkedin/venice/endToEnd/PushStatusStoreTest.java
+++ venice-test-common/src/integrationtest/java/com/linkedin/venice/endToEnd/PushStatusStoreTest.java
@@ -27,7 +27,8 @@ import java.util.Optional;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 import org.apache.commons.io.IOUtils;
-import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -35,7 +36,6 @@ import static com.linkedin.venice.ConfigKeys.*;
 import static com.linkedin.venice.hadoop.VenicePushJob.*;
 import static com.linkedin.venice.integration.utils.VeniceClusterWrapper.*;
 import static com.linkedin.venice.meta.IngestionMode.*;
-import static com.linkedin.venice.meta.PersistenceType.*;
 import static com.linkedin.venice.utils.TestPushUtils.*;
 import static org.testng.Assert.*;
 
@@ -48,12 +48,10 @@ public class PushStatusStoreTest {
   private ControllerClient parentControllerClient;
   private D2Client d2Client;
   private PushStatusStoreReader reader;
-  private Properties h2vProperties;
-  private VeniceProperties backendConfig;
   private String storeName;
 
-  @BeforeMethod
-  public void setup() throws Exception {
+  @BeforeClass
+  public void setup() {
     Properties extraProperties = new Properties();
     extraProperties.setProperty(SERVER_PROMOTION_TO_LEADER_REPLICA_DELAY_SECONDS, Long.toString(1L));
     Utils.thisIsLocalhost();
@@ -75,24 +73,6 @@ public class PushStatusStoreTest {
     parentControllerClient = new ControllerClient(cluster.getClusterName(), parentController.getControllerUrl());
     d2Client = D2TestUtils.getAndStartD2Client(cluster.getZk().getAddress());
     reader = new PushStatusStoreReader(d2Client, TimeUnit.MINUTES.toSeconds(10));
-
-    storeName = TestUtils.getUniqueString("store");
-    // Produce input data.
-    File inputDir = getTempDataDirectory();
-    String inputDirPath = "file://" + inputDir.getAbsolutePath();
-    writeSimpleAvroFileWithIntToIntSchema(inputDir, true);
-    // Setup H2V job properties.
-    h2vProperties = defaultH2VProps(cluster, inputDirPath, storeName);
-    backendConfig = new PropertyBuilder()
-        .put(ConfigKeys.DATA_BASE_PATH, TestUtils.getTempDataDirectory().getAbsolutePath())
-        .put(ConfigKeys.PERSISTENCE_TYPE, PersistenceType.ROCKS_DB)
-        .put(CLIENT_USE_SYSTEM_STORE_REPOSITORY, true)
-        .put(CLIENT_SYSTEM_STORE_REPOSITORY_REFRESH_INTERVAL_SECONDS, 10)
-        .put(ConfigKeys.SERVER_ROCKSDB_STORAGE_CONFIG_CHECK_ENABLED, true)
-        .put(PUSH_STATUS_STORE_ENABLED, true)
-        .build();
-
-    // set up push status store
     String owner = "test";
     String zkSharedPushStatusStoreName = VeniceSystemStoreUtils.getSharedZkNameForDaVinciPushStatusStore(cluster.getClusterName());
     String zkSharedMetadataStoreName = VeniceSystemStoreUtils.getSharedZkNameForMetadataStore(cluster.getClusterName());
@@ -100,18 +80,9 @@ public class PushStatusStoreTest {
     TestUtils.assertCommand(parentControllerClient.newZkSharedStoreVersion(zkSharedPushStatusStoreName));
     TestUtils.assertCommand(parentControllerClient.createNewZkSharedStoreWithDefaultConfigs(zkSharedMetadataStoreName, owner));
     TestUtils.assertCommand(parentControllerClient.newZkSharedStoreVersion(zkSharedMetadataStoreName));
-    TestUtils.assertCommand(parentControllerClient.createNewStore(storeName, owner, DEFAULT_KEY_SCHEMA, DEFAULT_VALUE_SCHEMA));
-    TestUtils.assertCommand(parentControllerClient.updateStore(storeName, new UpdateStoreQueryParams()
-        .setStorageQuotaInByte(Store.UNLIMITED_STORAGE_QUOTA)));
-    TestUtils.assertCommand(parentControllerClient.createDaVinciPushStatusStore(storeName));
-    String metadataStoreTopic =
-        Version.composeKafkaTopic(VeniceSystemStoreUtils.getMetadataStoreName(storeName), 1);
-    // The corresponding metadata store should be materialized automatically.
-    TestUtils.waitForNonDeterministicPushCompletion(metadataStoreTopic, cluster.getControllerClient(), 30, TimeUnit.SECONDS,
-        Optional.empty());
   }
 
-  @AfterMethod
+  @AfterClass
   public void cleanup() {
     IOUtils.closeQuietly(reader);
     D2ClientUtils.shutdownClient(d2Client);
@@ -120,23 +91,35 @@ public class PushStatusStoreTest {
     IOUtils.closeQuietly(cluster);
   }
 
+  @BeforeMethod
+  public void setUpStore() {
+    storeName = TestUtils.getUniqueString("store");
+    String owner = "test";
+    // set up push status store
+    TestUtils.assertCommand(parentControllerClient.createNewStore(storeName, owner, DEFAULT_KEY_SCHEMA, DEFAULT_VALUE_SCHEMA));
+    TestUtils.assertCommand(parentControllerClient.updateStore(storeName, new UpdateStoreQueryParams()
+        .setStorageQuotaInByte(Store.UNLIMITED_STORAGE_QUOTA)
+        .setIncrementalPushEnabled(true)));
+    TestUtils.assertCommand(parentControllerClient.createDaVinciPushStatusStore(storeName));
+    String metadataStoreTopic =
+        Version.composeKafkaTopic(VeniceSystemStoreUtils.getMetadataStoreName(storeName), 1);
+    // The corresponding metadata store should be materialized automatically.
+    TestUtils.waitForNonDeterministicPushCompletion(metadataStoreTopic, cluster.getControllerClient(), 30, TimeUnit.SECONDS,
+        Optional.empty());
+  }
+
   @Test(dataProvider = "True-and-False", dataProviderClass = DataProviderUtils.class, timeOut = TEST_TIMEOUT)
-  public void testVenicePushJob(boolean isIsolated) throws Exception {
+  public void testKafkaPushJob(boolean isIsolated) throws Exception {
+    Properties h2vProperties = getH2VProperties();
     // setup initial version
     runH2V(h2vProperties, 1, cluster);
     int applicationListenerPort = Utils.getFreePort();
     int servicePort = Utils.getFreePort();
-    VeniceProperties backendConfig = new PropertyBuilder()
-        .put(DATA_BASE_PATH, TestUtils.getTempDataDirectory().getAbsolutePath())
-        .put(PERSISTENCE_TYPE, ROCKS_DB)
+    VeniceProperties backendConfig = getBackendConfigBuilder()
         .put(SERVER_INGESTION_MODE, isIsolated ? ISOLATED : BUILT_IN)
         .put(SERVER_INGESTION_ISOLATION_APPLICATION_PORT, applicationListenerPort)
         .put(SERVER_INGESTION_ISOLATION_SERVICE_PORT, servicePort)
         .put(D2_CLIENT_ZK_HOSTS_ADDRESS, cluster.getZk().getAddress())
-        .put(ConfigKeys.SERVER_ROCKSDB_STORAGE_CONFIG_CHECK_ENABLED, true)
-        .put(PUSH_STATUS_STORE_ENABLED, true)
-        .put(CLIENT_USE_SYSTEM_STORE_REPOSITORY, true)
-        .put(CLIENT_SYSTEM_STORE_REPOSITORY_REFRESH_INTERVAL_SECONDS, 10)
         .build();
     try (DaVinciClient daVinciClient = ServiceFactory.getGenericAvroDaVinciClient(storeName, cluster, new DaVinciConfig(), backendConfig)) {
       daVinciClient.subscribeAll().get();
@@ -163,20 +146,22 @@ public class PushStatusStoreTest {
 
   @Test(timeOut = TEST_TIMEOUT)
   public void testIncrementalPush() throws Exception {
-    assertFalse(parentControllerClient.updateStore(storeName, new UpdateStoreQueryParams()
-        .setIncrementalPushEnabled(true)).isError());
-    // setup initial version
+    VeniceProperties backendConfig = getBackendConfigBuilder().build();
+    Properties h2vProperties = getH2VProperties();
     runH2V(h2vProperties, 1, cluster);
     try (DaVinciClient daVinciClient = ServiceFactory.getGenericAvroDaVinciClient(storeName, cluster, new DaVinciConfig(), backendConfig)) {
       daVinciClient.subscribeAll().get();
+      h2vProperties = getH2VProperties();
       h2vProperties.setProperty(INCREMENTAL_PUSH, "true");
       runH2V(h2vProperties, 1, cluster);
-      // TODO: Add assertions
+      assertEquals(daVinciClient.get(1).get(), 1);
     }
   }
 
   @Test(timeOut = TEST_TIMEOUT)
   public void testAutomaticPurge() throws Exception {
+    VeniceProperties backendConfig = getBackendConfigBuilder().build();
+    Properties h2vProperties = getH2VProperties();
     // setup initial version
     runH2V(h2vProperties, 1, cluster);
     try (DaVinciClient daVinciClient = ServiceFactory.getGenericAvroDaVinciClient(storeName, cluster, new DaVinciConfig(), backendConfig)) {
@@ -192,7 +177,28 @@ public class PushStatusStoreTest {
     }
   }
 
-  private static void runH2V(Properties h2vProperties, int expectedVersionNumber, VeniceClusterWrapper cluster) {
+  private PropertyBuilder getBackendConfigBuilder() {
+    PropertyBuilder backendConfigBuilder = new PropertyBuilder()
+        .put(ConfigKeys.DATA_BASE_PATH, TestUtils.getTempDataDirectory().getAbsolutePath())
+        .put(ConfigKeys.PERSISTENCE_TYPE, PersistenceType.ROCKS_DB)
+        .put(CLIENT_USE_SYSTEM_STORE_REPOSITORY, true)
+        .put(CLIENT_SYSTEM_STORE_REPOSITORY_REFRESH_INTERVAL_SECONDS, 10)
+        .put(ConfigKeys.SERVER_ROCKSDB_STORAGE_CONFIG_CHECK_ENABLED, true)
+        .put(PUSH_STATUS_STORE_ENABLED, true);
+    return backendConfigBuilder;
+  }
+
+  private Properties getH2VProperties() throws Exception {
+    // Setup H2V job properties.
+    // Produce input data.
+    File inputDir = getTempDataDirectory();
+    String inputDirPath = "file://" + inputDir.getAbsolutePath();
+    writeSimpleAvroFileWithIntToIntSchema(inputDir, true);
+    Properties h2vProperties = defaultH2VProps(cluster, inputDirPath, storeName);
+    return h2vProperties;
+  }
+
+  private void runH2V(Properties h2vProperties, int expectedVersionNumber, VeniceClusterWrapper cluster) {
     long h2vStart = System.currentTimeMillis();
     String jobName = TestUtils.getUniqueString("batch-job-" + expectedVersionNumber);
     try (VenicePushJob job = new VenicePushJob(jobName, h2vProperties)) {
