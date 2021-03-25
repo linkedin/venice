c1cc970d6126a698cee95d32c27e450dc608fb1e
Gaojie Liu <goliu@linkedin.com>
2021-03-24 22:25:36 -0700

    [controller][common] Fixed NoStore exception for meta system store
    In Venice Controller, all the clusters in the same node
    is sharing the same 'HelixReadOnlyZKSharedSystemStoreRepository'
    and 'HelixReadOnlyZKSharedSchemaRepository' to avoid
    the unnecessary zookeeper watch overhead.
    But when the node steps down the leadership for some cluster,
    all the resources for this cluster will be cleared include
    these shared resources, so other clusters won't be able
    to access zk shared store anymore, which is causing
    NoStore exception when querying meta system stores.
    The fix is to introduce a Shared wrapper for these
    resources, so the clear function will do nothing, and
    all the cleanup will be handled in close function,
    which will only be triggered during Controller shutdown.
    
    This code change also bumped up the fast-avro dependency to suppress
    the large log entry of inferred compiling classpath.
    
    RB=2527158
    BUG=VENG-7373
    G=voldemort-reviewers
    R=xyin,yurzhou
    A=fvillega

---
+++ venice-controller/src/main/java/com/linkedin/venice/controller/helix/SharedHelixReadOnlyZKSharedSchemaRepository.java
@@ -0,0 +1,39 @@
+package com.linkedin.venice.controller.helix;
+
+import com.linkedin.venice.helix.HelixAdapterSerializer;
+import com.linkedin.venice.helix.HelixReadOnlyZKSharedSchemaRepository;
+import com.linkedin.venice.helix.HelixReadOnlyZKSharedSystemStoreRepository;
+import java.io.Closeable;
+import java.io.IOException;
+import org.apache.helix.zookeeper.impl.client.ZkClient;
+
+
+/**
+ * This class is intended to only be used in Controller, which is sharing one instance across
+ * all the clusters.
+ * Since the leader->standby transition for a given cluster will clear all the resources, this implementation
+ * will do nothing in {@link #clear()} since it will be shared across all the clusters, whose master are
+ * in the same Controller node.
+ */
+public class SharedHelixReadOnlyZKSharedSchemaRepository
+    extends HelixReadOnlyZKSharedSchemaRepository
+    implements Closeable {
+  public SharedHelixReadOnlyZKSharedSchemaRepository(HelixReadOnlyZKSharedSystemStoreRepository storeRepository,
+      ZkClient zkClient, HelixAdapterSerializer adapter, String systemStoreClusterName,
+      int refreshAttemptsForZkReconnect, long refreshIntervalForZkReconnectInMs) {
+    super(storeRepository, zkClient, adapter, systemStoreClusterName, refreshAttemptsForZkReconnect,
+        refreshIntervalForZkReconnectInMs);
+  }
+
+  @Override
+  public void clear() {
+    /**
+     * Do nothing here since it is shared across different clusters.
+     */
+  }
+
+  @Override
+  public void close() throws IOException {
+    super.clear();
+  }
+}
---
+++ venice-controller/src/main/java/com/linkedin/venice/controller/helix/SharedHelixReadOnlyZKSharedSystemStoreRepository.java
@@ -0,0 +1,35 @@
+package com.linkedin.venice.controller.helix;
+
+import com.linkedin.venice.helix.HelixAdapterSerializer;
+import com.linkedin.venice.helix.HelixReadOnlyZKSharedSystemStoreRepository;
+import java.io.Closeable;
+import java.io.IOException;
+import org.apache.helix.zookeeper.impl.client.ZkClient;
+
+/**
+ * This class is intended to only be used in Controller, which is sharing one instance across
+ * all the clusters.
+ * Since the leader->standby transition for a given cluster will clear all the resources, this implementation
+ * will do nothing in {@link #clear()} since it will be shared across all the clusters, whose master are
+ * in the same Controller node.
+ */
+public class SharedHelixReadOnlyZKSharedSystemStoreRepository
+    extends HelixReadOnlyZKSharedSystemStoreRepository
+    implements Closeable {
+  public SharedHelixReadOnlyZKSharedSystemStoreRepository(ZkClient zkClient, HelixAdapterSerializer compositeSerializer,
+      String systemStoreClusterName) {
+    super(zkClient, compositeSerializer, systemStoreClusterName);
+  }
+
+  @Override
+  public void clear() {
+    /**
+     * Do nothing here since it is shared across different clusters.
+     */
+  }
+
+  @Override
+  public void close() throws IOException {
+    super.clear();
+  }
+}
