576ec894ceee8402215ffe75db044097a8516b15
Jialin Liu <jlliu@linkedin.com>
2021-04-22 17:30:07 -0700

    [da-vinci] Split ingestion backend interface for Da Vinci and Venice Server
    This RB splits the original IngestionBackend interface into two interfaces: DaVinciIngestionBackend and VeniceIngestionBackend for different use cases, as some of the APIs are designed for only one of the product and splitting them make it more clear for future development.
    These two interfaces will extends a shared interface: IngetsionBackendBase, which contains the shared APIs for both products.
    DefaultIngestionBackend and IsolatedIngestionBackend implemenets both interfaces so they can be used in both products.
    
    RB=2560166
    BUG=VENG-7528
    G=voldemort-reviewers
    R=nithakka,xyin,yurzhou,goliu,zpolicze,shuhliu,mnhuang,lcli,sdwu,ssen,smaji,xnma,anaberez,fvillega,xhao
    A=goliu

---
+++ da-vinci-client/src/main/java/com/linkedin/davinci/ingestion/VeniceIngestionBackend.java
@@ -0,0 +1,22 @@
+package com.linkedin.davinci.ingestion;
+
+import com.linkedin.davinci.config.VeniceStoreConfig;
+import com.linkedin.davinci.helix.LeaderFollowerParticipantModel;
+import com.linkedin.davinci.notifier.VeniceNotifier;
+
+
+public interface VeniceIngestionBackend extends IngestionBackendBase {
+
+  void promoteToLeader(VeniceStoreConfig storeConfig, int partition,
+      LeaderFollowerParticipantModel.LeaderSessionIdChecker leaderSessionIdChecker);
+
+  void demoteToStandby(VeniceStoreConfig storeConfig, int partition,
+      LeaderFollowerParticipantModel.LeaderSessionIdChecker leaderSessionIdChecker);
+
+  void addOnlineOfflineIngestionNotifier(VeniceNotifier ingestionListener);
+
+  void addLeaderFollowerIngestionNotifier(VeniceNotifier ingestionListener);
+
+  void addPushStatusNotifier(VeniceNotifier pushStatusNotifier);
+
+}
