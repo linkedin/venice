13900923eba5da885c7ea949524e722da6a9e4c9
Felix GV <fvillega@linkedin.com>
2021-04-28 10:13:15 -0700

    [da-vinci-client][server][common] Introduced Lazy utility class
    The Lazy interface is a utility to implement the lazy initialization pattern in
    a generic way with minimal boilerplate.
    
    It is integrated only in the LeaderFollowerStoreIngestionTaskIngestionTask so
    far, where it is used for the VeniceWriter.
    
    RB=2581450
    BUG=VENG-7604
    G=voldemort-reviewers
    R=smaji,adietz,shuhliu,xnma,xyin,goliu,yurzhou,zpolicze,xhao,anaberez,mnhuang,ssen,jlliu,lcli,sdwu,nithakka
    A=goliu

---
+++ venice-common/src/main/java/com/linkedin/venice/utils/Lazy.java
@@ -0,0 +1,161 @@
+package com.linkedin.venice.utils;
+
+import com.linkedin.venice.annotation.Threadsafe;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+import java.util.Optional;
+
+
+/**
+ * This utility provides lazy initialization for a wrapped object.
+ *
+ * API-wise, it can be considered similar to {@link Optional} with the main difference that
+ * since it will never be {@link Optional#empty()}, the {@link #get()} function will never
+ * fail. Rather, {@link #get()} will result in initializing the object if initialization has
+ * not happened yet. All other functions mimic the behavior of their counterpart in
+ * {@link Optional} and do NOT trigger initialization if it has not happened yet.
+ *
+ * Note that {@link Lazy} does allow wrapping a null if that is what the lazy initialization
+ * returns. For the use case of lazy initializing to something which may or may not exist,
+ * the implementer should consider combining this class with {@link Optional}, thus
+ * resulting in a {@link Lazy<Optional<T>>}.
+ *
+ * It is recommended to use instances of {@link Lazy} exclusively in conjunction with the
+ * final keyword, such that it is not allowed to swap the reference to a new {@link Lazy}
+ * instance. Following the recommended usage allows making the assumption that there will
+ * be no more than one initialization, and that the transition from uninitialized to
+ * initialized is one-way and irreversible.
+ *
+ * This interface promises to be threadsafe if multiple concurrent calls happen before the
+ * first initialization finishes, and furthermore guarantees that the initialization
+ * routine will happen at most once.
+ */
+@Threadsafe
+public interface Lazy<T> {
+  /**
+   * @param supplier to initialize the wrapped value
+   * @return an instance of {@link Lazy} which will execute the {@param supplier} if needed
+   */
+  static <C> Lazy<C> of(Supplier<C> supplier) {
+    return new LazyImpl<>(supplier);
+  }
+
+  /**
+   * @return the wrapped value, with the side-effect of initializing it has not happened yet
+   */
+  T get();
+
+  /**
+   * @param consumer to pass the wrapped value to, only if it has already been initialized
+   */
+  void ifPresent(Consumer<? super T> consumer);
+
+  /**
+   * @return {@code true} if there is a value present (i.e. initialized), otherwise {@code false}
+   */
+  boolean isPresent();
+
+  /**
+   * If a value is initialized, and the value matches the given predicate,
+   * return an {@code Optional} describing the value, otherwise return an
+   * empty {@code Optional}.
+   *
+   * @param predicate a predicate to apply to the value, if initialized
+   * @return an {@code Optional} describing the value of this {@code Optional}
+   * if a value is present and the value matches the given predicate,
+   * otherwise an empty {@code Optional}
+   * @throws NullPointerException if the predicate is null
+   */
+  Optional<T> filter(Predicate<? super T> predicate);
+
+  /**
+   * If a value is initialized, apply the provided mapping function to it,
+   * and if the result is non-null, return an {@code Optional} describing the
+   * result.  Otherwise return an empty {@code Optional}.
+   *
+   * @apiNote This method supports post-processing on optional values, without
+   * the need to explicitly check for a return status.  For example, the
+   * following code traverses a stream of file names, selects one that has
+   * not yet been processed, and then opens that file, returning an
+   * {@code Optional<FileInputStream>}:
+   *
+   * <pre>{@code
+   *     Optional<FileInputStream> fis =
+   *         names.stream().filter(name -> !isProcessedYet(name))
+   *                       .findFirst()
+   *                       .map(name -> new FileInputStream(name));
+   * }</pre>
+   *
+   * Here, {@code findFirst} returns an {@code Optional<String>}, and then
+   * {@code map} returns an {@code Optional<FileInputStream>} for the desired
+   * file if one exists.
+   *
+   * @param <U> The type of the result of the mapping function
+   * @param mapper a mapping function to apply to the value, if initialized
+   * @return an {@code Optional} describing the result of applying a mapping
+   * function to the value of this {@code Optional}, if a value is initialized,
+   * otherwise an empty {@code Optional}
+   * @throws NullPointerException if the mapping function is null
+   */
+  <U> Optional<U> map(Function<? super T, ? extends U> mapper);
+
+  /**
+   * If a value is initialized, apply the provided {@code Optional}-bearing
+   * mapping function to it, return that result, otherwise return an empty
+   * {@code Optional}.  This method is similar to {@link #map(Function)},
+   * but the provided mapper is one whose result is already an {@code Optional},
+   * and if invoked, {@code flatMap} does not wrap it with an additional
+   * {@code Optional}.
+   *
+   * @param <U> The type parameter to the {@code Optional} returned by
+   *           the mapping function
+   * @param mapper a mapping function to apply to the value, if initialized
+   * @return the result of applying an {@code Optional}-bearing mapping
+   * function to the value of this {@code Optional}, if a value is initialized,
+   * otherwise an empty {@code Optional}
+   * @throws NullPointerException if the mapping function is null or returns
+   * a null result
+   */
+  <U> Optional<U> flatMap(Function<? super T, Optional<U>> mapper);
+
+  /**
+   * Return the value if initialized, otherwise return {@code other}.
+   *
+   * @param other the value to be returned if there is no value initialized, may
+   * be null
+   * @return the value, if initialized, otherwise {@code other}
+   */
+  T orElse(T other);
+
+  /**
+   * Return the value if initialized, otherwise invoke {@code other} and return
+   * the result of that invocation.
+   *
+   * @param other a {@code Supplier} whose result is returned if no value
+   * is initialized
+   * @return the value if initialized otherwise the result of {@code other.get()}
+   * @throws NullPointerException if value is not initialized and {@code other} is
+   * null
+   */
+  T orElseGet(Supplier<? extends T> other);
+
+  /**
+   * Return the contained value, if initialized, otherwise throw an exception
+   * to be created by the provided supplier.
+   *
+   * @apiNote A method reference to the exception constructor with an empty
+   * argument list can be used as the supplier. For example,
+   * {@code IllegalStateException::new}
+   *
+   * @param <X> Type of the exception to be thrown
+   * @param exceptionSupplier The supplier which will return the exception to
+   * be thrown
+   * @return the initialized value
+   * @throws X if there is no value initialized
+   * @throws NullPointerException if no value is initialized and
+   * {@code exceptionSupplier} is null
+   */
+  <X extends Throwable> T orElseThrow(Supplier<? extends X> exceptionSupplier) throws X;
+}
---
+++ venice-common/src/test/java/com/linkedin/venice/utils/LazyTest.java
@@ -0,0 +1,159 @@
+package com.linkedin.venice.utils;
+
+import com.linkedin.venice.exceptions.VeniceException;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Supplier;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class LazyTest {
+  @Test(timeOut = 5 * Time.MS_PER_SECOND)
+  public void test() throws ExecutionException, InterruptedException {
+    int numberOfFutures = 10;
+    ExecutorService executor = null;
+    try {
+      executor = Executors.newFixedThreadPool(numberOfFutures);
+
+      /** We create a {@link Supplier} that can reliably reproduce a race condition and detect the number of invocations. */
+      final Object lock = new Object();
+      final AtomicInteger startedInvocationCount = new AtomicInteger(0);
+      final AtomicInteger finishedInvocationCount = new AtomicInteger(0);
+      Supplier<Object> testSupplier = () -> {
+        startedInvocationCount.incrementAndGet();
+        synchronized (lock) {
+          try {
+            lock.wait();
+          } catch (InterruptedException e) {
+            throw new VeniceException(e);
+          }
+        }
+        finishedInvocationCount.incrementAndGet();
+        return new Object();
+      };
+
+      /** We verify whether the custom {@link Supplier} does indeed detect concurrent invocations. */
+      CompletableFuture<Object>[] futures = new CompletableFuture[numberOfFutures];
+      for (int f = 0; f < numberOfFutures; f++) {
+        futures[f] = CompletableFuture.supplyAsync(() -> testSupplier.get(), executor);
+      }
+      TestUtils.waitForNonDeterministicAssertion(1, TimeUnit.SECONDS, () ->
+          Assert.assertEquals(startedInvocationCount.get(), numberOfFutures,
+              "The testSuppliers should have all started running."));
+
+      Assert.assertEquals(finishedInvocationCount.get(), 0,
+          "The testSuppliers should not have finished running yet.");
+
+      for (int f = 0; f < numberOfFutures; f++) {
+        synchronized (lock) {
+          lock.notify();
+        }
+      }
+      Object[] supplierResults = new Object[numberOfFutures];
+      for (int f = 0; f < numberOfFutures; f++) {
+        supplierResults[f] = futures[f].get();
+      }
+      Assert.assertEquals(finishedInvocationCount.get(), numberOfFutures,
+          "The testSupplier did not successfully keep track of parallel executions.");
+      for (int f1 = 0; f1 < numberOfFutures; f1++) {
+        for (int f2 = 0; f2 < numberOfFutures; f2++) {
+          if (f1 != f2) {
+            Assert.assertNotEquals(supplierResults[f1], supplierResults[f2],
+                "The identity of the returned objects should all be different.");
+          }
+        }
+      }
+
+      /** Reset the state */
+      startedInvocationCount.set(0);
+      finishedInvocationCount.set(0);
+
+      /** Main code under test: does Lazy successfully prevent the race condition? */
+      Lazy<Object> lazyObject = Lazy.of(testSupplier);
+      CompletableFuture<Object>[] lazyInvocations = new CompletableFuture[numberOfFutures];
+      for (int f = 0; f < numberOfFutures; f++) {
+        lazyInvocations[f] = CompletableFuture.supplyAsync(() -> lazyObject.get(), executor);
+      }
+      TestUtils.waitForNonDeterministicAssertion(1, TimeUnit.SECONDS, () ->
+          Assert.assertEquals(startedInvocationCount.get(), 1,
+              "A single testSupplier should have started running."));
+      Assert.assertEquals(finishedInvocationCount.get(), 0,
+          "The testSupplier should not have finished running yet.");
+      Assert.assertFalse(lazyObject.isPresent(), "The lazyObject should not be present yet.");
+      AtomicBoolean isPresent = new AtomicBoolean(false);
+      lazyObject.ifPresent(o -> isPresent.set(true));
+      Assert.assertFalse(isPresent.get(), "The lazyObject should not be present yet.");
+      Optional<SomeWrapper> someWrapperOptional = lazyObject.map(o -> new SomeWrapper(o));
+      Assert.assertFalse(someWrapperOptional.isPresent(), "The lazyObject should not be present yet.");
+
+      synchronized (lock) {
+        lock.notify();
+      }
+      Object[] lazyInvocationResults = new Object[numberOfFutures];
+      for (int f = 0; f < numberOfFutures; f++) {
+        lazyInvocationResults[f] = lazyInvocations[f].get();
+      }
+      Assert.assertEquals(startedInvocationCount.get(), 1,
+          "Lazy did not ensure exactly one run of the supplier. startedInvocationCount: " + startedInvocationCount);
+      Assert.assertEquals(finishedInvocationCount.get(), 1,
+          "Lazy did not ensure exactly one run of the supplier. finishedInvocationCount: " + finishedInvocationCount);
+      Assert.assertTrue(lazyObject.isPresent(), "The lazyObject should now be present.");
+      lazyObject.ifPresent(o -> isPresent.set(true));
+      Assert.assertTrue(isPresent.get(), "The lazyObject should now be present.");
+      someWrapperOptional = lazyObject.map(o -> new SomeWrapper(o));
+      Assert.assertTrue(someWrapperOptional.isPresent(), "The lazyObject should now be present.");
+
+      for (int f1 = 0; f1 < numberOfFutures; f1++) {
+        for (int f2 = 0; f2 < numberOfFutures; f2++) {
+          Assert.assertEquals(lazyInvocationResults[f1], lazyInvocationResults[f2],
+              "The identity of the returned objects should all be the same.");
+        }
+        Assert.assertEquals(someWrapperOptional.get().getStuff(), lazyInvocationResults[f1],
+            "The identity of the returned objects should all be the same.");
+      }
+    } finally {
+      if (executor != null) {
+        executor.shutdownNow();
+      }
+    }
+  }
+
+  @Test
+  public void testNullSupplier() {
+    Lazy<Object> lazyObject = Lazy.of(() -> null);
+    Assert.assertFalse(lazyObject.isPresent(), "The lazyObject should not be present yet.");
+    AtomicBoolean isPresent = new AtomicBoolean(false);
+    lazyObject.ifPresent(o -> isPresent.set(true));
+    Assert.assertFalse(isPresent.get(), "The lazyObject should not be present yet.");
+    Optional<SomeWrapper> someWrapperOptional = lazyObject.map(o -> new SomeWrapper(o));
+    Assert.assertFalse(someWrapperOptional.isPresent(), "The lazyObject should not be present yet.");
+
+    // Trigger init
+    Assert.assertNull(lazyObject.get(), "The lazyObject should contain null.");
+
+    Assert.assertTrue(lazyObject.isPresent(), "The lazyObject should now be present.");
+    lazyObject.ifPresent(o -> isPresent.set(true));
+    Assert.assertTrue(isPresent.get(), "The lazyObject should now be present.");
+    someWrapperOptional = lazyObject.map(o -> new SomeWrapper(o));
+    Assert.assertTrue(someWrapperOptional.isPresent(), "The lazyObject should now be present.");
+    Assert.assertNull(someWrapperOptional.get().getStuff(), "The lazyObject should contain null.");
+  }
+
+  private static class SomeWrapper<T> {
+    private final T stuff;
+    SomeWrapper(T stuff) {
+      this.stuff = stuff;
+    }
+
+    public T getStuff() {
+      return stuff;
+    }
+  }
+}
