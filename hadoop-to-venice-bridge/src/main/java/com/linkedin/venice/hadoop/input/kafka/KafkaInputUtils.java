ced0fe717274d0cf81f7498c3a1690911c0e2176
Gaojie Liu <goliu@linkedin.com>
2021-04-28 16:52:29 -0700

    [h2v]Passed the right ssl properties to the consumer factory being used by KafkaInputFormat
    RB=2568637
    BUG=VENG-7469
    R=sdwu,xnma,xyin,jlliu,ssen,xhao,lcli,smaji,yurzhou,shuhliu,fvillega,zpolicze,nithakka,anaberez,mnhuang
    A=lcli

--- hadoop-to-venice-bridge/src/main/java/com/linkedin/venice/hadoop/input/kafka/KafkaInputUtils.java
+++ hadoop-to-venice-bridge/src/main/java/com/linkedin/venice/hadoop/input/kafka/KafkaInputUtils.java
@@ -1,12 +1,16 @@
 package com.linkedin.venice.hadoop.input.kafka;
 
 import com.linkedin.venice.ConfigKeys;
+import com.linkedin.venice.exceptions.VeniceException;
+import com.linkedin.venice.hadoop.ssl.SSLConfigurator;
+import com.linkedin.venice.hadoop.ssl.UserCredentialsFactory;
 import com.linkedin.venice.hadoop.utils.HadoopUtils;
 import com.linkedin.venice.kafka.KafkaClientFactory;
 import com.linkedin.venice.kafka.consumer.VeniceKafkaConsumerFactory;
 import com.linkedin.venice.serialization.KafkaKeySerializer;
 import com.linkedin.venice.serialization.avro.OptimizedKafkaValueSerializer;
 import com.linkedin.venice.utils.VeniceProperties;
+import java.io.IOException;
 import java.util.Properties;
 import org.apache.hadoop.mapred.JobConf;
 import org.apache.kafka.clients.CommonClientConfigs;
@@ -18,13 +22,22 @@ import static com.linkedin.venice.hadoop.VenicePushJob.*;
 public class KafkaInputUtils {
 
   public static KafkaClientFactory getConsumerFactory(JobConf config) {
-    Properties properties = HadoopUtils.getProps(config);
-    properties.setProperty(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, config.get(KAFKA_INPUT_BROKER_URL));
+    Properties sslProperties = null;
+    if (config.get(SSL_CONFIGURATOR_CLASS_CONFIG) != null) {
+      SSLConfigurator configurator = SSLConfigurator.getSSLConfigurator(config.get(SSL_CONFIGURATOR_CLASS_CONFIG));
+      try {
+        sslProperties = configurator.setupSSLConfig(HadoopUtils.getProps(config), UserCredentialsFactory.getHadoopUserCredentials());
+      } catch (IOException e) {
+        throw new VeniceException("Could not get user credential", e);
+      }
+    }
+    Properties consumerFactoryProperties = new Properties(sslProperties);
+    consumerFactoryProperties.setProperty(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, config.get(KAFKA_INPUT_BROKER_URL));
     /**
      * This is used to bypass the check in {@link VeniceKafkaConsumerFactory#getKafkaZkAddress}.
      */
-    properties.setProperty(ConfigKeys.KAFKA_ZK_ADDRESS, "fake_zk_address");
-    return new VeniceKafkaConsumerFactory(new VeniceProperties(properties));
+    consumerFactoryProperties.setProperty(ConfigKeys.KAFKA_ZK_ADDRESS, "fake_zk_address");
+    return new VeniceKafkaConsumerFactory(new VeniceProperties(consumerFactoryProperties));
   }
 
   public static Properties getConsumerProperties() {
