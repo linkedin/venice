linkedin/venice

commit ee3fe158e3d6893b94753e5fadf272c2f342f5b8

[server] Add instrumented Kafka admin wrapper to emit metrics
RB=2589620
BUG=VENG-7621
G=voldemort-reviewers
R=nithakka,ssen,sdwu,xhao,xnma,goliu,mnhuang,smaji,yurzhou,shuhliu,zpolicze,adietz,xyin,jlliu,fvillega,anaberez
A=fvillega,mnhuang

venice-common/src/main/java/com/linkedin/venice/kafka/KafkaClientFactory.java
@@ -1,18 +1,26 @@
 package com.linkedin.venice.kafka;
 
 import com.linkedin.venice.ConfigKeys;
+import com.linkedin.venice.kafka.admin.InstrumentedKafkaAdmin;
+import com.linkedin.venice.kafka.admin.KafkaAdminClient;
 import com.linkedin.venice.kafka.admin.KafkaAdminWrapper;
+import com.linkedin.venice.kafka.admin.ScalaAdminUtils;
 import com.linkedin.venice.kafka.consumer.ApacheKafkaConsumer;
 import com.linkedin.venice.kafka.consumer.KafkaConsumerWrapper;
 import com.linkedin.venice.utils.ReflectUtils;
+import io.tehuti.metrics.MetricsRepository;
+import java.util.Optional;
 import java.util.Properties;
 import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.log4j.Logger;
 
 import static com.linkedin.venice.ConfigConstants.*;
 
 
 public abstract class KafkaClientFactory {
+  private static final Logger LOGGER = Logger.getLogger(KafkaClientFactory.class);
+
   public KafkaConsumerWrapper getConsumer(Properties props) {
     return new ApacheKafkaConsumer(setupSSL(props));
   }
@@ -21,7 +29,7 @@ public abstract class KafkaClientFactory {
     return new KafkaConsumer<>(setupSSL(properties));
   }
 
-  public KafkaAdminWrapper getKafkaAdminClient() {
+  public KafkaAdminWrapper getKafkaAdminClient(Optional<MetricsRepository> optionalMetricsRepository) {
     KafkaAdminWrapper adminWrapper = ReflectUtils.callConstructor(
         ReflectUtils.loadClass(getKafkaAdminClass()),
         new Class[0],
@@ -33,6 +41,14 @@ public abstract class KafkaClientFactory {
       properties.put(ConfigKeys.KAFKA_ADMIN_GET_TOPIC_CONFG_MAX_RETRY_TIME_SEC, DEFAULT_KAFKA_ADMIN_GET_TOPIC_CONFIG_RETRY_IN_SECONDS);
     }
     adminWrapper.initialize(properties);
+    if (optionalMetricsRepository.isPresent()) {
+      // Use Kafka bootstrap server to identify which Kafka admin client stats it is
+      String kafkaAdminStatsName = "KafkaAdminStats_" + getKafkaBootstrapServers();
+      adminWrapper = new InstrumentedKafkaAdmin(adminWrapper, optionalMetricsRepository.get(), kafkaAdminStatsName);
+      LOGGER.info("Created instrumented topic manager for Kafka cluster with bootstrap server " + getKafkaBootstrapServers());
+    } else {
+      LOGGER.info("Created non-instrumented topic manager for Kafka cluster with bootstrap server " + getKafkaBootstrapServers());
+    }
     return adminWrapper;
   }
 

venice-common/src/main/java/com/linkedin/venice/stats/KafkaAdminWrapperStats.java
@@ -1,0 +1,54 @@
+package com.linkedin.venice.stats;
+
+import io.tehuti.metrics.MetricsRepository;
+import io.tehuti.metrics.Sensor;
+import io.tehuti.metrics.stats.Avg;
+import io.tehuti.metrics.stats.Max;
+import io.tehuti.metrics.stats.Min;
+import io.tehuti.metrics.stats.OccurrenceRate;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class KafkaAdminWrapperStats extends AbstractVeniceStats {
+
+  public enum OCCURRENCE_LATENCY_SENSOR_TYPE {
+    CREATE_TOPIC,
+    DELETE_TOPIC,
+    SET_TOPIC_CONFIG,
+    GET_ALL_TOPIC_RETENTIONS,
+    GET_TOPIC_CONFIG,
+    GET_TOPIC_CONFIG_WITH_RETRY,
+    CONTAINS_TOPIC,
+    GET_ALL_TOPIC_CONFIG,
+    IS_TOPIC_DELETION_UNDER_WAY;
+  }
+
+  private final Map<OCCURRENCE_LATENCY_SENSOR_TYPE, Sensor> sensorsByTypes;
+
+  public KafkaAdminWrapperStats(MetricsRepository metricsRepository, String name) {
+    super(metricsRepository, name);
+    Map<OCCURRENCE_LATENCY_SENSOR_TYPE, Sensor> tmpRateSensorsByTypes = new HashMap<>(OCCURRENCE_LATENCY_SENSOR_TYPE.values().length);
+    for (OCCURRENCE_LATENCY_SENSOR_TYPE sensorType : OCCURRENCE_LATENCY_SENSOR_TYPE.values()) {
+      final String sensorName = sensorType.name().toLowerCase();
+      tmpRateSensorsByTypes.put(
+          sensorType,
+          registerSensor(
+              sensorName,
+              new OccurrenceRate(),
+              new Max(),
+              new Min(),
+              new Avg(),
+              TehutiUtils.getPercentileStat(getName() + AbstractVeniceStats.DELIMITER + sensorName)
+          )
+      );
+    }
+
+    this.sensorsByTypes = Collections.unmodifiableMap(tmpRateSensorsByTypes);
+  }
+
+  public void recordLatency(OCCURRENCE_LATENCY_SENSOR_TYPE sensor_type, long requestLatencyMs) {
+    sensorsByTypes.get(sensor_type).record(requestLatencyMs);
+  }
+}

da-vinci-client/src/main/java/com/linkedin/davinci/kafka/consumer/KafkaStoreIngestionService.java
@@ -254,12 +254,12 @@ public class KafkaStoreIngestionService extends AbstractVeniceService implements
     this.topicManagerRepository = new TopicManagerRepository(
         veniceConsumerFactory.getKafkaBootstrapServers(),
         veniceConsumerFactory.getKafkaZkAddress(),
-        veniceConsumerFactory);
+        veniceConsumerFactory, metricsRepository);
 
     this.topicManagerRepositoryJavaBased = new TopicManagerRepository(
         veniceConsumerFactory.getKafkaBootstrapServers(),
         veniceConsumerFactory.getKafkaZkAddress(),
-        veniceConsumerJavaBasedFactory);
+        veniceConsumerJavaBasedFactory, metricsRepository);
 
     VeniceNotifier notifier = new LogNotifier();
     this.onlineOfflineNotifiers.add(notifier);

da-vinci-client/src/main/java/com/linkedin/davinci/store/memory/InMemoryStorageEngineFactory.java
@@ -7,6 +7,7 @@ import com.linkedin.venice.meta.PersistenceType;
 import com.linkedin.davinci.store.AbstractStorageEngine;
 import com.linkedin.davinci.store.StorageEngineFactory;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -35,7 +36,7 @@ public class InMemoryStorageEngineFactory extends StorageEngineFactory {
   @Override
   public Set<String> getPersistedStoreNames() {
     // Nothing to restore here
-    return new HashSet<>();
+    return Collections.emptySet();
   }
 
   @Override

da-vinci-client/src/main/java/com/linkedin/davinci/store/rocksdb/RocksDBStorageEngine.java
@@ -14,6 +14,7 @@ import com.linkedin.venice.utils.Utils;
 import com.linkedin.venice.utils.VeniceProperties;
 import java.io.File;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 import org.apache.commons.io.FileUtils;
@@ -93,7 +94,7 @@ class RocksDBStorageEngine extends AbstractStorageEngine<RocksDBStoragePartition
     File storeDbDir = new File(storeDbPath);
     if (!storeDbDir.exists()) {
       LOGGER.info("Store dir: " + storeDbPath + " doesn't exist");
-      return new HashSet<>();
+      return Collections.emptySet();
     }
     if (!storeDbDir.isDirectory()) {
       throw new VeniceException("Store dir: " + storeDbPath + " is not a directory!!!");

venice-common/src/main/java/com/linkedin/venice/kafka/TopicManager.java
@@ -3,19 +3,18 @@ package com.linkedin.venice.kafka;
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import com.linkedin.venice.exceptions.VeniceException;
-import com.linkedin.venice.kafka.admin.KafkaAdminClient;
 import com.linkedin.venice.kafka.admin.KafkaAdminWrapper;
-import com.linkedin.venice.kafka.admin.ScalaAdminUtils;
 import com.linkedin.venice.kafka.protocol.KafkaMessageEnvelope;
 import com.linkedin.venice.message.KafkaKey;
 import com.linkedin.venice.serialization.KafkaKeySerializer;
 import com.linkedin.venice.serialization.avro.OptimizedKafkaValueSerializer;
 import com.linkedin.venice.utils.Time;
 import com.linkedin.venice.utils.Utils;
-
+import io.tehuti.metrics.MetricsRepository;
 import java.io.Closeable;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -86,6 +85,7 @@ public class TopicManager implements Closeable {
   private final long topicMinLogCompactionLagMs;
   private final KafkaClientFactory kafkaClientFactory;
   private final boolean isConcurrentTopicDeleteRequestsEnabled;
+  private final Optional<MetricsRepository> optionalMetricsRepository;
 
   // Mutable, lazily initialized or populated, state
   private KafkaAdminWrapper kafkaAdmin;
@@ -105,7 +105,8 @@ public class TopicManager implements Closeable {
       int topicDeletionStatusPollIntervalMs,
       long topicMinLogCompactionLagMs,
       boolean isConcurrentTopicDeleteRequestsEnabled,
-      KafkaClientFactory kafkaClientFactory) {
+      KafkaClientFactory kafkaClientFactory,
+      Optional<MetricsRepository> optionalMetricsRepository) {
     this.kafkaOperationTimeoutMs = kafkaOperationTimeoutMs;
     this.topicDeletionStatusPollIntervalMs = topicDeletionStatusPollIntervalMs;
     this.topicMinLogCompactionLagMs = topicMinLogCompactionLagMs;
@@ -113,8 +114,10 @@ public class TopicManager implements Closeable {
     this.kafkaClientFactory = kafkaClientFactory;
     this.kafkaBootstrapServers = kafkaClientFactory.getKafkaBootstrapServers();
     this.kafkaZkAddress = kafkaClientFactory.getKafkaZkAddress();
+    this.optionalMetricsRepository = optionalMetricsRepository;
   }
 
+  // This constructor is used mostly for testing purpose
   public TopicManager(
       int kafkaOperationTimeoutMs,
       int topicDeletionStatusPollIntervalMs,
@@ -125,7 +128,9 @@ public class TopicManager implements Closeable {
         topicDeletionStatusPollIntervalMs,
         topicMinLogCompactionLagMs,
         DEFAULT_CONCURRENT_TOPIC_DELETION_REQUEST_POLICY,
-        kafkaClientFactory);
+        kafkaClientFactory,
+        Optional.empty()
+    );
   }
 
   /**
@@ -643,13 +648,13 @@ public class TopicManager implements Closeable {
     // since querying offset against non-existing topic could cause endless retrying.
     if (!containsTopicAndAllPartitionsAreOnline(topic)) {
       logger.warn("Topic: " + topic + " doesn't exist, returning empty map for latest offsets");
-      return new HashMap<>();
+      return Collections.emptyMap();
     }
     KafkaConsumer<byte[], byte[]> consumer = getRawBytesConsumer();
     List<PartitionInfo> partitionInfoList = consumer.partitionsFor(topic);
     if (null == partitionInfoList || partitionInfoList.isEmpty()) {
       logger.warn("Unexpected! Topic: " + topic + " has a null partition set, returning empty map for latest offsets");
-      return new HashMap<>();
+      return Collections.emptyMap();
     }
 
     Map<Integer, Long> latestOffsets = partitionInfoList.stream()
@@ -716,14 +721,14 @@ public class TopicManager implements Closeable {
     TopicPartition topicPartition = new TopicPartition(topic, partition);
     long latestOffset;
     try {
-      consumer.assign(Arrays.asList(topicPartition));
-      consumer.seekToEnd(Arrays.asList(topicPartition));
+      consumer.assign(Collections.singletonList(topicPartition));
+      consumer.seekToEnd(Collections.singletonList(topicPartition));
       latestOffset = consumer.position(topicPartition);
     } catch (org.apache.kafka.common.errors.TimeoutException ex) {
       throw new VeniceOperationAgainstKafkaTimedOut("Timeout exception when seeking to end to get latest offset"
           + " for topic: " + topic + " and partition: " + partition, ex);
     } finally {
-      consumer.assign(Arrays.asList());
+      consumer.assign(Collections.emptyList());
     }
     return latestOffset;
   }
@@ -1071,14 +1076,8 @@ public class TopicManager implements Closeable {
 
   private synchronized KafkaAdminWrapper getKafkaAdmin() {
     if (null == kafkaAdmin) {
-      String kafkaAdminName = "Unknown";
-      kafkaAdmin = kafkaClientFactory.getKafkaAdminClient();
-      if (kafkaAdmin instanceof KafkaAdminClient) {
-        kafkaAdminName = KafkaAdminClient.class.getName();
-      } else if (kafkaAdmin instanceof ScalaAdminUtils) {
-        kafkaAdminName = ScalaAdminUtils.class.getName();
-      }
-      logger.info(this.getClass().getSimpleName() + " is using kafka admin client: " + kafkaAdminName);
+      kafkaAdmin = kafkaClientFactory.getKafkaAdminClient(optionalMetricsRepository);
+      logger.info(this.getClass().getSimpleName() + " is using kafka admin client: " + kafkaAdmin.getClassName());
     }
     return kafkaAdmin;
   }

venice-common/src/main/java/com/linkedin/venice/kafka/TopicManagerRepository.java
@@ -4,8 +4,10 @@ import com.linkedin.venice.exceptions.VeniceException;
 import com.linkedin.venice.kafka.admin.ScalaAdminUtils;
 import com.linkedin.venice.utils.Pair;
 import com.linkedin.venice.utils.concurrent.VeniceConcurrentHashMap;
+import io.tehuti.metrics.MetricsRepository;
 import java.io.Closeable;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Function;
 import org.apache.log4j.Logger;
@@ -36,7 +38,9 @@ public class TopicManagerRepository implements Closeable {
       int topicDeletionStatusPollIntervalMs,
       long topicMinLogCompactionLagMs,
       boolean isConcurrentTopicDeleteRequestsEnabled,
-      KafkaClientFactory kafkaClientFactory) {
+      KafkaClientFactory kafkaClientFactory,
+      MetricsRepository metricsRepository
+  ) {
     this.localKafkaBootstrapServers = localKafkaBootstrapServers;
     this.localKafkaZkAddress = localKafkaZkAddress;
     this.kafkaOperationTimeoutMs = kafkaOperationTimeoutMs;
@@ -46,11 +50,12 @@ public class TopicManagerRepository implements Closeable {
     this.kafkaClientFactory = kafkaClientFactory;
     topicManagerCreator = (kafkaServerAndZk) ->
         new TopicManager(
-          this.kafkaOperationTimeoutMs,
-          this.topicDeletionStatusPollIntervalMs,
-          this.topicMinLogCompactionLagMs,
-          this.isConcurrentTopicDeleteRequestsEnabled,
-          this.kafkaClientFactory.clone(kafkaServerAndZk.getFirst(), kafkaServerAndZk.getSecond())
+            this.kafkaOperationTimeoutMs,
+            this.topicDeletionStatusPollIntervalMs,
+            this.topicMinLogCompactionLagMs,
+            this.isConcurrentTopicDeleteRequestsEnabled,
+            this.kafkaClientFactory.clone(kafkaServerAndZk.getFirst(), kafkaServerAndZk.getSecond()),
+            Optional.of(metricsRepository)
       );
   }
 
@@ -60,24 +65,37 @@ public class TopicManagerRepository implements Closeable {
       int kafkaOperationTimeoutMs,
       int topicDeletionStatusPollIntervalMs,
       long topicMinLogCompactionLagMs,
-      KafkaClientFactory kafkaClientFactory) {
-    this(localKafkaBootstrapServers,
+      KafkaClientFactory kafkaClientFactory,
+      MetricsRepository metricsRepository
+  ) {
+    this(
+        localKafkaBootstrapServers,
         localKafkaZkAddress,
         kafkaOperationTimeoutMs,
         topicDeletionStatusPollIntervalMs,
         topicMinLogCompactionLagMs,
         DEFAULT_CONCURRENT_TOPIC_DELETION_REQUEST_POLICY,
-        kafkaClientFactory);
+        kafkaClientFactory,
+        metricsRepository
+    );
   }
 
-  public TopicManagerRepository(String localKafkaBootstrapServers, String localKafkaZkAddress, KafkaClientFactory kafkaClientFactory) {
-    this(localKafkaBootstrapServers,
+  public TopicManagerRepository(
+      String localKafkaBootstrapServers,
+      String localKafkaZkAddress,
+      KafkaClientFactory kafkaClientFactory,
+      MetricsRepository metricsRepository
+  ) {
+    this(
+        localKafkaBootstrapServers,
         localKafkaZkAddress,
         DEFAULT_KAFKA_OPERATION_TIMEOUT_MS,
         DEFAULT_TOPIC_DELETION_STATUS_POLL_INTERVAL_MS,
         DEFAULT_KAFKA_MIN_LOG_COMPACTION_LAG_MS,
         DEFAULT_CONCURRENT_TOPIC_DELETION_REQUEST_POLICY,
-        kafkaClientFactory);
+        kafkaClientFactory,
+        metricsRepository
+    );
   }
 
   /**

venice-common/src/main/java/com/linkedin/venice/kafka/admin/InstrumentedKafkaAdmin.java
@@ -1,0 +1,159 @@
+package com.linkedin.venice.kafka.admin;
+
+import com.linkedin.venice.stats.KafkaAdminWrapperStats;
+import com.linkedin.venice.utils.SystemTime;
+import com.linkedin.venice.utils.Time;
+import com.linkedin.venice.utils.Utils;
+import io.tehuti.metrics.MetricsRepository;
+import java.io.IOException;
+import java.util.Map;
+import java.util.Properties;
+import org.apache.kafka.common.KafkaFuture;
+
+
+/**
+ * This class delegates another {@link KafkaAdminWrapper} instance and keeps track of the invocation rate of methods
+ * on the delegated instance
+ */
+public class InstrumentedKafkaAdmin implements KafkaAdminWrapper {
+  private final KafkaAdminWrapper kafkaAdmin;
+  private final KafkaAdminWrapperStats kafkaAdminWrapperStats;
+  private final Time time;
+
+
+  public InstrumentedKafkaAdmin(KafkaAdminWrapper kafkaAdmin, MetricsRepository metricsRepository, String statsName) {
+    this(kafkaAdmin, metricsRepository, statsName, new SystemTime());
+  }
+
+  public InstrumentedKafkaAdmin(
+      KafkaAdminWrapper kafkaAdmin,
+      MetricsRepository metricsRepository,
+      String statsName,
+      Time time
+  ) {
+    this.kafkaAdmin = Utils.notNull(kafkaAdmin);
+    this.time = Utils.notNull(time);
+    this.kafkaAdminWrapperStats = new KafkaAdminWrapperStats(
+        Utils.notNull(metricsRepository),
+        Utils.stringNotNullNorEmpty(statsName)
+    );
+  }
+
+  @Override
+  public void initialize(Properties properties) {
+    kafkaAdmin.initialize(properties);
+  }
+
+  @Override
+  public void createTopic(String topicName, int numPartitions, int replication, Properties topicProperties) {
+    final long startTimeMs = time.getMilliseconds();
+    kafkaAdmin.createTopic(topicName, numPartitions, replication, topicProperties);
+    kafkaAdminWrapperStats.recordLatency(
+        KafkaAdminWrapperStats.OCCURRENCE_LATENCY_SENSOR_TYPE.CREATE_TOPIC,
+        calculateDurationMs(startTimeMs)
+    );
+  }
+
+  @Override
+  public KafkaFuture<Void> deleteTopic(String topicName) {
+    final long startTimeMs = time.getMilliseconds();
+    KafkaFuture<Void> res = kafkaAdmin.deleteTopic(topicName);
+    // This latency measurement is not accurate since this is an async API. But we measure it anyways since we record
+    // the occurrence rate at least
+    kafkaAdminWrapperStats.recordLatency(
+        KafkaAdminWrapperStats.OCCURRENCE_LATENCY_SENSOR_TYPE.DELETE_TOPIC,
+        calculateDurationMs(startTimeMs)
+    );
+    return res;
+  }
+
+  @Override
+  public void setTopicConfig(String topicName, Properties topicProperties) {
+    final long startTimeMs = time.getMilliseconds();
+    kafkaAdmin.setTopicConfig(topicName, topicProperties);
+    kafkaAdminWrapperStats.recordLatency(
+        KafkaAdminWrapperStats.OCCURRENCE_LATENCY_SENSOR_TYPE.SET_TOPIC_CONFIG,
+        calculateDurationMs(startTimeMs)
+    );
+  }
+
+  @Override
+  public Map<String, Long> getAllTopicRetentions() {
+    final long startTimeMs = time.getMilliseconds();
+    final Map<String, Long> res = kafkaAdmin.getAllTopicRetentions();
+    kafkaAdminWrapperStats.recordLatency(
+        KafkaAdminWrapperStats.OCCURRENCE_LATENCY_SENSOR_TYPE.GET_ALL_TOPIC_RETENTIONS,
+        calculateDurationMs(startTimeMs)
+    );
+    return res;
+  }
+
+  @Override
+  public Properties getTopicConfig(String topicName) {
+    final long startTimeMs = time.getMilliseconds();
+    final Properties res = kafkaAdmin.getTopicConfig(topicName);
+    kafkaAdminWrapperStats.recordLatency(
+        KafkaAdminWrapperStats.OCCURRENCE_LATENCY_SENSOR_TYPE.GET_TOPIC_CONFIG,
+        calculateDurationMs(startTimeMs)
+    );
+    return res;
+  }
+
+  @Override
+  public Properties getTopicConfigWithRetry(String topicName) {
+    final long startTimeMs = time.getMilliseconds();
+    final Properties res = kafkaAdmin.getTopicConfigWithRetry(topicName);
+    kafkaAdminWrapperStats.recordLatency(
+        KafkaAdminWrapperStats.OCCURRENCE_LATENCY_SENSOR_TYPE.GET_TOPIC_CONFIG_WITH_RETRY,
+        calculateDurationMs(startTimeMs)
+    );
+    return res;
+  }
+
+  @Override
+  public boolean containsTopic(String topic) {
+    final long startTimeMs = time.getMilliseconds();
+    final boolean res = kafkaAdmin.containsTopic(topic);
+    kafkaAdminWrapperStats.recordLatency(
+        KafkaAdminWrapperStats.OCCURRENCE_LATENCY_SENSOR_TYPE.CONTAINS_TOPIC,
+        calculateDurationMs(startTimeMs)
+    );
+    return res;
+  }
+
+  @Override
+  public Map<String, Properties> getAllTopicConfig() {
+    final long startTimeMs = time.getMilliseconds();
+    final Map<String, Properties> res = kafkaAdmin.getAllTopicConfig();
+    kafkaAdminWrapperStats.recordLatency(
+        KafkaAdminWrapperStats.OCCURRENCE_LATENCY_SENSOR_TYPE.GET_ALL_TOPIC_CONFIG,
+        calculateDurationMs(startTimeMs)
+    );
+    return res;
+  }
+
+  @Override
+  public boolean isTopicDeletionUnderway() {
+    final long startTimeMs = time.getMilliseconds();
+    final boolean res = kafkaAdmin.isTopicDeletionUnderway();
+    kafkaAdminWrapperStats.recordLatency(
+        KafkaAdminWrapperStats.OCCURRENCE_LATENCY_SENSOR_TYPE.IS_TOPIC_DELETION_UNDER_WAY,
+        calculateDurationMs(startTimeMs)
+    );
+    return res;
+  }
+
+  @Override
+  public void close() throws IOException {
+    kafkaAdmin.close();
+  }
+
+  @Override
+  public String getClassName() {
+    return String.format("%s delegated by %s", kafkaAdmin.getClassName(), InstrumentedKafkaAdmin.class.getName());
+  }
+
+  private long calculateDurationMs(final long startTimeMs) {
+    return time.getMilliseconds() - startTimeMs;
+  }
+}

commit 2833328e384dba6c41e0e27eff1bcd880e1891db

[controller][server] Fixed metric naming in KafkaAdminWrapperStats
This RB fixs a regression from ee3fe158. There are two issues:
1. Need to check whether a metric is already registered;
2. Metric name includes hostname which contains invalid characters.

RB=2591843
G=voldemort-reviewers
R=anaberez,ssen,goliu,xhao,xnma,shuhliu,smaji,zpolicze,fvillega,lcli,yurzhou,xyin,sdwu,adietz,jlliu,nithakka
A=lcli

venice-common/src/main/java/com/linkedin/venice/kafka/KafkaClientFactory.java
@@ -7,6 +7,7 @@ import com.linkedin.venice.kafka.admin.KafkaAdminWrapper;
 import com.linkedin.venice.kafka.admin.ScalaAdminUtils;
 import com.linkedin.venice.kafka.consumer.ApacheKafkaConsumer;
 import com.linkedin.venice.kafka.consumer.KafkaConsumerWrapper;
+import com.linkedin.venice.stats.TehutiUtils;
 import com.linkedin.venice.utils.ReflectUtils;
 import io.tehuti.metrics.MetricsRepository;
 import java.util.Optional;
@@ -43,7 +44,7 @@ public abstract class KafkaClientFactory {
     adminWrapper.initialize(properties);
     if (optionalMetricsRepository.isPresent()) {
       // Use Kafka bootstrap server to identify which Kafka admin client stats it is
-      String kafkaAdminStatsName = "KafkaAdminStats_" + getKafkaBootstrapServers();
+      String kafkaAdminStatsName = "KafkaAdminStats_" + TehutiUtils.fixMalformedMetricName(getKafkaBootstrapServers());
       adminWrapper = new InstrumentedKafkaAdmin(adminWrapper, optionalMetricsRepository.get(), kafkaAdminStatsName);
       LOGGER.info("Created instrumented topic manager for Kafka cluster with bootstrap server " + getKafkaBootstrapServers());
     } else {

venice-common/src/main/java/com/linkedin/venice/stats/KafkaAdminWrapperStats.java
@@ -34,7 +34,7 @@ public class KafkaAdminWrapperStats extends AbstractVeniceStats {
       final String sensorName = sensorType.name().toLowerCase();
       tmpRateSensorsByTypes.put(
           sensorType,
-          registerSensor(
+          registerSensorIfAbsent(
               sensorName,
               new OccurrenceRate(),
               new Max(),

venice-schema-common/src/main/java/com/linkedin/venice/stats/TehutiUtils.java
@@ -108,6 +108,15 @@ public class TehutiUtils {
   }
 
   /**
+   * A valid metric name needs to pass the test in {@link javax.management.ObjectName}. This helper function will
+   * try to fix all invalid character mentioned in the above function to avoid MalformedObjectNameException; besides,
+   * dot(.) will also be replaced since dot is a separator used in InGraph.
+   */
+  public static String fixMalformedMetricName(String metricName) {
+    return metricName.replaceAll("[\\*?=:\".]", "_");
+  }
+
+  /**
    * Generate a ratio stat that is based on two arbitrary {@link Rate}. It calculates the proportion of one
    * Stat over the sum of two Stats. This method is mostly used to calculate the bad request ratio (bad / (good + bad))
    *