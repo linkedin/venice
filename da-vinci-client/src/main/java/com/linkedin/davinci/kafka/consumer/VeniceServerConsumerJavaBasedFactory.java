5ac1ef49c5f21f32b46ef276b512ff80523fce7c
Subhadeep Sen <ssen@linkedin.com>
2021-05-26 12:52:42 -0700

    [server] Fix an issue in leader offset measurement from remote kafka
    In this commit 2a6041e9d01c59aefdfe7e6da678d1ec52afa06c the intention
    was to measure offset from remote kafka using Java based Kafka admin
    client without depending on kafka.admin.class config. But the change
    didn't work correctly because of the following reason.
    Internally TopicManagerRepository makes a clone of the passed
    KafkaClientFactory. Since VeniceServerConsumerJavaBasedFactory didn't
    implement the clone api the TopicManager never got an instance of this class.
    It got an instance of VeniceServerConsumerFactory class instead. Now
    depending on kafka.admin.class config this VeniceServerConsumerFactory class
    works either works with Scala based admin client prod or java based
    admin client (in prod this config points to scala client)
    If it's using scala client it makes the containsTopic check to fail in getLatestOffset()
    and finally no offset could be measured.
    
    This has been a miss during testing as in integration test we always use the
    java based admin client and in EI also java based admin client is
    enabled.
    
    RB=2612768
    BUG=VENG-7709
    G=voldemort-reviewers
    R=mnhuang,xhao,goliu,xnma,yurzhou,zpolicze,fvillega,adietz,lcli,smaji,anaberez,xyin,shuhliu,jlliu,nithakka,sdwu
    A=mnhuang

--- da-vinci-client/src/main/java/com/linkedin/davinci/kafka/consumer/VeniceServerConsumerJavaBasedFactory.java
+++ da-vinci-client/src/main/java/com/linkedin/davinci/kafka/consumer/VeniceServerConsumerJavaBasedFactory.java
@@ -1,7 +1,12 @@
 package com.linkedin.davinci.kafka.consumer;
 
 import com.linkedin.davinci.config.VeniceServerConfig;
+import com.linkedin.venice.kafka.KafkaClientFactory;
 import com.linkedin.venice.kafka.admin.KafkaAdminClient;
+import com.linkedin.venice.utils.VeniceProperties;
+import java.util.Properties;
+
+import static com.linkedin.venice.ConfigKeys.*;
 
 
 public class VeniceServerConsumerJavaBasedFactory extends VeniceServerConsumerFactory {
@@ -12,5 +17,13 @@ public class VeniceServerConsumerJavaBasedFactory extends VeniceServerConsumerFa
   @Override
   protected String getKafkaAdminClass() {
     return KafkaAdminClient.class.getName();
+  }
+
+  @Override
+  protected KafkaClientFactory clone(String kafkaBootstrapServers, String kafkaZkAddress) {
+    Properties clonedProperties = this.serverConfig.getClusterProperties().toProperties();
+    clonedProperties.setProperty(KAFKA_BOOTSTRAP_SERVERS, kafkaBootstrapServers);
+    clonedProperties.setProperty(KAFKA_ZK_ADDRESS, kafkaZkAddress);
+    return new VeniceServerConsumerJavaBasedFactory(new VeniceServerConfig(new VeniceProperties(clonedProperties)));
   }
 }
