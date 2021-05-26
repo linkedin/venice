86a3b9b5515a633d283bc098526f28362ea1d414
Hao Xu <xhao@linkedin.com>
2021-05-26 12:33:47 -0700

    [server][consumer] Improvement on shared kafka consumer assignment by considering both topics and partitions.
    Previously when KafkaConsumerService assign consumer, we only concern partitions, by finding shared consumer from consumer pool
    with least partitions. However, there is a delay between StoreIngestionTask consumer ask (topic attach) from the KafkaConsumerService
    and the actual subscription of partitions. During startup, several StoreIngestionTasks asking consumer in a short time range,
    it could cause first consumer from consumer pool is assigned to several StoreIngestionTasks while other consumer is zero loaded.
    Then there will be a huge imbalance in partitions for these consumers. It will cause low ingestion speed because kafka consumer with too
    many partitions will have terrible performance.
    
    This improvment can resolve this issue by:
    1) Always assign zero-loaded consumer to new StoreIngestionTask when we have zero-loaded StoreIngestionTask in topic.
    2) If there is no zero-loaded StoreIngestionTask, we will find consumer with least loaded StoreIngestionTask in partitions.
    
    Since we will have >10 consumers avaible in the pool, this will mitigate the delay issue of topic attach and partiiton subscription.
    
    Also add unit test for KafkaConsumerService for this feature.
    
    RB=2614247

--- da-vinci-client/src/test/java/com/linkedin/davinci/kafka/consumer/KafkaConsumerServiceTest.java
+++ da-vinci-client/src/test/java/com/linkedin/davinci/kafka/consumer/KafkaConsumerServiceTest.java
@@ -1,18 +1,18 @@
 package com.linkedin.davinci.kafka.consumer;
 
-
-import com.linkedin.davinci.kafka.consumer.KafkaConsumerService;
-import com.linkedin.davinci.kafka.consumer.StoreIngestionTask;
+import com.linkedin.davinci.stats.KafkaConsumerServiceStats;
 import com.linkedin.venice.exceptions.VeniceException;
 import com.linkedin.venice.kafka.KafkaClientFactory;
 import com.linkedin.venice.kafka.consumer.KafkaConsumerWrapper;
 import com.linkedin.venice.meta.Version;
-import com.linkedin.davinci.stats.KafkaConsumerServiceStats;
 import com.linkedin.venice.throttle.EventThrottler;
 import com.linkedin.venice.utils.TestUtils;
+import java.util.HashSet;
 import java.util.Properties;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import org.apache.kafka.clients.CommonClientConfigs;
+import org.apache.kafka.common.TopicPartition;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -20,6 +20,73 @@ import static org.mockito.Mockito.*;
 
 
 public class KafkaConsumerServiceTest {
+
+  @Test
+  public void testGetConsumer() throws Exception {
+    SharedKafkaConsumer consumer1 = mock(SharedKafkaConsumer.class);
+    when(consumer1.hasSubscription()).thenReturn(true);
+
+    SharedKafkaConsumer consumer2 = mock(SharedKafkaConsumer.class);
+    when(consumer2.hasSubscription()).thenReturn(true);
+
+    String storeName1 = TestUtils.getUniqueString("test_consumer_service1");
+    StoreIngestionTask task1 = mock(StoreIngestionTask.class);
+    String topicForStoreName1 = Version.composeKafkaTopic(storeName1, 1);
+    when(task1.getVersionTopic()).thenReturn(topicForStoreName1);
+    when(task1.isHybridMode()).thenReturn(true);
+
+    String storeName2 = TestUtils.getUniqueString("test_consumer_service2");
+    String topicForStoreName2 = Version.composeKafkaTopic(storeName2, 1);
+    StoreIngestionTask task2 = mock(StoreIngestionTask.class);
+    when(task2.getVersionTopic()).thenReturn(topicForStoreName2);
+    when(task2.isHybridMode()).thenReturn(true);
+
+
+
+    KafkaClientFactory factory = mock(KafkaClientFactory.class);
+    when(factory.getConsumer(any())).thenReturn(consumer1, consumer2);
+
+    Properties properties = new Properties();
+    properties.put(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, "test_kafka_url");
+
+    KafkaConsumerService consumerService = new KafkaConsumerService(factory, properties, 1000l,
+        2, mock(EventThrottler.class), mock(EventThrottler.class), mock(KafkaConsumerServiceStats.class),
+        TimeUnit.MINUTES.toMillis(1));
+
+    KafkaConsumerWrapper assignedConsumerForTask1 = consumerService.getConsumer(task1);
+    KafkaConsumerWrapper assignedConsumerForTask2 = consumerService.getConsumer(task2);
+    Assert.assertNotEquals(assignedConsumerForTask1, assignedConsumerForTask2,
+        "We should avoid to share consumer when there is consumer not assigned topic.");
+
+    // Get partitions assigned to those two consumers
+    Set<TopicPartition> consumer1AssignedPartitions = getTopicPartitionsSet(topicForStoreName1, 5);
+    when(consumer1.getAssignment()).thenReturn(consumer1AssignedPartitions);
+
+    Set<TopicPartition> consumer2AssignedPartitions = getTopicPartitionsSet(topicForStoreName2, 3);
+
+    when(consumer2.getAssignment()).thenReturn(consumer2AssignedPartitions);
+    SharedKafkaConsumer sharedAssignedConsumerForTask1 = (SharedKafkaConsumer) assignedConsumerForTask1;
+    sharedAssignedConsumerForTask1.setCurrentAssignment(consumer1AssignedPartitions);
+    SharedKafkaConsumer sharedAssignedConsumerForTask2 = (SharedKafkaConsumer) assignedConsumerForTask2;
+    sharedAssignedConsumerForTask2.setCurrentAssignment(consumer2AssignedPartitions);
+
+    String storeName3 = TestUtils.getUniqueString("test_consumer_service3");
+    String topicForStoreName3 = Version.composeKafkaTopic(storeName3, 1);
+    StoreIngestionTask task3 = mock(StoreIngestionTask.class);
+    when(task3.getVersionTopic()).thenReturn(topicForStoreName3);
+    when(task3.isHybridMode()).thenReturn(true);
+    KafkaConsumerWrapper assignedConsumerForTask3 = consumerService.getConsumer(task3);
+    Assert.assertEquals(assignedConsumerForTask3, assignedConsumerForTask2,
+        "The assigned consumer should come with least partitions, when no zero loaded consumer available.");
+  }
+
+  private Set<TopicPartition> getTopicPartitionsSet(String topic, int partitionNum) {
+    Set<TopicPartition> topicPartitionsSet = new HashSet<>();
+    for (int i = 0; i < partitionNum; i++) {
+      topicPartitionsSet.add(new TopicPartition(topic, i));
+    }
+    return topicPartitionsSet;
+  }
 
   @Test
   public void testGetConsumerForHybridMode() throws Exception {
