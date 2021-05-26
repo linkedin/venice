5b741705ccdf65899f5de8c653d6dc8813027ff1
xnma <xnma@linkedin.com>
2021-05-26 14:27:04 -0700

    [server] remove getEventTimeEpochMs to prevent ConcurrentModificationException
    RB=2614448
    BUG=VOLDCS-5302
    G=voldemort-reviewers
    R=jlliu,shuhliu,lcli,mnhuang,xhao,sdwu,nithakka,xyin,zpolicze,anaberez,adietz,smaji,yurzhou,fvillega,goliu,ssen
    A=fvillega

--- venice-common/src/test/java/com/linkedin/venice/offsets/TestOffsetRecord.java
+++ venice-common/src/test/java/com/linkedin/venice/offsets/TestOffsetRecord.java
@@ -24,38 +24,4 @@ public class TestOffsetRecord {
     Assert.assertEquals(offsetRecord2, offsetRecord1);
   }
 
-  @Test
-  public void testGetEventTimeEpochMs() {
-    OffsetRecord offsetRecord = new OffsetRecord(AvroProtocolDefinition.PARTITION_STATE.getSerializer());
-
-    VeniceProperties veniceProperties = new VeniceProperties(new Properties());
-    GUID producerGuid1 = GuidUtils.getGUID(veniceProperties);
-    GUID producerGuid2 = GuidUtils.getGUID(veniceProperties);
-
-    // Sanity check
-    Assert.assertFalse(producerGuid1.equals(producerGuid2), "Producer GUIDs should be unique!");
-
-    long firstTimestamp = 1;
-    long secondTimestamp = 2;
-    long thirdTimestamp = 3;
-
-    ProducerPartitionState producerPartitionState1 = new ProducerPartitionState();
-    producerPartitionState1.messageTimestamp = firstTimestamp;
-
-    ProducerPartitionState producerPartitionState2 = new ProducerPartitionState();
-    producerPartitionState2.messageTimestamp = secondTimestamp;
-
-    offsetRecord.setProducerPartitionState(producerGuid1, producerPartitionState1);
-    offsetRecord.setProducerPartitionState(producerGuid2, producerPartitionState2);
-
-    Assert.assertEquals(offsetRecord.getEventTimeEpochMs(), secondTimestamp,
-        "OffsetRecord should return the latest timestamp that it has seen!");
-
-    producerPartitionState1.messageTimestamp = thirdTimestamp;
-
-    offsetRecord.setProducerPartitionState(producerGuid1, producerPartitionState1);
-
-    Assert.assertEquals(offsetRecord.getEventTimeEpochMs(), thirdTimestamp,
-        "OffsetRecord should return the latest timestamp that it has seen!");
-  }
 }
