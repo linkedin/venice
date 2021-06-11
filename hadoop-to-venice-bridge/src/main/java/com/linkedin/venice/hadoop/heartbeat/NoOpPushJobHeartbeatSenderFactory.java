40894bed086e040ba3eeab91ac0da7b7e34a8fca
Lincong Li <lcli@linkedin.com>
2021-06-11 16:49:48 -0700

    [h2v] Pass SSL properties to VeniceWriter in PushJobHeartbeatSender
    RB=2642049
    G=voldemort-reviewers
    R=anaberez,smaji,yurzhou,fvillega,xhao,zpolicze,ssen,goliu,nithakka,jlliu,sdwu,adietz,shuhliu,xnma,xyin,mnhuang
    A=nithakka

--- hadoop-to-venice-bridge/src/main/java/com/linkedin/venice/hadoop/heartbeat/NoOpPushJobHeartbeatSenderFactory.java
+++ hadoop-to-venice-bridge/src/main/java/com/linkedin/venice/hadoop/heartbeat/NoOpPushJobHeartbeatSenderFactory.java
@@ -1,16 +1,17 @@
 package com.linkedin.venice.hadoop.heartbeat;
 
 import com.linkedin.venice.controllerapi.ControllerClient;
-import com.linkedin.venice.security.SSLFactory;
 import com.linkedin.venice.utils.VeniceProperties;
 import java.util.Optional;
+import java.util.Properties;
+
 
 public class NoOpPushJobHeartbeatSenderFactory implements PushJobHeartbeatSenderFactory {
     @Override
     public PushJobHeartbeatSender createHeartbeatSender(
         VeniceProperties properties,
         ControllerClient controllerClient,
-        Optional<SSLFactory> sslFactory
+        Optional<Properties> sslProperties
     ) {
         return new NoOpPushJobHeartbeatSender();
     }
