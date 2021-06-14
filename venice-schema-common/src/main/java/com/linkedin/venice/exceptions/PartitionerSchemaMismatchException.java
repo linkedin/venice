4ff056544f4ab7b39f1d20aaeadf06b4cea0d3ba
Zac Policzer <zpoliczer@linkedin.com>
2021-06-14 14:26:33 -0700

    [davinci]Tweaks to partitioner interfaces for checkSchema
    In response to some usability disucssion with Alexei, we just make it required that checkSchema should throw an exception.
    
    We also add some needed constructors to the new exception type.
    
    RB=2644771
    G=voldemort-reviewers
    R=jlliu,sdwu,smaji,anaberez,fvillega,shuhliu,lcli,xnma,ssen,mnhuang,yurzhou,adietz,nithakka,goliu,xhao,xyin
    A=anaberez

--- venice-schema-common/src/main/java/com/linkedin/venice/exceptions/PartitionerSchemaMismatchException.java
+++ venice-schema-common/src/main/java/com/linkedin/venice/exceptions/PartitionerSchemaMismatchException.java
@@ -5,6 +5,18 @@ import org.apache.http.HttpStatus;
 
 public class PartitionerSchemaMismatchException extends VeniceException {
 
+  public PartitionerSchemaMismatchException(){
+    super();
+  }
+  public PartitionerSchemaMismatchException(String s){
+    super(s);
+  }
+  public PartitionerSchemaMismatchException(Throwable t){
+    super(t);
+  }
+  public PartitionerSchemaMismatchException(String s, Throwable t){
+    super(s,t);
+  }
   public int getHttpStatusCode(){
     return HttpStatus.SC_BAD_REQUEST;
   }
