9824923ad88aa0498e112a14d1481479d6725c5b
Angela Dietz <adietz@adietz-mn1.linkedin.biz>
2021-06-15 09:12:05 -0700

    [davinci] Implement Venice Predicates
    Created interface for Venice predicates and implementations for AND and EQUALS predicates.
    Created PredicateBuilder class containing user facing static methods which return new Predicate objects.
    This design currently allows users to create predicates in the following way:
    Predicate predicate = and(
            equalTo("profileGeo", "us"),
            equalTo("profileOccupationId", 1)
        );
    
    RB=2646730
    BUG=VENG-7777
    G=voldemort-reviewers
    R=goliu,nithakka,smaji,fvillega,jlliu,xyin,yurzhou,sdwu,zpolicze,shuhliu,anaberez,ssen,xhao,xnma,lcli,mnhuang
    A=smaji,shuhliu,fvillega

---
+++ da-vinci-client/src/main/java/com/linkedin/davinci/predicate/PredicateBuilder.java
@@ -0,0 +1,17 @@
+package com.linkedin.davinci.predicate;
+
+import com.linkedin.venice.annotation.Experimental;
+
+
+public class PredicateBuilder {
+
+  @Experimental
+  public static Predicate and(Predicate... predicates){
+    return new AndPredicate(predicates);
+  }
+
+  @Experimental
+  public static Predicate equalTo(String fieldName, Object expectedValue){
+    return new EqualsRelationalOperator(fieldName, expectedValue);
+  }
+}
