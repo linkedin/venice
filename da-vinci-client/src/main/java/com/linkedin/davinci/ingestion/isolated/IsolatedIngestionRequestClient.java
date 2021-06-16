f5864a7748905302825c30ec4b179c38d1683423
Jialin Liu <jlliu@linkedin.com>
2021-06-16 11:07:18 -0700

    [da-vinci][server] Replace Netty client with HttpAsyncClient to provide request timeout feature for ingestion isolation
    This RB uses HttpAsyncClient to serve as http transport implementation to replace existing Netty async client implementation. This HttpAsyncClient provides easy blocking access and timeout feature, so ingestion request client won't stuck forever in any case.
    
    Also, this RB adds ThreadLocal layer to ingestion client in IsolatedIngestionBackend to provide thread safety for mutli-thread access.
    
    RB=2660664
    BUG=VENG-7199
    G=voldemort-reviewers
    R=anaberez,sdwu,yurzhou,smaji,lcli,adietz,xhao,xyin,shuhliu,zpolicze,fvillega,nithakka,mnhuang,ssen,xnma,goliu
    A=lcli

--- da-vinci-client/src/main/java/com/linkedin/davinci/ingestion/isolated/IsolatedIngestionRequestClient.java
+++ da-vinci-client/src/main/java/com/linkedin/davinci/ingestion/isolated/IsolatedIngestionRequestClient.java
@@ -1,11 +1,12 @@
 package com.linkedin.davinci.ingestion.isolated;
 
-import com.linkedin.davinci.ingestion.IngestionRequestTransport;
+import com.linkedin.davinci.ingestion.HttpClientTransport;
 import com.linkedin.venice.ingestion.protocol.IngestionTaskReport;
 import com.linkedin.venice.ingestion.protocol.enums.IngestionAction;
 import com.linkedin.venice.ingestion.protocol.enums.IngestionReportType;
 import java.io.Closeable;
 import org.apache.log4j.Logger;
+
 
 /**
  * IsolatedIngestionRequestClient sends requests to monitor service in main process and retrieves responses.
@@ -13,10 +14,10 @@ import org.apache.log4j.Logger;
 public class IsolatedIngestionRequestClient implements Closeable {
   private static final Logger logger = Logger.getLogger(IsolatedIngestionRequestClient.class);
 
-  private final IngestionRequestTransport ingestionRequestTransport;
+  private final HttpClientTransport httpClientTransport;
 
   public IsolatedIngestionRequestClient(int port) {
-    ingestionRequestTransport = new IngestionRequestTransport(port);
+    httpClientTransport = new HttpClientTransport(port);
   }
 
   public void reportIngestionStatus(IngestionTaskReport report) {
@@ -26,7 +27,7 @@ public class IsolatedIngestionRequestClient implements Closeable {
     logger.info(String.format("Sending ingestion report %s, isPositive: %b, message: %s for partition: %d of topic: %s at offset: %d",
         IngestionReportType.valueOf(report.reportType), report.isPositive, report.message, partitionId, topicName, report.offset));
     try {
-      ingestionRequestTransport.sendRequest(IngestionAction.REPORT, report);
+      httpClientTransport.sendRequest(IngestionAction.REPORT, report);
     } catch (Exception e) {
       logger.warn("Failed to send report with exception for topic: " + topicName + ", partition: " + partitionId , e);
     }
@@ -34,6 +35,6 @@ public class IsolatedIngestionRequestClient implements Closeable {
 
   @Override
   public void close() {
-    ingestionRequestTransport.close();
+    httpClientTransport.close();
   }
 }
