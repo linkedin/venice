5ab9f4d4bdd2c564c9e312c3565cc2b57c6ef8ce
Sourav Maji <smaji@linkedin.com>
2021-06-24 12:09:04 -0700

    [da-vinci-client] Separate the drainer metrics for sorted and unsorted ingestions
    RB=2666492
    BUG=VENG-7870
    G=voldemort-reviewers
    R=jlliu,goliu,yurzhou,xhao,xyin,nithakka,anaberez,ssen,fvillega,lcli,xnma,sdwu,zpolicze,adietz,mnhuang,shuhliu
    A=mnhuang

--- da-vinci-client/src/test/java/com/linkedin/davinci/kafka/consumer/StoreBufferServiceTest.java
+++ da-vinci-client/src/test/java/com/linkedin/davinci/kafka/consumer/StoreBufferServiceTest.java
@@ -1,5 +1,6 @@
 package com.linkedin.davinci.kafka.consumer;
 
+import com.linkedin.davinci.config.VeniceServerConfig;
 import com.linkedin.venice.exceptions.VeniceChecksumException;
 import com.linkedin.venice.exceptions.VeniceException;
 import com.linkedin.venice.kafka.protocol.KafkaMessageEnvelope;
@@ -78,6 +79,45 @@ public class StoreBufferServiceTest {
     bufferService.putConsumerRecord(cr, mockTask, null);
     bufferService.internalDrainBufferedRecordsFromTopicPartition(topic, partition, 3, 50);
     Assert.fail("Exception should be thrown here");
+  }
+
+  @Test
+  public void testGetDrainerIndexForConsumerRecordSeparateDrainer() {
+    String topic = TestUtils.getUniqueString("test_topic");
+    int partitionCount = 32;
+    int drainerNum = 16;
+    int[] drainerPartitionCount = new int[drainerNum];
+    for (int i = 0; i < drainerNum; ++i) {
+      drainerPartitionCount[i] = 0;
+    }
+    VeniceServerConfig serverConfig = mock(VeniceServerConfig.class);
+    doReturn(8).when(serverConfig).getDrainerPoolSizeSortedInput();
+    doReturn(8).when(serverConfig).getDrainerPoolSizeUnsortedInput();
+    doReturn(1000l).when(serverConfig).getStoreWriterBufferNotifyDelta();
+    doReturn(10000l).when(serverConfig).getStoreWriterBufferMemoryCapacity();
+
+
+    SeparatedStoreBufferService bufferService = new SeparatedStoreBufferService(serverConfig);
+    for (int partition = 0; partition < partitionCount; ++partition) {
+      ConsumerRecord<KafkaKey, KafkaMessageEnvelope> cr = new ConsumerRecord<>(topic, partition, 100, null, null);
+      int drainerIndex;
+      if (partition < 16) {
+        drainerIndex = bufferService.sortedServiceDelegate.getDrainerIndexForConsumerRecord(cr);
+        ++drainerPartitionCount[drainerIndex];
+      } else {
+        drainerIndex = bufferService.unsortedServiceDelegate.getDrainerIndexForConsumerRecord(cr);
+        ++drainerPartitionCount[drainerIndex + 8];
+      }
+    }
+
+    for (int i = 0; i < drainerNum; i++) {
+      Assert.assertNotNull(bufferService.getDrainerQueueMemoryUsage(i));
+    }
+     
+    int avgPartitionCountPerDrainer = partitionCount / drainerNum;
+    for (int i = 0; i < drainerNum; ++i) {
+      Assert.assertEquals(drainerPartitionCount[i], avgPartitionCountPerDrainer);
+    }
   }
 
   @Test
