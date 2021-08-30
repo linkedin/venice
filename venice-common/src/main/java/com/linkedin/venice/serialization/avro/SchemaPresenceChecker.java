a803f09b329f183413f410e63dbd7865e1ca1adc
Xun Yin <xyin@linkedin.com>
2021-08-30 11:42:19 -0700

    [venice-server] Add meta system store schema check during SN start-up
    Check the meta system store schema version to make sure it exists in the schema repository
    during SN start-up to prevent out of order deployment. i.e. server deployed with a schema
    change before controller gets a chance to evolve the new schema.
    
    RB=2789909
    BUG=VENG-8061
    R=lcli,goliu,jlliu,nithakka,mnhuang,anaberez,shuhliu,fvillega,ssen,smaji,yurzhou,zpolicze,xhao,adietz,sdwu,akozma,xnma
    A=ssen

--- venice-common/src/main/java/com/linkedin/venice/serialization/avro/SchemaPresenceChecker.java
+++ venice-common/src/main/java/com/linkedin/venice/serialization/avro/SchemaPresenceChecker.java
@@ -28,16 +28,18 @@ public class SchemaPresenceChecker {
     this.avroProtocolDefinition = avroProtocolDefinition;
   }
 
-
   private void verifySchemaIsPresent(int protocolVersion, boolean retry) {
     for (int attempt = 1; attempt <= MAX_ATTEMPTS_FOR_SCHEMA_READER; attempt++) {
       try {
         Schema newProtocolSchema = schemaReader.getValueSchema(protocolVersion);
         if (null == newProtocolSchema) {
           throw new VeniceMessageException(
-              "Failed to retrieve protocol version '" + protocolVersion + " with remote fetch using " + SchemaReader.class.getSimpleName());
+              "Failed to retrieve protocol version '" + protocolVersion + "' with remote fetch using "
+                  + SchemaReader.class.getSimpleName() + " for " + avroProtocolDefinition.name());
         }
-        LOGGER.info("Discovered new protocol version '" + protocolVersion + "', and successfully retrieved it.");
+        LOGGER.info(
+            "Discovered new protocol version '" + protocolVersion + "', and successfully retrieved it for protocol "
+                + avroProtocolDefinition.name());
         LOGGER.debug("Schema:\n" + newProtocolSchema.toString(true));
         break;
       } catch (Exception e) {
@@ -45,9 +47,10 @@ public class SchemaPresenceChecker {
           throw new VeniceException("Failed to retrieve new protocol schema version (" + protocolVersion + ") after "
               + MAX_ATTEMPTS_FOR_SCHEMA_READER + " attempts.", e);
         }
-        LOGGER.error("Caught an exception while trying to fetch a new protocol schema version (" + protocolVersion
-            + "). Attempt #" + attempt + "/" + MAX_ATTEMPTS_FOR_SCHEMA_READER + ". Will sleep "
-            + WAIT_TIME_BETWEEN_SCHEMA_READER_ATTEMPTS_IN_MS + " ms and try again.", e);
+        LOGGER.error("Caught an exception while trying to fetch a new protocol schema version (protocol: "
+            + avroProtocolDefinition.name() + ", version: " + protocolVersion + "). Attempt #" + attempt + "/"
+            + MAX_ATTEMPTS_FOR_SCHEMA_READER + ". Will sleep " + WAIT_TIME_BETWEEN_SCHEMA_READER_ATTEMPTS_IN_MS
+            + " ms and try again.", e);
         Utils.sleep(WAIT_TIME_BETWEEN_SCHEMA_READER_ATTEMPTS_IN_MS);
       }
     }
@@ -60,14 +63,16 @@ public class SchemaPresenceChecker {
    * @param retry -- should retry fetching the schema or not
    */
   public void verifySchemaVersionPresentOrExit(Optional<Integer> protocolVersion, boolean retry) {
-    int version = protocolVersion.isPresent() ? protocolVersion.get() : avroProtocolDefinition.getCurrentProtocolVersion();
+    int version =
+        protocolVersion.isPresent() ? protocolVersion.get() : avroProtocolDefinition.getCurrentProtocolVersion();
     try {
       verifySchemaIsPresent(version, retry);
       LOGGER.info("SchemaPresenceChecker: The schema " + avroProtocolDefinition.name() + " current version " + version
           + " is found");
     } catch (VeniceException e) {
-      String errorMsg = "SchemaVersionNotFound: The schema " + avroProtocolDefinition.name() + " current version " + version
-          + " is not present in ZK, exiting application";
+      String errorMsg =
+          "SchemaVersionNotFound: The schema " + avroProtocolDefinition.name() + " current version " + version
+              + " is not present in ZK, exiting application";
       LOGGER.fatal(errorMsg);
       throw new VeniceException(errorMsg, e);
     }
