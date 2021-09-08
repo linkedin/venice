8a82455182109bd1102e11ffa214850c9658bdaf
Sourav Maji <smaji@linkedin.com>
2021-09-08 11:51:33 -0700

    Update Map compare to allow comparision of key by key
    RB=2810732
    BUG=VENG-8173
    G=voldemort-reviewers
    R=xyin,jlliu,nithakka,fvillega,zpolicze,anaberez,akozma,ssen,yurzhou,mnhuang,shuhliu,xnma,sdwu,lcli,xhao,goliu,adietz
    A=goliu

--- venice-common/src/main/java/com/linkedin/venice/utils/AvroCompatibilityUtils.java
+++ venice-common/src/main/java/com/linkedin/venice/utils/AvroCompatibilityUtils.java
@@ -3,7 +3,7 @@ package com.linkedin.venice.utils;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.Map;
-import org.apache.avro.AvroRuntimeException;
+import java.util.Set;
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericData;
 import org.apache.avro.generic.IndexedRecord;
@@ -17,26 +17,26 @@ public class AvroCompatibilityUtils {
    * Backport compareTo() method from SpecificRecordBase.
    */
   public static boolean compare(Object o1, Object o2) {
-    return compareSpecificData(o1, o2, ((SpecificRecordBase)o1).getSchema(), true) == 0;
+    return compareSpecificData(o1, o2, ((SpecificRecordBase)o1).getSchema()) == 0;
   }
 
   /**
    * Backport compare() method implementation from SpecificData in avro v1.7
    */
-  public static int compareSpecificData(Object o1, Object o2, Schema s, boolean eq) {
+  public static int compareSpecificData(Object o1, Object o2, Schema s) {
     switch (s.getType()) {
       case ENUM:
         if (o1 instanceof Enum)
           return ((Enum)o1).ordinal() - ((Enum)o2).ordinal();
       default:
-        return compareGenericData(o1, o2, s, eq);
+        return compareGenericData(o1, o2, s);
     }
   }
 
   /**
    * Backport compare() method implementation from GenericData in avro v1.7
    */
-  public static int compareGenericData(Object o1, Object o2, Schema s, boolean equals) {
+  public static int compareGenericData(Object o1, Object o2, Schema s) {
     if (o1 == o2) return 0;
     switch (s.getType()) {
       case RECORD:
@@ -47,7 +47,7 @@ public class AvroCompatibilityUtils {
           String name = f.name();
           int compare =
               compareGenericData(getField(o1, name, pos), getField(o2, name, pos),
-                  f.schema(), equals);
+                  f.schema());
           if (compare != 0)                         // not equal
             return f.order() == Schema.Field.Order.DESCENDING ? -compare : compare;
         }
@@ -61,19 +61,30 @@ public class AvroCompatibilityUtils {
         Iterator e2 = a2.iterator();
         Schema elementType = s.getElementType();
         while(e1.hasNext() && e2.hasNext()) {
-          int compare = compareGenericData(e1.next(), e2.next(), elementType, equals);
+          int compare = compareGenericData(e1.next(), e2.next(), elementType);
           if (compare != 0) return compare;
         }
         return e1.hasNext() ? 1 : (e2.hasNext() ? -1 : 0);
       case MAP:
-        if (equals)
-          return ((Map)o1).equals(o2) ? 0 : 1;
-        throw new AvroRuntimeException("Can't compare maps!");
+        Map map1 = (Map)o1;
+        Map map2 = (Map)o2;
+        if (map1.size() != map2.size())
+          return map1.size() - map2.size();
+        Set<Map.Entry> set = map1.entrySet();
+        for (Map.Entry entry : set) {
+          Object value2 = map2.get(entry.getKey());
+          if (null == value2) {
+            return -1;
+          }
+          int compare = compareGenericData(entry.getValue(), value2, s.getValueType());
+          if (compare != 0) return compare;
+        }
+        return 0;
       case UNION:
         int i1 = GenericData.get().resolveUnion(s, o1);
         int i2 = GenericData.get().resolveUnion(s, o2);
         return (i1 == i2)
-            ? compareGenericData(o1, o2, s.getTypes().get(i1), equals)
+            ? compareGenericData(o1, o2, s.getTypes().get(i1))
             : i1 - i2;
       case NULL:
         return 0;
