6b55f6decf755b5bab594d6f6753b11cd36d1f78
Xun Yin <xyin@linkedin.com>
2021-10-26 11:29:05 -0700

    [da-vinci] Add retry to NativeMetadataRepository.subscribe and resiliency in refresh
    NativeMetadataRepository subscribe should have retries in case meta system store requests fail
    either due to GC or later in blueshift we may have 30s pauses which will timeout d2r2.
    
    Using RetriableStoreClient with backoff in ThinClientMetaStoreBasedRepository implementation.
    As for the DaVinci client implementation we already have retry implemented in the MetadataReader
    for SchemaReader when retrieving schemas for meta system store.
    
    RB=2924482
    BUG=VENG-8249
    G=voldemort-reviewers
    R=lelu,zpolicze,akozma,nithakka,smaji,fvillega,lcli,jlliu,ssen,shuhliu,xhao,goliu,adietz,mnhuang,yurzhou,anaberez,sdwu,xnma
    A=goliu

--- venice-thin-client/src/main/java/com/linkedin/venice/client/store/RetriableStoreClient.java
+++ venice-thin-client/src/main/java/com/linkedin/venice/client/store/RetriableStoreClient.java
@@ -10,6 +10,7 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionException;
+import java.util.concurrent.ExecutionException;
 import java.util.function.Supplier;
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericRecord;
@@ -17,24 +18,30 @@ import org.apache.commons.httpclient.HttpStatus;
 
 
 public class RetriableStoreClient<K, V> extends StatTrackingStoreClient<K, V> {
+  private final boolean retryOnAllErrors;
+  private final long retryBackOffInMs;
+  private final int retryCount;
 
   public RetriableStoreClient(InternalAvroStoreClient<K, V> innerStoreClient, ClientConfig clientConfig) {
     super(innerStoreClient, clientConfig);
+    retryOnAllErrors = clientConfig.isRetryOnAllErrorsEnabled();
+    retryBackOffInMs = clientConfig.getRetryBackOffInMs();
+    retryCount = clientConfig.getRetryCount();
   }
 
   /**
-   * Adding retry logic on router error as this method returning the completionstage value.
+   * Adding retry logic on router error as this method returning the completion stage value.
    */
   @Override
   public CompletableFuture<V> get(K key) throws VeniceClientException {
     CompletableFuture<V> innerFuture = super.get(key);
     Supplier<CompletableFuture<V>> supplier = () -> super.get(key);
 
-    return retryOnRouterError(innerFuture, supplier, RequestType.SINGLE_GET);
+    return retryOnError(innerFuture, supplier, RequestType.SINGLE_GET);
   }
 
   /**
-   * Skip retrying getRaw for now as it ues R2 threadpool, doing retry might cause deadlock.
+   * Skip retrying getRaw for now as it ues R2 thread pool, doing retry might cause deadlock.
 
   *@Override
   *public CompletableFuture<byte[]> getRaw(String requestPath, Optional<ClientStats> stats, long preRequestTimeInNS) {
@@ -52,36 +59,50 @@ public class RetriableStoreClient<K, V> extends StatTrackingStoreClient<K, V> {
     Supplier<CompletableFuture<Map<K, GenericRecord>>> supplier = () ->  super.compute(computeRequestWrapper, keys, resultSchema,
         stats, preRequestTimeInNS);
 
-    return retryOnRouterError(innerFuture, supplier, RequestType.COMPUTE);
+    return retryOnError(innerFuture, supplier, RequestType.COMPUTE);
   }
 
   /**
-   *  Adding retry logic on router error as this method returning the completionstage value.
+   *  Adding retry logic on router error as this method returning the completion stage value.
    */
   @Override
   public CompletableFuture<Map<K, V>> batchGet(Set<K> keys) throws VeniceClientException {
     CompletableFuture<Map<K, V>> innerFuture = super.batchGet(keys);
     Supplier<CompletableFuture<Map<K,V>>> supplier = () -> super.batchGet(keys);
 
-    return retryOnRouterError(innerFuture, supplier, RequestType.MULTI_GET);
+    return retryOnError(innerFuture, supplier, RequestType.MULTI_GET);
   }
 
-  private <T> CompletableFuture<T> retryOnRouterError(CompletableFuture<T> origFuture, Supplier<CompletableFuture<T>> supplier, RequestType requestType) {
+  private <T> CompletableFuture<T> retryOnError(CompletableFuture<T> origFuture, Supplier<CompletableFuture<T>> supplier, RequestType requestType) {
     CompletableFuture<T> retryFuture = new CompletableFuture<>();
 
     origFuture.whenComplete((T val, Throwable throwable) -> {
       if (null != throwable) {
-        if (isRetriableException(throwable)) {
+        int attempt = 0;
+        Throwable retryThrowable = throwable;
+        while (!retryFuture.isDone() && attempt < retryCount
+            && isRetriableException(retryThrowable)) {
+          attempt++;
           recordRetryCount(requestType);
-          supplier.get().whenComplete((T retryVal, Throwable retryThrowable) -> {
-            if (retryThrowable != null) {
-              retryFuture.completeExceptionally(retryThrowable);
-            } else {
-              retryFuture.complete(retryVal);
+          if (retryBackOffInMs > 0) {
+            try {
+              Thread.sleep(retryBackOffInMs);
+            } catch (InterruptedException e) {
+              retryFuture.completeExceptionally(e);
+              return;
             }
-          });
-        } else {
-          retryFuture.completeExceptionally(throwable);
+          }
+          try {
+            T retryVal = supplier.get().get();
+            retryFuture.complete(retryVal);
+            retryThrowable = null;
+            break;
+          } catch (Throwable t) {
+            retryThrowable = t;
+          }
+        }
+        if (retryThrowable != null) {
+          retryFuture.completeExceptionally(retryThrowable);
         }
       } else {
         retryFuture.complete(val);
@@ -92,16 +113,27 @@ public class RetriableStoreClient<K, V> extends StatTrackingStoreClient<K, V> {
 
   /**
    * Checks if the exception can be retried. Currently it checks for Router health error(Http code: SC_SERVICE_UNAVAILABLE)
+   * or always return true if ClientConfig is configured to consider all errors retriable (except InterruptedException).
    * @param throwable type of Throwable that can be retried.
    * @return true if it can be retried.
    */
   private boolean isRetriableException(Throwable throwable) {
-    if (throwable instanceof CompletionException) {
-      Throwable innerException = throwable.getCause();
-      if (innerException instanceof VeniceClientHttpException) {
-        VeniceClientHttpException exception = (VeniceClientHttpException) innerException;
-        return exception.getHttpStatus() == HttpStatus.SC_SERVICE_UNAVAILABLE;
-      }
+    if (throwable instanceof InterruptedException) {
+      // do not retry InterruptedException
+      return false;
+    }
+    if (retryOnAllErrors) {
+      return true;
+    }
+    Throwable innerException;
+    if (throwable instanceof CompletionException || throwable instanceof ExecutionException) {
+      innerException = throwable.getCause();
+    } else {
+      innerException = throwable;
+    }
+    if (innerException instanceof VeniceClientHttpException) {
+      VeniceClientHttpException exception = (VeniceClientHttpException) innerException;
+      return exception.getHttpStatus() == HttpStatus.SC_SERVICE_UNAVAILABLE;
     }
     return false;
   }
