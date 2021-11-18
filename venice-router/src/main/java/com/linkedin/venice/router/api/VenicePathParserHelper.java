2072f3a1903f24e377a7195cc42b6b8fe602c155
Zac Policzer <zpolicze@zpolicze-mn1.linkedin.biz>
2021-11-17 16:06:48 -0800

    [venice-router] Add query parameter for enable quota api that delays reenable quota
    Dyno team has asked that we provide utility for taking a delayed action to reenable the quota after sending the request.
    
    RB=2972517
    G=voldemort-reviewers
    R=nithakka,akozma,ssen,yurzhou,mnhuang,xyin,adietz,sdwu,shuhliu,xnma,xhao,fvillega,lcli,goliu,smaji,lelu,jlliu,anaberez
    A=nithakka

--- venice-router/src/main/java/com/linkedin/venice/router/api/VenicePathParserHelper.java
+++ venice-router/src/main/java/com/linkedin/venice/router/api/VenicePathParserHelper.java
@@ -6,6 +6,14 @@ import com.linkedin.venice.router.utils.VeniceRouterUtils;
 import io.netty.handler.codec.http.HttpRequest;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.nio.charset.Charset;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.utils.URLEncodedUtils;
 import org.apache.log4j.Logger;
 
 import static com.linkedin.venice.router.api.VenicePathParser.*;
@@ -20,6 +28,7 @@ public class VenicePathParserHelper {
   private String resourceType = null;
   private String resourceName = null;
   private String key = null;
+  private Map<String,String> queryParameters = null;
 
   public static VenicePathParserHelper parseRequest(HttpRequest request) {
     if (request instanceof BasicFullHttpRequest) {
@@ -36,6 +45,26 @@ public class VenicePathParserHelper {
       basicFullHttpRequest.attr(VeniceRouterUtils.PATHPARSER_ATTRIBUTE_KEY).set(helper);
     }
     return helper;
+  }
+
+  /**
+   * We provide this method as a utility function as opposed to storing the query parameters in this class.  We do this
+   * because we only really need these parameters for some very specific circumstances, so we avoid keeping around extra
+   * maps on heap in the data path.
+   *
+   * @param request request to have it's query parameters extracted
+   * @return a map keyed by the parameter name and accompanied by it's associated value.
+   */
+  public Map<String,String> extractQueryParamters(HttpRequest request) {
+    List<NameValuePair> params = null;
+    Map<String, String> queryParams = new HashMap<>();
+    try {
+      params = URLEncodedUtils.parse(new URI(request.uri()), Charset.forName("UTF-8"));
+      queryParams = params.stream().collect(Collectors.toMap(NameValuePair::getName, NameValuePair::getValue));
+    } catch (URISyntaxException e) {
+      logger.warn("Failed to parse uri: " + request.uri());
+    }
+    return queryParams;
   }
 
   private VenicePathParserHelper(String uri){
