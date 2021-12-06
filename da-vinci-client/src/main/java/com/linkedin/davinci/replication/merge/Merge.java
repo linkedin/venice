593bf6ccb75453354a544b90cb4f07f06457faf3
Zac Policzer <zpolicze@zpolicze-mn1.linkedin.biz>
2021-12-06 11:25:23 -0800

    [venice-server][AA] Deletes should always win on ties
    Tweak to make sure that deletes always win on ties in put path and delete path (diverged previousely)
    
    RB=3009125
    BUG=VENG-8896
    G=voldemort-reviewers
    R=sdwu,xhao,smaji,nithakka,goliu,xnma,yurzhou,lelu,shuhliu,jlliu,lcli,adietz,xyin,fvillega,ssen,anaberez,akozma,mnhuang
    A=xhao,mnhuang

--- da-vinci-client/src/main/java/com/linkedin/davinci/replication/merge/Merge.java
+++ da-vinci-client/src/main/java/com/linkedin/davinci/replication/merge/Merge.java
@@ -115,10 +115,12 @@ interface Merge<T> {
   }
 
   static Object compareAndReturn(Object o1, Object o2) {
+    // nulls win comparison on the basis that we prefer deletes to win in the case
+    // where there is a tie on RMD timestamp comparison.
     if (o1 == null) {
-      return o2;
-    } else if (o2 == null) {
       return o1;
+    } else if (o2 == null) {
+      return o2;
     }
     // for same object always return first object o1
     if (o1.hashCode() >= o2.hashCode()) {
