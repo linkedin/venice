f6ab0033b809022c36012115b43be144f35e3437
Shuhui Liu <shuhliu@linkedin.com>
2022-01-12 12:28:03 -0800

    [test] Move compare-store test out of TestAdminToolEndToEnd
    Compare-store is only supported in parent controller, and its integ test requires
    multi colo setup. But other tests in TestAdminToolEndToEnd only need single colo
    setup. To save resources, move compare-store test to another test class, which has
    already set up multi colo environment.
    
    RB=3081952
    G=voldemort-reviewers
    R=fvillega,ssen,xhao,lelu,zpolicze,adietz,nithakka,akozma,xyin,anaberez,sumane,mnhuang,lcli,sdwu,yurzhou,xnma,smaji,goliu,jlliu
    A=xhao

--- venice-test-common/src/integrationtest/java/com/linkedin/venice/controller/TestFabricBuildup.java
+++ venice-test-common/src/integrationtest/java/com/linkedin/venice/controller/TestFabricBuildup.java
@@ -2,15 +2,22 @@ package com.linkedin.venice.controller;
 
 import com.linkedin.venice.controllerapi.ControllerClient;
 import com.linkedin.venice.controllerapi.NewStoreResponse;
+import com.linkedin.venice.controllerapi.SchemaResponse;
+import com.linkedin.venice.controllerapi.StoreComparisonResponse;
 import com.linkedin.venice.controllerapi.StoreResponse;
+import com.linkedin.venice.controllerapi.VersionCreationResponse;
+import com.linkedin.venice.integration.utils.MirrorMakerWrapper;
 import com.linkedin.venice.integration.utils.ServiceFactory;
 import com.linkedin.venice.integration.utils.VeniceControllerWrapper;
 import com.linkedin.venice.integration.utils.VeniceMultiClusterWrapper;
 import com.linkedin.venice.integration.utils.VeniceTwoLayerMultiColoMultiClusterWrapper;
 import com.linkedin.venice.meta.StoreInfo;
+import com.linkedin.venice.utils.TestPushUtils;
 import com.linkedin.venice.utils.TestUtils;
 import com.linkedin.venice.utils.Utils;
 import java.util.List;
+import java.util.Optional;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 import org.testng.Assert;
@@ -18,8 +25,9 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import static com.linkedin.venice.ConfigKeys.*;
 
-public class TestStoresMetadataCopyOver {
+public class TestFabricBuildup {
   private static final int TEST_TIMEOUT = 90_000; // ms
 
   private static final int NUMBER_OF_CHILD_DATACENTERS = 2;
@@ -33,13 +41,21 @@ public class TestStoresMetadataCopyOver {
 
   @BeforeClass
   public void setUp() {
+    Properties properties = new Properties();
+    properties.setProperty(CONTROLLER_ENABLE_BATCH_PUSH_FROM_ADMIN_IN_CHILD, "true");
     multiColoMultiClusterWrapper = ServiceFactory.getVeniceTwoLayerMultiColoMultiClusterWrapper(
         NUMBER_OF_CHILD_DATACENTERS,
         NUMBER_OF_CLUSTERS,
         1,
         1,
         1,
-        1);
+        1,
+        1,
+        Optional.empty(),
+        Optional.of(properties),
+        Optional.empty(),
+        false,
+        MirrorMakerWrapper.DEFAULT_TOPIC_WHITELIST);
 
     childDatacenters = multiColoMultiClusterWrapper.getClusters();
     parentControllers = multiColoMultiClusterWrapper.getParentControllers();
@@ -61,8 +77,6 @@ public class TestStoresMetadataCopyOver {
     ControllerClient parentControllerClient = ControllerClient.constructClusterControllerClient(clusterName, parentController.getControllerUrl());
 
     ControllerClient dc0Client = ControllerClient.constructClusterControllerClient(clusterName, childDatacenters.get(0).getControllerConnectString());
-    // ControllerClient dc1Client = ControllerClient.constructClusterControllerClient(clusterName, childDatacenters.get(1).getControllerConnectString());
-
     // Create a test store only in dc0 colo
     NewStoreResponse newStoreResponse = dc0Client.retryableRequest(3, c -> c.createNewStore(storeName,
         "", "\"string\"", "\"string\""));
@@ -72,9 +86,39 @@ public class TestStoresMetadataCopyOver {
     // Call metadata copy over to copy dc0's store configs to dc1
     parentControllerClient.copyOverStoresMetadata("dc-0","dc-1");
     ControllerClient dc1Client = ControllerClient.constructClusterControllerClient(clusterName, childDatacenters.get(1).getControllerConnectString());
-    System.out.println("The dc0 controller url is " + childDatacenters.get(0).getControllerConnectString());
-    System.out.println("The dc1 controller url is " + childDatacenters.get(1).getControllerConnectString());
     checkStoreConfig(dc1Client, storeName);
+  }
+
+  @Test(timeOut = TEST_TIMEOUT)
+  public void testCompareStore() {
+    String clusterName = CLUSTER_NAMES[0];
+    VeniceControllerWrapper parentController = parentControllers.stream().filter(c -> c.isMasterController(clusterName)).findAny().get();
+    try (ControllerClient parentControllerClient = new ControllerClient(clusterName, parentController.getControllerUrl());
+        ControllerClient childControllerClient0 = new ControllerClient(clusterName, childDatacenters.get(0).getControllerConnectString());
+        ControllerClient childControllerClient1 = new ControllerClient(clusterName, childDatacenters.get(1).getControllerConnectString())) {
+      String testStoreName = Utils.getUniqueString("test-store");
+      NewStoreResponse newStoreResponse = childControllerClient0.createNewStore(testStoreName, "test",
+          "\"string\"", TestPushUtils.NESTED_SCHEMA_STRING);
+      Assert.assertFalse(newStoreResponse.isError());
+      checkStoreConfig(childControllerClient0, testStoreName);
+      newStoreResponse = childControllerClient1.createNewStore(testStoreName, "test", "\"string\"",
+          TestPushUtils.NESTED_SCHEMA_STRING);
+      Assert.assertFalse(newStoreResponse.isError());
+      checkStoreConfig(childControllerClient1, testStoreName);
+
+      // Only modify the store in dc-0
+      VersionCreationResponse versionCreationResponse = childControllerClient0.emptyPush(testStoreName,
+          Utils.getUniqueString("empty-push-1"), 1L);
+      Assert.assertFalse(versionCreationResponse.isError());
+      SchemaResponse schemaResponse = childControllerClient0.addValueSchema(testStoreName, TestPushUtils.NESTED_SCHEMA_STRING_V2);
+      Assert.assertFalse(schemaResponse.isError());
+
+      StoreComparisonResponse response = parentControllerClient.compareStore(testStoreName, "dc-0", "dc-1");
+      // Make sure the diffs are discovered.
+      Assert.assertFalse(response.getPropertyDiff().isEmpty());
+      Assert.assertFalse(response.getSchemaDiff().isEmpty());
+      Assert.assertFalse(response.getVersionStateDiff().isEmpty());
+    }
   }
 
   private static void checkStoreConfig(ControllerClient childControllerClient,
