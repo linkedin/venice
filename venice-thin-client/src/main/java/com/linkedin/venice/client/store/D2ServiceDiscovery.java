linkedin/venice

commit 16eec8067d8501398281837e09132d3ea29afa31

[da-vinci] [client] Fix InterruptedException handling in ParticipantStoreConsumptionTask and D2ServiceDiscovery
RB=3089000
G=voldemort-reviewers
R=mnhuang,smaji,sdwu,lelu,xnma,lcli,yurzhou,xyin,adietz,jlliu,xhao,fvillega,goliu,shuhliu,ssen,sumane,akozma,zpolicze,nithakka
A=fvillega

venice-thin-client/src/main/java/com/linkedin/venice/client/store/D2ServiceDiscovery.java
@@ -2,18 +2,19 @@ package com.linkedin.venice.client.store;
 
 import com.linkedin.venice.client.exceptions.ServiceDiscoveryException;
 import com.linkedin.venice.client.store.transport.D2TransportClient;
-import com.linkedin.venice.client.store.transport.TransportClient;
 import com.linkedin.venice.client.store.transport.TransportClientResponse;
 import com.linkedin.venice.controllerapi.D2ServiceDiscoveryResponseV2;
-import io.tehuti.utils.SystemTime;
-import io.tehuti.utils.Time;
-import java.util.Collections;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
+import com.linkedin.venice.exceptions.VeniceException;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.codehaus.jackson.map.DeserializationConfig;
 import org.codehaus.jackson.map.ObjectMapper;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 import static com.linkedin.venice.controllerapi.D2ServiceDiscoveryResponseV2.*;
 
@@ -24,58 +25,45 @@ import static com.linkedin.venice.controllerapi.D2ServiceDiscoveryResponseV2.*;
  */
 public class D2ServiceDiscovery {
   private static final Logger LOGGER = LogManager.getLogger(D2ServiceDiscovery.class);
+
+  private static final ObjectMapper OBJECT_MAPPER =
+      new ObjectMapper().disable(DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES);
+
   public static final String TYPE_D2_SERVICE_DISCOVERY = "discover_cluster";
-  private final Time time = new SystemTime(); // TODO: Make it injectable if we need to control time via tests.
 
-  public TransportClient getD2TransportClientForStore(D2TransportClient client, String storeName, boolean retryOnFailure) {
-    D2ServiceDiscoveryResponseV2 d2ServiceDiscoveryResponse = discoverD2Service(client, storeName, retryOnFailure);
-    client.setServiceName(d2ServiceDiscoveryResponse.getD2Service());
-    return client;
+  public D2ServiceDiscoveryResponseV2 find(D2TransportClient client, String storeName) {
+    return find(client, storeName, true);
   }
 
-  public D2ServiceDiscoveryResponseV2 discoverD2Service(D2TransportClient client, String storeName) {
-    return discoverD2Service(client, storeName, true);
-  }
+  public D2ServiceDiscoveryResponseV2 find(D2TransportClient client, String storeName, boolean retryOnFailure) {
+    int maxAttempts = retryOnFailure ? 10 : 1;
+    String requestPath = TYPE_D2_SERVICE_DISCOVERY + "/" + storeName;
+    Map<String, String> requestHeaders = Collections.singletonMap(D2_SERVICE_DISCOVERY_RESPONSE_V2_ENABLED, "true");
 
-  public D2ServiceDiscoveryResponseV2 discoverD2Service(D2TransportClient client, String storeName, boolean retryOnFailure) {
-    try {
-      TransportClientResponse response = null;
-      int currentAttempt = 0;
-      final int MAX_ATTEMPT = retryOnFailure ? 10 : 1;
-      final long SLEEP_TIME_BETWEEN_ATTEMPTS = 5 * Time.MS_PER_SECOND;
-      while (response == null) {
-        if (currentAttempt >= MAX_ATTEMPT) {
-          throw new ServiceDiscoveryException("Failed to find d2 service for store " + storeName + " after " + MAX_ATTEMPT + " attempts.");
+    for (int attempt = 0; attempt < maxAttempts; ++attempt) {
+      try {
+        if (attempt > 0) {
+          TimeUnit.SECONDS.sleep(3);
         }
-        if (currentAttempt > 0) {
-          // Back off
-          LOGGER.warn("Failed to fetch from the service discovery endpoint. Attempt: " + currentAttempt + "/" + MAX_ATTEMPT
-              + ". Will sleep " + SLEEP_TIME_BETWEEN_ATTEMPTS + " ms and retry.");
-          time.sleep(SLEEP_TIME_BETWEEN_ATTEMPTS);
+        TransportClientResponse response = client.get(requestPath, requestHeaders).get();
+        D2ServiceDiscoveryResponseV2 result = OBJECT_MAPPER.readValue(response.getBody(), D2ServiceDiscoveryResponseV2.class);
+        if (result.isError()) {
+          throw new VeniceException(result.getError());
         }
-        CompletableFuture<TransportClientResponse> responseFuture = client.get(
-            TYPE_D2_SERVICE_DISCOVERY + "/" + storeName,
-            Collections.singletonMap(D2_SERVICE_DISCOVERY_RESPONSE_V2_ENABLED, "true"));
-        try {
-          response = responseFuture.get();
-        } catch (ExecutionException e) {
-          LOGGER.warn("ExecutionException when trying to get the service discovery response", e);
-          response = null;
-        }
-        currentAttempt++;
+        LOGGER.info("Found d2 service {} for {}", result.getD2Service(), storeName);
+        return result;
+
+      } catch (ExecutionException e) {
+        LOGGER.warn("Failed to find d2 service for {}, attempt {}/{}, reason {}", storeName, attempt + 1, maxAttempts, e.getCause());
+
+      } catch (InterruptedException e) {
+        Thread.currentThread().interrupt();
+        throw new ServiceDiscoveryException("Failed to find d2 service for " + storeName, e);
+
+      } catch (Exception e) {
+        throw new ServiceDiscoveryException("Failed to find d2 service for " + storeName, e);
       }
-      byte[] body = response.getBody();
-      ObjectMapper mapper = new ObjectMapper()
-          .configure(DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-      D2ServiceDiscoveryResponseV2 d2ServiceDiscoveryResponse = mapper.readValue(body, D2ServiceDiscoveryResponseV2.class);
-      if (d2ServiceDiscoveryResponse.isError()) {
-        throw new ServiceDiscoveryException(
-            "Failed to find d2 service for store: " + storeName + ". " + d2ServiceDiscoveryResponse.getError());
-      }
-      LOGGER.info("Found d2 service: " + d2ServiceDiscoveryResponse.getD2Service() + " for store: " + storeName);
-      return d2ServiceDiscoveryResponse;
-    } catch (Exception e) {
-      throw new ServiceDiscoveryException("Failed to find d2 service for store: " + storeName, e);
     }
+    throw new ServiceDiscoveryException("Failed to find d2 service for " + storeName + " after " + maxAttempts + " attempts");
   }
 }

da-vinci-client/src/main/java/com/linkedin/davinci/client/AvroGenericDaVinciClient.java
@@ -574,7 +574,7 @@ public class AvroGenericDaVinciClient<K, V> implements DaVinciClient<K, V>, Avro
                                             ", clientClass=" + client.getClass());
       }
       D2ServiceDiscovery serviceDiscovery = new D2ServiceDiscovery();
-      D2ServiceDiscoveryResponseV2 response = serviceDiscovery.discoverD2Service((D2TransportClient) client, getStoreName());
+      D2ServiceDiscoveryResponseV2 response = serviceDiscovery.find((D2TransportClient) client, getStoreName());
       logger.info("Venice service discovered" +
                       ", clusterName=" + response.getCluster() +
                       ", zkAddress=" + response.getZkAddress() +

da-vinci-client/src/main/java/com/linkedin/davinci/ingestion/main/MainIngestionRequestClient.java
@@ -79,7 +79,6 @@ public class MainIngestionRequestClient implements Closeable {
             IsolatedIngestionServer.class,
             Collections.singletonList(configFilePath),
             jvmArgs,
-            Optional.empty(),
             false
         );
         logger.info("Forked new isolated ingestion process at PID: " + forkedIngestionProcess.pid());

da-vinci-client/src/main/java/com/linkedin/davinci/kafka/consumer/KafkaStoreIngestionService.java
@@ -52,7 +52,6 @@ import com.linkedin.venice.throttle.EventThrottler;
 import com.linkedin.venice.utils.ComplementSet;
 import com.linkedin.venice.utils.DaemonThreadFactory;
 import com.linkedin.venice.utils.DiskUsage;
-import com.linkedin.venice.utils.LatencyUtils;
 import com.linkedin.venice.utils.Pair;
 import com.linkedin.venice.utils.PartitionUtils;
 import com.linkedin.venice.utils.Time;
@@ -522,28 +521,29 @@ public class KafkaStoreIngestionService extends AbstractVeniceService implements
         cacheBackend);
   }
 
-  private static void shutdownExecutorService(ExecutorService executorService, boolean force, String name) {
-    if (null == executorService) {
+  private static void shutdownExecutorService(ExecutorService executor, String name, boolean force) {
+    if (executor == null) {
       return;
     }
-    if (force) {
-      executorService.shutdownNow();
-    } else {
-      executorService.shutdown();
-    }
+
+    long startTime = System.currentTimeMillis();
     try {
-      long startTimeMs = System.currentTimeMillis();
-      if (!executorService.awaitTermination(60, TimeUnit.SECONDS) && !force) {
-        logger.error("Couldn't stop the thread pool: " + name + " after " + LatencyUtils.getElapsedTimeInMs(startTimeMs) + "(ms)");
-        executorService.shutdownNow();
-        if (!executorService.awaitTermination(30, TimeUnit.SECONDS)) {
-          logger.error("Still couldn't stop the thread pool: " + name + " after " + LatencyUtils.getElapsedTimeInMs(startTimeMs) + "(ms)");
+      executor.shutdown();
+      if (force || !executor.awaitTermination(60, TimeUnit.SECONDS)) {
+        if (!force) {
+          logger.warn("Failed to gracefully shutdown executor {}", name);
+        }
+        executor.shutdownNow();
+        if (!executor.awaitTermination(30, TimeUnit.SECONDS)) {
+          logger.error("Failed to shutdown executor {}", name);
         }
       }
     } catch (InterruptedException e) {
-      logger.warn("Received interruptedException in stopping thread pool: " + name);
-      executorService.shutdownNow();
-      Thread.currentThread().interrupt();
+      logger.warn("Executor shutdown is interrupted");
+      executor.shutdownNow();
+      currentThread().interrupt();
+    } finally {
+      logger.info("{} shutdown took {} ms.", name, System.currentTimeMillis() - startTime);
     }
   }
 
@@ -554,15 +554,15 @@ public class KafkaStoreIngestionService extends AbstractVeniceService implements
   @Override
   public void stopInner() {
     Utils.closeQuietlyWithErrorLogged(participantStoreConsumptionTask);
-    shutdownExecutorService(participantStoreConsumerExecutorService, true, "participantStoreConsumerExecutorService");
+    shutdownExecutorService(participantStoreConsumerExecutorService, "participantStoreConsumerExecutorService", true);
 
     /*
      * We would like to gracefully shutdown {@link #ingestionExecutorService},
      * so that it will have an opportunity to checkpoint the processed offset.
      */
     topicNameToIngestionTaskMap.values().forEach(StoreIngestionTask::close);
-    shutdownExecutorService(ingestionExecutorService, false, "ingestionExecutorService");
-    shutdownExecutorService(cacheWarmingExecutorService, true, "cacheWarmingExecutorService");
+    shutdownExecutorService(ingestionExecutorService, "ingestionExecutorService", false);
+    shutdownExecutorService(cacheWarmingExecutorService, "cacheWarmingExecutorService", true);
 
     Utils.closeQuietlyWithErrorLogged(aggKafkaConsumerService);
 

da-vinci-client/src/main/java/com/linkedin/davinci/kafka/consumer/ParticipantStoreConsumptionTask.java
@@ -58,20 +58,18 @@ public class ParticipantStoreConsumptionTask implements Runnable, Closeable {
   @Override
   public void run() {
     logger.info("Started running " + getClass().getSimpleName());
-    String exceptionMessage = "Exception thrown while running " + getClass().getSimpleName() + " thread";
 
     while (!isClosing.get()) {
       try {
         Thread.sleep(participantMessageConsumptionDelayMs);
 
-        ParticipantMessageKey key = new ParticipantMessageKey();
-        key.messageType = ParticipantMessageType.KILL_PUSH_JOB.getValue();
-
         for (String topic : storeIngestionService.getIngestingTopicsWithVersionStatusNotOnline()) {
           try {
+            ParticipantMessageKey key = new ParticipantMessageKey();
+            key.messageType = ParticipantMessageType.KILL_PUSH_JOB.getValue();
             key.resourceName = topic;
-            String clusterName = clusterInfoProvider.getVeniceCluster(Version.parseStoreFromKafkaTopicName(topic));
 
+            String clusterName = clusterInfoProvider.getVeniceCluster(Version.parseStoreFromKafkaTopicName(topic));
             if (clusterName != null) {
               ParticipantMessageValue value;
               if (icProvider != null) {
@@ -89,6 +87,9 @@ public class ParticipantStoreConsumptionTask implements Runnable, Closeable {
                 }
               }
             }
+          } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            break;
           } catch (Exception e) {
             if (!filter.isRedundantException(e.getMessage())) {
               logger.error("Unexpected exception while trying to check or kill ingestion topic: " + topic, e);
@@ -98,11 +99,11 @@ public class ParticipantStoreConsumptionTask implements Runnable, Closeable {
         }
       } catch (InterruptedException e) {
         break;
-      } catch (Throwable e) {
+      } catch (Exception e) {
         // Some expected exception can be thrown during initializing phase of the participant store or if participant
         // store is disabled.
         if (!filter.isRedundantException(e.getMessage())) {
-          logger.error(exceptionMessage, e);
+          logger.error("Exception thrown while running " + getClass().getSimpleName() + " thread", e);
         }
         stats.recordKillPushJobFailedConsumption();
       }

da-vinci-client/src/main/java/com/linkedin/davinci/kafka/consumer/StoreBufferService.java
@@ -176,10 +176,10 @@ public class StoreBufferService extends AbstractStoreBufferService {
           topicToTimeSpent.compute(topicPartition, (K,V) ->  (V == null ? 0 : V) + System.currentTimeMillis() - startTime );
         } catch (Throwable e) {
           if (e instanceof InterruptedException) {
-            LOGGER.error("Drainer " + drainerIndex + "received InterruptedException, will exit");
+            LOGGER.error("Drainer " + drainerIndex + " received InterruptedException, will exit");
             break;
           }
-          LOGGER.error("Drainer " + drainerIndex + "received throwable in drainer thread:  ", e);
+          LOGGER.error("Drainer " + drainerIndex + " received throwable in drainer thread:  ", e);
           if (consumerRecord != null) {
             String consumerRecordString = consumerRecord.toString();
             if (consumerRecordString.length() > 1024) {

venice-common/src/main/java/com/linkedin/venice/utils/ForkedJavaProcess.java
@@ -74,42 +74,45 @@ public final class ForkedJavaProcess extends Process {
 
   private Thread processReaper;
 
-  public static ForkedJavaProcess exec(Class appClass, List<String> args, List<String> jvmArgs,
-      Optional<String> extraLoggerName, boolean registerShutdownHook, Optional<Supplier<String>> classPathSupplier) throws IOException {
-    LOGGER.info("Forking " + appClass.getSimpleName() + " with arguments " + args + " and jvm arguments " + jvmArgs +
-        " extraLoggerName: " + (extraLoggerName.isPresent() ? extraLoggerName.get() : ""));
+  public static ForkedJavaProcess exec(
+      Class appClass,
+      List<String> args,
+      List<String> jvmArgs,
+      String classPath,
+      boolean killOnExit,
+      Optional<String> loggerPrefix) throws IOException {
+    LOGGER.info("Forking " + appClass.getSimpleName() + " with arguments " + args + " and jvm arguments " + jvmArgs);
 
     List<String> command = new ArrayList<>();
     command.add(JAVA_PATH);
     command.add("-cp");
-    if (classPathSupplier.isPresent()) {
-      command.add(classPathSupplier.get().get());
-    } else {
-      command.add(getClasspath());
-    }
+    command.add(classPath);
 
     command.addAll(DEFAULT_JAVA_ARGS);
-    // Add user provided customized JVM arguments for child process.
     command.addAll(jvmArgs);
 
     command.add(appClass.getCanonicalName());
     command.addAll(args);
 
     Process process = new ProcessBuilder(command).redirectErrorStream(true).start();
-    Logger logger = LogManager.getLogger((extraLoggerName.map(s -> s + ", ").orElse("")) + appClass.getSimpleName() + ", PID=" + getPidOfProcess(process));
-    return new ForkedJavaProcess(process, logger, registerShutdownHook);
+    Logger logger = LogManager.getLogger(loggerPrefix.map(s -> s + ", ").orElse("") + appClass.getSimpleName() + ", PID=" + getPidOfProcess(process));
+    return new ForkedJavaProcess(process, logger, killOnExit);
   }
 
-  public static ForkedJavaProcess exec(Class appClass, List<String> args, List<String> jvmArgs, Optional<String> extraLoggerName, boolean registerShutdownHook) throws IOException {
-    return exec(appClass, args, jvmArgs, extraLoggerName, registerShutdownHook, Optional.empty());
+  public static ForkedJavaProcess exec(Class appClass, List<String> args, List<String> jvmArgs, boolean killOnExit, Optional<String> loggerPrefix) throws IOException {
+    return exec(appClass, args, jvmArgs, getClasspath(), killOnExit, loggerPrefix);
   }
 
-  public static ForkedJavaProcess exec(Class appClass, List<String> args, List<String> jvmArgs, Optional<String> extraLoggerName) throws IOException {
-    return exec(appClass, args, jvmArgs, extraLoggerName, true);
+  public static ForkedJavaProcess exec(Class appClass, List<String> args, List<String> jvmArgs, boolean killOnExit) throws IOException {
+    return exec(appClass, args, jvmArgs, killOnExit, Optional.empty());
+  }
+
+  public static ForkedJavaProcess exec(Class appClass, List<String> args, List<String> jvmArgs) throws IOException {
+    return exec(appClass, args, jvmArgs, true, Optional.empty());
   }
 
   public static ForkedJavaProcess exec(Class appClass, String... args) throws IOException {
-    return exec(appClass, Arrays.asList(args), Collections.emptyList(), Optional.empty());
+    return exec(appClass, Arrays.asList(args), Collections.emptyList());
   }
 
   public static String getClasspath() {
@@ -125,7 +128,7 @@ public final class ForkedJavaProcess extends Process {
 
       /*
         Prepend WEB-INF/lib classpath to the forked Java process's classpath so the forked
-        process will have correct(newest) jar version for every dependencies when deployed in regular-war fashion.
+        process will have correct(newest) jar version for every dependency when deployed in regular-war fashion.
        */
       for (File file : new ArrayList<>(classpathDirs)) {
         if (!file.getPath().contains("WEB-INF/lib")) {
@@ -136,7 +139,7 @@ public final class ForkedJavaProcess extends Process {
 
       /*
         Prepend extra_webapp_resources/lib classpath to the forked Java process's classpath so the forked
-        process will have correct(newest) jar version for every dependencies when deployed in thin-war fashion.
+        process will have correct(newest) jar version for every dependency when deployed in thin-war fashion.
        */
       for (File file : new ArrayList<>(classpathDirs)) {
         if (!file.getPath().contains("extra_webapp_resources")) {
@@ -151,10 +154,10 @@ public final class ForkedJavaProcess extends Process {
   /**
    * Construction should happen via {@link #exec(Class, String...)}
    */
-  private ForkedJavaProcess(Process process, Logger logger, boolean registerShutdownHook) {
+  private ForkedJavaProcess(Process process, Logger logger, boolean killOnExit) {
     this.logger = logger;
     this.process = process;
-    if (registerShutdownHook) {
+    if (killOnExit) {
       this.processReaper = new Thread(this::destroy);
       Runtime.getRuntime().addShutdownHook(processReaper);
     }

venice-compatibility-test/src/test/java/com/linkedin/venice/VeniceClientCompatibilityTest.java
@@ -71,9 +71,9 @@ public class VeniceClientCompatibilityTest {
         VeniceClusterInitializer.class,
         Arrays.asList(storeName, routerPort),
         Collections.emptyList(),
-        Optional.empty(),
+        new ClassPathSupplierForVeniceCluster().get(),
         true,
-        Optional.of(new ClassPathSupplierForVeniceCluster()));
+        Optional.empty());
 
     veniceClient = ClientFactory.getGenericAvroClient(
         ClientConfig.defaultGenericClientConfig(storeName).setVeniceURL(routerAddress));

commit 1d432742c30ab253053d965331decd4335acf48c

[client] Fix regression introduced in 16eec8067
RB=3089099
G=voldemort-reviewers
R=yurzhou,lelu,nithakka,sdwu,adietz,xhao,xnma,sumane,shuhliu,mnhuang,lcli,jlliu,zpolicze,goliu,xyin,akozma,ssen,fvillega,smaji
A=fvillega

venice-thin-client/src/main/java/com/linkedin/venice/client/store/D2ServiceDiscovery.java
@@ -46,6 +46,10 @@ public class D2ServiceDiscovery {
           TimeUnit.SECONDS.sleep(3);
         }
         TransportClientResponse response = client.get(requestPath, requestHeaders).get();
+        if (response == null) {
+          LOGGER.warn("Failed to find d2 service for {}, attempt {}/{}", storeName, attempt + 1, maxAttempts);
+          continue;
+        }
         D2ServiceDiscoveryResponseV2 result = OBJECT_MAPPER.readValue(response.getBody(), D2ServiceDiscoveryResponseV2.class);
         if (result.isError()) {
           throw new VeniceException(result.getError());