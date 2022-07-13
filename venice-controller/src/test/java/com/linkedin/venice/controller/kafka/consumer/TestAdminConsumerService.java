linkedin/venice

commit dd1014db79a074711846172fca7aeee38678bcc8

[da-vinci-client][server][controller] Auto-close idle Kafka consumers
New AutoClosingKafkaConsumer class: This implementation of Kafka's Consumer
interface starts off completely inert, and lazily instantiates a
KafkaConsumer when the first partition is subscribed to. Conversely, it
closes itself when the last subscribed partition is removed. It can go back
and forth like this indefinitely.

There are two motivations for this wrapper:

1. Efficiency. Reaping unused consumers in a way that is transparent to the
   using code is attractive, as it encapsulates the resource recuperation
   into a single location.

2. Stability. There are cases where swapping all brokers of a Kafka cluster
   while some Consumer instance is idle causes issues. Letting consumers
   auto-close themselves when not in use is a way to mitigate that issue.
   Note that it is not a perfect mitigation, since it is possible for a
   consumer to still be idle (i.e. not polling) while having some subscribed
   partitions. In the Venice use case, it is assumed that this edge case
   doesn't happen.

Other changes:

- New config in servers and controllers to enable the above behavior:
  - auto.close.idle.consumers (default: false, but true in the tests)

- All consumer factories now take a Optional<MetricsParameters> in their
  constructor, which is used by the AutoClosingKafkaConsumer to report the
  following new stats:
  - auto_close.{OccurrenceRate, Count, Total}
  - auto_create.{OccurrenceRate, Count, Total}
  - duration_from_close_to_create_ms.{Min, Max, Avg}
  - duration_from_create_to_close_ms.{Min, Max, Avg}
  - created_consumers_count.Gauge
  - closed_consumers_count.Gauge

- TopicManager constructor changes:
  - Removed the isConcurrentTopicDeleteRequestsEnabled param, since it was
    only wired in halfway, but not actually configurable. It is now fully
    hard-coded, rather than pretending to be configurable.
  - Removed the PartitionOffsetFetcher param, and instead it is now always

venice-test-common/src/main/java/com/linkedin/venice/utils/TestUtils.java
@@ -443,7 +443,7 @@ public class TestUtils {
     }
 
     @Override
-    protected KafkaClientFactory clone(String kafkaBootstrapServers, String kafkaZkAddress) {
+    protected KafkaClientFactory clone(String kafkaBootstrapServers, String kafkaZkAddress, Optional<MetricsParameters> metricsParameters) {
       return new TestKafkaClientFactory(kafkaBootstrapServers, kafkaZkAddress);
     }
   }

da-vinci-client/src/main/java/com/linkedin/davinci/config/VeniceServerConfig.java
@@ -3,6 +3,7 @@ package com.linkedin.davinci.config;
 import com.linkedin.davinci.store.rocksdb.RocksDBServerConfig;
 import com.linkedin.venice.exceptions.ConfigurationException;
 import com.linkedin.venice.exceptions.VeniceException;
+import com.linkedin.venice.kafka.KafkaClientFactory;
 import com.linkedin.venice.kafka.admin.KafkaAdminClient;
 import com.linkedin.venice.meta.IngestionMode;
 import com.linkedin.venice.utils.Time;
@@ -262,6 +263,8 @@ public class VeniceServerConfig extends VeniceClusterConfig {
 
   private final boolean enableLiveConfigBasedKafkaThrottling;
 
+  private final boolean autoCloseIdleConsumersEnabled;
+
   public VeniceServerConfig(VeniceProperties serverProperties) throws ConfigurationException {
     this(serverProperties, Optional.empty());
   }
@@ -408,6 +411,7 @@ public class VeniceServerConfig extends VeniceClusterConfig {
     fastClassSchemaWarmupTimeout = serverProperties.getLong(SERVER_SCHEMA_FAST_CLASS_WARMUP_TIMEOUT, 2*Time.MS_PER_MINUTE);
     schemaPresenceCheckEnabled = serverProperties.getBoolean(SERVER_SCHEMA_PRESENCE_CHECK_ENABLED, true);
     enableLiveConfigBasedKafkaThrottling = serverProperties.getBoolean(SERVER_ENABLE_LIVE_CONFIG_BASED_KAFKA_THROTTLING, false);
+    autoCloseIdleConsumersEnabled = serverProperties.getBoolean(AUTO_CLOSE_IDLE_CONSUMERS_ENABLED, KafkaClientFactory.DEFAULT_AUTO_CLOSE_IDLE_CONSUMERS_ENABLED);
   }
 
   public int getListenerPort() {
@@ -783,5 +787,9 @@ public class VeniceServerConfig extends VeniceClusterConfig {
 
   public boolean isLiveConfigBasedKafkaThrottlingEnabled() {
     return enableLiveConfigBasedKafkaThrottling;
+  }
+
+  public boolean isAutoCloseIdleConsumersEnabled() {
+    return autoCloseIdleConsumersEnabled;
   }
 }

da-vinci-client/src/main/java/com/linkedin/davinci/kafka/consumer/KafkaStoreIngestionService.java
@@ -26,6 +26,7 @@ import com.linkedin.venice.compression.CompressionStrategy;
 import com.linkedin.venice.exceptions.VeniceException;
 import com.linkedin.venice.exceptions.VeniceNoStoreException;
 import com.linkedin.venice.helix.HelixReadOnlyZKSharedSchemaRepository;
+import com.linkedin.venice.kafka.KafkaClientFactory.MetricsParameters;
 import com.linkedin.venice.kafka.TopicManagerRepository;
 import com.linkedin.venice.kafka.consumer.ApacheKafkaConsumer;
 import com.linkedin.venice.kafka.protocol.state.PartitionState;
@@ -199,15 +200,20 @@ public class KafkaStoreIngestionService extends AbstractVeniceService implements
     this.topicLockManager = new ResourceAutoClosableLockManager(() -> new ReentrantLock());
 
     VeniceServerConfig serverConfig = veniceConfigLoader.getVeniceServerConfig();
-    VeniceServerConsumerFactory veniceConsumerFactory = new VeniceServerConsumerFactory(serverConfig, kafkaMessageEnvelopeSchemaReader);
+    VeniceServerConsumerFactory veniceConsumerFactory = new VeniceServerConsumerFactory(
+        serverConfig,
+        kafkaMessageEnvelopeSchemaReader,
+        Optional.of(new MetricsParameters(VeniceServerConsumerFactory.class.getSimpleName(), metricsRepository))
+    );
 
     /**
      * This new veniceConsumerJavaBasedFactory (underneath it works with java based admin client only) is needed for leader_offset_lag metrics to work.
      * TODO: This should be removed once the VeniceServerConsumerFactory uses java based admin client in production reliably.
      */
     VeniceServerConsumerJavaBasedFactory veniceConsumerJavaBasedFactory = new VeniceServerConsumerJavaBasedFactory(
-            serverConfig,
-            kafkaMessageEnvelopeSchemaReader
+        serverConfig,
+        kafkaMessageEnvelopeSchemaReader,
+        Optional.of(new MetricsParameters(VeniceServerConsumerJavaBasedFactory.class.getSimpleName(), metricsRepository))
     );
 
     Properties veniceWriterProperties = veniceConfigLoader.getVeniceClusterConfig().getClusterProperties().toProperties();

da-vinci-client/src/main/java/com/linkedin/davinci/kafka/consumer/VeniceServerConsumerFactory.java
@@ -5,8 +5,6 @@ import com.linkedin.davinci.config.VeniceServerConfig;
 import com.linkedin.venice.client.schema.SchemaReader;
 import com.linkedin.venice.exceptions.VeniceException;
 import com.linkedin.venice.kafka.KafkaClientFactory;
-import com.linkedin.venice.kafka.consumer.ApacheKafkaConsumer;
-import com.linkedin.venice.kafka.consumer.KafkaConsumerWrapper;
 import com.linkedin.venice.utils.KafkaSSLUtils;
 import com.linkedin.venice.utils.VeniceProperties;
 import java.util.Optional;
@@ -21,8 +19,12 @@ import static org.apache.kafka.common.config.SslConfigs.*;
 public class VeniceServerConsumerFactory extends KafkaClientFactory {
   protected final VeniceServerConfig serverConfig;
 
-  public VeniceServerConsumerFactory(VeniceServerConfig serverConfig, Optional<SchemaReader> kafkaMessageEnvelopeSchemaReader) {
-    super(kafkaMessageEnvelopeSchemaReader);
+  public VeniceServerConsumerFactory(
+      VeniceServerConfig serverConfig,
+      Optional<SchemaReader> kafkaMessageEnvelopeSchemaReader,
+      Optional<MetricsParameters> metricsParameters
+  ) {
+    super(kafkaMessageEnvelopeSchemaReader, metricsParameters, serverConfig.isAutoCloseIdleConsumersEnabled());
     this.serverConfig = serverConfig;
   }
 
@@ -66,15 +68,19 @@ public class VeniceServerConsumerFactory extends KafkaClientFactory {
   }
 
   @Override
-  protected KafkaClientFactory clone(String kafkaBootstrapServers, String kafkaZkAddress) {
-    Properties clonedProperties = this.serverConfig.getClusterProperties().toProperties();
-    clonedProperties.setProperty(KAFKA_BOOTSTRAP_SERVERS, kafkaBootstrapServers);
-    clonedProperties.setProperty(KAFKA_ZK_ADDRESS, kafkaZkAddress);
-    return new VeniceServerConsumerFactory(new VeniceServerConfig(new VeniceProperties(clonedProperties)), kafkaMessageEnvelopeSchemaReader);
+  protected boolean isKafkaConsumerOffsetCollectionEnabled() {
+    return serverConfig.isKafkaConsumerOffsetCollectionEnabled();
   }
 
   @Override
-  public KafkaConsumerWrapper getConsumer(Properties props) {
-    return new ApacheKafkaConsumer(setupSSL(props), serverConfig.isKafkaConsumerOffsetCollectionEnabled());
+  protected KafkaClientFactory clone(String kafkaBootstrapServers, String kafkaZkAddress, Optional<MetricsParameters> metricsParameters) {
+    Properties clonedProperties = this.serverConfig.getClusterProperties().toProperties();
+    clonedProperties.setProperty(KAFKA_BOOTSTRAP_SERVERS, kafkaBootstrapServers);
+    clonedProperties.setProperty(KAFKA_ZK_ADDRESS, kafkaZkAddress);
+    return new VeniceServerConsumerFactory(
+        new VeniceServerConfig(new VeniceProperties(clonedProperties)),
+        kafkaMessageEnvelopeSchemaReader,
+        metricsParameters
+    );
   }
 }

da-vinci-client/src/main/java/com/linkedin/davinci/kafka/consumer/VeniceServerConsumerJavaBasedFactory.java
@@ -5,6 +5,7 @@ import com.linkedin.venice.client.schema.SchemaReader;
 import com.linkedin.venice.kafka.KafkaClientFactory;
 import com.linkedin.venice.kafka.admin.KafkaAdminClient;
 import com.linkedin.venice.utils.VeniceProperties;
+import io.tehuti.metrics.MetricsRepository;
 import java.util.Optional;
 import java.util.Properties;
 
@@ -12,8 +13,12 @@ import static com.linkedin.venice.ConfigKeys.*;
 
 
 public class VeniceServerConsumerJavaBasedFactory extends VeniceServerConsumerFactory {
-  public VeniceServerConsumerJavaBasedFactory(VeniceServerConfig serverConfig, Optional<SchemaReader> kafkaMessageEnvelopeSchemaReader) {
-    super(serverConfig, kafkaMessageEnvelopeSchemaReader);
+  public VeniceServerConsumerJavaBasedFactory(
+      VeniceServerConfig serverConfig,
+      Optional<SchemaReader> kafkaMessageEnvelopeSchemaReader,
+      Optional<MetricsParameters> metricsParameters
+  ) {
+    super(serverConfig, kafkaMessageEnvelopeSchemaReader, metricsParameters);
   }
 
   @Override
@@ -22,10 +27,14 @@ public class VeniceServerConsumerJavaBasedFactory extends VeniceServerConsumerFa
   }
 
   @Override
-  protected KafkaClientFactory clone(String kafkaBootstrapServers, String kafkaZkAddress) {
+  protected KafkaClientFactory clone(String kafkaBootstrapServers, String kafkaZkAddress, Optional<MetricsParameters> metricsParameters) {
     Properties clonedProperties = this.serverConfig.getClusterProperties().toProperties();
     clonedProperties.setProperty(KAFKA_BOOTSTRAP_SERVERS, kafkaBootstrapServers);
     clonedProperties.setProperty(KAFKA_ZK_ADDRESS, kafkaZkAddress);
-    return new VeniceServerConsumerJavaBasedFactory(new VeniceServerConfig(new VeniceProperties(clonedProperties)), kafkaMessageEnvelopeSchemaReader);
+    return new VeniceServerConsumerJavaBasedFactory(
+        new VeniceServerConfig(new VeniceProperties(clonedProperties)),
+        kafkaMessageEnvelopeSchemaReader,
+        metricsParameters
+    );
   }
 }

venice-common/src/main/java/com/linkedin/venice/ConfigKeys.java
@@ -1709,5 +1709,10 @@ public class ConfigKeys {
 
   public static final String SERVER_ENABLE_LIVE_CONFIG_BASED_KAFKA_THROTTLING = "server.enable.live.config.based.kafka.throttling";
 
+  /**
+   * Enable usage of {@link com.linkedin.venice.kafka.consumer.AutoClosingKafkaConsumer} instead of the raw
+   * {@link org.apache.kafka.clients.consumer.KafkaConsumer}.
+   */
+  public static final String AUTO_CLOSE_IDLE_CONSUMERS_ENABLED = "auto.close.idle.consumers";
 }
 

commit bf79c73ed9fb01f2a7dab00ac9e5e5ed4b6e8a7d

[test] Added a unit test for the AdminConsumerService
This test triggers the path which had regression in
dd1014db79a074711846172fca7aeee38678bcc8 and which was fixed in
e8a97a20b5a5d477d3874b97f4690271aae0ad7e

Unrelated:

- Removed some dead function and dead params from TestUtils.
- Muted a noisy stacktrace in the router initialization.

RB=3030594
BUG=VENG-8923
G=voldemort-reviewers
R=lcli,nithakka,jlliu,xnma,goliu,xyin,anaberez,sumane,akozma,zpolicze,lelu,sdwu,yurzhou,adietz,ssen,xhao,mnhuang,smaji,shuhliu
A=anaberez

venice-router/src/main/java/com/linkedin/venice/router/RouterServer.java
@@ -374,7 +374,7 @@ public class RouterServer extends AbstractVeniceService {
       serverSocketChannelClass = EpollServerSocketChannel.class;
     } catch (LinkageError error) {
       useEpoll = false;
-      logger.info("Epoll is only supported on Linux; switching to NIO", error);
+      logger.info("Epoll is only supported on Linux; switching to NIO");
       workerEventLoopGroup = new NioEventLoopGroup(config.getNettyClientEventLoopThreads(), workerExecutor);
       channelClass = NioSocketChannel.class;
       serverEventLoopGroup = new NioEventLoopGroup(ROUTER_BOSS_THREAD_NUM);

venice-test-common/src/main/java/com/linkedin/venice/utils/TestUtils.java
@@ -76,6 +76,7 @@ import io.tehuti.metrics.MetricsRepository;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.ByteBuffer;
+import java.nio.file.Paths;
 import java.security.Permission;
 import java.util.ArrayDeque;
 import java.util.Arrays;
@@ -363,27 +364,6 @@ public class TestUtils {
     });
   }
 
-  public static void waitForNonDeterministicIncrementalPushCompletion(String topicName, String incrementalPushVersion,
-      ControllerClient controllerClient, long timeout, TimeUnit timeoutUnit, Optional<Logger> logger) {
-    waitForNonDeterministicCompletion(timeout, timeoutUnit, () -> {
-      String emptyPushStatus = controllerClient.queryJobStatus(topicName, Optional.of(incrementalPushVersion)).getStatus();
-      boolean ignoreError = false;
-      try {
-        assertNotEquals(emptyPushStatus, ExecutionStatus.ERROR.toString(), "Unexpected incremental push failure");
-        ignoreError = true;
-        assertEquals(emptyPushStatus, ExecutionStatus.END_OF_INCREMENTAL_PUSH_RECEIVED.toString(),
-            "Incremental push is yet to complete");
-        return true;
-      } catch (AssertionError | VerifyError e) {
-        if (ignoreError) {
-          logger.ifPresent(value -> value.info(e.getMessage()));
-          return false;
-        }
-        throw e;
-      }
-    });
-  }
-
   public static Store createTestStore(String name, String owner, long createdTime) {
       Store store = new ZKStore(name, owner, createdTime, PersistenceType.IN_MEMORY, RoutingStrategy.CONSISTENT_HASH,
           ReadStrategy.ANY_OF_ONLINE, OfflinePushStrategy.WAIT_ALL_REPLICAS,
@@ -450,6 +430,21 @@ public class TestUtils {
     Map<String,VeniceControllerConfig> configMap = new HashMap<>();
     configMap.put(controllerConfig.getClusterName(),controllerConfig);
     return new VeniceControllerMultiClusterConfig(configMap);
+  }
+
+  public static Properties getPropertiesForControllerConfig() throws IOException {
+    String currentPath = Paths.get("").toAbsolutePath().toString();
+    if (currentPath.endsWith("venice-controller")) {
+      currentPath += "/..";
+    }
+    VeniceProperties clusterProps =
+        Utils.parseProperties(currentPath + "/venice-server/config/cluster.properties");
+    VeniceProperties baseControllerProps =
+        Utils.parseProperties(currentPath + "/venice-controller/config/controller.properties");
+    Properties properties = new Properties();
+    properties.putAll(clusterProps.toProperties());
+    properties.putAll(baseControllerProps.toProperties());
+    return properties;
   }
 
   public static String getClusterToDefaultD2String(String cluster) {
@@ -589,8 +584,10 @@ public class TestUtils {
     });
   }
 
-  public static void verifySystemStoreInAllRegions(String regularStoreName, VeniceSystemStoreType systemStoreType,
-      ControllerClient parentControllerClient, List<ControllerClient> controllerClientList) {
+  public static void verifySystemStoreInAllRegions(
+      String regularStoreName,
+      VeniceSystemStoreType systemStoreType,
+      List<ControllerClient> controllerClientList) {
     String systemStoreName = systemStoreType.getSystemStoreName(regularStoreName);
     TestUtils.waitForNonDeterministicAssertion(60, TimeUnit.SECONDS, () -> {
       for (ControllerClient client : controllerClientList) {
@@ -637,7 +634,7 @@ public class TestUtils {
     }
   }
 
-  public static StoreIngestionTaskFactory.Builder getStoreIngestionTaskBuilder(String storeName, int versionNumber) {
+  public static StoreIngestionTaskFactory.Builder getStoreIngestionTaskBuilder(String storeName) {
     VeniceServerConfig mockVeniceServerConfig = mock(VeniceServerConfig.class);
     doReturn(false).when(mockVeniceServerConfig).isHybridQuotaEnabled();
     VeniceProperties mockVeniceProperties = mock(VeniceProperties.class);

da-vinci-client/src/test/java/com/linkedin/davinci/kafka/consumer/PushTimeoutTest.java
@@ -34,7 +34,7 @@ public class PushTimeoutTest {
     Queue<VeniceNotifier> notifiers = new ArrayDeque<>();
     notifiers.add(exceptionCaptorNotifier);
 
-    StoreIngestionTaskFactory.Builder builder = TestUtils.getStoreIngestionTaskBuilder(storeName, versionNumber)
+    StoreIngestionTaskFactory.Builder builder = TestUtils.getStoreIngestionTaskBuilder(storeName)
         .setLeaderFollowerNotifiersQueue(notifiers);
 
     Store mockStore = builder.getMetadataRepo().getStoreOrThrow(storeName);
@@ -81,7 +81,7 @@ public class PushTimeoutTest {
 
     StorageMetadataService mockStorageMetadataService = mock(StorageMetadataService.class);
 
-    StoreIngestionTaskFactory.Builder builder = TestUtils.getStoreIngestionTaskBuilder(storeName, versionNumber)
+    StoreIngestionTaskFactory.Builder builder = TestUtils.getStoreIngestionTaskBuilder(storeName)
         .setLeaderFollowerNotifiersQueue(notifiers)
         .setStorageMetadataService(mockStorageMetadataService);
 

da-vinci-client/src/test/java/com/linkedin/davinci/kafka/consumer/StoreIngestionTaskTest.java
@@ -2554,7 +2554,7 @@ public class StoreIngestionTaskTest {
     doReturn(true).when(mockVeniceProperties).isEmpty();
     doReturn(mockVeniceProperties).when(mockVeniceServerConfig).getKafkaConsumerConfigsForLocalConsumption();
 
-    StoreIngestionTaskFactory.Builder builder = TestUtils.getStoreIngestionTaskBuilder(storeName, 1)
+    StoreIngestionTaskFactory.Builder builder = TestUtils.getStoreIngestionTaskBuilder(storeName)
         .setTopicManagerRepository(mockTopicManagerRepository)
         .setStorageMetadataService(mockStorageMetadataService)
         .setMetadataRepository(mockReadOnlyStoreRepository)

venice-controller/src/test/java/com/linkedin/venice/controller/AbstractTestVeniceHelixAdmin.java
@@ -1,6 +1,5 @@
 package com.linkedin.venice.controller;
 
-import com.linkedin.venice.ConfigKeys;
 import com.linkedin.venice.VeniceStateModel;
 import com.linkedin.venice.common.VeniceSystemStoreUtils;
 import com.linkedin.venice.exceptions.VeniceException;
@@ -23,9 +22,7 @@ import com.linkedin.venice.utils.Utils;
 import com.linkedin.venice.utils.VeniceProperties;
 import io.tehuti.metrics.MetricsRepository;
 import java.io.IOException;
-import java.nio.file.Paths;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
@@ -166,17 +163,7 @@ class AbstractTestVeniceHelixAdmin {
   }
 
   Properties getControllerProperties(String clusterName) throws IOException {
-    String currentPath = Paths.get("").toAbsolutePath().toString();
-    if (currentPath.endsWith("venice-controller")) {
-      currentPath += "/..";
-    }
-    VeniceProperties clusterProps = Utils.parseProperties(currentPath + "/venice-server/config/cluster.properties");
-    VeniceProperties baseControllerProps =
-        Utils.parseProperties(currentPath + "/venice-controller/config/controller.properties");
-    clusterProps.getString(ConfigKeys.CLUSTER_NAME);
-    Properties properties = new Properties();
-    properties.putAll(clusterProps.toProperties());
-    properties.putAll(baseControllerProps.toProperties());
+    Properties properties = TestUtils.getPropertiesForControllerConfig();
     properties.put(ENABLE_TOPIC_REPLICATOR, false);
     properties.put(KAFKA_ZK_ADDRESS, kafkaZkAddress);
     properties.put(ZOOKEEPER_ADDRESS, zkAddress);

venice-controller/src/test/java/com/linkedin/venice/controller/kafka/consumer/TestAdminConsumerService.java
@@ -1,0 +1,83 @@
+package com.linkedin.venice.controller.kafka.consumer;
+
+import com.linkedin.venice.controller.VeniceControllerConfig;
+import com.linkedin.venice.controller.VeniceHelixAdmin;
+import com.linkedin.venice.helix.HelixAdapterSerializer;
+import com.linkedin.venice.kafka.KafkaClientFactory;
+import com.linkedin.venice.utils.PropertyBuilder;
+import com.linkedin.venice.utils.TestUtils;
+import com.linkedin.venice.utils.Utils;
+import com.linkedin.venice.utils.VeniceProperties;
+import io.tehuti.metrics.MetricsRepository;
+import java.io.IOException;
+import java.util.Optional;
+import org.apache.helix.zookeeper.impl.client.ZkClient;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static com.linkedin.venice.ConfigKeys.*;
+import static org.mockito.Mockito.*;
+
+public class TestAdminConsumerService {
+  @Test
+  public void testMutlipleAdminConsumerServiceWithSameMetricsRepo() throws IOException {
+    MetricsRepository metricsRepository = new MetricsRepository();
+
+    String someClusterName = "clusterName";
+    String adminTopicSourceRegion = "parent";
+
+    VeniceProperties props = new PropertyBuilder()
+        .put(TestUtils.getPropertiesForControllerConfig())
+        .put(CLUSTER_TO_D2, TestUtils.getClusterToDefaultD2String(someClusterName))
+        .put(ADMIN_TOPIC_REMOTE_CONSUMPTION_ENABLED, true)
+        .put(ADMIN_TOPIC_SOURCE_REGION, adminTopicSourceRegion)
+        .put(NATIVE_REPLICATION_FABRIC_WHITELIST, adminTopicSourceRegion)
+        .put(CHILD_DATA_CENTER_KAFKA_URL_PREFIX + "." + adminTopicSourceRegion, "blah")
+        .put(CHILD_DATA_CENTER_KAFKA_ZK_PREFIX + "." + adminTopicSourceRegion, "blah")
+        .put(CHILD_CLUSTER_ALLOWLIST, someClusterName)
+        .build();
+    VeniceControllerConfig controllerConfig = new VeniceControllerConfig(props);
+
+
+    ControllerKafkaClientFactory consumerFactory = new ControllerKafkaClientFactory(
+        controllerConfig,
+        Optional.of(new KafkaClientFactory.MetricsParameters("test", metricsRepository))
+    );
+
+    VeniceHelixAdmin admin = mock(VeniceHelixAdmin.class);
+    doReturn(mock(ZkClient.class)).when(admin).getZkClient();
+    doReturn(mock(HelixAdapterSerializer.class)).when(admin).getAdapterSerializer();
+    doReturn(consumerFactory).when(admin).getVeniceConsumerFactory();
+
+    AdminConsumerService adminConsumerService1 = null;
+    AdminConsumerService adminConsumerService2 = null;
+    try {
+      adminConsumerService1 = new AdminConsumerService(
+          "cluster1",
+          admin,
+          controllerConfig,
+          metricsRepository,
+          Optional.empty()
+      );
+
+      /**
+       * The creation of a second {@link AdminConsumerService} crashed after introducing a regression
+       * which caused duplicate metrics getting registered.
+       */
+      try {
+        adminConsumerService2 = new AdminConsumerService(
+            "cluster2",
+            admin,
+            controllerConfig,
+            metricsRepository,
+            Optional.empty()
+        );
+      } catch (Exception e) {
+        Assert.fail("Creating a second " + AdminConsumerService.class.getSimpleName() + " should not fail", e);
+      }
+    } finally {
+      Utils.closeQuietlyWithErrorLogged(adminConsumerService1);
+      Utils.closeQuietlyWithErrorLogged(adminConsumerService2);
+    }
+  }
+}

venice-test-common/src/integrationtest/java/com/linkedin/venice/endToEnd/ActiveActiveReplicationForHybridTest.java
@@ -299,13 +299,15 @@ public class ActiveActiveReplicationForHybridTest {
       VersionCreationResponse versionCreationResponse = parentControllerClient.emptyPush(storeName, Utils.getUniqueString("empty-hybrid-push"), 1L);
 
       //disable the purging of transientRecord buffer using reflection.
+      // TODO: Clean up this reflection stuff... Why do we even need this at all? And is there not a better way?
       if (useTransientRecordCache) {
         for (VeniceMultiClusterWrapper veniceColo : multiColoMultiClusterWrapper.getClusters()) {
           VeniceClusterWrapper veniceCluster = veniceColo.getClusters().get(clusterName);
           // Wait for push to complete in the colo
-          TestUtils.waitForNonDeterministicAssertion(30, TimeUnit.SECONDS, false, true, () ->
-              Assert.assertEquals(veniceCluster.getControllerClient().getStore(storeName).getStore().getCurrentVersion(),
-                  Version.parseVersionFromKafkaTopicName(versionCreationResponse.getKafkaTopic())));
+          veniceCluster.useControllerClient(controllerClient ->
+              TestUtils.waitForNonDeterministicAssertion(30, TimeUnit.SECONDS, false, true, () ->
+                  Assert.assertEquals(controllerClient.getStore(storeName).getStore().getCurrentVersion(),
+                      Version.parseVersionFromKafkaTopicName(versionCreationResponse.getKafkaTopic()))));
           for (VeniceServerWrapper veniceServerWrapper : veniceCluster.getVeniceServers()){
             VeniceServer veniceServer = veniceServerWrapper.getVeniceServer();
             StoreIngestionTask ingestionTask = veniceServer.getKafkaStoreIngestionService().getStoreIngestionTask(versionCreationResponse.getKafkaTopic());
@@ -639,8 +641,9 @@ public class ActiveActiveReplicationForHybridTest {
         ControllerClient dc2Client = new ControllerClient(clusterName, childDatacenters.get(2).getControllerConnectString())) {
       List<ControllerClient> dcControllerClientList = Arrays.asList(dc0Client, dc1Client, dc2Client);
       TestUtils.createAndVerifyStoreInAllRegions(storeName, parentControllerClient, dcControllerClientList);
-      TestUtils.verifySystemStoreInAllRegions(storeName, VeniceSystemStoreType.META_STORE, parentControllerClient, dcControllerClientList);
-      TestUtils.verifySystemStoreInAllRegions(storeName, VeniceSystemStoreType.DAVINCI_PUSH_STATUS_STORE, parentControllerClient, dcControllerClientList);
+      TestUtils.verifySystemStoreInAllRegions(storeName, VeniceSystemStoreType.META_STORE, dcControllerClientList);
+      TestUtils.verifySystemStoreInAllRegions(storeName, VeniceSystemStoreType.DAVINCI_PUSH_STATUS_STORE,
+          dcControllerClientList);
       TestUtils.assertCommand(parentControllerClient.updateStore(storeName, new UpdateStoreQueryParams()
           .setLeaderFollowerModel(true)
           .setHybridRewindSeconds(10)