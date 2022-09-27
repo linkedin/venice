6cdd370bf9689a5aab6c22cab87fe9af4168d5dd
Zac Policzer <zpolicze@zpolicze-mn1.linkedin.biz>
2022-09-27 12:53:28 -0700

    Revert "[server] change function name to address comment on PR"
    This reverts commit 47e30e930180aecd9600a3a1bddf169e13833871.

--- clients/da-vinci-client/src/main/java/com/linkedin/davinci/kafka/consumer/ActiveActiveStoreIngestionTask.java
+++ clients/da-vinci-client/src/main/java/com/linkedin/davinci/kafka/consumer/ActiveActiveStoreIngestionTask.java
@@ -563,7 +563,7 @@ public class ActiveActiveStoreIngestionTask extends LeaderFollowerStoreIngestion
       int valueLen = updatedValueBytes.remaining();
 
       // Make sure there is capacity in the drainer queue before occupying space in the transient record cache
-      maybeBlockOnDrainerCapacity();
+      blockOnDrainerCapacity();
       partitionConsumptionState.setTransientRecord(
           kafkaClusterId,
           consumerRecord.offset(),
