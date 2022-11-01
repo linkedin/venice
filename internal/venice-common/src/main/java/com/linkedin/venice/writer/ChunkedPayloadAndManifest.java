087cdce9a84ebcaf4391ce00d3791252d9ae1a61
Jialin Liu <jlliu@linkedin.com>
2022-10-31 21:37:48 -0700

    [common] Adding chunking support for large RMD payload in Venice Writer (#86)

---
+++ internal/venice-common/src/main/java/com/linkedin/venice/writer/ChunkedPayloadAndManifest.java
@@ -0,0 +1,26 @@
+package com.linkedin.venice.writer;
+
+import com.linkedin.venice.storage.protocol.ChunkedValueManifest;
+import java.nio.ByteBuffer;
+
+
+/**
+ * This class contains both chunked results and manifest for a specific payload.
+ */
+public class ChunkedPayloadAndManifest {
+  ByteBuffer[] payloadChunks;
+  ChunkedValueManifest chunkedValueManifest;
+
+  public ChunkedPayloadAndManifest(ByteBuffer[] payloadChunks, ChunkedValueManifest chunkedValueManifest) {
+    this.payloadChunks = payloadChunks;
+    this.chunkedValueManifest = chunkedValueManifest;
+  }
+
+  public ByteBuffer[] getPayloadChunks() {
+    return payloadChunks;
+  }
+
+  public ChunkedValueManifest getChunkedValueManifest() {
+    return chunkedValueManifest;
+  }
+}
