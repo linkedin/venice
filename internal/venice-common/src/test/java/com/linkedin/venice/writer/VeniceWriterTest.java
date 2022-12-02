eb3aa28735fd7bfcd5b840ff239c9615619d2c4a
Jialin Liu <sixpluszero630@gmail.com>
2022-12-01 20:34:45 -0800

    Add first unit test: test RMD chunking can send rmd chunk and save rmd manifest to last chunk

--- internal/venice-common/src/test/java/com/linkedin/venice/writer/VeniceWriterTest.java
+++ internal/venice-common/src/test/java/com/linkedin/venice/writer/VeniceWriterTest.java
@@ -1,5 +1,6 @@
 package com.linkedin.venice.writer;
 
+import static com.linkedin.venice.writer.VeniceWriter.*;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.anyInt;
 import static org.mockito.Mockito.anyString;
@@ -24,8 +25,14 @@ import com.linkedin.venice.kafka.protocol.Put;
 import com.linkedin.venice.kafka.protocol.enums.MessageType;
 import com.linkedin.venice.message.KafkaKey;
 import com.linkedin.venice.partitioner.DefaultVenicePartitioner;
+import com.linkedin.venice.serialization.KeyWithChunkingSuffixSerializer;
 import com.linkedin.venice.serialization.VeniceKafkaSerializer;
+import com.linkedin.venice.serialization.avro.AvroProtocolDefinition;
+import com.linkedin.venice.serialization.avro.ChunkedValueManifestSerializer;
 import com.linkedin.venice.serialization.avro.VeniceAvroKafkaSerializer;
+import com.linkedin.venice.storage.protocol.ChunkId;
+import com.linkedin.venice.storage.protocol.ChunkedKeySuffix;
+import com.linkedin.venice.storage.protocol.ChunkedValueManifest;
 import com.linkedin.venice.utils.SystemTime;
 import com.linkedin.venice.utils.TestUtils;
 import com.linkedin.venice.utils.Time;
@@ -33,6 +40,7 @@ import com.linkedin.venice.utils.Utils;
 import com.linkedin.venice.utils.VeniceProperties;
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -279,7 +287,7 @@ public class VeniceWriterTest {
     Assert.assertEquals(value6.producerMetadata.logicalTimestamp, VeniceWriter.APP_DEFAULT_LOGICAL_TS);
   }
 
-  @Test(timeOut = 30000)
+  @Test(timeOut = 10000)
   public void testReplicationMetadataChunking() throws ExecutionException, InterruptedException, TimeoutException {
     KafkaProducerWrapper mockedProducer = mock(KafkaProducerWrapper.class);
     Future mockedFuture = mock(Future.class);
@@ -287,6 +295,9 @@ public class VeniceWriterTest {
     when(mockedProducer.getNumberOfPartitions(any(), anyInt(), any())).thenReturn(1);
     when(mockedProducer.sendMessage(anyString(), any(), any(), anyInt(), any())).thenReturn(mockedFuture);
     Properties writerProperties = new Properties();
+    writerProperties.put(ENABLE_CHUNKING, true);
+    writerProperties.put(ENABLE_RMD_CHUNKING, true);
+
     String stringSchema = "\"string\"";
     VeniceKafkaSerializer serializer = new VeniceAvroKafkaSerializer(stringSchema);
     String testTopic = "test";
@@ -295,41 +306,28 @@ public class VeniceWriterTest {
         .setWriteComputeSerializer(serializer)
         .setPartitioner(new DefaultVenicePartitioner())
         .setTime(SystemTime.INSTANCE)
-        // .setChunkingEnabled(true)
-        // .setRmdChunkingEnabled(true)
         .build();
     VeniceWriter<Object, Object, Object> writer =
         new VeniceWriter(veniceWriterOptions, new VeniceProperties(writerProperties), () -> mockedProducer);
 
-    long ctime = System.currentTimeMillis();
     ByteBuffer replicationMetadata = ByteBuffer.wrap(new byte[] { 0xa, 0xb });
     PutMetadata putMetadata = new PutMetadata(1, replicationMetadata);
-    DeleteMetadata deleteMetadata = new DeleteMetadata(1, 1, replicationMetadata);
 
-    /*
+    StringBuilder stringBuilder = new StringBuilder();
+    for (int i = 0; i < 50000; i++) {
+      stringBuilder.append("abcdefghabcdefghabcdefghabcdefgh");
+    }
+    String valueString = stringBuilder.toString();
+
     writer.put(
         Integer.toString(1),
-        Integer.toString(1),
-        1,
-        null,
-        VeniceWriter.DEFAULT_LEADER_METADATA_WRAPPER,
-        ctime,
-        Optional.empty());
-    */
-    writer.put(
-        Integer.toString(2),
-        Integer.toString(2),
+        valueString,
         1,
         null,
         VeniceWriter.DEFAULT_LEADER_METADATA_WRAPPER,
         VeniceWriter.APP_DEFAULT_LOGICAL_TS,
         Optional.of(putMetadata));
-    // writer.update(Integer.toString(3), Integer.toString(2), 1, 1, null, ctime);
-    // writer.delete(Integer.toString(4), null, VeniceWriter.DEFAULT_LEADER_METADATA_WRAPPER, ctime);
-    // writer.delete(Integer.toString(5), null, VeniceWriter.DEFAULT_LEADER_METADATA_WRAPPER, deleteMetadata);
-    // writer.put(Integer.toString(6), Integer.toString(1), 1, null, VeniceWriter.DEFAULT_LEADER_METADATA_WRAPPER);
     ArgumentCaptor<KafkaKey> kafkaKeyArgumentCaptor = ArgumentCaptor.forClass(KafkaKey.class);
-
     ArgumentCaptor<KafkaMessageEnvelope> kafkaMessageEnvelopeArgumentCaptor =
         ArgumentCaptor.forClass(KafkaMessageEnvelope.class);
     verify(mockedProducer, atLeast(2)).sendMessage(
@@ -338,17 +336,117 @@ public class VeniceWriterTest {
         kafkaMessageEnvelopeArgumentCaptor.capture(),
         anyInt(),
         any());
+    KeyWithChunkingSuffixSerializer keyWithChunkingSuffixSerializer = new KeyWithChunkingSuffixSerializer();
+    byte[] serializedKey = serializer.serialize(testTopic, Integer.toString(1));
+    byte[] serializedValue = serializer.serialize(testTopic, valueString);
+    byte[] serializedRmd = replicationMetadata.array();
+    int availableMessageSize = DEFAULT_MAX_SIZE_FOR_USER_PAYLOAD_PER_MESSAGE_IN_BYTES - serializedKey.length;
 
-    // verify replicationMetadata is encoded correctly for Put.
-    KafkaMessageEnvelope value2 = kafkaMessageEnvelopeArgumentCaptor.getAllValues().get(1);
-    Assert.assertEquals(value2.messageType, MessageType.PUT.getValue());
-    Put put = (Put) value2.payloadUnion;
-    Assert.assertEquals(put.schemaId, 1);
-    Assert.assertEquals(put.replicationMetadataVersionId, 1);
-    Assert.assertEquals(put.replicationMetadataPayload, ByteBuffer.wrap(new byte[] { 0xa, 0xb }));
-    Assert.assertEquals(value2.producerMetadata.logicalTimestamp, VeniceWriter.APP_DEFAULT_LOGICAL_TS);
-    KafkaKey key2 = kafkaKeyArgumentCaptor.getAllValues().get(1);
-    Assert.assertEquals(key2.getKey(), serializer.serialize(testTopic, Integer.toString(2)));
+    // The order should be SOS, valueChunk1, valueChunk2, replicationMetadataChunk1, manifest for value and RMD.
+    Assert.assertEquals(kafkaMessageEnvelopeArgumentCaptor.getAllValues().size(), 5);
+
+    // Verify value of the 1st chunk.
+    KafkaMessageEnvelope actualValue1 = kafkaMessageEnvelopeArgumentCaptor.getAllValues().get(1);
+    Assert.assertEquals(actualValue1.messageType, MessageType.PUT.getValue());
+    Assert.assertEquals(((Put) actualValue1.payloadUnion).schemaId, -10);
+    Assert.assertEquals(((Put) actualValue1.payloadUnion).replicationMetadataVersionId, -1);
+    Assert.assertEquals(((Put) actualValue1.payloadUnion).replicationMetadataPayload, ByteBuffer.allocate(0));
+    Assert.assertEquals(((Put) actualValue1.payloadUnion).putValue.array().length, availableMessageSize + 4);
+    Assert.assertEquals(actualValue1.producerMetadata.logicalTimestamp, VENICE_DEFAULT_LOGICAL_TS);
+
+    // Verify value of the 2nd chunk.
+    KafkaMessageEnvelope actualValue2 = kafkaMessageEnvelopeArgumentCaptor.getAllValues().get(2);
+    Assert.assertEquals(actualValue2.messageType, MessageType.PUT.getValue());
+    Assert.assertEquals(((Put) actualValue2.payloadUnion).schemaId, -10);
+    Assert.assertEquals(((Put) actualValue2.payloadUnion).replicationMetadataVersionId, -1);
+    Assert.assertEquals(((Put) actualValue2.payloadUnion).replicationMetadataPayload, ByteBuffer.allocate(0));
+    Assert.assertEquals(
+        ((Put) actualValue2.payloadUnion).putValue.array().length,
+        (serializedValue.length - availableMessageSize) + 4);
+    Assert.assertEquals(actualValue2.producerMetadata.logicalTimestamp, VENICE_DEFAULT_LOGICAL_TS);
+
+    ChunkedValueManifestSerializer chunkedValueManifestSerializer = new ChunkedValueManifestSerializer(true);
+
+    final ChunkedValueManifest chunkedValueManifest = new ChunkedValueManifest();
+    chunkedValueManifest.schemaId = 1;
+    chunkedValueManifest.keysWithChunkIdSuffix = new ArrayList<>(2);
+    chunkedValueManifest.size = serializedValue.length;
+
+    // Verify key of the 1st value chunk.
+    ChunkedKeySuffix chunkedKeySuffix = new ChunkedKeySuffix();
+    chunkedKeySuffix.isChunk = true;
+    chunkedKeySuffix.chunkId = new ChunkId();
+    ProducerMetadata producerMetadata = actualValue1.producerMetadata;
+    chunkedKeySuffix.chunkId.producerGUID = producerMetadata.producerGUID;
+    chunkedKeySuffix.chunkId.segmentNumber = producerMetadata.segmentNumber;
+    chunkedKeySuffix.chunkId.messageSequenceNumber = producerMetadata.messageSequenceNumber;
+
+    ByteBuffer keyWithSuffix =
+        ByteBuffer.wrap(keyWithChunkingSuffixSerializer.serializeChunkedKey(serializedKey, chunkedKeySuffix));
+    chunkedValueManifest.keysWithChunkIdSuffix.add(keyWithSuffix);
+    KafkaKey expectedKey1 = new KafkaKey(MessageType.PUT, keyWithSuffix.array());
+    KafkaKey actualKey1 = kafkaKeyArgumentCaptor.getAllValues().get(1);
+    Assert.assertEquals(actualKey1.getKey(), expectedKey1.getKey());
+
+    // Verify key of the 2nd value chunk.
+    chunkedKeySuffix.chunkId.chunkIndex = 1;
+    keyWithSuffix =
+        ByteBuffer.wrap(keyWithChunkingSuffixSerializer.serializeChunkedKey(serializedKey, chunkedKeySuffix));
+    chunkedValueManifest.keysWithChunkIdSuffix.add(keyWithSuffix);
+    KafkaKey expectedKey2 = new KafkaKey(MessageType.PUT, keyWithSuffix.array());
+    KafkaKey actualKey2 = kafkaKeyArgumentCaptor.getAllValues().get(2);
+    Assert.assertEquals(actualKey2.getKey(), expectedKey2.getKey());
+
+    // Check value of the 1st RMD chunk.
+    KafkaMessageEnvelope actualValue3 = kafkaMessageEnvelopeArgumentCaptor.getAllValues().get(3);
+    Assert.assertEquals(actualValue3.messageType, MessageType.PUT.getValue());
+    Assert.assertEquals(((Put) actualValue3.payloadUnion).schemaId, -10);
+    Assert.assertEquals(((Put) actualValue3.payloadUnion).replicationMetadataVersionId, -1);
+    Assert.assertEquals(((Put) actualValue3.payloadUnion).putValue, ByteBuffer.allocate(0));
+    Assert.assertEquals(
+        ((Put) actualValue3.payloadUnion).replicationMetadataPayload.array().length,
+        serializedRmd.length + 4);
+    Assert.assertEquals(actualValue3.producerMetadata.logicalTimestamp, VENICE_DEFAULT_LOGICAL_TS);
+
+    // Check key of the 1st RMD chunk.
+    ChunkedValueManifest chunkedRmdManifest = new ChunkedValueManifest();
+    chunkedRmdManifest.schemaId = 1;
+    chunkedRmdManifest.keysWithChunkIdSuffix = new ArrayList<>(1);
+    chunkedRmdManifest.size = serializedRmd.length;
+    chunkedKeySuffix = new ChunkedKeySuffix();
+    chunkedKeySuffix.isChunk = true;
+    chunkedKeySuffix.chunkId = new ChunkId();
+    producerMetadata = actualValue3.producerMetadata;
+    chunkedKeySuffix.chunkId.producerGUID = producerMetadata.producerGUID;
+    chunkedKeySuffix.chunkId.segmentNumber = producerMetadata.segmentNumber;
+    chunkedKeySuffix.chunkId.messageSequenceNumber = producerMetadata.messageSequenceNumber;
+    keyWithSuffix =
+        ByteBuffer.wrap(keyWithChunkingSuffixSerializer.serializeChunkedKey(serializedKey, chunkedKeySuffix));
+    chunkedRmdManifest.keysWithChunkIdSuffix.add(keyWithSuffix);
+    KafkaKey expectedKey3 = new KafkaKey(MessageType.PUT, keyWithSuffix.array());
+    KafkaKey actualKey3 = kafkaKeyArgumentCaptor.getAllValues().get(3);
+    Assert.assertEquals(actualKey3.getKey(), expectedKey3.getKey());
+
+    // Check key of the manifest.
+    byte[] topLevelKey = keyWithChunkingSuffixSerializer.serializeNonChunkedKey(serializedKey);
+    KafkaKey expectedKey4 = new KafkaKey(MessageType.PUT, topLevelKey);
+    KafkaKey actualKey4 = kafkaKeyArgumentCaptor.getAllValues().get(4);
+    Assert.assertEquals(actualKey4.getKey(), expectedKey4.getKey());
+
+    // Check manifest for both value and rmd.
+    KafkaMessageEnvelope actualValue4 = kafkaMessageEnvelopeArgumentCaptor.getAllValues().get(4);
+    Assert.assertEquals(actualValue4.messageType, MessageType.PUT.getValue());
+    Assert.assertEquals(
+        ((Put) actualValue4.payloadUnion).schemaId,
+        AvroProtocolDefinition.CHUNKED_VALUE_MANIFEST.getCurrentProtocolVersion());
+    Assert.assertEquals(((Put) actualValue4.payloadUnion).replicationMetadataVersionId, putMetadata.getRmdVersionId());
+    Assert.assertEquals(
+        ((Put) actualValue4.payloadUnion).replicationMetadataPayload,
+        ByteBuffer.wrap(chunkedValueManifestSerializer.serialize(testTopic, chunkedRmdManifest)));
+    Assert.assertEquals(
+        ((Put) actualValue4.payloadUnion).putValue,
+        ByteBuffer.wrap(chunkedValueManifestSerializer.serialize(testTopic, chunkedValueManifest)));
+    Assert.assertEquals(actualValue4.producerMetadata.logicalTimestamp, APP_DEFAULT_LOGICAL_TS);
 
   }
 
