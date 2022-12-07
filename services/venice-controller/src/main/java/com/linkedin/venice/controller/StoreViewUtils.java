52c961461f38569bdc7de5e72873b463da0803f5
Zac Policzer <zpoliczer@linkedin.com>
2022-12-06 23:25:49 -0800

    [compat][controller] Tweaks to avro protocol to deprecate ViewType fr… (#124)
    * [compat][controller] Tweaks to avro protocol to deprecate ViewType from the schema and instead use a string to describe the class path
    of the view type that we'll load.  Also add ChangeCaptureView.
    
    * [controller][compat] Update avro protocol definition and add ViewUtils
    
    ViewUtils will encapsulate the reflective methods we'll rely on to build concrete
    instantiations of views.  Kind of like a factory.
    
    * [controller] Address comments and add recordApply interface
    
    also fully deprecate viewType enum
    
    * [admin-tool] Update tool tip to comply with refactoring
    
    EOM
    
    Co-authored-by: Zac Policzer <zpolicze@zpolicze-mn1.linkedin.biz>

--- services/venice-controller/src/main/java/com/linkedin/venice/controller/StoreViewUtils.java
+++ services/venice-controller/src/main/java/com/linkedin/venice/controller/StoreViewUtils.java
@@ -3,7 +3,6 @@ package com.linkedin.venice.controller;
 import com.linkedin.venice.controller.kafka.protocol.admin.StoreViewConfigRecord;
 import com.linkedin.venice.meta.ViewConfig;
 import com.linkedin.venice.meta.ViewConfigImpl;
-import com.linkedin.venice.meta.ViewType;
 import com.linkedin.venice.systemstore.schemas.StoreViewConfig;
 import java.util.HashMap;
 import java.util.Map;
@@ -11,12 +10,14 @@ import java.util.stream.Collectors;
 
 
 public class StoreViewUtils {
+  static String CHANGE_CAPTURE_VIEW_CLASS = "com.linkedin.venice.views.ChangeCaptureView";
+
   static Map<String, StoreViewConfigRecord> convertStringMapViewToStoreViewConfigRecord(Map<String, String> stringMap) {
     // TODO: Today we only support one kind of view and it takes no arguments. So all this returns is a map with a
     // single element that is using ChangeCapture as the output type and whatever key is passed in.
     Map<String, StoreViewConfigRecord> mergedViewConfigRecords = new HashMap<>();
     if (!stringMap.isEmpty()) {
-      StoreViewConfigRecord newViewConfigRecord = new StoreViewConfigRecord(ViewType.CHANGE_CAPTURE.value, null);
+      StoreViewConfigRecord newViewConfigRecord = new StoreViewConfigRecord(CHANGE_CAPTURE_VIEW_CLASS, null);
       mergedViewConfigRecords.put(stringMap.entrySet().iterator().next().getKey(), newViewConfigRecord);
     }
     return mergedViewConfigRecords;
@@ -27,7 +28,7 @@ public class StoreViewUtils {
     // single element that is using ChangeCapture as the output type and whatever key is passed in.
     Map<String, StoreViewConfig> mergedViewConfigs = new HashMap<>();
     if (!stringMap.isEmpty()) {
-      StoreViewConfig newStoreViewConfig = new StoreViewConfig(ViewType.CHANGE_CAPTURE.value, null);
+      StoreViewConfig newStoreViewConfig = new StoreViewConfig(CHANGE_CAPTURE_VIEW_CLASS, null);
       mergedViewConfigs.put(stringMap.entrySet().iterator().next().getKey(), newStoreViewConfig);
     }
     return mergedViewConfigs;
