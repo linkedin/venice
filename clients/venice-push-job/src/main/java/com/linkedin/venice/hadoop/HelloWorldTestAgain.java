365760dbb50117390d8a1f98c53c6732ac83ed8e
Xin(Adam) Chen <adchen@linkedin.com>
2023-01-17 13:04:38 -0800

    test diffCoverage with a new upstream

---
+++ clients/venice-push-job/src/main/java/com/linkedin/venice/hadoop/HelloWorldTestAgain.java
@@ -0,0 +1,11 @@
+package com.linkedin.venice.hadoop;
+
+public class HelloWorldTestAgain {
+  public static void checkNumber(int n) {
+    if (n >= 0) {
+      System.out.println("positive");
+    } else {
+      System.out.println("negative");
+    }
+  }
+}
---
+++ clients/venice-push-job/src/test/java/com/linkedin/venice/hadoop/TestHelloWorldTestAgain.java
@@ -0,0 +1,16 @@
+package com.linkedin.venice.hadoop;
+
+import org.testng.annotations.Test;
+
+
+public class TestHelloWorldTestAgain {
+  @Test
+  public void testPositive() {
+    HelloWorldTestAgain.checkNumber(1);
+  }
+
+  // @Test
+  // public void testNegative() {
+  // HelloWorldTestAgain.checkNumber(-1);
+  // }
+}
