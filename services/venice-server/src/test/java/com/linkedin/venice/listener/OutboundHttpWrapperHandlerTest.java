cf3a7a4ad0552493017e1d555cb2ae223840d05c
avedmala <abhinav.vedmala@gmail.com>
2023-02-28 13:36:38 -0800

    [server] addressed manoj review comments

---
+++ services/venice-server/src/test/java/com/linkedin/venice/listener/OutboundHttpWrapperHandlerTest.java
@@ -0,0 +1,12 @@
+package com.linkedin.venice.listener;
+
+import static org.testng.Assert.*;
+
+import org.testng.annotations.Test;
+
+
+public class OutboundHttpWrapperHandlerTest {
+  @Test
+  public void testWrite() {
+  }
+}
