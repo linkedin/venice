3373cd6e8768fbdc020b8c010fb1ca3637ad1215
Jialin Liu <jlliu@linkedin.com>
2023-03-09 17:09:58 -0800

    [server] Isolated ingestion server should resume LEADER state when restarting forked ingestion process upon crash (#270)
    When isolated ingestion process crash, we do have detection and restart mechanism but we do not keep leader status so when resuming ongoing TP ingestion, we will not resume the LEADER partition replica state correctly and LEADER partition could stuck forever.
    This PR fixes it by keeping track of the LEADER status and resend STANDBY->LEADER for those LEADER replicas during restart of the forked process. No matter it was in LEADER or in transition to LEADER, this message will let it go through the promotion so it could become LEADER again.

--- clients/da-vinci-client/src/test/java/com/linkedin/davinci/ingestion/main/MainIngestionMonitorServiceTest.java
+++ clients/da-vinci-client/src/test/java/com/linkedin/davinci/ingestion/main/MainIngestionMonitorServiceTest.java
@@ -1,11 +1,17 @@
 package com.linkedin.davinci.ingestion.main;
 
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import com.linkedin.venice.exceptions.VeniceException;
 import com.linkedin.venice.utils.concurrent.VeniceConcurrentHashMap;
+import java.util.Collections;
 import java.util.Map;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -21,7 +27,8 @@ public class MainIngestionMonitorServiceTest {
     mainTopicIngestionStatus.setPartitionIngestionStatusToIsolatedIngestion(2);
     mainTopicIngestionStatus.setPartitionIngestionStatusToIsolatedIngestion(3);
     topicIngestionStatusMap.put(topic, mainTopicIngestionStatus);
-
+    Map<String, Map<Integer, Boolean>> topicPartitionLeaderStatusMap = new VeniceConcurrentHashMap<>();
+    topicPartitionLeaderStatusMap.put(topic, Collections.singletonMap(3, Boolean.TRUE));
     MainIngestionRequestClient client = mock(MainIngestionRequestClient.class);
     when(client.startConsumption(topic, 0))
         .thenThrow(new VeniceException("Not expected to start remote consumption on local resource"));
@@ -29,12 +36,21 @@ public class MainIngestionMonitorServiceTest {
         .thenThrow(new VeniceException("Simulate zombie resource ingestion failure"));
     when(client.startConsumption(topic, 2)).thenReturn(Boolean.TRUE);
     when(client.startConsumption(topic, 3)).thenReturn(Boolean.TRUE);
+    when(client.promoteToLeader(topic, 3)).thenReturn(Boolean.TRUE);
 
     MainIngestionMonitorService monitorService = mock(MainIngestionMonitorService.class);
     when(monitorService.getTopicIngestionStatusMap()).thenReturn(topicIngestionStatusMap);
+    when(monitorService.getTopicPartitionLeaderStatusMap()).thenReturn(topicPartitionLeaderStatusMap);
+
+    ReentrantReadWriteLock readWriteLock = new ReentrantReadWriteLock();
     when(monitorService.createClient()).thenReturn(client);
     when(monitorService.resumeOngoingIngestionTasks()).thenCallRealMethod();
+    when(monitorService.isTopicPartitionInLeaderState(anyString(), anyInt())).thenCallRealMethod();
+    when(monitorService.getForkProcessActionLock()).thenReturn(readWriteLock);
+    Assert.assertTrue(monitorService.isTopicPartitionInLeaderState(topic, 3));
     Assert.assertEquals(monitorService.resumeOngoingIngestionTasks(), 2);
+    verify(client, times(0)).promoteToLeader(topic, 2);
+    verify(client, times(1)).promoteToLeader(topic, 3);
 
   }
 }
