953da3ed5102c1325a06a4b51b297ab77cbb82ae
Xun Yin <xyin@linkedin.com>
2023-03-14 21:09:31 -0700

    [da-vinci][fast-client][test] DaVinci and fast client compatibility fix (#253)
    The main issue is that when using DaVinci based store repository there is a cyclic dependency
    between DaVinciBackend and the DaVinci baased store repository. This can cause double initialization
    of the singleton DaVinciBackend which results in multiple attempts of RocksDB open operation.
    
    The fix is to introduce a new thin-client based metadata and deprecate the DaVinci based metadata. FC
    must use the thin-client based metadata if it's used with DVC in the same instance/application.
    
    These are meant to be short term fixes. The long term solution is to remove the DVC dependency from
    fast-client and all these problems will go away.

--- internal/venice-test-common/src/integrationtest/java/com/linkedin/venice/fastclient/meta/VeniceClientBasedMetadataIntegrationTest.java
+++ internal/venice-test-common/src/integrationtest/java/com/linkedin/venice/fastclient/meta/VeniceClientBasedMetadataIntegrationTest.java
@@ -1,27 +1,18 @@
 package com.linkedin.venice.fastclient.meta;
 
-import static com.linkedin.venice.ConfigKeys.CLIENT_SYSTEM_STORE_REPOSITORY_REFRESH_INTERVAL_SECONDS;
-import static com.linkedin.venice.ConfigKeys.CLIENT_USE_DA_VINCI_BASED_SYSTEM_STORE_REPOSITORY;
-import static com.linkedin.venice.ConfigKeys.CLIENT_USE_SYSTEM_STORE_REPOSITORY;
-import static com.linkedin.venice.ConfigKeys.DATA_BASE_PATH;
-import static com.linkedin.venice.ConfigKeys.PERSISTENCE_TYPE;
-import static com.linkedin.venice.meta.PersistenceType.ROCKS_DB;
+import static com.linkedin.venice.system.store.MetaStoreWriter.KEY_STRING_STORE_NAME;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 
-import com.linkedin.d2.balancer.D2Client;
-import com.linkedin.davinci.client.DaVinciClient;
-import com.linkedin.davinci.client.DaVinciConfig;
-import com.linkedin.davinci.client.factory.CachingDaVinciClientFactory;
 import com.linkedin.r2.transport.common.Client;
-import com.linkedin.venice.D2.D2ClientUtils;
+import com.linkedin.venice.client.store.AvroSpecificStoreClient;
 import com.linkedin.venice.common.VeniceSystemStoreType;
 import com.linkedin.venice.controllerapi.VersionCreationResponse;
 import com.linkedin.venice.fastclient.ClientConfig;
 import com.linkedin.venice.fastclient.utils.ClientTestUtils;
-import com.linkedin.venice.integration.utils.D2TestUtils;
 import com.linkedin.venice.integration.utils.ServiceFactory;
+import com.linkedin.venice.integration.utils.VeniceClusterCreateOptions;
 import com.linkedin.venice.integration.utils.VeniceClusterWrapper;
 import com.linkedin.venice.integration.utils.VeniceRouterWrapper;
 import com.linkedin.venice.meta.OnlineInstanceFinder;
@@ -33,14 +24,16 @@ import com.linkedin.venice.partitioner.VenicePartitioner;
 import com.linkedin.venice.schema.SchemaEntry;
 import com.linkedin.venice.serializer.RecordSerializer;
 import com.linkedin.venice.serializer.SerializerDeserializerFactory;
+import com.linkedin.venice.system.store.MetaStoreDataType;
 import com.linkedin.venice.systemstore.schemas.StoreMetaKey;
 import com.linkedin.venice.systemstore.schemas.StoreMetaValue;
-import com.linkedin.venice.utils.PropertyBuilder;
+import com.linkedin.venice.utils.DataProviderUtils;
+import com.linkedin.venice.utils.SslUtils;
 import com.linkedin.venice.utils.TestUtils;
 import com.linkedin.venice.utils.Time;
 import com.linkedin.venice.utils.Utils;
-import com.linkedin.venice.utils.VeniceProperties;
 import io.tehuti.metrics.MetricsRepository;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -48,33 +41,41 @@ import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import org.apache.avro.Schema;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 
-public class DaVinciClientBasedMetadataTest {
+public class VeniceClientBasedMetadataIntegrationTest {
   protected static final int KEY_COUNT = 100;
   protected static final long TIME_OUT = 60 * Time.MS_PER_SECOND;
 
   private final VenicePartitioner defaultPartitioner = new DefaultVenicePartitioner();
 
-  protected VeniceClusterWrapper veniceCluster;
+  protected ClientConfig clientConfig;
   protected String storeName;
-  private VeniceProperties daVinciBackendConfig;
-  protected DaVinciClientBasedMetadata daVinciClientBasedMetadata;
+  protected VeniceClusterWrapper veniceCluster;
+  protected VeniceClientBasedMetadata veniceClientBasedMetadata;
+
   private RecordSerializer<Object> keySerializer;
   private Client r2Client;
-  private D2Client d2Client;
-  private CachingDaVinciClientFactory daVinciClientFactory;
-  private DaVinciClient<StoreMetaKey, StoreMetaValue> daVinciClientForMetaStore;
-  protected ClientConfig clientConfig;
+  private AvroSpecificStoreClient<StoreMetaKey, StoreMetaValue> thinClientForMetaStore = null;
 
   @BeforeClass
   public void setUp() throws Exception {
-    veniceCluster = ServiceFactory.getVeniceCluster(1, 2, 1, 2, 100, true, false);
+    Utils.thisIsLocalhost();
+    VeniceClusterCreateOptions options = new VeniceClusterCreateOptions.Builder().numberOfControllers(1)
+        .numberOfServers(2)
+        .numberOfRouters(1)
+        .replicationFactor(2)
+        .partitionSize(100)
+        .minActiveReplica(1)
+        .sslToStorageNodes(true)
+        .sslToKafka(false)
+        .build();
+    veniceCluster = ServiceFactory.getVeniceCluster(options);
     r2Client = ClientTestUtils.getR2Client();
-    d2Client = D2TestUtils.getAndStartD2Client(veniceCluster.getZk().getAddress());
     createStore();
     String metaSystemStoreName = VeniceSystemStoreType.META_STORE.getSystemStoreName(storeName);
     veniceCluster.useControllerClient(controllerClient -> {
@@ -89,23 +90,20 @@ public class DaVinciClientBasedMetadataTest {
           controllerClient,
           30,
           TimeUnit.SECONDS);
-      daVinciBackendConfig = new PropertyBuilder().put(DATA_BASE_PATH, Utils.getTempDataDirectory().getAbsolutePath())
-          .put(PERSISTENCE_TYPE, ROCKS_DB)
-          .put(CLIENT_USE_SYSTEM_STORE_REPOSITORY, true)
-          .put(CLIENT_USE_DA_VINCI_BASED_SYSTEM_STORE_REPOSITORY, true)
-          .put(CLIENT_SYSTEM_STORE_REPOSITORY_REFRESH_INTERVAL_SECONDS, 1)
-          .build();
     });
-
-    daVinciClientFactory = new CachingDaVinciClientFactory(
-        d2Client,
-        VeniceRouterWrapper.CLUSTER_DISCOVERY_D2_SERVICE_NAME,
-        new MetricsRepository(),
-        daVinciBackendConfig);
-    daVinciClientForMetaStore = daVinciClientFactory.getAndStartSpecificAvroClient(
-        VeniceSystemStoreType.META_STORE.getSystemStoreName(storeName),
-        new DaVinciConfig(),
-        StoreMetaValue.class);
+    thinClientForMetaStore = com.linkedin.venice.client.store.ClientFactory.getAndStartSpecificAvroClient(
+        com.linkedin.venice.client.store.ClientConfig
+            .defaultSpecificClientConfig(metaSystemStoreName, StoreMetaValue.class)
+            .setVeniceURL(veniceCluster.getRandomRouterURL())
+            .setSslFactory(SslUtils.getVeniceLocalSslFactory()));
+    TestUtils.waitForNonDeterministicAssertion(30, TimeUnit.SECONDS, () -> {
+      Assert.assertNotNull(
+          thinClientForMetaStore
+              .get(
+                  MetaStoreDataType.STORE_CLUSTER_CONFIG
+                      .getStoreMetaKey(Collections.singletonMap(KEY_STRING_STORE_NAME, storeName)))
+              .get());
+    });
     keySerializer =
         SerializerDeserializerFactory.getAvroGenericSerializer(Schema.parse(VeniceClusterWrapper.DEFAULT_KEY_SCHEMA));
 
@@ -115,11 +113,10 @@ public class DaVinciClientBasedMetadataTest {
     clientConfigBuilder.setR2Client(r2Client);
     clientConfigBuilder.setMetricsRepository(new MetricsRepository());
     clientConfigBuilder.setSpeculativeQueryEnabled(true);
-    clientConfigBuilder.setDaVinciClientForMetaStore(daVinciClientForMetaStore);
     clientConfigBuilder.setMetadataRefreshIntervalInSeconds(1); // Faster refreshes for faster tests
     clientConfig = clientConfigBuilder.build();
-    daVinciClientBasedMetadata = new DaVinciClientBasedMetadata(clientConfig);
-    daVinciClientBasedMetadata.start();
+    veniceClientBasedMetadata = new ThinClientBasedMetadata(clientConfig, thinClientForMetaStore);
+    veniceClientBasedMetadata.start();
   }
 
   protected void createStore() throws Exception {
@@ -132,13 +129,18 @@ public class DaVinciClientBasedMetadataTest {
     ReadOnlyStoreRepository storeRepository = routerWrapper.getMetaDataRepository();
     OnlineInstanceFinder onlineInstanceFinder = routerWrapper.getRoutingDataRepository();
     assertEquals(
-        daVinciClientBasedMetadata.getCurrentStoreVersion(),
+        veniceClientBasedMetadata.getCurrentStoreVersion(),
         storeRepository.getStore(storeName).getCurrentVersion());
     List<Version> versions = storeRepository.getStore(storeName).getVersions();
     assertFalse(versions.isEmpty(), "Version list cannot be empty.");
     byte[] keyBytes = keySerializer.serialize(1);
     for (Version version: versions) {
-      verifyMetadata(onlineInstanceFinder, version.getNumber(), version.getPartitionCount(), keyBytes);
+      verifyMetadata(
+          veniceClientBasedMetadata,
+          onlineInstanceFinder,
+          version.getNumber(),
+          version.getPartitionCount(),
+          keyBytes);
     }
     // Make two new versions before checking the metadata again
     veniceCluster.createVersion(storeName, KEY_COUNT);
@@ -148,28 +150,33 @@ public class DaVinciClientBasedMetadataTest {
         30,
         TimeUnit.SECONDS,
         () -> assertEquals(
-            daVinciClientBasedMetadata.getCurrentStoreVersion(),
+            veniceClientBasedMetadata.getCurrentStoreVersion(),
             storeRepository.getStore(storeName).getCurrentVersion()));
     versions = storeRepository.getStore(storeName).getVersions();
     assertFalse(versions.isEmpty(), "Version list cannot be empty.");
     for (Version version: versions) {
-      verifyMetadata(onlineInstanceFinder, version.getNumber(), version.getPartitionCount(), keyBytes);
+      verifyMetadata(
+          veniceClientBasedMetadata,
+          onlineInstanceFinder,
+          version.getNumber(),
+          version.getPartitionCount(),
+          keyBytes);
     }
   }
 
   @Test(timeOut = TIME_OUT)
   public void testMetadataSchemaRetriever() {
     ReadOnlySchemaRepository schemaRepository = veniceCluster.getRandomVeniceRouter().getSchemaRepository();
-    assertEquals(daVinciClientBasedMetadata.getKeySchema(), schemaRepository.getKeySchema(storeName).getSchema());
+    assertEquals(veniceClientBasedMetadata.getKeySchema(), schemaRepository.getKeySchema(storeName).getSchema());
     SchemaEntry latestValueSchema = schemaRepository.getSupersetOrLatestValueSchema(storeName);
-    assertEquals(daVinciClientBasedMetadata.getLatestValueSchemaId().intValue(), latestValueSchema.getId());
-    assertEquals(daVinciClientBasedMetadata.getLatestValueSchema(), latestValueSchema.getSchema());
-    assertEquals(daVinciClientBasedMetadata.getValueSchema(latestValueSchema.getId()), latestValueSchema.getSchema());
-    assertEquals(daVinciClientBasedMetadata.getValueSchemaId(latestValueSchema.getSchema()), latestValueSchema.getId());
+    assertEquals(veniceClientBasedMetadata.getLatestValueSchemaId().intValue(), latestValueSchema.getId());
+    assertEquals(veniceClientBasedMetadata.getLatestValueSchema(), latestValueSchema.getSchema());
+    assertEquals(veniceClientBasedMetadata.getValueSchema(latestValueSchema.getId()), latestValueSchema.getSchema());
+    assertEquals(veniceClientBasedMetadata.getValueSchemaId(latestValueSchema.getSchema()), latestValueSchema.getId());
   }
 
-  @Test(timeOut = TIME_OUT)
-  public void testMetaSystemStoreVersionBump() {
+  @Test(timeOut = TIME_OUT, dataProvider = "True-and-False", dataProviderClass = DataProviderUtils.class)
+  public void testMetaSystemStoreVersionBump(boolean useThinClientBasedMetadata) {
     // Bump the underlying system store version twice and make sure DaVinciClientBasedMetadata is still subscribed to
     // the correct meta system store version.
     String metaSystemStoreName = VeniceSystemStoreType.META_STORE.getSystemStoreName(storeName);
@@ -197,11 +204,12 @@ public class DaVinciClientBasedMetadataTest {
         30,
         TimeUnit.SECONDS,
         () -> assertEquals(
-            daVinciClientBasedMetadata.getCurrentStoreVersion(),
+            veniceClientBasedMetadata.getCurrentStoreVersion(),
             storeRepository.getStore(storeName).getCurrentVersion()));
   }
 
   private void verifyMetadata(
+      VeniceClientBasedMetadata veniceClientBasedMetadata,
       OnlineInstanceFinder onlineInstanceFinder,
       int versionNumber,
       int partitionCount,
@@ -211,12 +219,12 @@ public class DaVinciClientBasedMetadataTest {
     TestUtils.waitForNonDeterministicAssertion(30, TimeUnit.SECONDS, () -> {
       assertEquals(
           defaultPartitioner.getPartitionId(key, partitionCount),
-          daVinciClientBasedMetadata.getPartitionId(versionNumber, key));
+          veniceClientBasedMetadata.getPartitionId(versionNumber, key));
       Set<String> routerReadyToServeView = onlineInstanceFinder.getReadyToServeInstances(resourceName, partitionId)
           .stream()
           .map(instance -> instance.getUrl(true))
           .collect(Collectors.toSet());
-      Set<String> metadataView = new HashSet<>(daVinciClientBasedMetadata.getReplicas(versionNumber, partitionId));
+      Set<String> metadataView = new HashSet<>(veniceClientBasedMetadata.getReplicas(versionNumber, partitionId));
       assertEquals(
           metadataView.size(),
           routerReadyToServeView.size(),
@@ -229,16 +237,10 @@ public class DaVinciClientBasedMetadataTest {
 
   @AfterClass
   public void cleanUp() {
-    Utils.closeQuietlyWithErrorLogged(daVinciClientBasedMetadata);
-    Utils.closeQuietlyWithErrorLogged(daVinciClientForMetaStore);
-    Utils.closeQuietlyWithErrorLogged(daVinciClientFactory);
-    if (d2Client != null) {
-      D2ClientUtils.shutdownClient(d2Client);
-    }
+    Utils.closeQuietlyWithErrorLogged(veniceClientBasedMetadata);
     if (r2Client != null) {
       r2Client.shutdown(null);
     }
     Utils.closeQuietlyWithErrorLogged(veniceCluster);
   }
-
 }
