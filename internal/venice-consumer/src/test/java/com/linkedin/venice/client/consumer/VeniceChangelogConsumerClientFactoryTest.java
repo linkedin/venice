78673a64732f5bc864ba3b2f874c50d37aa16943
Zac Policzer <zpoliczer@linkedin.com>
2023-04-12 15:14:27 -0700

    [changelog] Add d2Client as a passable parameter to change capture client config (#344)
    * Add d2Client as a passable parameter to change capture client config
    
    This will enable us to leverage offspring's lifecycle aware to manage the d2clients
    
    * address comments
    
    * more comments tweaks
    
    ---------
    
    Co-authored-by: Zac Policzer <zpolicze@zpolicze-mn1.linkedin.biz>

--- internal/venice-consumer/src/test/java/com/linkedin/venice/client/consumer/VeniceChangelogConsumerClientFactoryTest.java
+++ internal/venice-consumer/src/test/java/com/linkedin/venice/client/consumer/VeniceChangelogConsumerClientFactoryTest.java
@@ -1,10 +1,14 @@
 package com.linkedin.venice.client.consumer;
 
-import static com.linkedin.venice.integration.utils.VeniceControllerWrapper.*;
-import static org.testng.Assert.*;
+import static org.mockito.Mockito.*;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.linkedin.d2.balancer.D2Client;
+import com.linkedin.data.ByteString;
+import com.linkedin.r2.message.rest.RestResponse;
 import com.linkedin.venice.client.store.schemas.TestKeyRecord;
 import com.linkedin.venice.controllerapi.D2ControllerClient;
+import com.linkedin.venice.controllerapi.D2ServiceDiscoveryResponse;
 import com.linkedin.venice.controllerapi.StoreResponse;
 import com.linkedin.venice.meta.StoreInfo;
 import com.linkedin.venice.meta.ViewConfig;
@@ -12,10 +16,15 @@ import com.linkedin.venice.meta.ViewConfigImpl;
 import com.linkedin.venice.schema.SchemaReader;
 import com.linkedin.venice.serialization.KafkaKeySerializer;
 import com.linkedin.venice.serialization.avro.KafkaValueSerializer;
+import com.linkedin.venice.utils.ObjectMapperFactory;
 import com.linkedin.venice.views.ChangeCaptureView;
+import io.tehuti.metrics.MetricsRepository;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
@@ -27,9 +36,10 @@ import org.testng.annotations.Test;
 public class VeniceChangelogConsumerClientFactoryTest {
   private static final String STORE_NAME = "dybbuk_store";
   private static final String VIEW_NAME = "mazzikim_view";
+  private static final String TEST_CLUSTER = "golem_cluster";
 
   @Test
-  public void testGetChangelogConsumer() {
+  public void testGetChangelogConsumer() throws ExecutionException, InterruptedException, JsonProcessingException {
     Properties consumerProperties = new Properties();
     String localKafkaUrl = "http://www.fooAddress.linkedin.com:16337";
     consumerProperties.put(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, localKafkaUrl);
@@ -44,7 +54,7 @@ public class VeniceChangelogConsumerClientFactoryTest {
     ChangelogClientConfig globalChangelogClientConfig =
         new ChangelogClientConfig().setConsumerProperties(consumerProperties).setSchemaReader(mockSchemaReader);
     VeniceChangelogConsumerClientFactory veniceChangelogConsumerClientFactory =
-        new VeniceChangelogConsumerClientFactory(globalChangelogClientConfig);
+        new VeniceChangelogConsumerClientFactory(globalChangelogClientConfig, new MetricsRepository());
     D2ControllerClient mockControllerClient = Mockito.mock(D2ControllerClient.class);
 
     veniceChangelogConsumerClientFactory.setD2ControllerClient(mockControllerClient);
@@ -68,6 +78,42 @@ public class VeniceChangelogConsumerClientFactoryTest {
     globalChangelogClientConfig.setViewName(VIEW_NAME);
     consumer = veniceChangelogConsumerClientFactory.getChangelogConsumer(STORE_NAME);
     Assert.assertTrue(consumer instanceof VeniceChangelogConsumerImpl);
+
+    D2ServiceDiscoveryResponse serviceDiscoveryResponse = new D2ServiceDiscoveryResponse();
+    serviceDiscoveryResponse.setCluster(TEST_CLUSTER);
+    serviceDiscoveryResponse.setD2Service("TEST_ROUTER_D2_SERVICE");
+    serviceDiscoveryResponse.setServerD2Service("TEST_SERVER_D2_SERVICE");
+    serviceDiscoveryResponse.setName(STORE_NAME);
+    String discoverClusterResponse = ObjectMapperFactory.getInstance().writeValueAsString(serviceDiscoveryResponse);
+
+    RestResponse discoverClusterRestResponse = mock(RestResponse.class);
+    doReturn(ByteString.unsafeWrap(discoverClusterResponse.getBytes(StandardCharsets.UTF_8)))
+        .when(discoverClusterRestResponse)
+        .getEntity();
+
+    // Verify branches which have a managed D2Client passed in for the controller client
+    D2Client mockD2Client = Mockito.mock(D2Client.class);
+    Future mockClusterDiscoveryFuture = Mockito.mock(Future.class);
+    Mockito.when(mockClusterDiscoveryFuture.get()).thenReturn(discoverClusterRestResponse);
+    Mockito.when(mockD2Client.restRequest(Mockito.any())).thenReturn(mockClusterDiscoveryFuture);
+
+    globalChangelogClientConfig.setD2Client(mockD2Client).setD2ControllerClient(null).setControllerRequestRetryCount(1);
+
+    VeniceChangelogConsumerClientFactory.ViewClassGetter mockViewGetter = (
+        String storeName,
+        String viewName,
+        D2ControllerClient d2ControllerClient,
+        int retries) -> ChangeCaptureView.class.getCanonicalName();
+
+    veniceChangelogConsumerClientFactory =
+        new VeniceChangelogConsumerClientFactory(globalChangelogClientConfig, new MetricsRepository());
+    veniceChangelogConsumerClientFactory.setViewClassGetter(mockViewGetter);
+    veniceChangelogConsumerClientFactory.setD2ControllerClient(mockControllerClient);
+    veniceChangelogConsumerClientFactory.setConsumer(mockKafkaConsumer);
+
+    consumer = veniceChangelogConsumerClientFactory.getChangelogConsumer(STORE_NAME);
+    Assert.assertTrue(consumer instanceof VeniceChangelogConsumerImpl);
+
   }
 
   @Test
@@ -86,10 +132,9 @@ public class VeniceChangelogConsumerClientFactoryTest {
     ChangelogClientConfig globalChangelogClientConfig =
         new ChangelogClientConfig().setConsumerProperties(consumerProperties).setSchemaReader(mockSchemaReader);
     VeniceChangelogConsumerClientFactory veniceChangelogConsumerClientFactory =
-        new VeniceChangelogConsumerClientFactory(globalChangelogClientConfig);
+        new VeniceChangelogConsumerClientFactory(globalChangelogClientConfig, new MetricsRepository());
     D2ControllerClient mockControllerClient = Mockito.mock(D2ControllerClient.class);
 
-    veniceChangelogConsumerClientFactory.setD2ControllerClient(mockControllerClient);
     veniceChangelogConsumerClientFactory.setConsumer(mockKafkaConsumer);
 
     StoreResponse mockStoreResponse = Mockito.mock(StoreResponse.class);
