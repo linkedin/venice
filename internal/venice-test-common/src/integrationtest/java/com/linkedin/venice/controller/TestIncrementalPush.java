ae5c1263892ba7b15f5bdfba5044f0b81513c1df
Sushant Mane <sumane@linkedin.com>
2023-04-14 18:49:38 -0700

    [test] Fix flaky TestIncrementalPush::testGetOfflineStatusIncrementalPush

--- internal/venice-test-common/src/integrationtest/java/com/linkedin/venice/controller/TestIncrementalPush.java
+++ internal/venice-test-common/src/integrationtest/java/com/linkedin/venice/controller/TestIncrementalPush.java
@@ -3,7 +3,9 @@ package com.linkedin.venice.controller;
 import com.linkedin.venice.controllerapi.UpdateStoreQueryParams;
 import com.linkedin.venice.controllerapi.VersionCreationResponse;
 import com.linkedin.venice.integration.utils.ServiceFactory;
+import com.linkedin.venice.integration.utils.VeniceClusterCreateOptions;
 import com.linkedin.venice.integration.utils.VeniceClusterWrapper;
+import com.linkedin.venice.meta.BackupStrategy;
 import com.linkedin.venice.pushmonitor.ExecutionStatus;
 import com.linkedin.venice.utils.TestUtils;
 import com.linkedin.venice.utils.Time;
@@ -18,6 +20,9 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 
+/**
+ * TestIncrementalPush tests the incremental push feature
+ */
 public class TestIncrementalPush {
   private VeniceClusterWrapper cluster;
   int partitionSize = 1000;
@@ -26,17 +31,12 @@ public class TestIncrementalPush {
 
   @BeforeMethod
   public void setUp() {
-    int numberOfController = 1;
-    int numberOfRouter = 1;
-
     cluster = ServiceFactory.getVeniceCluster(
-        numberOfController,
-        numberOfServer,
-        numberOfRouter,
-        replicaFactor,
-        partitionSize,
-        false,
-        false);
+        new VeniceClusterCreateOptions.Builder().numberOfServers(numberOfServer)
+            .replicationFactor(replicaFactor)
+            .partitionSize(partitionSize)
+            .minActiveReplica(replicaFactor - 1)
+            .build());
   }
 
   @AfterMethod
@@ -44,67 +44,70 @@ public class TestIncrementalPush {
     cluster.close();
   }
 
-  @Test(timeOut = 120 * Time.MS_PER_SECOND)
+  @Test(timeOut = 2 * Time.MS_PER_MINUTE, invocationCount = 20)
   public void testGetOfflineStatusIncrementalPush() {
     String storeName = Utils.getUniqueString("testIncPushStore");
     int partitionCount = 2;
     long storageQuota = (long) partitionCount * partitionSize;
     cluster.getNewStore(storeName);
+
     UpdateStoreQueryParams params = new UpdateStoreQueryParams();
     params.setIncrementalPushEnabled(true)
         .setHybridRewindSeconds(1)
         .setHybridOffsetLagThreshold(1)
-        .setStorageQuotaInByte(storageQuota);
+        .setStorageQuotaInByte(storageQuota)
+        .setBackupStrategy(BackupStrategy.KEEP_MIN_VERSIONS)
+        .setNumVersionsToPreserve(3);
     cluster.updateStore(storeName, params);
 
-    VersionCreationResponse response = cluster.getNewVersion(storeName);
-    Assert.assertFalse(response.isError());
-    String topicName = response.getKafkaTopic();
+    // store version 1
+    VersionCreationResponse v1Response = cluster.getNewVersion(storeName);
+    Assert.assertFalse(v1Response.isError());
+    String versionTopic1 = v1Response.getKafkaTopic();
     // push version 1 to completion
-    VeniceWriter<String, String, byte[]> veniceWriter = cluster.getVeniceWriter(topicName);
-    veniceWriter.broadcastStartOfPush(new HashMap<>());
-    veniceWriter.broadcastEndOfPush(new HashMap<>());
+    VeniceWriter<String, String, byte[]> veniceWriterVt1 = cluster.getVeniceWriter(versionTopic1);
+    veniceWriterVt1.broadcastStartOfPush(new HashMap<>());
+    veniceWriterVt1.broadcastEndOfPush(new HashMap<>());
+    // write incremental push to store version 1
+    String incPushV1 = "incPush1";
+    veniceWriterVt1.broadcastStartOfIncrementalPush(incPushV1, new HashMap<>());
+    veniceWriterVt1.broadcastEndOfIncrementalPush(incPushV1, new HashMap<>());
 
-    // start inc push
-    String incPushVerison = "incPush1";
-    veniceWriter.broadcastStartOfIncrementalPush(incPushVerison, new HashMap<>());
-    veniceWriter.broadcastEndOfIncrementalPush(incPushVerison, new HashMap<>());
-
-    params = new UpdateStoreQueryParams();
-    // set up store for inc push to rt
-
-    cluster.updateStore(storeName, params);
-    response = cluster.getNewVersion(storeName);
-    topicName = response.getKafkaTopic();
-
-    // broadcast to VT
-    veniceWriter = cluster.getVeniceWriter(topicName);
-    veniceWriter.broadcastStartOfPush(new HashMap<>());
-    veniceWriter.broadcastEndOfPush(new HashMap<>());
-
-    String finalTopicName1 = topicName;
-    // validate it still returns valid status from backup version
-    String finalIncPushVerison1 = incPushVerison;
     TestUtils.waitForNonDeterministicCompletion(
-        3,
-        TimeUnit.SECONDS,
+        1,
+        TimeUnit.MINUTES,
         () -> cluster.getLeaderVeniceController()
             .getVeniceAdmin()
-            .getOffLinePushStatus(cluster.getClusterName(), finalTopicName1, Optional.of(finalIncPushVerison1), null)
+            .getOffLinePushStatus(cluster.getClusterName(), versionTopic1, Optional.of(incPushV1), null)
             .getExecutionStatus()
             .equals(ExecutionStatus.END_OF_INCREMENTAL_PUSH_RECEIVED));
 
-    incPushVerison = "incPush2";
-    veniceWriter.broadcastStartOfIncrementalPush(incPushVerison, new HashMap<>());
-    String finalTopicName = topicName;
-    String finalIncPushVerison = incPushVerison;
-    // validate current version query works
+    // store version 2
+    VersionCreationResponse v2Response = cluster.getNewVersion(storeName);
+    String versionTopic2 = v2Response.getKafkaTopic();
+    // push version 2 to completion
+    VeniceWriter<String, String, byte[]> veniceWriterVt2 = cluster.getVeniceWriter(versionTopic2);
+    veniceWriterVt2.broadcastStartOfPush(new HashMap<>());
+    veniceWriterVt2.broadcastEndOfPush(new HashMap<>());
+
     TestUtils.waitForNonDeterministicCompletion(
-        3,
-        TimeUnit.SECONDS,
+        1,
+        TimeUnit.MINUTES,
         () -> cluster.getLeaderVeniceController()
             .getVeniceAdmin()
-            .getOffLinePushStatus(cluster.getClusterName(), finalTopicName, Optional.of(finalIncPushVerison), null)
+            .getOffLinePushStatus(cluster.getClusterName(), versionTopic2, Optional.of(incPushV1), null)
+            .getExecutionStatus()
+            .equals(ExecutionStatus.END_OF_INCREMENTAL_PUSH_RECEIVED));
+
+    String incPush2 = "incPush2";
+    veniceWriterVt2.broadcastStartOfIncrementalPush(incPush2, new HashMap<>());
+    // validate current version query works
+    TestUtils.waitForNonDeterministicCompletion(
+        1,
+        TimeUnit.MINUTES,
+        () -> cluster.getLeaderVeniceController()
+            .getVeniceAdmin()
+            .getOffLinePushStatus(cluster.getClusterName(), versionTopic2, Optional.of(incPush2), null)
             .getExecutionStatus()
             .equals(ExecutionStatus.START_OF_INCREMENTAL_PUSH_RECEIVED));
   }
