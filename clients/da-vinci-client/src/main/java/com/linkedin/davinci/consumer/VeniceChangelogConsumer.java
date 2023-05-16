8b34beb0dc5a3d25740e2d0ec42e26eea337da04
Zac Policzer <zpoliczer@linkedin.com>
2023-05-15 17:14:28 -0700

    [changelog][client] Add seek and checkpointing API's to ChangeCapture client (#384)
    * [changelog-client] Add chunking and compression support to change capture client
    
    We add to the change capture client compression and chunking by utilizing a local
    storage engine thats held in memory.  A client will only consume events in serial per
    partition, and since we only have one producer at a time, we'll only buffer one message
    at a time.  Zombie writes may cause the client to leak buffer space temporarily, so
    we make a point to continously drop and clear out the storage engines as we receive
    new events to keep memory footprint low.
    
    Also added some config validation to creating views. Chunking is now a mandatory
    config for the change capture view (which shouldn't be too arduous since we already
    require it for enabling write compute which is the most likely case for change capture
    to be enabled).
    
    * tweaks to unit tests and deleted unused code
    
    * Improve test coverage and address comments
    
    * improve code coverage and fix spot bugs check
    
    * Addressed review comments and test failures
    
    * Address PR comments
    
    * tweak test to make sure it doesn't leak kafkabroker and it's properly closed at end of test
    
    * Remove unused var
    
    * Remove * import in test class
    
    * remove test view from integration test
    
    * Small test to see if the integration test will stop hanging after removing ZSTD compression test case
    
    * experimentally remove controller config validation
    
    Just to see what this does to the test
    
    * Add back test cases since the sub test now passes
    
    * Remove zstd test case
    
    * Remove test case permutations
    
    EOM
    
    * Remove chunk max size
    
    * revert test class changes
    
    * Tweak test further to pass this time
    
    * Address comments
    
    * Subsume venice-consumer into davinci module
    
    * stash testing changes
    
    * Respond to comments. We also standardize the behavior of the chnage capture clients to start reading from the beginning of the version topic.
    
    This is NOT the desirable behavior for some clients, but it makes the behavior make sense with checkpointing (where bootstrap is default, and if you don't
    want to bootstrap, you should seek before polling). This means that we need to be able to select the right compression and chunking adapters when processing each message.
    This is why we've included this in this change.
    
    * Split out view tests to see if that improves flakiness
    
    * touch up to fix static analysis
    
    * Add new test case to seperate out some of the flakiness from the ingestion test suite
    
    * tweak to controller client management to see if that fixes test flakiness
    
    * change test ordering
    
    * set priority to enforce test run ordering
    
    * Add seek api's
    
    * more tweaks
    
    * Add support for seek as well as per partition compression. Some code simplification.
    
    * Tweaks to pass coverage and static analysis
    
    * Integrate with new offset interface
    
    * Tweak pubsub serialization with test
    
    * Tweak method name from offset to position
    
    * Address comments and tweak test to make it a bit more clear.  Also fix some latent bugs found when increasing partitino count in test cases
    
    * Fix static-analysis
    
    * Add unit test
    
    * fixes to unit test
    
    * add more javadoc based on PR comments
    
    * address PR comments
    
    * Add synchro's
    
    * remove unnecessary sync
    
    * Shrink down synchronized scope to avoid deadlock
    
    * Remove null check we don't need
    
    ---------
    
    Co-authored-by: Zac Policzer <zpolicze@zpolicze-mn1.linkedin.biz>

--- clients/da-vinci-client/src/main/java/com/linkedin/davinci/consumer/VeniceChangelogConsumer.java
+++ clients/da-vinci-client/src/main/java/com/linkedin/davinci/consumer/VeniceChangelogConsumer.java
@@ -26,6 +26,80 @@ public interface VeniceChangelogConsumer<K, V> {
   CompletableFuture<Void> subscribe(Set<Integer> partitions);
 
   /**
+   * Seek to the beginning of the push for a set of partitions.  This is analogous to doing a bootstrap of data for the consumer.
+   * This seek will ONLY seek to the beginning of the version which is currently serving data, and the consumer will switch
+   * to reading data from a new version (should one get created) once it has read up to the point in the change capture stream
+   * that indicates the version swap (which can only occur after consuming all the data in the last push). This instructs
+   * the consumer to consume data from the batch push.
+   *
+   * @param partitions the set of partitions to seek with
+   * @return a future which completes when the operation has succeeded for all partitions.
+   * @throws VeniceException if seek operation failed for any of the partitions, or seeking was performed on unsubscribed partitions
+   */
+  CompletableFuture<Void> seekToBeginningOfPush(Set<Integer> partitions);
+
+  /**
+   * Seek to the beginning of the push for subscribed partitions. See {@link #seekToBeginningOfPush(Set)} for more information.
+   *
+   * @return a future which completes when the partitions are ready to be consumed data
+   * @throws VeniceException if seek operation failed for any of the partitions.
+   */
+  CompletableFuture<Void> seekToBeginningOfPush();
+
+  /**
+   * Seek to the end of the last push for a given set of partitions. This instructs the consumer to begin consuming events
+   * which are transmitted to Venice following the last batch push.
+   *
+   * @param partitions the set of partitions to seek with
+   * @return a future which completes when the operation has succeeded for all partitions.
+   * @throws VeniceException if seek operation failed for any of the partitions, or seeking was performed on unsubscribed partitions
+   */
+  CompletableFuture<Void> seekToEndOfPush(Set<Integer> partitions);
+
+  /**
+   * Seek to the end of the push for all subscribed partitions. See {@link #seekToEndOfPush(Set)} for more information.
+   *
+   * @return a future which completes when the operation has succeeded for all partitions.
+   * @throws VeniceException if seek operation failed for any of the partitions.
+   */
+  CompletableFuture<Void> seekToEndOfPush();
+
+  /**
+   * Seek to the end of events which have been transmitted to Venice and start consuming new events. This will ONLY
+   * consume events transmitted via nearline and incremental push. It will not read batch push data.
+   *
+   * @param partitions the set of partitions to seek with
+   * @return a future which completes when the operation has succeeded for all partitions.
+   * @throws VeniceException if seek operation failed for any of the partitions, or seeking was performed on unsubscribed partitions
+   */
+  CompletableFuture<Void> seekToTail(Set<Integer> partitions);
+
+  /**
+   * Seek to the end of events which have been transmitted to Venice for all subscribed partitions. See {@link #seekToTail(Set)} for more information.
+   *
+   * @return a future which completes when the operation has succeeded for all partitions.
+   * @throws VeniceException if seek operation failed for any of the partitions.
+   */
+  CompletableFuture<Void> seekToTail();
+
+  /**
+   * Seek the provided checkpoints for the specified partitions.
+   *
+   * Note about checkpoints:
+   *
+   * Checkpoints have the following properties and should be considered:
+   *    -Checkpoints are NOT comparable or valid across partitions.
+   *    -Checkpoints are NOT comparable or valid across colos
+   *    -Checkpoints are NOT comparable across store versions
+   *    -It is not possible to determine the number of events between two checkpoints
+   *    -It is possible that a checkpoint is no longer on retention. In such case, we will return an exception to the caller.
+   * @param checkpoints
+   * @return a future which completes when seek has completed for all partitions
+   * @throws VeniceException if seek operation failed for any of the partitions
+   */
+  CompletableFuture<Void> seekToCheckpoint(Set<VeniceChangeCoordinate> checkpoints);
+
+  /**
    * Subscribe all partitions belonging to a specific store.
    *
    * @return a future which completes when all partitions are ready to be consumed data
@@ -56,7 +130,7 @@ public interface VeniceChangelogConsumer<K, V> {
    * @return a collection of messages since the last fetch for the subscribed list of topic partitions
    * @throws a VeniceException if polling operation fails
    */
-  Collection<PubSubMessage<K, ChangeEvent<V>, Long>> poll(long timeoutInMs);
+  Collection<PubSubMessage<K, ChangeEvent<V>, VeniceChangeCoordinate>> poll(long timeoutInMs);
 
   /**
    * Release the internal resources.
