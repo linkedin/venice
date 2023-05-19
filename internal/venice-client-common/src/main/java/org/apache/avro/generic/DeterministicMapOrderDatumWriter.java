e27ea81335e1c85eabe2e8bca41d0a6ae93fdf20
Nisarg Thakkar <nithakkar@linkedin.com>
2023-05-19 08:20:28 -0700

    Fix Avro serialization errors when map contains keys of different subclasses

--- internal/venice-client-common/src/main/java/org/apache/avro/generic/DeterministicMapOrderDatumWriter.java
+++ internal/venice-client-common/src/main/java/org/apache/avro/generic/DeterministicMapOrderDatumWriter.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.ConcurrentModificationException;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 import org.apache.avro.Schema;
 import org.apache.avro.io.Encoder;
@@ -18,16 +19,36 @@ public interface DeterministicMapOrderDatumWriter {
 
   default void writeMapWithDeterministicOrder(Schema schema, Object datum, Encoder out) throws IOException {
     Schema valueSchemaType = schema.getValueType();
-    Map map = (Map) datum;
+    Map<? extends CharSequence, Object> map = (Map<? extends CharSequence, Object>) datum;
     final int expectedMapSize = map.size();
     int actualSize = 0;
     out.writeMapStart();
     out.setItemCount(expectedMapSize);
 
-    List<Map.Entry> sortedEntryList =
-        (List<Map.Entry>) map.entrySet().stream().sorted(Map.Entry.comparingByKey()).collect(Collectors.toList());
+    List<Map.Entry<? extends CharSequence, Object>> sortedEntryList = map.entrySet().stream().sorted((e1, e2) -> {
+      // TODO: Replace with CharSequence#compare once the code has completely migrated to JDK11+
+      CharSequence cs1 = e1.getKey();
+      CharSequence cs2 = e2.getKey();
+      if (Objects.requireNonNull(cs1) == Objects.requireNonNull(cs2)) {
+        return 0;
+      }
 
-    for (Map.Entry entry: sortedEntryList) {
+      if (cs1.getClass() == cs2.getClass() && cs1 instanceof Comparable) {
+        return ((Comparable<Object>) cs1).compareTo(cs2);
+      }
+
+      for (int i = 0, len = Math.min(cs1.length(), cs2.length()); i < len; i++) {
+        char a = cs1.charAt(i);
+        char b = cs2.charAt(i);
+        if (a != b) {
+          return a - b;
+        }
+      }
+
+      return cs1.length() - cs2.length();
+    }).collect(Collectors.toList());
+
+    for (Map.Entry<? extends CharSequence, Object> entry: sortedEntryList) {
       out.startItem();
       out.writeString((CharSequence) entry.getKey().toString());
       internalWrite(valueSchemaType, entry.getValue(), out);
