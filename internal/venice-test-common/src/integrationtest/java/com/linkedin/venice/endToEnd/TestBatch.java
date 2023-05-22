664ba727bbf5edabd49c22dfcfc61b01b9eedbbf
Sourav Maji <smaji@linkedin.com>
2023-05-22 15:34:15 -0700

    [test][server] Minor logging change (#432)
    * [test][server] Minor logging change
    ---------
    
    Co-authored-by: Sourav Maji <tester@linkedin.com>

--- internal/venice-test-common/src/integrationtest/java/com/linkedin/venice/endToEnd/TestBatch.java
+++ internal/venice-test-common/src/integrationtest/java/com/linkedin/venice/endToEnd/TestBatch.java
@@ -580,8 +580,7 @@ public abstract class TestBatch {
                 put(KEY_STRING_VERSION_NUMBER, Integer.toString(1));
               }
             });
-        Object value = avroClient.get(key).get();
-        System.out.println(value);
+        avroClient.get(key).get();
       } catch (Exception e) {
         Assert.fail("get request to fetch schema from meta store fails", e);
       }
