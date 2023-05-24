linkedin/venice

commit 2d2f00e50c360ed36d5df1fa04efc5f4dfef837c

[router] Various perf-related cleanups (#412)
Removed lots of object allocation and string parsing from the router's
hot path.

Venice changes:

- ResourceAssignment::getPartitionAssignment does 1 fewer map lookup.
  Called at least once per request.

- Partition now carries two extra EnumMap instances, which are faster
  than the previous String-keyed map and also pre-populates the results
  of ready instances and working instances. Previously, there were lots
  of hot path collection instantiation in these functions.

- PartitionAssignment now carries an extra array to do lookups faster
  than with the Integer-keyed map. It also now uses a new utility class 
  called ArrayCollection for cases that need to iterate on an array.

- VeniceChunkedResponse::write now does not synchronize every time.

- Adopted Fast-Avro everywhere in the router.

- Removed the partitionNames set from KeyPartitionSet, which was only
  used for error logging.

- VenicePartitionFinder improvements:
  - Added findPartitionNumber to get the partition for a key without
    string parsing.
  - Also made findPartitioner public so that it can be looked up once
    per multi-key request, rather than once per key.

- VeniceDelegateMode improvements:
  - getHelixGroupNum and getAssignedHelixGroupId don't use Optionals
    anymore.
  - ScatterGatherModeForMultiKeyRequest now reuses an ArrayList of
    ArrayLists when grouping keys by partitions, rather than a throwaway
    map of LinkedLists.

- VenicePath improvements:
  - Removed the keyIdxToRouterKey map from VenicePath, which is dead code
    since the router cache days. This map was getting populated with every
    key of each request, needlessly.
  - Removed string parsing of resource names from the VenicePath.
  - Removed Optional wrapping of the chunkedResponse, responseDecompressor
    and body.

Alpini changes:

- Removed Optionals from Headers. Also removed unused functions getAsDate,

services/venice-router/src/main/java/com/linkedin/venice/router/streaming/VeniceChunkedResponse.java
@@ -17,8 +17,8 @@ import com.linkedin.venice.router.api.path.VenicePath;
 import com.linkedin.venice.router.stats.AggRouterHttpRequestStats;
 import com.linkedin.venice.router.stats.RouterStats;
 import com.linkedin.venice.serializer.AvroSerializer;
+import com.linkedin.venice.serializer.FastSerializerDeserializerFactory;
 import com.linkedin.venice.serializer.RecordSerializer;
-import com.linkedin.venice.serializer.SerializerDeserializerFactory;
 import com.linkedin.venice.utils.RedundantExceptionFilter;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufAllocator;
@@ -49,7 +49,6 @@ import java.nio.ByteBuffer;
 import java.nio.channels.ClosedChannelException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Queue;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentLinkedQueue;
@@ -89,14 +88,15 @@ public class VeniceChunkedResponse {
    * to include meta data info, which are only available after processing the full request.
    */
   private static final RecordSerializer<StreamingFooterRecordV1> STREAMING_FOOTER_SERIALIZER =
-      SerializerDeserializerFactory.getAvroGenericSerializer(StreamingFooterRecordV1.getClassSchema());
+      FastSerializerDeserializerFactory.getAvroGenericSerializer(StreamingFooterRecordV1.getClassSchema());
   private static final Map<CharSequence, CharSequence> EMPTY_MAP = new HashMap<>();
   private static final RecordSerializer<MultiGetResponseRecordV1> MULTI_GET_RESPONSE_SERIALIZER =
-      SerializerDeserializerFactory.getAvroGenericSerializer(MultiGetResponseRecordV1.getClassSchema());
+      FastSerializerDeserializerFactory.getAvroGenericSerializer(MultiGetResponseRecordV1.getClassSchema());
   private static final RecordSerializer<ComputeResponseRecordV1> COMPUTE_RESPONSE_SERIALIZER =
-      SerializerDeserializerFactory.getAvroGenericSerializer(ComputeResponseRecordV1.getClassSchema());
+      FastSerializerDeserializerFactory.getAvroGenericSerializer(ComputeResponseRecordV1.getClassSchema());
 
-  private final VenicePath path;
+  private final String storeName;
+  private final RequestType requestType;
   private final RouterStats<AggRouterHttpRequestStats> routerStats;
   private final ChannelHandlerContext ctx;
   private final VeniceChunkedWriteHandler chunkedWriteHandler;
@@ -113,7 +113,7 @@ public class VeniceChunkedResponse {
 
   // Response metadata should be sent when everything is good.
   private HttpResponse responseMetadata;
-  private Optional<CompressionStrategy> responseCompression = Optional.empty();
+  private volatile CompressionStrategy responseCompression = null;
 
   protected static final RedundantExceptionFilter REDUNDANT_LOGGING_FILTER =
       RedundantExceptionFilter.getRedundantExceptionFilter();
@@ -152,20 +152,19 @@ public class VeniceChunkedResponse {
   }
 
   public VeniceChunkedResponse(
-      VenicePath path,
+      String storeName,
+      RequestType requestType,
       ChannelHandlerContext ctx,
       VeniceChunkedWriteHandler handler,
       RouterStats<AggRouterHttpRequestStats> routerStats) {
-    this.path = path;
+    this.storeName = storeName;
+    this.requestType = requestType;
     this.routerStats = routerStats;
-    /**
-     * At this point, the request type is still be {@link RequestType#MULTI_GET} or {@link RequestType#COMPUTE}.
-     */
-    RequestType requestType = path.getRequestType();
-    if (!requestType.equals(RequestType.MULTI_GET) && !requestType.equals(RequestType.COMPUTE)) {
+    if (!requestType.equals(RequestType.MULTI_GET_STREAMING) && !requestType.equals(RequestType.COMPUTE_STREAMING)) {
       throw new VeniceException(
           "Unexpected request type for streaming: " + requestType + ", and currently only"
-              + " the following types are supported: [" + RequestType.MULTI_GET + ", " + RequestType.COMPUTE + "]");
+              + " the following types are supported: [" + RequestType.MULTI_GET_STREAMING + ", "
+              + RequestType.COMPUTE_STREAMING + "]");
     }
     this.ctx = ctx;
     this.chunkedWriteHandler = handler;
@@ -276,27 +275,28 @@ public class VeniceChunkedResponse {
       ByteBuf byteBuf,
       CompressionStrategy compression,
       StreamingCallback<Long> callback) {
-    if (path.getRequestType().equals(RequestType.MULTI_GET_STREAMING)) {
-      synchronized (this) {
-        if (responseCompression.isPresent()) {
-          if (!responseCompression.get().equals(compression)) {
-            // Defensive code, not expected
-            LOGGER.error(
-                "Received inconsistent compression for the new write: {}, and previous compression: {}",
-                compression,
-                responseCompression.get());
-            /**
-             * Skip the write with wrong compression.
-             * {@link VeniceResponseAggregator#buildStreamingResponse} will perform the same check
-             * when all the responses returned by storage nodes are ready, and when the inconsistency happens,
-             * {@link VeniceResponseAggregator} will return an error response instead to indicate the issue.
-             */
-            return CompletableFuture.completedFuture(0l);
+    if (this.requestType.equals(RequestType.MULTI_GET_STREAMING)) {
+      if (this.responseCompression == null) {
+        synchronized (this) {
+          if (this.responseCompression == null) {
+            this.responseCompression = compression;
+            this.responseMetadata = RESPONSE_META_MAP_FOR_MULTI_GET.get(compression);
           }
-        } else {
-          responseCompression = Optional.of(compression);
-          responseMetadata = RESPONSE_META_MAP_FOR_MULTI_GET.get(compression);
         }
+      }
+      if (!this.responseCompression.equals(compression)) {
+        // Defensive code, not expected
+        LOGGER.error(
+            "Received inconsistent compression for the new write: {}, and previous compression: {}",
+            compression,
+            this.responseCompression);
+        /**
+         * Skip the write with wrong compression.
+         * {@link VeniceResponseAggregator#buildStreamingResponse} will perform the same check
+         * when all the responses returned by storage nodes are ready, and when the inconsistency happens,
+         * {@link VeniceResponseAggregator} will return an error response instead to indicate the issue.
+         */
+        return CompletableFuture.completedFuture(0l);
       }
     } else {
       responseMetadata = RESPONSE_META_FOR_COMPUTE;
@@ -355,8 +355,7 @@ public class VeniceChunkedResponse {
   }
 
   private void reportResponseSize() {
-    RequestType requestType = path.getRequestType();
-    routerStats.getStatsByType(requestType).recordResponseSize(path.getStoreName(), totalBytesReceived.get());
+    routerStats.getStatsByType(this.requestType).recordResponseSize(this.storeName, totalBytesReceived.get());
   }
 
   /**
@@ -385,22 +384,21 @@ public class VeniceChunkedResponse {
     AvroSerializer.ReusableObjects reusableObjects = AvroSerializer.REUSE.get();
     ByteBuffer footerByteBuffer = ByteBuffer.wrap(STREAMING_FOOTER_SERIALIZER.serialize(footerRecord, reusableObjects));
 
-    RequestType requestType = path.getRequestType();
     ByteBuf footerResponse;
-    if (requestType.equals(RequestType.MULTI_GET_STREAMING)) {
+    if (this.requestType.equals(RequestType.MULTI_GET_STREAMING)) {
       MultiGetResponseRecordV1 record = new MultiGetResponseRecordV1();
       record.keyIndex = KEY_ID_FOR_STREAMING_FOOTER;
       record.value = footerByteBuffer;
       record.schemaId = STREAMING_FOOTER_SCHEMA_ID;
       footerResponse = Unpooled.wrappedBuffer(MULTI_GET_RESPONSE_SERIALIZER.serialize(record, reusableObjects));
-    } else if (requestType.equals(RequestType.COMPUTE_STREAMING)) {
+    } else if (this.requestType.equals(RequestType.COMPUTE_STREAMING)) {
       ComputeResponseRecordV1 record = new ComputeResponseRecordV1();
       record.keyIndex = KEY_ID_FOR_STREAMING_FOOTER;
       record.value = footerByteBuffer;
       footerResponse = Unpooled.wrappedBuffer(COMPUTE_RESPONSE_SERIALIZER.serialize(record, reusableObjects));
     } else {
       // not possible
-      LOGGER.error("Received unsupported request type: {} for streaming response", requestType);
+      LOGGER.error("Received unsupported request type: {} for streaming response", this.requestType);
       return;
     }
 

gradle/spotbugs/exclude.xml
@@ -269,18 +269,6 @@
   <Match>
     <!--Spotbug suggests return empty array instead of null, but it'd require lots of refactoring-->
     <Bug pattern="PZLA_PREFER_ZERO_LENGTH_ARRAYS" />
-    <Or>
-      <Class name="com.linkedin.venice.hadoop.AbstractVeniceRecordReader"/>
-      <Class name="com.linkedin.venice.client.store.AvroComputeRequestBuilderV4"/>
-      <Class name="com.linkedin.venice.schema.vson.VsonAvroSerializer"/>
-      <Class name="com.linkedin.venice.router.api.DictionaryRetrievalService"/>
-      <Class name="com.linkedin.davinci.kafka.consumer.ActiveActiveStoreIngestionTask"/>
-      <Class name="com.linkedin.davinci.kafka.consumer.StoreWriteComputeProcessor"/>
-      <Class name="com.linkedin.davinci.store.blackhole.BlackHoleStorageEnginePartition"/>
-      <Class name="com.linkedin.davinci.store.memory.InMemoryStoragePartition"/>
-      <Class name="com.linkedin.davinci.store.rocksdb.RocksDBStoragePartition"/>
-      <Class name="com.linkedin.venice.client.schema.RouterBackedSchemaReader"/>
-    </Or>
   </Match>
   <Match>
     <!--No need to serialize a class created for testing-->

gradle/spotbugs/include.xml
@@ -71,7 +71,6 @@
       <Bug pattern="RI_REDUNDANT_INTERFACES"/>
       <Bug pattern="IT_NO_SUCH_ELEMENT"/>
       <Bug pattern="MS_PKGPROTECT"/>
-      <Bug pattern="PZLA_PREFER_ZERO_LENGTH_ARRAYS"/>
       <Bug pattern="SE_BAD_FIELD_INNER_CLASS"/>
       <Bug pattern="SE_COMPARATOR_SHOULD_BE_SERIALIZABLE"/>
       <Bug pattern="DCN_NULLPOINTER_EXCEPTION"/>

internal/alpini/common/alpini-common-base/src/main/java/com/linkedin/alpini/base/misc/Headers.java
@@ -1,7 +1,6 @@
 package com.linkedin.alpini.base.misc;
 
 import java.util.Collections;
-import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -44,17 +43,7 @@ public interface Headers extends Iterable<Map.Entry<String, String>> {
 
   List<CharSequence> getAll(CharSequence name);
 
-  Optional<CharSequence> get(CharSequence name);
-
-  Optional<Date> getAsDate(CharSequence name);
-
-  default Optional<Integer> getAsInt(CharSequence name) {
-    return get(name).map(CharSequence::toString).map(Integer::valueOf);
-  }
-
-  default Optional<Long> getAsLong(CharSequence name) {
-    return get(name).map(CharSequence::toString).map(Long::valueOf);
-  }
+  CharSequence get(CharSequence name);
 
   boolean contains(CharSequence name);
 
@@ -91,13 +80,8 @@ public interface Headers extends Iterable<Map.Entry<String, String>> {
     }
 
     @Override
-    public Optional<CharSequence> get(CharSequence name) {
-      return Optional.empty();
-    }
-
-    @Override
-    public Optional<Date> getAsDate(CharSequence name) {
-      return Optional.empty();
+    public CharSequence get(CharSequence name) {
+      return null;
     }
 
     @Override

internal/alpini/common/alpini-common-io/src/main/java/com/linkedin/alpini/io/IOUtils.java
@@ -1,15 +1,12 @@
 package com.linkedin.alpini.io;
 
-import com.linkedin.alpini.base.concurrency.CompletableFutureTask;
 import java.io.ByteArrayOutputStream;
 import java.io.Closeable;
-import java.io.FilterInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.StringWriter;
-import java.lang.reflect.Field;
 import java.nio.charset.Charset;
 import javax.annotation.WillClose;
 import org.apache.logging.log4j.LogManager;
@@ -25,8 +22,6 @@ public enum IOUtils {
   private static final Logger LOG = LogManager.getLogger(IOUtils.class);
 
   public static final int EOF = -1;
-
-  private static final Field FILTER_INPUT_STREAM_IN = getFilterInputStreamIn();
 
   public static void closeQuietly(@WillClose Closeable stream) {
     try (Closeable closeable = stream) {
@@ -79,23 +74,5 @@ public enum IOUtils {
       writer.write(buffer, 0, charsRead);
     }
     return writer.toString();
-  }
-
-  public static InputStream unwrapFilterInputStream(FilterInputStream inputStream) throws IOException {
-    try {
-      return InputStream.class.cast(FILTER_INPUT_STREAM_IN.get(inputStream));
-    } catch (IllegalAccessException e) {
-      throw new IOException(e);
-    }
-  }
-
-  private static Field getFilterInputStreamIn() {
-    CompletableFutureTask<Field> task = new CompletableFutureTask<Field>(() -> {
-      Field field = FilterInputStream.class.getDeclaredField("in");
-      field.setAccessible(true);
-      return field;
-    });
-    task.run();
-    return task.toCompletableFuture().join();
   }
 }

commit a0ecdac5613aec23d62dacddbe41dc1ca679b18a

[router] Fixed a NPE occurring in router's streaming batch get
This commit fixes a regression introduced in PR #412, where a race
condition would result in VeniceChunkedResponse's responseMetadata to be
not initialized yet. The field is actually not necessary at all, and its
content can be determined the one and only time it's used, rather than
stored ahead of time. Also eliminated the responseMetadataWriteInitiated
AtomicBoolean, since the responseCompression field being null already has
an equivalent meaning. Therefore, VeniceChunkedResponse now carries two
fewer properties.

services/venice-router/src/main/java/com/linkedin/venice/router/streaming/VeniceChunkedResponse.java
@@ -51,7 +51,6 @@ import java.util.Map;
 import java.util.Queue;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 import org.apache.logging.log4j.LogManager;
@@ -103,15 +102,16 @@ public class VeniceChunkedResponse {
 
   // Whether the response has already completed or not
   private boolean responseCompleteCalled = false;
-  // Whether the response metadata has been sent or not
-  private final AtomicBoolean responseMetadataWriteInitiated = new AtomicBoolean(false);
-
   private final AtomicLong totalBytesReceived = new AtomicLong(0);
   private final Queue<Chunk> chunksToWrite = new ConcurrentLinkedQueue<Chunk>();
   private final Queue<Chunk> chunksAwaitingCallback = new ConcurrentLinkedQueue<Chunk>();
 
-  // Response metadata should be sent when everything is good.
-  private HttpResponse responseMetadata;
+  /**
+   * Initialized upon the first write. If null, then no writes occurred yet, and therefore the response metadata was
+   * not sent either.
+   *
+   * In the case of streaming batch gets, we expect that all servers should return the same value for this.
+   */
   private volatile CompressionStrategy responseCompression = null;
 
   protected static final RedundantExceptionFilter REDUNDANT_LOGGING_FILTER =
@@ -230,7 +230,7 @@ public class VeniceChunkedResponse {
         finish(new StreamingCallbackOnlyFreeResponseOnSuccess<>(((SuccessfulStreamingResponse) msg), promise));
         return true;
       } else if (msg instanceof FullHttpResponse) {
-        if (!responseMetadataWriteInitiated.get()) {
+        if (VeniceChunkedResponse.this.responseCompression == null) {
           // Error response before sending out any data yet
           return false;
         }
@@ -246,7 +246,7 @@ public class VeniceChunkedResponse {
               "Unexpected response status: " + status + ", and only non 200 status is expected here");
         }
       }
-      throw new VeniceException("Unexpected message type received: " + msg.getClass());
+      throw new VeniceException("Unexpected message type received: " + (msg == null ? "null" : msg.getClass()));
     }
   }
 
@@ -274,40 +274,41 @@ public class VeniceChunkedResponse {
       ByteBuf byteBuf,
       CompressionStrategy compression,
       StreamingCallback<Long> callback) {
-    if (this.requestType.equals(RequestType.MULTI_GET_STREAMING)) {
-      if (this.responseCompression == null) {
-        synchronized (this) {
-          if (this.responseCompression == null) {
-            this.responseCompression = compression;
-            this.responseMetadata = RESPONSE_META_MAP_FOR_MULTI_GET.get(compression);
-          }
+    boolean isFirstWrite = false;
+    if (this.responseCompression == null) {
+      synchronized (this) {
+        if (this.responseCompression == null) {
+          this.responseCompression = compression;
+          isFirstWrite = true;
         }
       }
-      if (!this.responseCompression.equals(compression)) {
-        // Defensive code, not expected
-        LOGGER.error(
-            "Received inconsistent compression for the new write: {}, and previous compression: {}",
-            compression,
-            this.responseCompression);
-        /**
-         * Skip the write with wrong compression.
-         * {@link VeniceResponseAggregator#buildStreamingResponse} will perform the same check
-         * when all the responses returned by storage nodes are ready, and when the inconsistency happens,
-         * {@link VeniceResponseAggregator} will return an error response instead to indicate the issue.
-         */
-        return CompletableFuture.completedFuture(0l);
-      }
-    } else {
-      responseMetadata = RESPONSE_META_FOR_COMPUTE;
+    }
+
+    boolean isMultiGet = this.requestType.equals(RequestType.MULTI_GET_STREAMING);
+    if (isMultiGet && !this.responseCompression.equals(compression)) {
+      // Defensive code, not expected
+      LOGGER.error(
+          "Received inconsistent compression for the new write: {}, and previous compression: {}",
+          compression,
+          this.responseCompression);
+      /**
+       * Skip the write with wrong compression.
+       * {@link VeniceResponseAggregator#buildStreamingResponse} will perform the same check
+       * when all the responses returned by storage nodes are ready, and when the inconsistency happens,
+       * {@link VeniceResponseAggregator} will return an error response instead to indicate the issue.
+       */
+      return CompletableFuture.completedFuture(0l);
     }
 
     /**
      * We would like to send out response metadata as late as possible, so that the error happened earlier
      * (such as request parsing error) could still be sent out with the right status code
      */
-    if (responseMetadataWriteInitiated.compareAndSet(false, true)) {
+    if (isFirstWrite) {
       // Send out response metadata
       ChannelPromise writePromise = ctx.newPromise().addListener(new ResponseMetadataWriteListener());
+      HttpResponse responseMetadata =
+          isMultiGet ? RESPONSE_META_MAP_FOR_MULTI_GET.get(compression) : RESPONSE_META_FOR_COMPUTE;
       chunkedWriteHandler.write(ctx, responseMetadata, writePromise);
       /**
        * {@link ChunkedWriteHandler#resumeTransfer()} invocation will try to flush more data to the client since more

services/venice-router/src/test/java/com/linkedin/venice/router/streaming/VeniceChunkedResponseTest.java
@@ -1,0 +1,30 @@
+package com.linkedin.venice.router.streaming;
+
+import static org.mockito.Mockito.mock;
+import static org.testng.Assert.assertThrows;
+
+import com.linkedin.venice.exceptions.VeniceException;
+import com.linkedin.venice.read.RequestType;
+import com.linkedin.venice.router.stats.RouterStats;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelPromise;
+import org.testng.annotations.Test;
+
+
+public class VeniceChunkedResponseTest {
+  @Test
+  public void whetherToSkipMessage() {
+    VeniceChunkedWriteHandler chunkedWriteHandler = new VeniceChunkedWriteHandler();
+    /** The {@link VeniceChunkedResponse} is only instantiated so that it registers its callback into the handler */
+    new VeniceChunkedResponse(
+        "storeName",
+        RequestType.MULTI_GET_STREAMING,
+        mock(ChannelHandlerContext.class),
+        chunkedWriteHandler,
+        mock(RouterStats.class));
+    assertThrows(
+        // This used to throw a NPE as part of a previous regression, we want a better error message
+        VeniceException.class,
+        () -> chunkedWriteHandler.write(mock(ChannelHandlerContext.class), null, mock(ChannelPromise.class)));
+  }
+}