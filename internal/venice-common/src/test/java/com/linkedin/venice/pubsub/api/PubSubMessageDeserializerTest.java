255e5d76a1c9f1f0c4e9c6c4d03329b687807ef7
Sushant Mane <sumane@linkedin.com>
2023-06-13 09:52:47 -0700

    Add new tests

--- internal/venice-common/src/test/java/com/linkedin/venice/pubsub/api/PubSubMessageDeserializerTest.java
+++ internal/venice-common/src/test/java/com/linkedin/venice/pubsub/api/PubSubMessageDeserializerTest.java
@@ -1,5 +1,6 @@
 package com.linkedin.venice.pubsub.api;
 
+import static com.linkedin.venice.pubsub.api.PubSubMessageDeserializer.VENICE_TRANSPORT_PROTOCOL_HEADER;
 import static org.testng.Assert.assertEquals;
 
 import com.linkedin.venice.exceptions.VeniceMessageException;
@@ -24,32 +25,35 @@ import org.testng.annotations.Test;
  * Unit tests for {@link PubSubMessageDeserializer}
  */
 public class PubSubMessageDeserializerTest {
-  private PubSubTopicRepository pubSubTopicRepository;
-  private PubSubMessageDeserializer pubSubMessageDeserializer;
-  private PubSubTopicPartition pubSubTopicPartition;
+  private PubSubTopicRepository topicRepository;
+  private PubSubMessageDeserializer messageDeserializer;
+  private PubSubTopicPartition topicPartition;
+  private KafkaKeySerializer keySerializer;
+  private KafkaValueSerializer valueSerializer;
 
   @BeforeMethod
   public void setUp() {
-    pubSubTopicRepository = new PubSubTopicRepository();
-    pubSubMessageDeserializer = new PubSubMessageDeserializer(
+    topicRepository = new PubSubTopicRepository();
+    messageDeserializer = new PubSubMessageDeserializer(
         new OptimizedKafkaValueSerializer(),
         new LandFillObjectPool<>(KafkaMessageEnvelope::new),
         new LandFillObjectPool<>(KafkaMessageEnvelope::new));
-    pubSubTopicPartition = new PubSubTopicPartitionImpl(pubSubTopicRepository.getTopic("test"), 42);
+    topicPartition = new PubSubTopicPartitionImpl(topicRepository.getTopic("test"), 42);
+    keySerializer = new KafkaKeySerializer();
+    valueSerializer = new OptimizedKafkaValueSerializer();
   }
 
   @Test(expectedExceptions = IllegalStateException.class, expectedExceptionsMessageRegExp = ".*Illegal key header byte.*")
   public void testDeserializerFailsWhenKeyValueFormatIsInvalid() {
-    pubSubMessageDeserializer
-        .deserialize(pubSubTopicPartition, "key".getBytes(), "value".getBytes(), new PubSubMessageHeaders(), 11L, 12L);
+    messageDeserializer
+        .deserialize(topicPartition, "key".getBytes(), "value".getBytes(), new PubSubMessageHeaders(), 11L, 12L);
   }
 
   @Test(expectedExceptions = VeniceMessageException.class, expectedExceptionsMessageRegExp = ".*Received Magic Byte 'v' which is not supported by OptimizedKafkaValueSerializer.*")
   public void testDeserializerFailsWhenValueFormatIsInvalid() {
     KafkaKey key = new KafkaKey(MessageType.PUT, "key".getBytes());
-    KafkaKeySerializer keySerializer = new KafkaKeySerializer();
-    pubSubMessageDeserializer.deserialize(
-        pubSubTopicPartition,
+    messageDeserializer.deserialize(
+        topicPartition,
         keySerializer.serialize("test", key),
         "value".getBytes(),
         new PubSubMessageHeaders(),
@@ -60,16 +64,54 @@ public class PubSubMessageDeserializerTest {
   @Test
   public void testDeserializer() {
     KafkaKey key = new KafkaKey(MessageType.PUT, "key".getBytes());
-    KafkaKeySerializer keySerializer = new KafkaKeySerializer();
     KafkaMessageEnvelope value = getDummyValue();
-
-    KafkaValueSerializer valueSerializer = new OptimizedKafkaValueSerializer();
-
-    PubSubMessage<KafkaKey, KafkaMessageEnvelope, Long> message = pubSubMessageDeserializer.deserialize(
-        pubSubTopicPartition,
+    PubSubMessage<KafkaKey, KafkaMessageEnvelope, Long> message = messageDeserializer.deserialize(
+        topicPartition,
         keySerializer.serialize("test", key),
         valueSerializer.serialize("test", value),
         new PubSubMessageHeaders(),
+        11L,
+        12L);
+    // verify
+    KafkaKey actualKey = message.getKey();
+    assertEquals(actualKey.getKeyHeaderByte(), key.getKeyHeaderByte());
+    assertEquals(actualKey.getKey(), key.getKey());
+    assertEquals(message.getValue(), value);
+    assertEquals((long) message.getOffset(), 11);
+  }
+
+  @Test
+  public void testDeserializerValueWithSchemaFromPubSubMessageHeaders() {
+    KafkaKey key = new KafkaKey(MessageType.CONTROL_MESSAGE, "key".getBytes());
+    KafkaMessageEnvelope value = getDummyValue();
+    PubSubMessageHeader header =
+        new PubSubMessageHeader(VENICE_TRANSPORT_PROTOCOL_HEADER, KafkaMessageEnvelope.SCHEMA$.toString().getBytes());
+    PubSubMessage<KafkaKey, KafkaMessageEnvelope, Long> message = messageDeserializer.deserialize(
+        topicPartition,
+        keySerializer.serialize("test", key),
+        valueSerializer.serialize("test", value),
+        new PubSubMessageHeaders().add(header),
+        11L,
+        12L);
+
+    // verify
+    KafkaKey actualKey = message.getKey();
+    assertEquals(actualKey.getKeyHeaderByte(), key.getKeyHeaderByte());
+    assertEquals(actualKey.getKey(), key.getKey());
+    assertEquals(message.getValue(), value);
+    assertEquals((long) message.getOffset(), 11);
+  }
+
+  @Test
+  public void testDeserializerValueWithInvalidSchemaFromPubSubMessageHeaders() {
+    KafkaKey key = new KafkaKey(MessageType.CONTROL_MESSAGE, "key".getBytes());
+    KafkaMessageEnvelope value = getDummyValue();
+    PubSubMessageHeader header = new PubSubMessageHeader(VENICE_TRANSPORT_PROTOCOL_HEADER, "invalid".getBytes());
+    PubSubMessage<KafkaKey, KafkaMessageEnvelope, Long> message = messageDeserializer.deserialize(
+        topicPartition,
+        keySerializer.serialize("test", key),
+        valueSerializer.serialize("test", value),
+        new PubSubMessageHeaders().add(header),
         11L,
         12L);
 
