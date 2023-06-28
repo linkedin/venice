76456f5ef785001c4acdac8df1e84deac182d1f6
Lei Lu <lelu@linkedin.com>
2023-06-28 11:53:24 -0700

    [admin-tool] Add support for '--cluster' parameter in data recovery tools
    This changes adds support for taking '--cluster' (cluster name) as optional parameter
    to several data recovery tools. Because of this change, the '--stores' paramter is
    changed to optional. When both are present, '--stores' can ovewrite '--cluster'.

--- clients/venice-admin-tool/src/main/java/com/linkedin/venice/Command.java
+++ clients/venice-admin-tool/src/main/java/com/linkedin/venice/Command.java
@@ -449,16 +449,16 @@ public enum Command {
   ),
   EXECUTE_DATA_RECOVERY(
       "execute-data-recovery", "Execute data recovery for a group of stores",
-      new Arg[] { URL, RECOVERY_COMMAND, STORES, SOURCE_FABRIC, DEST_FABRIC, DATETIME },
-      new Arg[] { EXTRA_COMMAND_ARGS, DEBUG, NON_INTERACTIVE }
+      new Arg[] { URL, RECOVERY_COMMAND, SOURCE_FABRIC, DEST_FABRIC, DATETIME },
+      new Arg[] { STORES, CLUSTER, EXTRA_COMMAND_ARGS, DEBUG, NON_INTERACTIVE }
   ),
   ESTIMATE_DATA_RECOVERY_TIME(
       "estimate-data-recovery-time", "Estimates the time it would take to execute data recovery for a group of stores.",
-      new Arg[] { URL, STORES, DEST_FABRIC }
+      new Arg[] { URL, DEST_FABRIC }, new Arg[] { STORES, CLUSTER }
   ),
   MONITOR_DATA_RECOVERY(
       "monitor-data-recovery", "Monitor data recovery progress for a group of stores",
-      new Arg[] { URL, STORES, DEST_FABRIC, DATETIME }, new Arg[] { INTERVAL }
+      new Arg[] { URL, DEST_FABRIC, DATETIME }, new Arg[] { STORES, CLUSTER, INTERVAL }
   );
 
   private final String commandName;
