718552c6f8655ad424706f4aabaffb2872da4e30
rohit kundu <rohkundu@linkedin.com>
2023-07-27 16:07:35 -0700

    fix spotbugs error cause

--- clients/venice-client/src/test/java/com/linkedin/venice/fastclient/transport/GrpcTransportClientTest.java
+++ clients/venice-client/src/test/java/com/linkedin/venice/fastclient/transport/GrpcTransportClientTest.java
@@ -1,16 +1,13 @@
 package com.linkedin.venice.fastclient.transport;
 
-import com.linkedin.venice.protocols.VeniceReadServiceGrpc;
-import io.grpc.ManagedChannel;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 
 public class GrpcTransportClientTest {
-  private GrpcTransportClient grpcTransportClient;
-  private ManagedChannel mockChannel;
-  private VeniceReadServiceGrpc.VeniceReadServiceStub stub;
-
+  // private GrpcTransportClient grpcTransportClient;
+  // private ManagedChannel mockChannel;
+  // private VeniceReadServiceGrpc.VeniceReadServiceStub stub;
   @BeforeTest
   public void setUp() throws Exception {
 
