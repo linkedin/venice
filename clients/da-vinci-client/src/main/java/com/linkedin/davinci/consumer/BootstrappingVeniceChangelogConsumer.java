ae8ba705edeba6f63a9ab7a25daac658dae3c063
Zac Policzer <zacattackftw@gmail.com>
2023-08-01 13:30:38 -0700

    add javadoc

--- clients/da-vinci-client/src/main/java/com/linkedin/davinci/consumer/BootstrappingVeniceChangelogConsumer.java
+++ clients/da-vinci-client/src/main/java/com/linkedin/davinci/consumer/BootstrappingVeniceChangelogConsumer.java
@@ -6,38 +6,29 @@ import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 
 
+/**
+ * This interface is meant for users where local state must be built off of the entirety of a venice data set
+ * (i.e. Non-idempotent event ingestion), rather than dealing with an event at a time. THIS IS EXPENSIVE.
+ * It's highly recommended that users use the {@link VeniceChangelogConsumer} interface as a means to consume Venice
+ * Change capture data.
+ *
+ * Implementations of this interface rely on access to a compacted view to the data and scanning the entirety of that
+ * compacted view initial calls to poll(). This is the only supported pattern with this interface today. {@link VeniceChangelogConsumer}
+ * enables more fine control.  This interface is intentionally limited as implementors rely on local checkpointing and
+ * maintenance of state which might be easily corrupted with byzantine seek() calls.
+ * @param <K>
+ * @param <V>
+ */
 public interface BootstrappingVeniceChangelogConsumer<K, V> {
   /**
+   * Start performs both a topic subscription and catch up. The client will look at the latest offset in the server and
+   * sync bootstrap data up to that point in changes. Once that is done for all partitions, the future will complete.
    *
-   * seekWithBootstrap(), no timestamp.  Goes to end of offset for whatever topic we were consuming on.  Consume it all.
-   * NOTE: Provide config to optionally abort in case the data volume is huge.  Should log a message that says how many messages
-   * we'll consume? Whatever.
+   * NOTE: This future may take some time to complete depending on how much data needs to be ingested in order to catch
+   * up with the time that this client started.
    *
-   * So steps.
-   *
-   * If local bootstrap is enabled
-   *      -poll will persist to local rocksdb.
-   *      -seekWithBootstrap will catch up to the current time the local state and subsequent poll calls will
-   *       return the scan of the rocksdb data until it gets to the end. Once it gets to the end, we'll start returning
-   *       topic poll results.
-   *      -On startup after subscribe we'll check the current version and see what version we have locally. If the version
-   *       doesn't match, we'll need to make a call on if we clean up or not. This one is hard. I think we should do it like this.
-   *       If the LAST version we had persisted still has live CC topics and the next version is a repush, consume normally
-   *       and jump to the next version.  If the last version is no longer live drop, and if the next version isn't a repush drop.
-   *      -When a versionSwap is encountered, if the versionSwap isn't a repush, nuke everything. If it's a repush, keep it.
-   *
-   *
-   * Implementation:
-   *  -New config for local persistence.
-   *  -calls to seekWithBootstrap will just not do anything? Throw exception? Hum.
-   *  -Wraps the VeniceChangelog consumer and overrides some methods.  We'll need to override poll() to act on stuff.
-   *   actually doing that is kind of nice, we don't need to worry about compression or anything, we'll just let the internal client handle it.
-   *  -Use rocksdb storage partitions to store the data.
-   *
-   *
-   * @param partitions
-   * @param timeStamp
-   * @return
+   * @param partitions which partition id's to catch up with
+   * @return a future that completes once catch up is complete for all passed in partitions.
    */
   CompletableFuture<Void> start(Set<Integer> partitions);
 
@@ -45,6 +36,12 @@ public interface BootstrappingVeniceChangelogConsumer<K, V> {
 
   void stop() throws Exception;
 
+  /**
+   * polls for the next batch of change events. The first records returned following calling 'start()' will be from the bootstrap state.
+   * Once this state is consumed, subsequent calls to poll will be based off of recent updates to the Venice store.
+   * @param timeoutInMs
+   * @return
+   */
   Collection<PubSubMessage<K, ChangeEvent<V>, VeniceChangeCoordinate>> poll(long timeoutInMs);
 
 }
