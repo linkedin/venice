8a5a09a923e4637b7de4b7ff8d6efd3a4e55b57a
Min Huang <mnhuang@linkedin.com>
2023-08-14 10:54:34 -0700

    Added unit test

--- clients/da-vinci-client/src/test/java/com/linkedin/davinci/helix/VeniceLeaderFollowerStateModelTest.java
+++ clients/da-vinci-client/src/test/java/com/linkedin/davinci/helix/VeniceLeaderFollowerStateModelTest.java
@@ -2,10 +2,10 @@ package com.linkedin.davinci.helix;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import com.linkedin.davinci.stats.ParticipantStateTransitionStats;
 import com.linkedin.venice.meta.Store;
 import java.util.concurrent.CompletableFuture;
 import org.testng.annotations.Test;
@@ -23,7 +23,7 @@ public class VeniceLeaderFollowerStateModelTest extends
         mockReadOnlyStoreRepository,
         CompletableFuture.completedFuture(mockPushStatusAccessor),
         null,
-        mock(ParticipantStateTransitionStats.class));
+        mockParticipantStateTransitionStats);
   }
 
   @Override
@@ -50,5 +50,20 @@ public class VeniceLeaderFollowerStateModelTest extends
         testPartition,
         Store.BOOTSTRAP_TO_ONLINE_TIMEOUT_IN_HOURS,
         mockStoreIngestionService);
+  }
+
+  @Test
+  public void testGracefulDropForCurrentVersionResource() {
+    // if the resource is not the current serving version, state transition thread will not be blocked.
+    when(mockStore.getCurrentVersion()).thenReturn(2);
+    testStateModel.onBecomeDroppedFromOffline(mockMessage, mockContext);
+    verify(mockParticipantStateTransitionStats, never()).incrementThreadBlockedOnOfflineToDroppedTransitionCount();
+    verify(mockParticipantStateTransitionStats, never()).decrementThreadBlockedOnOfflineToDroppedTransitionCount();
+
+    // if the resource is the current serving version, state transition thread will be blocked.
+    when(mockStore.getCurrentVersion()).thenReturn(1);
+    testStateModel.onBecomeDroppedFromOffline(mockMessage, mockContext);
+    verify(mockParticipantStateTransitionStats, times(1)).incrementThreadBlockedOnOfflineToDroppedTransitionCount();
+    verify(mockParticipantStateTransitionStats, times(1)).decrementThreadBlockedOnOfflineToDroppedTransitionCount();
   }
 }
