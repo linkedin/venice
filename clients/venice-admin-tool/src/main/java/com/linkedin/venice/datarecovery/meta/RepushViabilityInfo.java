01b503a3532c49aa45aa652dbb76d88dfeebd09b
Lei Lu <lelu@linkedin.com>
2023-08-30 14:07:47 -0700

    [admin-tool] Improve data recovery task reporting
    Today, in data recovery client, getRepushViability inaccurately reports
    certain scenarios as failures, e.g. store's current version is newer
    than the provided timestamp, or an ongoing push is in-progress. These
    scenarios are in fact not failures, but cases that a new recovery action
    is not needed. However, there are other cases that we should consider
    as errors, e.g. when controller reponse contains errors.
    
    To report them correctly, this rb introduces a new boolean to
    RepushViabilityInfo data structure to differentiate them and react/report
    task status differently.

--- clients/venice-admin-tool/src/main/java/com/linkedin/venice/datarecovery/meta/RepushViabilityInfo.java
+++ clients/venice-admin-tool/src/main/java/com/linkedin/venice/datarecovery/meta/RepushViabilityInfo.java
@@ -3,6 +3,7 @@ package com.linkedin.venice.datarecovery.meta;
 public class RepushViabilityInfo {
   private boolean isHybrid;
   private boolean isViable;
+  private boolean isError = false;
   private RepushViabilityInfo.Result result;
 
   public RepushViabilityInfo() {
@@ -27,6 +28,14 @@ public class RepushViabilityInfo {
     isViable = viable;
   }
 
+  public boolean isError() {
+    return isError;
+  }
+
+  public void setError(boolean error) {
+    isError = error;
+  }
+
   public Result getResult() {
     return result;
   }
@@ -35,19 +44,26 @@ public class RepushViabilityInfo {
     this.result = result;
   }
 
-  public RepushViabilityInfo succeedWithResult(Result result) {
+  public RepushViabilityInfo viableWithResult(Result result) {
     setResult(result);
     setViable(true);
     return this;
   }
 
-  public RepushViabilityInfo failWithResult(Result result) {
+  public RepushViabilityInfo inViableWithResult(Result result) {
     setResult(result);
     setViable(false);
     return this;
   }
 
+  public RepushViabilityInfo inViableWithError(Result result) {
+    setResult(result);
+    setViable(false);
+    setError(true);
+    return this;
+  }
+
   public enum Result {
-    NOT_STARTED, SUCCESS, DISCOVERY_ERROR, NO_FUTURE_VERSION, TIMESTAMP_MISMATCH, ONGOING_PUSH, EXCEPTION_THROWN
+    NOT_STARTED, SUCCESS, DISCOVERY_ERROR, NO_CURRENT_VERSION, CURRENT_VERSION_IS_NEWER, ONGOING_PUSH, EXCEPTION_THROWN
   }
 }
