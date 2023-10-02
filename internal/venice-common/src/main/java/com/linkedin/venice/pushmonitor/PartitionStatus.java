8e9c8d650183abdcd1450ff26780105d81988677
Xin(Adam) Chen <adchen@linkedin.com>
2023-10-02 13:43:33 -0700

    remove it

--- internal/venice-common/src/main/java/com/linkedin/venice/pushmonitor/PartitionStatus.java
+++ internal/venice-common/src/main/java/com/linkedin/venice/pushmonitor/PartitionStatus.java
@@ -118,7 +118,6 @@ public class PartitionStatus implements Comparable<PartitionStatus> {
   @Override
   public String toString() {
     StringBuilder sb = new StringBuilder();
-    sb.append("Partition ").append(partitionId).append(": ");
     for (Map.Entry<String, ReplicaStatus> entry: replicaStatusMap.entrySet()) {
       sb.append(entry.getKey()).append(":").append(entry.getValue().getCurrentStatus().name()).append(" ");
     }
