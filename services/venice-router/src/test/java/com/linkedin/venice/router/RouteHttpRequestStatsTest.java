09414e3903cfbef6189e45271ce5986aad466115
Min Huang <mnhuang@linkedin.com>
2023-11-16 14:32:33 -0800

    Unit test fix

--- services/venice-router/src/test/java/com/linkedin/venice/router/RouteHttpRequestStatsTest.java
+++ services/venice-router/src/test/java/com/linkedin/venice/router/RouteHttpRequestStatsTest.java
@@ -34,15 +34,15 @@ public class RouteHttpRequestStatsTest {
     stats.recordPendingRequest("my_host1");
     stats.recordPendingRequest("my_host2");
 
-    Assert.assertEquals(reporter.query(".my_host1--pending_request_count.Gauge").value(), 1d);
+    Assert.assertEquals(stats.getPendingRequestCount("my_host1"), 1);
 
     stats.recordPendingRequest("my_host1");
-    Assert.assertEquals(reporter.query(".my_host1--pending_request_count.Gauge").value(), 2d);
+    Assert.assertEquals(stats.getPendingRequestCount("my_host1"), 2);
 
     stats.recordFinishedRequest("my_host1");
     stats.recordFinishedRequest("my_host2");
 
-    Assert.assertEquals(reporter.query(".my_host1--pending_request_count.Gauge").value(), 1d);
-    Assert.assertEquals(reporter.query(".my_host2--pending_request_count.Gauge").value(), 0d);
+    Assert.assertEquals(stats.getPendingRequestCount("my_host1"), 1);
+    Assert.assertEquals(stats.getPendingRequestCount("my_host2"), 0);
   }
 }
