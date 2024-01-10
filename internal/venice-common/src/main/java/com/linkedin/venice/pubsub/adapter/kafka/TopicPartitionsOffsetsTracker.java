51f6005d792fc6a422d0e188b772869b5dbb654c
Felix GV <fgv@linkedin.com>
2024-01-09 22:19:31 -0800

    Refactored TopicPartitionsOffsetsTracker some more based on Sushant's comments.
    It should no longer return stale end offsets nor stale lag.
    
    Also added a KafkaConsumerServiceStatsTest.

--- internal/venice-common/src/main/java/com/linkedin/venice/pubsub/adapter/kafka/TopicPartitionsOffsetsTracker.java
+++ internal/venice-common/src/main/java/com/linkedin/venice/pubsub/adapter/kafka/TopicPartitionsOffsetsTracker.java
@@ -11,19 +11,16 @@ import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.common.Metric;
 import org.apache.kafka.common.MetricName;
 import org.apache.kafka.common.TopicPartition;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 
 /**
  * This class tracks consumed topic partitions' offsets
  */
 public class TopicPartitionsOffsetsTracker {
-  private static final Logger LOGGER = LogManager.getLogger(TopicPartitionsOffsetsTracker.class);
-  private static final long INVALID = -1;
+  static final long INVALID = -1;
   private static final long DEFAULT_OFFSETS_UPDATE_INTERVAL_MS = 30 * Time.MS_PER_SECOND;
 
-  private final Map<TopicPartition, OffsetInfo> topicPartitionOffsetInfo;
+  private final Map<TopicPartition, KafkaPartitionTopicOffsetMetrics> topicPartitionOffsetInfo;
   private volatile long lastMetricsCollectedTime;
   private final long offsetsUpdateIntervalMs;
 
@@ -60,76 +57,42 @@ public class TopicPartitionsOffsetsTracker {
   public void updateEndAndCurrentOffsets(
       ConsumerRecords<byte[], byte[]> records,
       Consumer<byte[], byte[]> kafkaConsumer) {
-    long lastMetricsCollectedTimeSnapshot = lastMetricsCollectedTime;
-    long currentTime = System.currentTimeMillis();
-    long elapsedTime = currentTime - lastMetricsCollectedTimeSnapshot;
-    if (elapsedTime < offsetsUpdateIntervalMs) {
-      return; // Not yet
-    }
-    synchronized (this) {
-      /**
-       * N.B.: This race condition check could be defeated if we allowed the {@link offsetsUpdateIntervalMs} to be zero.
-       *       This is why the {@link #validateInterval(long)} function protects against this at construction-time.
-       *       We do allow an interval of zero in tests, via the package-private constructor, because these are not
-       *       multi-threaded.
-       */
-      if (lastMetricsCollectedTimeSnapshot == lastMetricsCollectedTime) {
-        lastMetricsCollectedTime = currentTime;
-      } else {
-        return; // Another thread got in first.
-      }
-    }
+
+    boolean lagMetricMissing = false;
 
     // Update current offset cache for all topics partition.
     List<ConsumerRecord<byte[], byte[]>> listOfRecordsForOnePartition;
     ConsumerRecord<byte[], byte[]> lastConsumerRecordOfPartition;
+    KafkaPartitionTopicOffsetMetrics offsetInfo;
     for (TopicPartition tp: records.partitions()) {
       listOfRecordsForOnePartition = records.records(tp);
       lastConsumerRecordOfPartition = listOfRecordsForOnePartition.get(listOfRecordsForOnePartition.size() - 1);
-      topicPartitionOffsetInfo.computeIfAbsent(tp, k -> new OffsetInfo()).currentOffset =
-          lastConsumerRecordOfPartition.offset();
+      offsetInfo = topicPartitionOffsetInfo.computeIfAbsent(tp, k -> new KafkaPartitionTopicOffsetMetrics());
+      offsetInfo.setCurrentOffset(lastConsumerRecordOfPartition.offset());
+      if (offsetInfo.isLagMetricMissing()) {
+        lagMetricMissing = true;
+      }
     }
 
+    long currentTime = System.currentTimeMillis();
+    long elapsedTime = currentTime - lastMetricsCollectedTime;
+    if (elapsedTime < offsetsUpdateIntervalMs && !lagMetricMissing) {
+      return; // Not yet
+    }
+
+    lastMetricsCollectedTime = currentTime;
     MetricName metricName;
     Metric metric;
     TopicPartition tp;
-    OffsetInfo offsetInfo;
-    long metricValue;
     for (Map.Entry<MetricName, ? extends Metric> entry: kafkaConsumer.metrics().entrySet()) {
       metricName = entry.getKey();
       metric = entry.getValue();
-      metricValue = getMetricEntryRecordsLag(metricName, metric);
 
-      if (metricValue != INVALID) {
-        tp = new TopicPartition(metricName.tags().get("topic"), Integer.parseInt(metricName.tags().get("partition")));
-        offsetInfo = topicPartitionOffsetInfo.computeIfAbsent(tp, k -> new OffsetInfo());
-        offsetInfo.lag = metricValue;
-        if (offsetInfo.currentOffset != INVALID) {
-          offsetInfo.endOffset = offsetInfo.currentOffset + metricValue;
-        }
-      }
-    }
-  }
-
-  /**
-   * @return the lag value if the metric is for lag, or {@link #INVALID} otherwise.
-   */
-  private long getMetricEntryRecordsLag(MetricName metricName, Metric metric) {
-    try {
       if (Objects.equals(metricName.name(), "records-lag")) {
-        Object metricValue = metric.metricValue();
-        if (metricValue instanceof Double) {
-          // Double is the way all metrics are internally represented in Kafka, but since we are dealing with lag, we
-          // want an integral type, so we cast it.
-          return ((Double) metricValue).longValue();
-        }
+        tp = new TopicPartition(metricName.tags().get("topic"), Integer.parseInt(metricName.tags().get("partition")));
+        offsetInfo = topicPartitionOffsetInfo.computeIfAbsent(tp, k -> new KafkaPartitionTopicOffsetMetrics());
+        offsetInfo.setLagMetric(metric);
       }
-      return INVALID;
-    } catch (Exception e) {
-      LOGGER.warn(
-          "Caught exception: {} when attempting to get consumer metrics. Incomplete metrics might be returned.",
-          e.getMessage());
-      return INVALID;
     }
   }
 
@@ -156,8 +119,8 @@ public class TopicPartitionsOffsetsTracker {
    * @return end offset of a topic partition if there is any.
    */
   public long getEndOffset(String topic, int partition) {
-    OffsetInfo offsetInfo = topicPartitionOffsetInfo.get(new TopicPartition(topic, partition));
-    return offsetInfo == null ? INVALID : offsetInfo.endOffset;
+    KafkaPartitionTopicOffsetMetrics offsetInfo = topicPartitionOffsetInfo.get(new TopicPartition(topic, partition));
+    return offsetInfo == null ? INVALID : offsetInfo.getEndOffset();
   }
 
   /**
@@ -167,13 +130,7 @@ public class TopicPartitionsOffsetsTracker {
    * @return end offset of a topic partition if there is any.
    */
   public long getOffsetLag(String topic, int partition) {
-    OffsetInfo offsetInfo = topicPartitionOffsetInfo.get(new TopicPartition(topic, partition));
-    return offsetInfo == null ? INVALID : offsetInfo.lag;
-  }
-
-  private static class OffsetInfo {
-    long currentOffset = INVALID;
-    long endOffset = INVALID;
-    long lag = INVALID;
+    KafkaPartitionTopicOffsetMetrics offsetInfo = topicPartitionOffsetInfo.get(new TopicPartition(topic, partition));
+    return offsetInfo == null ? INVALID : offsetInfo.getLag();
   }
 }
