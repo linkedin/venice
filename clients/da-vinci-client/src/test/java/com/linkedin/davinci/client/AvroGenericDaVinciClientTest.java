eb9b8502c787daf91bc58d02b5cfc0ae65c9e68a
Elijah Grimaldi <114044477+elijahgrimaldi@users.noreply.github.com>
2024-01-11 11:40:53 -0800

    [dvc] Check to clear local data on record transformer value schema change between reinitializations (#803)
    * Added check to clear local data on schema change between reinitializations

--- clients/da-vinci-client/src/test/java/com/linkedin/davinci/client/AvroGenericDaVinciClientTest.java
+++ clients/da-vinci-client/src/test/java/com/linkedin/davinci/client/AvroGenericDaVinciClientTest.java
@@ -4,8 +4,11 @@ import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
+import com.linkedin.davinci.store.rocksdb.RocksDBServerConfig;
 import com.linkedin.venice.meta.ReadOnlySchemaRepository;
 import com.linkedin.venice.schema.SchemaEntry;
+import com.linkedin.venice.utils.PropertyBuilder;
+import com.linkedin.venice.utils.VeniceProperties;
 import org.apache.avro.Schema;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -36,5 +39,15 @@ public class AvroGenericDaVinciClientTest {
         AvroGenericDaVinciClient.getValueSchemaIdForComputeRequest(storeName, computeValueSchema, repo),
         1);
     verify(repo).getValueSchemaId(storeName, computeValueSchemaString);
+  }
+
+  @Test
+  public void testPropertyBuilderWithRecordTransformer() {
+    String schema = "{\n" + "  \"type\": \"string\"\n" + "}\n";
+    VeniceProperties config =
+        new PropertyBuilder().put("kafka.admin.class", "name").put("record.transformer.value.schema", schema).build();
+    RocksDBServerConfig dbconfig = new RocksDBServerConfig(config);
+    Assert.assertEquals(schema, dbconfig.getTransformerValueSchema());
+
   }
 }
