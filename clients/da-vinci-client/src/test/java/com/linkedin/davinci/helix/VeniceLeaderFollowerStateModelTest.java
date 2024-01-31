6aecf4703b56f422b1c65733337b64d142d28964
Zac Policzer <zacattackftw@gmail.com>
2024-01-30 18:21:56 -0800

    More fixes

--- clients/da-vinci-client/src/test/java/com/linkedin/davinci/helix/VeniceLeaderFollowerStateModelTest.java
+++ clients/da-vinci-client/src/test/java/com/linkedin/davinci/helix/VeniceLeaderFollowerStateModelTest.java
@@ -11,9 +11,13 @@ import static org.mockito.Mockito.when;
 import com.linkedin.davinci.config.VeniceStoreVersionConfig;
 import com.linkedin.davinci.stats.ingestion.heartbeat.HeartbeatMonitoringService;
 import com.linkedin.venice.meta.Store;
+import com.linkedin.venice.meta.Version;
+import com.linkedin.venice.meta.VersionImpl;
 import io.tehuti.metrics.MetricsRepository;
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
+import org.mockito.Mockito;
 import org.testng.annotations.Test;
 
 
@@ -41,6 +45,8 @@ public class VeniceLeaderFollowerStateModelTest extends
   @Test
   public void testOnBecomeFollowerFromOffline() throws Exception {
     // if the resource is not the current serving version, latch is not placed.
+    Version version = new VersionImpl("mockStore.getName()", 2, "");
+    when(mockStore.getVersion(Mockito.anyInt())).thenReturn(Optional.of(version));
     when(mockStore.getCurrentVersion()).thenReturn(2);
     testStateModel.onBecomeStandbyFromOffline(mockMessage, mockContext);
     verify(mockNotifier, never()).waitConsumptionCompleted(
@@ -76,6 +82,8 @@ public class VeniceLeaderFollowerStateModelTest extends
 
   @Test
   public void testRemoveCVStateWhenBecomeOfflineFromStandby() {
+    Version version = new VersionImpl("mockStore.getName()", 2, "");
+    when(mockStore.getVersion(Mockito.anyInt())).thenReturn(Optional.of(version));
     when(mockStore.getCurrentVersion()).thenReturn(2);
     when(mockIngestionBackend.stopConsumption(any(VeniceStoreVersionConfig.class), eq(testPartition)))
         .thenReturn(CompletableFuture.completedFuture(null));
