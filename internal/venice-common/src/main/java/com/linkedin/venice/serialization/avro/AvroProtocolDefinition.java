56f6870b758570f7f75c557ee8489c0fec7ac349
Sourav Maji <tester@linkedin.com>
2024-03-12 14:35:18 -0700

    added config to control unsued schema deletion

--- internal/venice-common/src/main/java/com/linkedin/venice/serialization/avro/AvroProtocolDefinition.java
+++ internal/venice-common/src/main/java/com/linkedin/venice/serialization/avro/AvroProtocolDefinition.java
@@ -143,7 +143,7 @@ public enum AvroProtocolDefinition {
   /**
    * Value schema for metadata system store.
    */
-  METADATA_SYSTEM_SCHEMA_STORE(17, StoreMetaValue.class),
+  METADATA_SYSTEM_SCHEMA_STORE(18, StoreMetaValue.class),
 
   /**
    * Key schema for push status system store.
