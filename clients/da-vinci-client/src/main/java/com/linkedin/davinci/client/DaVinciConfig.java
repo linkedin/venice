f8a2b37ec39b15d26b03317843d01c05359fd2e7
Gaojie Liu <goliu@linkedin.com>
2024-04-01 15:15:01 -0700

    [dvc] Removed DVC remote query feature
    As it is hard to predict the DVC KPS and we don't have a valid
    use case, which would query remote Venice on purpose, this PR
    removes this feature completely after marking it as deprecated
    for some time.

--- clients/da-vinci-client/src/main/java/com/linkedin/davinci/client/DaVinciConfig.java
+++ clients/da-vinci-client/src/main/java/com/linkedin/davinci/client/DaVinciConfig.java
@@ -25,20 +25,6 @@ public class DaVinciConfig {
   private StorageClass storageClass = StorageClass.MEMORY_BACKED_BY_DISK;
 
   /**
-   * Indicates how to handle access to not-subscribed partitions.
-   *
-   * This feature will be completely removed in a later release.
-   * Here are the reasons this feature doesn't work well:
-   * 1. Remote Venice query is much slower than DaVinci local lookup.
-   * 2. Typically, Venice Backend doesn't provision enough capacity for DaVinci use cases since we normally don't know
-   *    the qps of DaVinci apps and for most of the scenarios, the DaVinci qps can be very high, and we don't want to
-   *    pre-allocate a lot of backend resources for this feature since the remote resources won't be leveraged most of
-   *    the time.
-   */
-  @Deprecated
-  private NonLocalAccessPolicy nonLocalAccessPolicy = NonLocalAccessPolicy.FAIL_FAST;
-
-  /**
    * Cache settings
    */
   private ObjectCacheConfig cacheConfig;
@@ -62,14 +48,13 @@ public class DaVinciConfig {
     return new DaVinciConfig().setManaged(isManaged())
         .setIsolated(isIsolated())
         .setStorageClass(getStorageClass())
-        .setNonLocalAccessPolicy(getNonLocalAccessPolicy())
         .setCacheConfig(getCacheConfig());
   }
 
   @Override
   public String toString() {
     return "DaVinciConfig{" + "managed=" + managed + ", isolated=" + isolated + ", storageClass=" + storageClass
-        + ", nonLocalAccessPolicy=" + nonLocalAccessPolicy + ", cacheConfig=" + cacheConfig + "}";
+        + ", cacheConfig=" + cacheConfig + "}";
   }
 
   public boolean isManaged() {
@@ -96,15 +81,6 @@ public class DaVinciConfig {
 
   public DaVinciConfig setStorageClass(StorageClass storageClass) {
     this.storageClass = storageClass;
-    return this;
-  }
-
-  public NonLocalAccessPolicy getNonLocalAccessPolicy() {
-    return nonLocalAccessPolicy;
-  }
-
-  public DaVinciConfig setNonLocalAccessPolicy(NonLocalAccessPolicy nonLocalAccessPolicy) {
-    this.nonLocalAccessPolicy = nonLocalAccessPolicy;
     return this;
   }
 
