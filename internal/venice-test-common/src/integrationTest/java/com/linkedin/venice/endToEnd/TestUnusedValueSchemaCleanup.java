a24d121e3b03ee2455941c3110e8511d4455e458
Sourav Maji <tester@linkedin.com>
2024-04-11 13:10:06 -0700

    updated test

--- internal/venice-test-common/src/integrationTest/java/com/linkedin/venice/endToEnd/TestUnusedValueSchemaCleanup.java
+++ internal/venice-test-common/src/integrationTest/java/com/linkedin/venice/endToEnd/TestUnusedValueSchemaCleanup.java
@@ -4,14 +4,16 @@ import static com.linkedin.venice.ConfigKeys.CONTROLLER_MIN_SCHEMA_COUNT_TO_KEEP
 import static com.linkedin.venice.ConfigKeys.CONTROLLER_UNUSED_SCHEMA_CLEANUP_INTERVAL_SECONDS;
 import static com.linkedin.venice.ConfigKeys.CONTROLLER_UNUSED_VALUE_SCHEMA_CLEANUP_ENABLED;
 import static com.linkedin.venice.ConfigKeys.DEFAULT_MAX_NUMBER_OF_PARTITIONS;
-import static com.linkedin.venice.ConfigKeys.DEFAULT_NUMBER_OF_PARTITION_FOR_HYBRID;
 import static com.linkedin.venice.ConfigKeys.DEFAULT_PARTITION_SIZE;
+import static com.linkedin.venice.ConfigKeys.SERVER_PROMOTION_TO_LEADER_REPLICA_DELAY_SECONDS;
+import static com.linkedin.venice.ConfigKeys.TOPIC_CLEANUP_SLEEP_INTERVAL_BETWEEN_TOPIC_LIST_FETCH_MS;
 import static com.linkedin.venice.utils.IntegrationTestPushUtils.createStoreForJob;
 import static com.linkedin.venice.utils.TestWriteUtils.NAME_RECORD_V1_SCHEMA;
 import static com.linkedin.venice.utils.TestWriteUtils.NAME_RECORD_V2_SCHEMA;
 import static com.linkedin.venice.utils.TestWriteUtils.NAME_RECORD_V3_SCHEMA;
 import static com.linkedin.venice.utils.TestWriteUtils.getTempDataDirectory;
 
+import com.linkedin.venice.controller.VeniceHelixAdmin;
 import com.linkedin.venice.controllerapi.ControllerClient;
 import com.linkedin.venice.controllerapi.UpdateStoreQueryParams;
 import com.linkedin.venice.integration.utils.ServiceFactory;
@@ -29,7 +31,6 @@ import java.util.Optional;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
-import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -45,15 +46,22 @@ public class TestUnusedValueSchemaCleanup {
   private List<VeniceMultiClusterWrapper> childDatacenters;
   private VeniceTwoLayerMultiRegionMultiClusterWrapper multiRegionMultiClusterWrapper;
 
+  private VeniceHelixAdmin veniceHelixAdmin;
+
   @BeforeClass
   public void setUp() {
     Properties controllerProps = new Properties();
-    controllerProps.put(DEFAULT_NUMBER_OF_PARTITION_FOR_HYBRID, 2);
-    controllerProps.put(DEFAULT_MAX_NUMBER_OF_PARTITIONS, 3);
-    controllerProps.put(DEFAULT_PARTITION_SIZE, 1024);
-    controllerProps.put(CONTROLLER_UNUSED_SCHEMA_CLEANUP_INTERVAL_SECONDS, 1);
+    controllerProps.put(DEFAULT_MAX_NUMBER_OF_PARTITIONS, 1);
+    controllerProps.put(DEFAULT_PARTITION_SIZE, 10);
+    controllerProps
+        .setProperty(TOPIC_CLEANUP_SLEEP_INTERVAL_BETWEEN_TOPIC_LIST_FETCH_MS, String.valueOf(Long.MAX_VALUE));
+    // controllerProps.put(ConfigKeys.CONTROLLER_AUTO_MATERIALIZE_META_SYSTEM_STORE, false);
+    controllerProps.put(CONTROLLER_UNUSED_SCHEMA_CLEANUP_INTERVAL_SECONDS, 10);
     controllerProps.put(CONTROLLER_MIN_SCHEMA_COUNT_TO_KEEP, 1);
     controllerProps.put(CONTROLLER_UNUSED_VALUE_SCHEMA_CLEANUP_ENABLED, true);
+    Properties serverProperties = new Properties();
+    serverProperties.put(SERVER_PROMOTION_TO_LEADER_REPLICA_DELAY_SECONDS, 1L);
+
     multiRegionMultiClusterWrapper = ServiceFactory.getVeniceTwoLayerMultiRegionMultiClusterWrapper(
         NUMBER_OF_CHILD_DATACENTERS,
         NUMBER_OF_CLUSTERS,
@@ -64,9 +72,11 @@ public class TestUnusedValueSchemaCleanup {
         1,
         Optional.of(controllerProps),
         Optional.of(controllerProps),
-        Optional.empty());
+        Optional.of(serverProperties));
 
     childDatacenters = multiRegionMultiClusterWrapper.getChildRegions();
+    veniceHelixAdmin =
+        (VeniceHelixAdmin) childDatacenters.get(0).getControllers().values().iterator().next().getVeniceAdmin();
   }
 
   @AfterClass(alwaysRun = true)
@@ -84,22 +94,23 @@ public class TestUnusedValueSchemaCleanup {
     Properties props =
         IntegrationTestPushUtils.defaultVPJProps(multiRegionMultiClusterWrapper, inputDirPath, storeName);
     String keySchemaStr = "\"string\"";
-    UpdateStoreQueryParams storeParms = new UpdateStoreQueryParams().setPartitionCount(1)
-        .setUnusedSchemaDeletionEnabled(true)
-        .setStorageNodeReadQuotaEnabled(true);
+    UpdateStoreQueryParams storeParms = new UpdateStoreQueryParams().setUnusedSchemaDeletionEnabled(true);
     String parentControllerURLs = multiRegionMultiClusterWrapper.getControllerConnectString();
 
     try (ControllerClient parentControllerClient = new ControllerClient(CLUSTER_NAMES[0], parentControllerURLs)) {
       createStoreForJob(CLUSTER_NAMES[0], keySchemaStr, NAME_RECORD_V1_SCHEMA.toString(), props, storeParms).close();
       parentControllerClient.addValueSchema(storeName, NAME_RECORD_V2_SCHEMA.toString());
       parentControllerClient.addValueSchema(storeName, NAME_RECORD_V3_SCHEMA.toString());
-      Assert.assertFalse(parentControllerClient.getValueSchema(storeName, 1).isError());
       TestWriteUtils.runPushJob("Test push job", props);
       TestUtils.waitForNonDeterministicPushCompletion(
           Version.composeKafkaTopic(storeName, 1),
           parentControllerClient,
           30,
           TimeUnit.SECONDS);
+      TestUtils.waitForNonDeterministicCompletion(
+          20,
+          TimeUnit.SECONDS,
+          () -> veniceHelixAdmin.getValueSchemas(CLUSTER_NAMES[0], storeName).size() == 1);
     }
   }
 
