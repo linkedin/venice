7eefea8ba80fa9fabb4675f98add8969da03c8be
Sushant Mane <sumane@linkedin.com>
2024-04-29 12:53:53 -0700

    Add metrics for TopicManager internal tracking

--- internal/venice-common/src/main/java/com/linkedin/venice/pubsub/manager/TopicManagerStats.java
+++ internal/venice-common/src/main/java/com/linkedin/venice/pubsub/manager/TopicManagerStats.java
@@ -5,6 +5,7 @@ import com.linkedin.venice.stats.TehutiUtils;
 import com.linkedin.venice.utils.LatencyUtils;
 import io.tehuti.metrics.MetricsRepository;
 import io.tehuti.metrics.Sensor;
+import io.tehuti.metrics.stats.AsyncGauge;
 import io.tehuti.metrics.stats.Avg;
 import io.tehuti.metrics.stats.Max;
 import io.tehuti.metrics.stats.Min;
@@ -23,7 +24,7 @@ class TopicManagerStats extends AbstractVeniceStats {
     CREATE_TOPIC, DELETE_TOPIC, LIST_ALL_TOPICS, SET_TOPIC_CONFIG, GET_ALL_TOPIC_RETENTIONS, GET_TOPIC_CONFIG,
     GET_TOPIC_CONFIG_WITH_RETRY, CONTAINS_TOPIC, GET_SOME_TOPIC_CONFIGS, CONTAINS_TOPIC_WITH_RETRY,
     GET_TOPIC_LATEST_OFFSETS, GET_PARTITION_LATEST_OFFSETS, PARTITIONS_FOR, GET_OFFSET_FOR_TIME,
-    GET_PRODUCER_TIMESTAMP_OF_LAST_DATA_MESSAGE
+    GET_PRODUCER_TIMESTAMP_OF_LAST_DATA_MESSAGE, CONSUMER_ACQUISITION_WAIT_TIME
   }
 
   TopicManagerStats(MetricsRepository metricsRepository, String pubSubAddress) {
@@ -56,5 +57,24 @@ class TopicManagerStats extends AbstractVeniceStats {
     }
     // convert ns to us and record
     sensorsByTypes.get(sensorType).record(LatencyUtils.getLatencyInMS(startTimeInNs));
+  }
+
+  void registerTopicMetadataFetcherSensors(TopicMetadataFetcher topicMetadataFetcher) {
+    registerSensorIfAbsent(
+        new AsyncGauge(
+            (ignored, ignored2) -> topicMetadataFetcher.getCurrentConsumerPoolSize(),
+            "available_consumer_count"));
+    registerSensorIfAbsent(
+        new AsyncGauge(
+            (ignored, ignored2) -> topicMetadataFetcher.getAsyncTaskActiveThreadCount(),
+            "async_task_active_thread_count"));
+    registerSensorIfAbsent(
+        new AsyncGauge(
+            (ignored, ignored2) -> topicMetadataFetcher.getAsyncTaskQueueLength(),
+            "async_task_queue_length"));
+    registerSensorIfAbsent(
+        new AsyncGauge(
+            (ignored, ignored2) -> topicMetadataFetcher.getConsumerWaitListSize(),
+            "consumer_wait_list_size"));
   }
 }
--- internal/venice-common/src/main/java/com/linkedin/venice/pubsub/manager/TopicMetadataFetcher.java
+++ internal/venice-common/src/main/java/com/linkedin/venice/pubsub/manager/TopicMetadataFetcher.java
@@ -1,6 +1,7 @@
 package com.linkedin.venice.pubsub.manager;
 
 import static com.linkedin.venice.pubsub.PubSubConstants.PUBSUB_OFFSET_API_TIMEOUT_DURATION_DEFAULT_VALUE;
+import static com.linkedin.venice.pubsub.manager.TopicManagerStats.SENSOR_TYPE.CONSUMER_ACQUISITION_WAIT_TIME;
 import static com.linkedin.venice.pubsub.manager.TopicManagerStats.SENSOR_TYPE.CONTAINS_TOPIC;
 import static com.linkedin.venice.pubsub.manager.TopicManagerStats.SENSOR_TYPE.GET_OFFSET_FOR_TIME;
 import static com.linkedin.venice.pubsub.manager.TopicManagerStats.SENSOR_TYPE.GET_PARTITION_LATEST_OFFSETS;
@@ -49,6 +50,7 @@ import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Supplier;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -86,6 +88,7 @@ class TopicMetadataFetcher implements Closeable {
   private final Map<PubSubTopicPartition, ValueAndExpiryTime<Long>> lastProducerTimestampCache =
       new VeniceConcurrentHashMap<>();
   private final long cachedEntryTtlInNs;
+  private final AtomicInteger consumerWaitListSize = new AtomicInteger(0);
 
   TopicMetadataFetcher(
       String pubSubClusterAddress,
@@ -125,6 +128,8 @@ class TopicMetadataFetcher implements Closeable {
         new DaemonThreadFactory("TopicMetadataFetcherThreadPool"));
     threadPoolExecutor.allowCoreThreadTimeOut(true);
 
+    stats.registerTopicMetadataFetcherSensors(this);
+
     LOGGER.info(
         "Initialized TopicMetadataFetcher for pubSubClusterAddress: {} with consumer pool size: {} and thread pool size: {}",
         pubSubClusterAddress,
@@ -152,10 +157,16 @@ class TopicMetadataFetcher implements Closeable {
   // acquire the consumer from the pool
   private PubSubConsumerAdapter acquireConsumer() {
     try {
-      return pubSubConsumerPool.take();
+      consumerWaitListSize.incrementAndGet();
+      long startTime = System.nanoTime();
+      PubSubConsumerAdapter consumerAdapter = pubSubConsumerPool.take();
+      stats.recordLatency(CONSUMER_ACQUISITION_WAIT_TIME, startTime);
+      return consumerAdapter;
     } catch (InterruptedException e) {
       Thread.currentThread().interrupt();
       throw new VeniceException("Interrupted while acquiring pubSubConsumerAdapter", e);
+    } finally {
+      consumerWaitListSize.decrementAndGet();
     }
   }
 
@@ -749,6 +760,22 @@ class TopicMetadataFetcher implements Closeable {
     void setUpdateInProgressStatus(boolean status) {
       this.isUpdateInProgress.set(status);
     }
+  }
+
+  int getCurrentConsumerPoolSize() {
+    return pubSubConsumerPool.size();
+  }
+
+  int getAsyncTaskActiveThreadCount() {
+    return threadPoolExecutor.getActiveCount();
+  }
+
+  int getAsyncTaskQueueLength() {
+    return threadPoolExecutor.getQueue().size();
+  }
+
+  int getConsumerWaitListSize() {
+    return consumerWaitListSize.get();
   }
 
   @Override
