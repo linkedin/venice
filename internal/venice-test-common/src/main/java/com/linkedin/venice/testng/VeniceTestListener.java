5c0e66a203be1e9cfd0c6cad27597035543f06a9
Felix GV <fgv@linkedin.com>
2024-05-15 16:36:09 -0400

    [test] Fix for cross-test interrupt contamination (#986)
    There is an issue either in Gradle or TestNG where a test which
    sets the interrupt flag on the current thread can cause a later
    test to fail, due to checking for the interrupt flag (e.g. in
    sleep and syncrhonization-related primitives).
    
    This commit fixes this by clearing the interrupt flag via the
    VeniceTestListener::onTestStart listener. Also registered this
    listener to unit tests (it was previously used only for integ
    tests), and added a minimal unit test which deterministically
    reproduces this issue (in the absence of the listener).
    
    Miscellaneous:
    
    - Added calls to super in all functions of VeniceTestListener,
      as recommended in the parent class' JavaDoc.

--- internal/venice-test-common/src/main/java/com/linkedin/venice/testng/VeniceTestListener.java
+++ internal/venice-test-common/src/main/java/com/linkedin/venice/testng/VeniceTestListener.java
@@ -11,14 +11,20 @@ public class VeniceTestListener extends TestListenerAdapter {
 
   @Override
   public void onTestStart(ITestResult result) {
+    super.onTestStart(result);
+
+    /** N.B.: {@link Thread#interrupted()} not only retrieves, but also clears, the interrupt flag */
+    boolean interruptCleared = Thread.interrupted();
     LOGGER.info(
-        "\n\n######## TEST ######## {}({}) - STARTED",
+        "\n\n######## TEST ######## {}({}){} - STARTED",
         result.getMethod().getQualifiedName(),
-        result.getParameters());
+        result.getParameters(),
+        interruptCleared ? " (interrupt cleared)" : "");
   }
 
   @Override
   public void onTestSuccess(ITestResult result) {
+    super.onTestSuccess(result);
     LOGGER.info(
         "######## TEST ######## {}({}) - PASSED\n\n",
         result.getMethod().getQualifiedName(),
@@ -27,6 +33,7 @@ public class VeniceTestListener extends TestListenerAdapter {
 
   @Override
   public void onTestFailure(ITestResult result) {
+    super.onTestFailure(result);
     LOGGER.info(
         "######## TEST ######## {}({}) - FAILED\n\n",
         result.getMethod().getQualifiedName(),
@@ -35,6 +42,7 @@ public class VeniceTestListener extends TestListenerAdapter {
 
   @Override
   public void onTestSkipped(ITestResult result) {
+    super.onTestSkipped(result);
     LOGGER.info(
         "######## TEST ######## {}({}) - SKIPPED\n\n",
         result.getMethod().getQualifiedName(),
@@ -43,6 +51,7 @@ public class VeniceTestListener extends TestListenerAdapter {
 
   @Override
   public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+    super.onTestFailedButWithinSuccessPercentage(result);
     LOGGER.info(
         "######## TEST ######## {}({}) - FLAKY\n\n",
         result.getMethod().getQualifiedName(),
