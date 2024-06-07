c67ca952b44ea58a1f15c1bade8c0a76116d5ca7
Xin(Adam) Chen <adchen@linkedin.com>
2024-06-07 14:52:07 -0700

    add version to the blob manager interface

--- internal/venice-common/src/main/java/com/linkedin/venice/blobtransfer/P2PBlobTransferManager.java
+++ internal/venice-common/src/main/java/com/linkedin/venice/blobtransfer/P2PBlobTransferManager.java
@@ -10,13 +10,15 @@ import java.util.concurrent.CompletionStage;
  */
 public interface P2PBlobTransferManager<T> extends BlobTransferManager<T> {
   /**
-   * For P2P blob transfer, only GET should be implemented and there's no need to implement PUT method by default.
-   * It's subject to change depending on the use cases to use P2P blob transfer.
+   * For P2P blob transfer, only GET should be implemented and there's no need to implement PUT method by default. It's
+   * subject to change depending on the use cases to use P2P blob transfer.
+   *
    * @param storeName
+   * @param version
    * @param partition
    * @return the type of the object returned from the underlying blob client to indicate the upload status
    */
-  default CompletionStage<T> put(String storeName, int partition) {
+  default CompletionStage<T> put(String storeName, int version, int partition) {
     throw new VeniceException("PUT method is not supported for P2P Blob Transfer");
   }
 }
