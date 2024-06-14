57a6058b5748d1180d79faf6d669eeac0ffe1f48
Sebastian Infante Murguia <smurguia@linkedin.com>
2024-06-14 09:50:09 -0700

    [server][davinci] Created interface for Blob dicovery and added a server implementation

---
+++ clients/da-vinci-client/src/main/java/com/linkedin/davinci/blob/ServerBlobFinder.java
@@ -0,0 +1,54 @@
+package com.linkedin.davinci.blob;
+
+import com.linkedin.venice.exceptions.VeniceException;
+import com.linkedin.venice.helix.HelixCustomizedViewOfflinePushRepository;
+import com.linkedin.venice.meta.Instance;
+import com.linkedin.venice.meta.Partition;
+import com.linkedin.venice.meta.Version;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+public class ServerBlobFinder implements BlobFinder {
+  private final HelixCustomizedViewOfflinePushRepository customizedViewRepository;
+
+  private static final Logger LOGGER = LogManager.getLogger(ServerBlobFinder.class);
+
+  public ServerBlobFinder(CompletableFuture<HelixCustomizedViewOfflinePushRepository> customizedViewRepository) {
+    this.customizedViewRepository = customizedViewRepository.join();
+  }
+
+  @Override
+  public BlobResponse findBlob(String storeName, int version, int partitionId) {
+    BlobResponse response = new BlobResponse();
+    try {
+      String currentVersionResource = Version.composeKafkaTopic(storeName, version);
+      // Get the partition assignments for the current version
+      List<String> partitionValues = new ArrayList<>();
+      for (Partition partition: customizedViewRepository.getPartitionAssignments(currentVersionResource)
+          .getAllPartitions()) {
+        if (partition.getId() == partitionId) {
+          for (Instance instance: partition.getReadyToServeInstances()) {
+            String host = instance.getHost();
+            partitionValues.add(host);
+          }
+          break;
+        }
+      }
+      response.setPartitionValues(partitionValues);
+    } catch (VeniceException e) {
+      response.setError(true);
+      String errorMsg =
+          "Error finding blob for store: " + storeName + ", version: " + version + ", partitionId: " + partitionId;
+      response.setMessage(errorMsg + ".\n Error: " + e.getMessage());
+      LOGGER.warn(errorMsg, e);
+
+    }
+
+    return response;
+  }
+
+}
