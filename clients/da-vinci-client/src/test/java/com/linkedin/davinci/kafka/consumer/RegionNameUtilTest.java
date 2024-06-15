688a97868598863cab05b308a56fa6c9a10a3716
Sushant Mane <sumane@linkedin.com>
2024-06-15 05:48:06 -0700

    [server] Log region name from which the record was originated

---
+++ clients/da-vinci-client/src/test/java/com/linkedin/davinci/kafka/consumer/RegionNameUtilTest.java
@@ -0,0 +1,35 @@
+package com.linkedin.davinci.kafka.consumer;
+
+import static org.testng.Assert.assertEquals;
+
+import com.linkedin.venice.kafka.protocol.LeaderMetadata;
+import com.linkedin.venice.utils.PubSubHelper;
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
+import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
+import org.testng.annotations.Test;
+
+
+public class RegionNameUtilTest {
+  @Test
+  public void testGetRegionName() {
+    Int2ObjectMap<String> regionIdToNameMap = new Int2ObjectOpenHashMap<>();
+    regionIdToNameMap.put(0, "region0");
+    regionIdToNameMap.put(1, "region1");
+    regionIdToNameMap.put(2, "region2");
+
+    PubSubHelper.MutablePubSubMessage message = PubSubHelper.getDummyPubSubMessage(false);
+    message.getValue().leaderMetadataFooter = new LeaderMetadata();
+
+    message.getValue().leaderMetadataFooter.upstreamKafkaClusterId = 0;
+    assertEquals(RegionNameUtil.getRegionName(message, regionIdToNameMap), "region0");
+
+    message.getValue().leaderMetadataFooter.upstreamKafkaClusterId = 1;
+    assertEquals(RegionNameUtil.getRegionName(message, regionIdToNameMap), "region1");
+
+    message.getValue().leaderMetadataFooter.upstreamKafkaClusterId = -1;
+    assertEquals(RegionNameUtil.getRegionName(message, regionIdToNameMap), "");
+
+    assertEquals(RegionNameUtil.getRegionName(null, regionIdToNameMap), "");
+    assertEquals(RegionNameUtil.getRegionName(message, null), "");
+  }
+}
