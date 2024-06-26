0ddf4f2e116cd8b14c553b6056abc718c73725a0
Sebastian Infante Murguia <6dinformaticasinfante@gmail.com>
2024-06-26 10:14:33 -0700

    [client][davinci]Blob Snapshot Manager (#1039)
    * [server][davinci] Created interface for Blob dicovery and added a server implementation
    
    * Created the BlobSnapshotManager that takes care of hybrid snapshots
    
    * Adjusted concurrent users logic and added a test case to test it
    
    * Cleaned up notes
    
    * Implemented Kai's suggestions
    
    * Added looger error for no path found
    
    * Implemented comments
    
    * Implemented comments
    
    * Implemented comments
    
    * Implemented comments

--- internal/venice-common/src/main/java/com/linkedin/venice/blobtransfer/BlobFinder.java
+++ internal/venice-common/src/main/java/com/linkedin/venice/blobtransfer/BlobFinder.java
@@ -1,9 +1,10 @@
 package com.linkedin.venice.blobtransfer;
 
 public interface BlobFinder {
-
-  // This method will look through the partitions for the store and version provided until it finds the partition
-  // requested, it will then return the URL of the instances that are ready to serve in the partition.
+  /**
+   * This method will look through the partitions for the store and version provided until it finds the partition
+   * requested, it will then return the URL of the instances that are ready to serve in the partition.
+   */
   BlobPeersDiscoveryResponse discoverBlobPeers(String storeName, int version, int partitionId);
 
 }
