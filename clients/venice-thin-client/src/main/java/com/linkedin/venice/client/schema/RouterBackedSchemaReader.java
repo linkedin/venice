db752f01bebc9d289609baae8eccbb8460d1de32
Sourav Maji <tester@linkedin.com>
2024-07-17 17:17:31 -0700

    [venice-thin-client] Allow refresh while fetching value from router

--- clients/venice-thin-client/src/main/java/com/linkedin/venice/client/schema/RouterBackedSchemaReader.java
+++ clients/venice-thin-client/src/main/java/com/linkedin/venice/client/schema/RouterBackedSchemaReader.java
@@ -187,7 +187,9 @@ public class RouterBackedSchemaReader implements SchemaReader {
 
   @Override
   public Schema getValueSchema(int id) {
-    SchemaEntry valueSchemaEntry = maybeFetchValueSchemaEntryById(id, false);
+    // Should call with refresh as any router transient error can lead to null schema stored in the map
+    // `valueSchemaEntryMap`
+    SchemaEntry valueSchemaEntry = maybeFetchValueSchemaEntryById(id, true);
     if (!isValidSchemaEntry(valueSchemaEntry)) {
       LOGGER.warn("Got null value schema from Venice for store: {} and id: {}", storeName, id);
       return null;
@@ -663,8 +665,8 @@ public class RouterBackedSchemaReader implements SchemaReader {
 
       response = RetryUtils.executeWithMaxAttempt(
           () -> (responseFuture.get()),
-          3,
-          Duration.ofNanos(1),
+          5,
+          Duration.ofMillis(100),
           Collections.singletonList(ExecutionException.class));
     } catch (Exception e) {
       throw new VeniceClientException(
