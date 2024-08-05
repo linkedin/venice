fa9c5c3084bf1cbbc528428c86eeea4f46196fe7
Gaojie Liu <goliu@linkedin.com>
2024-08-05 10:21:23 -0700

    Fixed a bug in DaVinci bootstrapping mode

--- clients/da-vinci-client/src/main/java/com/linkedin/davinci/kafka/consumer/StoreIngestionTask.java
+++ clients/da-vinci-client/src/main/java/com/linkedin/davinci/kafka/consumer/StoreIngestionTask.java
@@ -4062,7 +4062,7 @@ public abstract class StoreIngestionTask implements Runnable, Closeable {
 
   public boolean hasAllPartitionReportedCompleted() {
     for (Map.Entry<Integer, PartitionConsumptionState> entry: partitionConsumptionStateMap.entrySet()) {
-      if (entry.getValue().isCompletionReported()) {
+      if (!entry.getValue().isCompletionReported()) {
         return false;
       }
     }
