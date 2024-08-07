b9f02e8cd196118f06865759c5e372d9bb1056e6
Koorous Vargha <kvargha@linkedin.com>
2024-08-07 10:46:19 -0700

    Don't Lazily init some dvc metrics

--- clients/da-vinci-client/src/main/java/com/linkedin/davinci/stats/StuckConsumerRepairStats.java
+++ clients/da-vinci-client/src/main/java/com/linkedin/davinci/stats/StuckConsumerRepairStats.java
@@ -1,34 +1,33 @@
 package com.linkedin.davinci.stats;
 
 import com.linkedin.venice.stats.AbstractVeniceStats;
-import com.linkedin.venice.utils.lazy.Lazy;
 import io.tehuti.metrics.MetricsRepository;
 import io.tehuti.metrics.Sensor;
 import io.tehuti.metrics.stats.OccurrenceRate;
 
 
 public class StuckConsumerRepairStats extends AbstractVeniceStats {
-  private Lazy<Sensor> stuckConsumerFound;
-  private Lazy<Sensor> ingestionTaskRepair;
-  private Lazy<Sensor> repairFailure;
+  private Sensor stuckConsumerFound;
+  private Sensor ingestionTaskRepair;
+  private Sensor repairFailure;
 
   public StuckConsumerRepairStats(MetricsRepository metricsRepository) {
     super(metricsRepository, "StuckConsumerRepair");
 
-    this.stuckConsumerFound = Lazy.of(() -> registerSensor("stuck_consumer_found", new OccurrenceRate()));
-    this.ingestionTaskRepair = Lazy.of(() -> registerSensor("ingestion_task_repair", new OccurrenceRate()));
-    this.repairFailure = Lazy.of(() -> registerSensor("repair_failure", new OccurrenceRate()));
+    this.stuckConsumerFound = registerSensor("stuck_consumer_found", new OccurrenceRate());
+    this.ingestionTaskRepair = registerSensor("ingestion_task_repair", new OccurrenceRate());
+    this.repairFailure = registerSensor("repair_failure", new OccurrenceRate());
   }
 
   public void recordStuckConsumerFound() {
-    stuckConsumerFound.get().record();
+    stuckConsumerFound.record();
   }
 
   public void recordIngestionTaskRepair() {
-    ingestionTaskRepair.get().record();
+    ingestionTaskRepair.record();
   }
 
   public void recordRepairFailure() {
-    repairFailure.get().record();
+    repairFailure.record();
   }
 }
