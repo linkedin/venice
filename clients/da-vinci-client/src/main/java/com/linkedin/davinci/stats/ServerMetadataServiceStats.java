1650f931e446433f402b041f437de051ad088b2c
Koorous Vargha <kvargha@linkedin.com>
2024-08-07 19:19:32 -0700

    Don't Lazily init some dvc metrics

--- clients/da-vinci-client/src/main/java/com/linkedin/davinci/stats/ServerMetadataServiceStats.java
+++ clients/da-vinci-client/src/main/java/com/linkedin/davinci/stats/ServerMetadataServiceStats.java
@@ -1,7 +1,6 @@
 package com.linkedin.davinci.stats;
 
 import com.linkedin.venice.stats.AbstractVeniceStats;
-import com.linkedin.venice.utils.lazy.Lazy;
 import io.tehuti.metrics.MetricsRepository;
 import io.tehuti.metrics.Sensor;
 import io.tehuti.metrics.stats.Rate;
@@ -11,27 +10,25 @@ public class ServerMetadataServiceStats extends AbstractVeniceStats {
   /**
    * Measure the number of time request based metadata endpoint was invoked
    */
-  private final Lazy<Sensor> requestBasedMetadataInvokeCount;
+  private final Sensor requestBasedMetadataInvokeCount;
 
   /**
    * Measure the number of time request based metadata endpoint failed to respond
    */
-  private final Lazy<Sensor> requestBasedMetadataFailureCount;
+  private final Sensor requestBasedMetadataFailureCount;
 
   public ServerMetadataServiceStats(MetricsRepository metricsRepository) {
     super(metricsRepository, "ServerMetadataStats");
 
-    this.requestBasedMetadataInvokeCount =
-        Lazy.of(() -> registerSensorIfAbsent("request_based_metadata_invoke_count", new Rate()));
-    this.requestBasedMetadataFailureCount =
-        Lazy.of(() -> registerSensorIfAbsent("request_based_metadata_failure_count", new Rate()));
+    this.requestBasedMetadataInvokeCount = registerSensorIfAbsent("request_based_metadata_invoke_count", new Rate());
+    this.requestBasedMetadataFailureCount = registerSensorIfAbsent("request_based_metadata_failure_count", new Rate());
   }
 
   public void recordRequestBasedMetadataInvokeCount() {
-    requestBasedMetadataInvokeCount.get().record();
+    requestBasedMetadataInvokeCount.record();
   }
 
   public void recordRequestBasedMetadataFailureCount() {
-    requestBasedMetadataFailureCount.get().record();
+    requestBasedMetadataFailureCount.record();
   }
 }
