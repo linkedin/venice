d17737b592d71906bae0e5e471560a22217d5cb5
Koorous Vargha <kvargha@linkedin.com>
2024-08-07 18:20:44 -0700

    Lazily init some dvc metrics

--- clients/da-vinci-client/src/main/java/com/linkedin/davinci/stats/KafkaConsumerServiceStats.java
+++ clients/da-vinci-client/src/main/java/com/linkedin/davinci/stats/KafkaConsumerServiceStats.java
@@ -3,6 +3,7 @@ package com.linkedin.davinci.stats;
 import com.linkedin.venice.stats.AbstractVeniceStats;
 import com.linkedin.venice.stats.LongAdderRateGauge;
 import com.linkedin.venice.utils.Time;
+import com.linkedin.venice.utils.lazy.Lazy;
 import io.tehuti.metrics.MetricsRepository;
 import io.tehuti.metrics.Sensor;
 import io.tehuti.metrics.stats.AsyncGauge;
@@ -24,25 +25,25 @@ import java.util.function.LongSupplier;
 
 public class KafkaConsumerServiceStats extends AbstractVeniceStats {
   private final LongAdderRateGauge pollRequestSensor;
-  private final Sensor pollRequestLatencySensor;
+  private final Lazy<Sensor> pollRequestLatencySensor;
   private final Sensor pollResultNumSensor;
   private final LongAdderRateGauge pollNonZeroResultNumSensor;
 
-  private final Sensor pollRequestError;
-  private final Sensor consumerRecordsProducingToWriterBufferLatencySensor;
-  private final Sensor detectedDeletedTopicNumSensor;
-  private final Sensor detectedNoRunningIngestionTopicPartitionNumSensor;
-  private final Sensor delegateSubscribeLatencySensor;
-  private final Sensor updateCurrentAssignmentLatencySensor;
-  private final Sensor maxPartitionsPerConsumer;
-  private final Sensor minPartitionsPerConsumer;
-  private final Sensor avgPartitionsPerConsumer;
-  private final Sensor getOffsetLagIsAbsentSensor;
-  private final Sensor getOffsetLagIsPresentSensor;
-  private final Sensor getLatestOffsetIsAbsentSensor;
-  private final Sensor getLatestOffsetIsPresentSensor;
+  private final Lazy<Sensor> pollRequestError;
+  private final Lazy<Sensor> consumerRecordsProducingToWriterBufferLatencySensor;
+  private final Lazy<Sensor> detectedDeletedTopicNumSensor;
+  private final Lazy<Sensor> detectedNoRunningIngestionTopicPartitionNumSensor;
+  private final Lazy<Sensor> delegateSubscribeLatencySensor;
+  private final Lazy<Sensor> updateCurrentAssignmentLatencySensor;
+  private final Lazy<Sensor> maxPartitionsPerConsumer;
+  private final Lazy<Sensor> minPartitionsPerConsumer;
+  private final Lazy<Sensor> avgPartitionsPerConsumer;
+  private final Lazy<Sensor> getOffsetLagIsAbsentSensor;
+  private final Lazy<Sensor> getOffsetLagIsPresentSensor;
+  private final Lazy<Sensor> getLatestOffsetIsAbsentSensor;
+  private final Lazy<Sensor> getLatestOffsetIsPresentSensor;
   private final Sensor byteSizeSensor;
-  private final Sensor idleTimeSensor;
+  private final Lazy<Sensor> idleTimeSensor;
 
   public KafkaConsumerServiceStats(
       MetricsRepository metricsRepository,
@@ -71,12 +72,12 @@ public class KafkaConsumerServiceStats extends AbstractVeniceStats {
      */
 
     // the consumer idle time
-    idleTimeSensor = registerSensor("idle_time", new Max());
+    idleTimeSensor = Lazy.of(() -> registerSensor("idle_time", new Max()));
     // the number of poll requests
     pollRequestSensor =
         registerOnlyTotalRate("consumer_poll_request", totalStats, () -> totalStats.pollRequestSensor, time);
     // Notice that "pollRequestLatencySensor" only reports correct data when consumer task threads are not stuck
-    pollRequestLatencySensor = registerSensor("consumer_poll_request_latency", new Avg(), new Max());
+    pollRequestLatencySensor = Lazy.of(() -> registerSensor("consumer_poll_request_latency", new Avg(), new Max()));
     pollNonZeroResultNumSensor = registerOnlyTotalRate(
         "consumer_poll_non_zero_result_num",
         totalStats,
@@ -93,36 +94,39 @@ public class KafkaConsumerServiceStats extends AbstractVeniceStats {
             "max_elapsed_time_since_last_successful_poll"));
     // consumer record number per second returned by Kafka consumer poll.
 
-    pollRequestError = registerSensor("consumer_poll_error", new OccurrenceRate());
+    pollRequestError = Lazy.of(() -> registerSensor("consumer_poll_error", new OccurrenceRate()));
     // To measure 'put' latency of consumer records blocking queue
     consumerRecordsProducingToWriterBufferLatencySensor =
-        registerSensor("consumer_records_producing_to_write_buffer_latency", new Avg(), new Max());
-    detectedDeletedTopicNumSensor = registerSensor("detected_deleted_topic_num", new Total());
+        Lazy.of(() -> registerSensor("consumer_records_producing_to_write_buffer_latency", new Avg(), new Max()));
+    detectedDeletedTopicNumSensor = Lazy.of(() -> registerSensor("detected_deleted_topic_num", new Total()));
     detectedNoRunningIngestionTopicPartitionNumSensor =
-        registerSensor("detected_no_running_ingestion_topic_partition_num", new Total());
-    delegateSubscribeLatencySensor = registerSensor("delegate_subscribe_latency", new Avg(), new Max());
-    updateCurrentAssignmentLatencySensor = registerSensor("update_current_assignment_latency", new Avg(), new Max());
+        Lazy.of(() -> registerSensor("detected_no_running_ingestion_topic_partition_num", new Total()));
+    delegateSubscribeLatencySensor = Lazy.of(() -> registerSensor("delegate_subscribe_latency", new Avg(), new Max()));
+    updateCurrentAssignmentLatencySensor =
+        Lazy.of(() -> registerSensor("update_current_assignment_latency", new Avg(), new Max()));
 
-    minPartitionsPerConsumer = registerSensor("min_partitions_per_consumer", new Gauge());
-    maxPartitionsPerConsumer = registerSensor("max_partitions_per_consumer", new Gauge());
-    avgPartitionsPerConsumer = registerSensor("avg_partitions_per_consumer", new Gauge());
+    minPartitionsPerConsumer = Lazy.of(() -> registerSensor("min_partitions_per_consumer", new Gauge()));
+    maxPartitionsPerConsumer = Lazy.of(() -> registerSensor("max_partitions_per_consumer", new Gauge()));
+    avgPartitionsPerConsumer = Lazy.of(() -> registerSensor("avg_partitions_per_consumer", new Gauge()));
 
-    Sensor getOffsetLagSensor = registerSensor("getOffsetLag", new OccurrenceRate());
-    Sensor[] offsetLagParent = new Sensor[] { getOffsetLagSensor };
-    this.getOffsetLagIsAbsentSensor = registerSensor("getOffsetLagIsAbsent", offsetLagParent, new OccurrenceRate());
-    this.getOffsetLagIsPresentSensor = registerSensor("getOffsetLagIsPresent", offsetLagParent, new OccurrenceRate());
+    Lazy<Sensor[]> offsetLagParent =
+        Lazy.of(() -> new Sensor[] { registerSensor("getOffsetLag", new OccurrenceRate()) });
+    this.getOffsetLagIsAbsentSensor =
+        Lazy.of(() -> registerSensor("getOffsetLagIsAbsent", offsetLagParent.get(), new OccurrenceRate()));
+    this.getOffsetLagIsPresentSensor =
+        Lazy.of(() -> registerSensor("getOffsetLagIsPresent", offsetLagParent.get(), new OccurrenceRate()));
 
-    Sensor getLatestOffsetSensor = registerSensor("getLatestOffset", new OccurrenceRate());
-    Sensor[] latestOffsetParent = new Sensor[] { getLatestOffsetSensor };
+    Lazy<Sensor[]> latestOffsetParent =
+        Lazy.of(() -> new Sensor[] { registerSensor("getLatestOffset", new OccurrenceRate()) });
     this.getLatestOffsetIsAbsentSensor =
-        registerSensor("getLatestOffsetIsAbsent", latestOffsetParent, new OccurrenceRate());
+        Lazy.of(() -> registerSensor("getLatestOffsetIsAbsent", latestOffsetParent.get(), new OccurrenceRate()));
     this.getLatestOffsetIsPresentSensor =
-        registerSensor("getLatestOffsetIsPresent", latestOffsetParent, new OccurrenceRate());
+        Lazy.of(() -> registerSensor("getLatestOffsetIsPresent", latestOffsetParent.get(), new OccurrenceRate()));
   }
 
   public void recordPollRequestLatency(double latency) {
     pollRequestSensor.record();
-    pollRequestLatencySensor.record(latency);
+    pollRequestLatencySensor.get().record(latency);
   }
 
   public void recordPollResultNum(int count) {
@@ -134,55 +138,55 @@ public class KafkaConsumerServiceStats extends AbstractVeniceStats {
   }
 
   public void recordConsumerRecordsProducingToWriterBufferLatency(double latency) {
-    consumerRecordsProducingToWriterBufferLatencySensor.record(latency);
+    consumerRecordsProducingToWriterBufferLatencySensor.get().record(latency);
   }
 
   public void recordPollError() {
-    pollRequestError.record();
+    pollRequestError.get().record();
   }
 
   public void recordDetectedDeletedTopicNum(int count) {
-    detectedDeletedTopicNumSensor.record(count);
+    detectedDeletedTopicNumSensor.get().record(count);
   }
 
   public void recordDetectedNoRunningIngestionTopicPartitionNum(int count) {
-    detectedNoRunningIngestionTopicPartitionNumSensor.record(count);
+    detectedNoRunningIngestionTopicPartitionNumSensor.get().record(count);
   }
 
   public void recordDelegateSubscribeLatency(double value) {
-    delegateSubscribeLatencySensor.record(value);
+    delegateSubscribeLatencySensor.get().record(value);
   }
 
   public void recordUpdateCurrentAssignmentLatency(double value) {
-    updateCurrentAssignmentLatencySensor.record(value);
+    updateCurrentAssignmentLatencySensor.get().record(value);
   }
 
   public void recordMinPartitionsPerConsumer(int count) {
-    minPartitionsPerConsumer.record(count);
+    minPartitionsPerConsumer.get().record(count);
   }
 
   public void recordMaxPartitionsPerConsumer(int count) {
-    maxPartitionsPerConsumer.record(count);
+    maxPartitionsPerConsumer.get().record(count);
   }
 
   public void recordAvgPartitionsPerConsumer(int count) {
-    avgPartitionsPerConsumer.record(count);
+    avgPartitionsPerConsumer.get().record(count);
   }
 
   public void recordOffsetLagIsAbsent() {
-    getOffsetLagIsAbsentSensor.record();
+    getOffsetLagIsAbsentSensor.get().record();
   }
 
   public void recordOffsetLagIsPresent() {
-    getOffsetLagIsPresentSensor.record();
+    getOffsetLagIsPresentSensor.get().record();
   }
 
   public void recordLatestOffsetIsAbsent() {
-    getLatestOffsetIsAbsentSensor.record();
+    getLatestOffsetIsAbsentSensor.get().record();
   }
 
   public void recordLatestOffsetIsPresent() {
-    getLatestOffsetIsPresentSensor.record();
+    getLatestOffsetIsPresentSensor.get().record();
   }
 
   public void recordByteSizePerPoll(double count) {
@@ -190,6 +194,6 @@ public class KafkaConsumerServiceStats extends AbstractVeniceStats {
   }
 
   public void recordConsumerIdleTime(double time) {
-    idleTimeSensor.record(time);
+    idleTimeSensor.get().record(time);
   }
 }
