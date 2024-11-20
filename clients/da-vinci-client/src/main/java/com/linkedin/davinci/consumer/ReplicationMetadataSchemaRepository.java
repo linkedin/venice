ed14d0a3495a7b10f6eb4d3241c4d7138773b06f
Zac Policzer <zacattackftw@gmail.com>
2024-11-19 17:41:24 -0800

    [common][client] Fix metadata schema fetcher cache to use map instead of list
    It seems the current schema cache implementation is prone to bugs if there are gaps in the cache or if the number of elements goes to double digits.  This fixes it by using a map instead.

--- clients/da-vinci-client/src/main/java/com/linkedin/davinci/consumer/ReplicationMetadataSchemaRepository.java
+++ clients/da-vinci-client/src/main/java/com/linkedin/davinci/consumer/ReplicationMetadataSchemaRepository.java
@@ -4,36 +4,37 @@ import com.linkedin.venice.controllerapi.ControllerClient;
 import com.linkedin.venice.controllerapi.MultiSchemaResponse;
 import com.linkedin.venice.exceptions.VeniceException;
 import com.linkedin.venice.schema.rmd.RmdSchemaEntry;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 
 public class ReplicationMetadataSchemaRepository {
   private final ControllerClient controllerClient;
 
-  private List<RmdSchemaEntry> cachedReplicationMetadataSchemas = new ArrayList<>();
+  private Map<Integer, RmdSchemaEntry> cachedReplicationMetadataSchemas = new ConcurrentHashMap<>();
 
   public ReplicationMetadataSchemaRepository(ControllerClient controllerClient) {
     this.controllerClient = controllerClient;
   }
 
   public RmdSchemaEntry getReplicationMetadataSchemaById(String storeName, int replicationMetadataSchemaId) {
-    if (cachedReplicationMetadataSchemas.size() < replicationMetadataSchemaId) {
+    if (!cachedReplicationMetadataSchemas.containsKey(replicationMetadataSchemaId)) {
       MultiSchemaResponse multiReplicationSchemaResponse = controllerClient.getAllReplicationMetadataSchemas(storeName);
       if (multiReplicationSchemaResponse.isError()) {
         throw new VeniceException(
             "Failed to get store replication info for store: " + storeName + " with error: "
                 + multiReplicationSchemaResponse.getError());
       }
-      cachedReplicationMetadataSchemas = Arrays.stream(multiReplicationSchemaResponse.getSchemas())
-          .map(schema -> new RmdSchemaEntry(schema.getRmdValueSchemaId(), schema.getId(), schema.getSchemaStr()))
-          .collect(Collectors.toList());
-      if (cachedReplicationMetadataSchemas.size() < replicationMetadataSchemaId) {
+
+      for (MultiSchemaResponse.Schema schema: multiReplicationSchemaResponse.getSchemas()) {
+        cachedReplicationMetadataSchemas.putIfAbsent(
+            schema.getId(),
+            new RmdSchemaEntry(schema.getRmdValueSchemaId(), schema.getId(), schema.getSchemaStr()));
+      }
+      if (!cachedReplicationMetadataSchemas.containsKey(replicationMetadataSchemaId)) {
         throw new VeniceException("No available store replication metadata schema for store: " + storeName);
       }
     }
-    return cachedReplicationMetadataSchemas.get(replicationMetadataSchemaId - 1);
+    return cachedReplicationMetadataSchemas.get(replicationMetadataSchemaId);
   }
 }
