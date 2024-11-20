28f18877d9867d673ff19c9e401d158b1eddf96e
Zac Policzer <zacattackftw@gmail.com>
2024-11-20 11:29:53 -0800

    switch to sparse list

--- clients/da-vinci-client/src/main/java/com/linkedin/davinci/consumer/ReplicationMetadataSchemaRepository.java
+++ clients/da-vinci-client/src/main/java/com/linkedin/davinci/consumer/ReplicationMetadataSchemaRepository.java
@@ -4,21 +4,20 @@ import com.linkedin.venice.controllerapi.ControllerClient;
 import com.linkedin.venice.controllerapi.MultiSchemaResponse;
 import com.linkedin.venice.exceptions.VeniceException;
 import com.linkedin.venice.schema.rmd.RmdSchemaEntry;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import com.linkedin.venice.utils.SparseConcurrentList;
 
 
 public class ReplicationMetadataSchemaRepository {
   private final ControllerClient controllerClient;
 
-  private Map<Integer, RmdSchemaEntry> cachedReplicationMetadataSchemas = new ConcurrentHashMap<>();
+  private SparseConcurrentList<RmdSchemaEntry> cachedReplicationMetadataSchemas = new SparseConcurrentList<>();
 
   public ReplicationMetadataSchemaRepository(ControllerClient controllerClient) {
     this.controllerClient = controllerClient;
   }
 
   public RmdSchemaEntry getReplicationMetadataSchemaById(String storeName, int replicationMetadataSchemaId) {
-    if (!cachedReplicationMetadataSchemas.containsKey(replicationMetadataSchemaId)) {
+    if (!cachedReplicationMetadataSchemas.contains(replicationMetadataSchemaId)) {
       MultiSchemaResponse multiReplicationSchemaResponse = controllerClient.getAllReplicationMetadataSchemas(storeName);
       if (multiReplicationSchemaResponse.isError()) {
         throw new VeniceException(
@@ -31,7 +30,7 @@ public class ReplicationMetadataSchemaRepository {
             schema.getRmdValueSchemaId(),
             key -> new RmdSchemaEntry(schema.getRmdValueSchemaId(), schema.getId(), schema.getSchemaStr()));
       }
-      if (!cachedReplicationMetadataSchemas.containsKey(replicationMetadataSchemaId)) {
+      if (!cachedReplicationMetadataSchemas.contains(replicationMetadataSchemaId)) {
         throw new VeniceException("No available store replication metadata schema for store: " + storeName);
       }
     }
