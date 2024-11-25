468a673801e693df3fb1678315a6412655e6f03e
Manoj Nagarajan <mnagarajan@linkedin.com>
2024-11-25 12:33:48 -0800

    address review comments

--- internal/venice-client-common/src/main/java/com/linkedin/venice/stats/AbstractVeniceAggStats.java
+++ internal/venice-client-common/src/main/java/com/linkedin/venice/stats/AbstractVeniceAggStats.java
@@ -36,20 +36,26 @@ public abstract class AbstractVeniceAggStats<T extends AbstractVeniceStats> {
     this.totalStats = statsSupplier.get(metricsRepository, STORE_NAME_FOR_TOTAL_STAT, clusterName, null);
   }
 
+  /**
+   * clusterName is used to create per cluster aggregate stats and {@link com.linkedin.venice.stats.dimensions.VeniceMetricsDimensions} <br>
+   * If perClusterAggregate is true, it will create per cluster aggregates with storeName as "total.<clusterName>"
+   */
   public AbstractVeniceAggStats(
       String clusterName,
       MetricsRepository metricsRepository,
       StatsSupplier<T> statsSupplier,
       boolean perClusterAggregate) {
-    this(
-        clusterName,
+    if (perClusterAggregate && clusterName == null) {
+      throw new IllegalArgumentException("perClusterAggregate cannot be true when clusterName is null");
+    }
+    this.clusterName = clusterName;
+    this.metricsRepository = metricsRepository;
+    this.statsFactory = statsSupplier;
+    this.totalStats = statsSupplier.get(
         metricsRepository,
-        statsSupplier,
-        statsSupplier.get(
-            metricsRepository,
-            perClusterAggregate ? STORE_NAME_FOR_TOTAL_STAT + "." + clusterName : STORE_NAME_FOR_TOTAL_STAT,
-            clusterName,
-            null));
+        perClusterAggregate ? STORE_NAME_FOR_TOTAL_STAT + "." + clusterName : STORE_NAME_FOR_TOTAL_STAT,
+        clusterName,
+        null);
   }
 
   public T getStoreStats(String storeName) {
