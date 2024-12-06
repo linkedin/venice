b520ab8d2cbb1f2ec302a88da74ff96a5c3d76da
Kristy Lee <18691669+kristyelee@users.noreply.github.com>
2024-12-06 12:35:46 -0800

    Integration Test [In Writing]

--- internal/venice-test-common/src/integrationTest/java/com/linkedin/venice/endToEnd/DaVinciClientTest.java
+++ internal/venice-test-common/src/integrationTest/java/com/linkedin/venice/endToEnd/DaVinciClientTest.java
@@ -1284,20 +1284,111 @@ public class DaVinciClientTest {
   }
 
   @Test(timeOut = TEST_TIMEOUT, dataProvider = "dv-client-config-provider", dataProviderClass = DataProviderUtils.class)
-  public void testBootstrapSubscription(DaVinciConfig daVinciConfig) throws Exception {
-    String storeName = createStoreWithMetaSystemStoreAndPushStatusSystemStore(KEY_COUNT);
+  public void testBootstrapSubscription(DaVinciConfig clientConfig) throws Exception {
+    String storeName1 = createStoreWithMetaSystemStoreAndPushStatusSystemStore(KEY_COUNT);
+    String storeName2 = createStoreWithMetaSystemStoreAndPushStatusSystemStore(KEY_COUNT);
+    String storeName3 = createStoreWithMetaSystemStoreAndPushStatusSystemStore(KEY_COUNT);
+    String baseDataPath = Utils.getTempDataDirectory().getAbsolutePath();
+    VeniceProperties backendConfig = new PropertyBuilder().put(CLIENT_USE_SYSTEM_STORE_REPOSITORY, true)
+        .put(CLIENT_SYSTEM_STORE_REPOSITORY_REFRESH_INTERVAL_SECONDS, 1)
+        .put(DATA_BASE_PATH, baseDataPath)
+        .put(PERSISTENCE_TYPE, ROCKS_DB)
+        .put(DA_VINCI_CURRENT_VERSION_BOOTSTRAPPING_SPEEDUP_ENABLED, true)
+        .put(PUSH_STATUS_STORE_ENABLED, true)
+        .put(DAVINCI_PUSH_STATUS_CHECK_INTERVAL_IN_MS, 1000)
+        .build();
 
-    VeniceProperties backendConfig = new PropertyBuilder().build();
+    MetricsRepository metricsRepository = new MetricsRepository();
 
-    Set<Integer> keySet = new HashSet<>();
-    for (int i = 0; i < KEY_COUNT; ++i) {
-      keySet.add(i);
+    // Test multiple clients sharing the same ClientConfig/MetricsRepository & base data path
+    try (CachingDaVinciClientFactory factory = new CachingDaVinciClientFactory(
+        d2Client,
+        VeniceRouterWrapper.CLUSTER_DISCOVERY_D2_SERVICE_NAME,
+        metricsRepository,
+        backendConfig)) {
+      DaVinciClient<Integer, Object> client1 = factory.getAndStartGenericAvroClient(storeName1, clientConfig);
+
+      // Test non-existent key access
+      client1.subscribeAll().get();
+      assertNull(client1.get(KEY_COUNT + 1).get());
+
+      // Test single-get access
+      Map<Integer, Integer> keyValueMap = new HashMap<>();
+      for (int k = 0; k < KEY_COUNT; ++k) {
+        assertEquals(client1.get(k).get(), 1);
+        keyValueMap.put(k, 1);
+      }
+
+      // Test batch-get access
+      assertEquals(client1.batchGet(keyValueMap.keySet()).get(), keyValueMap);
+
+      // Test automatic new version ingestion
+      for (int i = 0; i < 2; ++i) {
+        // Test per-version partitioning parameters
+        int partitionCount = i + 1;
+        String iString = String.valueOf(i);
+        cluster.useControllerClient(controllerClient -> {
+          ControllerResponse response = controllerClient.updateStore(
+              storeName1,
+              new UpdateStoreQueryParams().setPartitionerClass(ConstantVenicePartitioner.class.getName())
+                  .setPartitionCount(partitionCount)
+                  .setPartitionerParams(
+                      Collections.singletonMap(ConstantVenicePartitioner.CONSTANT_PARTITION, iString)));
+          assertFalse(response.isError(), response.getError());
+        });
+
+        Integer expectedValue = cluster.createVersion(storeName1, KEY_COUNT);
+        TestUtils.waitForNonDeterministicAssertion(TEST_TIMEOUT, TimeUnit.MILLISECONDS, () -> {
+          for (int k = 0; k < KEY_COUNT; ++k) {
+            Object readValue = client1.get(k).get();
+            assertEquals(readValue, expectedValue);
+          }
+        });
+      }
+
+      // Test multiple client ingesting different stores concurrently
+      DaVinciClient<Integer, Integer> client2 = factory.getAndStartGenericAvroClient(storeName2, clientConfig);
+      DaVinciClient<Integer, Integer> client3 = factory.getAndStartGenericAvroClient(storeName3, clientConfig);
+      CompletableFuture.allOf(client2.subscribeAll(), client3.subscribeAll()).get();
+      assertEquals(client2.batchGet(keyValueMap.keySet()).get(), keyValueMap);
+      assertEquals(client3.batchGet(keyValueMap.keySet()).get(), keyValueMap);
+
+      // TODO(jlliu): Re-enable this test-case after fixing store deletion that is flaky due to
+      // CLIENT_USE_SYSTEM_STORE_REPOSITORY.
+      // // Test read from a store that is being deleted concurrently
+      // try (ControllerClient controllerClient = cluster.getControllerClient()) {
+      // ControllerResponse response = controllerClient.disableAndDeleteStore(storeName2);
+      // assertFalse(response.isError(), response.getError());
+      // TestUtils.waitForNonDeterministicAssertion(TEST_TIMEOUT, TimeUnit.MILLISECONDS, () -> {
+      // assertThrows(VeniceClientException.class, () -> client2.get(KEY_COUNT / 3).get());
+      // });
+      // }
+      client2.unsubscribeAll();
     }
 
-    try (DaVinciClient<Integer, Object> client = ServiceFactory
-        .getGenericAvroDaVinciClient(storeName, cluster.getZk().getAddress(), daVinciConfig, backendConfig)) {
-      client.subscribe(Collections.singleton(0)).get();
-      assertThrows(() -> client.batchGet(keySet).get());
+    // Test bootstrap-time junk removal
+    cluster.useControllerClient(controllerClient -> {
+      ControllerResponse response = controllerClient.disableAndDeleteStore(storeName3);
+      assertFalse(response.isError(), response.getError());
+    });
+
+    // Test managed clients & data cleanup
+    try (CachingDaVinciClientFactory factory = new CachingDaVinciClientFactory(
+        d2Client,
+        VeniceRouterWrapper.CLUSTER_DISCOVERY_D2_SERVICE_NAME,
+        new MetricsRepository(),
+        backendConfig,
+        Optional.of(Collections.singleton(storeName1)))) {
+      assertNotEquals(FileUtils.sizeOfDirectory(new File(baseDataPath)), 0);
+
+      DaVinciClient<Integer, Object> client1 = factory.getAndStartGenericAvroClient(storeName1, clientConfig);
+      client1.subscribeAll().get();
+      client1.unsubscribeAll();
+      // client2 was removed explicitly above via disableAndDeleteStore()
+      // client3 is expected to be removed by the factory during bootstrap
+      TestUtils.waitForNonDeterministicAssertion(60, TimeUnit.SECONDS, true, () -> {
+        assertEquals(FileUtils.sizeOfDirectory(new File(baseDataPath)), 0);
+      });
     }
   }
 
