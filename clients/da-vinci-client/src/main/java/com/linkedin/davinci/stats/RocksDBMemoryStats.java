c153a83ea3e5ff56f679ef52c0ac5dfa63e73584
Sourav Maji <tester@linkedin.com>
2025-01-31 15:40:17 -0800

    add new API to access CF metric value

--- clients/da-vinci-client/src/main/java/com/linkedin/davinci/stats/RocksDBMemoryStats.java
+++ clients/da-vinci-client/src/main/java/com/linkedin/davinci/stats/RocksDBMemoryStats.java
@@ -112,14 +112,13 @@ public class RocksDBMemoryStats extends AbstractVeniceStats {
         // Lock down the list of RocksDB interfaces while the collection is ongoing
         synchronized (hostedRocksDBPartitions) {
           for (RocksDBStoragePartition dbPartition: hostedRocksDBPartitions.values()) {
-            if (!(dbPartition instanceof ReplicationMetadataRocksDBStoragePartition)) {
-              continue;
-            }
-            try {
-              total += dbPartition.getRocksDBStatValue(metric);
-            } catch (VeniceException e) {
-              LOGGER.warn("Could not get rocksDB metric {} with error:", metric, e);
-              continue;
+            if (dbPartition instanceof ReplicationMetadataRocksDBStoragePartition) {
+              try {
+                total += dbPartition.getRocksDBCFStatValue(metric);
+              } catch (VeniceException e) {
+                LOGGER.warn("Could not get rocksDB CF metric {} with error:", metric, e);
+                continue;
+              }
             }
             if (INSTANCE_METRIC_DOMAINS.contains(metric)) {
               // Collect this metric once from any available partition and move on
