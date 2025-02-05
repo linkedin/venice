f167714e55c0b8787790dbdcc6a1b07317d2bb2d
Whitney Deng <whdeng@linkedin.com>
2025-02-04 16:17:54 -0800

    [style] save config key strings as configs

--- services/venice-controller/src/main/java/com/linkedin/venice/controller/repush/RepushOrchestratorConfig.java
+++ services/venice-controller/src/main/java/com/linkedin/venice/controller/repush/RepushOrchestratorConfig.java
@@ -5,17 +5,23 @@ import com.linkedin.venice.utils.VeniceProperties;
 
 
 public class RepushOrchestratorConfig {
+  public static final String AIRFLOW_URL_CFG_KEY = "airflowUrl";
+  public static final String DAG_ID_CFG_KEY = "dagId";
+  public static final String AIRFLOW_USERNAME_CFG_KEY = "airflowUsername";
+  public static final String AIRFLOW_PASSWORD_CFG_KEY = "airflowPassword";
+
   private final String airflowUrl;
   private final String airflowUsername;
   private final String airflowPassword;
   private final String dagId;
   private final Client client;
 
+  // TODO: exception here if any of the configs are empty
   public RepushOrchestratorConfig(VeniceProperties repushClusterConfigs, Client client) {
-    this.airflowUrl = repushClusterConfigs.getString("airflowUrl");
-    this.airflowUsername = repushClusterConfigs.getString("airflowUsername");
-    this.airflowPassword = repushClusterConfigs.getString("airflowPassword");
-    this.dagId = repushClusterConfigs.getString("dagId");
+    this.airflowUrl = repushClusterConfigs.getString(AIRFLOW_URL_CFG_KEY);
+    this.airflowUsername = repushClusterConfigs.getString(AIRFLOW_USERNAME_CFG_KEY);
+    this.airflowPassword = repushClusterConfigs.getString(AIRFLOW_PASSWORD_CFG_KEY);
+    this.dagId = repushClusterConfigs.getString(DAG_ID_CFG_KEY);
     this.client = client;
   }
 
