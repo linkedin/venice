8bac2f4de6cfec39a15bc1b169893baffaa127be
Whitney Deng <whdeng@linkedin.com>
2025-02-05 14:22:17 -0800

    [controller][config] remove Client in RepushOrchestratorConfig

--- services/venice-controller/src/main/java/com/linkedin/venice/controller/repush/RepushOrchestratorConfig.java
+++ services/venice-controller/src/main/java/com/linkedin/venice/controller/repush/RepushOrchestratorConfig.java
@@ -1,6 +1,5 @@
 package com.linkedin.venice.controller.repush;
 
-import com.linkedin.r2.transport.common.Client;
 import com.linkedin.venice.utils.VeniceProperties;
 
 
@@ -14,15 +13,13 @@ public class RepushOrchestratorConfig {
   private final String airflowUsername;
   private final String airflowPassword;
   private final String dagId;
-  private final Client client;
 
   // TODO: exception here if any of the configs are empty
-  public RepushOrchestratorConfig(VeniceProperties repushClusterConfigs, Client client) {
+  public RepushOrchestratorConfig(VeniceProperties repushClusterConfigs) {
     this.airflowUrl = repushClusterConfigs.getString(AIRFLOW_URL_CFG_KEY);
     this.airflowUsername = repushClusterConfigs.getString(AIRFLOW_USERNAME_CFG_KEY);
     this.airflowPassword = repushClusterConfigs.getString(AIRFLOW_PASSWORD_CFG_KEY);
     this.dagId = repushClusterConfigs.getString(DAG_ID_CFG_KEY);
-    this.client = client;
   }
 
   public String getAirflowUrl() {
@@ -39,9 +36,5 @@ public class RepushOrchestratorConfig {
 
   public String getDagId() {
     return dagId;
-  }
-
-  public Client getClient() {
-    return client;
   }
 }
