55d765c1dcfc863e86c64a4a316bf9a2b1eb8a57
Whitney Deng <whdeng@linkedin.com>
2025-02-11 11:01:18 -0800

    [pr] [style] readable names

--- services/venice-controller/src/main/java/com/linkedin/venice/controller/repush/RepushJobResponse.java
+++ services/venice-controller/src/main/java/com/linkedin/venice/controller/repush/RepushJobResponse.java
@@ -7,20 +7,19 @@ import com.linkedin.venice.controllerapi.ControllerResponse;
  * Data model of response from a repush job trigger request for a store
  */
 public class RepushJobResponse extends ControllerResponse {
-  private final String execId;
-  private final String execUrl;
+  private final String executionRunId;
+  private final String executionUrl;
 
-  public RepushJobResponse(String execId, String execUrl) {
-    this.execId = execId;
-    this.execUrl = execUrl;
+  public RepushJobResponse(String executionRunId, String executionUrl) {
+    this.executionRunId = executionRunId;
+    this.executionUrl = executionUrl;
   }
 
-  public String getExecId() {
-    return execId;
+  public String getExecutionRunId() {
+    return executionRunId;
   }
 
-  public String getExecUrl() {
-    return execUrl;
+  public String getExecutionUrl() {
+    return executionUrl;
   }
-
 }
