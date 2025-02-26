5c3733f08e8936e591c43348a0bb764b95b6f7ce
Minh Nguyen <minnguyen@linkedin.com>
2025-02-26 15:07:26 -0800

    Update the test for serializer

--- services/venice-controller/src/test/java/com/linkedin/venice/controller/kafka/protocol/admin/AdminOperationSerializerTest.java
+++ services/venice-controller/src/test/java/com/linkedin/venice/controller/kafka/protocol/admin/AdminOperationSerializerTest.java
@@ -3,9 +3,6 @@ package com.linkedin.venice.controller.kafka.protocol.admin;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.anyInt;
 import static org.mockito.Mockito.doCallRealMethod;
-import static org.mockito.Mockito.intThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.mockStatic;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
@@ -16,8 +13,6 @@ import com.linkedin.venice.controller.kafka.protocol.serializer.AdminOperationSe
 import com.linkedin.venice.exceptions.VeniceMessageException;
 import java.nio.ByteBuffer;
 import java.util.Collections;
-import org.apache.avro.Schema;
-import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 import org.testng.annotations.Test;
 
@@ -35,59 +30,45 @@ public class AdminOperationSerializerTest {
 
   @Test
   public void testAdminOperationSerializer() {
-    try (MockedStatic<AdminOperationSerializer> mockedStatic = mockStatic(AdminOperationSerializer.class)) {
-      // For schema IDs <= 0, throw exception.
-      mockedStatic.when(() -> AdminOperationSerializer.getSchema(intThat(arg -> arg <= 0)))
-          .thenThrow(new VeniceMessageException("Admin operation schema version: " + 0 + " doesn't exist"));
+    // Create an AdminOperation object with latest version
+    UpdateStore updateStore = (UpdateStore) AdminMessageType.UPDATE_STORE.getNewInstance();
+    updateStore.clusterName = "clusterName";
+    updateStore.storeName = "storeName";
+    updateStore.owner = "owner";
+    updateStore.partitionNum = 20;
+    updateStore.currentVersion = 1;
+    updateStore.enableReads = true;
+    updateStore.enableWrites = true;
+    updateStore.replicateAllConfigs = true;
+    updateStore.updatedConfigsList = Collections.emptyList();
+    // Purposely set to true. This field doesn't exist in v74, will be dropped during serialization
+    // Default value of this field is False.
+    updateStore.separateRealTimeTopicEnabled = true;
+    AdminOperation adminMessage = new AdminOperation();
+    adminMessage.operationType = AdminMessageType.UPDATE_STORE.getValue();
+    adminMessage.payloadUnion = updateStore;
+    adminMessage.executionId = 1;
 
-      // For schema IDs > 0 and <= 84, call the real method.
-      mockedStatic.when(() -> AdminOperationSerializer.getSchema(intThat(arg -> arg > 0 && arg <= 84)))
-          .thenCallRealMethod();
+    doCallRealMethod().when(adminOperationSerializer).serialize(any(), anyInt());
+    doCallRealMethod().when(adminOperationSerializer).deserialize(any(), anyInt());
 
-      // For schema IDs > 84, return a mock schema (for demonstration).
-      Schema mockSchema = mock(Schema.class);
-      mockedStatic.when(() -> AdminOperationSerializer.getSchema(intThat(arg -> arg > 84))).thenReturn(mockSchema);
+    // Serialize the AdminOperation object with writer schema id v74
+    byte[] serializedBytes = adminOperationSerializer.serialize(adminMessage, 74);
 
-      // Create an AdminOperation object with v84
-      UpdateStore updateStore = (UpdateStore) AdminMessageType.UPDATE_STORE.getNewInstance();
-      updateStore.clusterName = "clusterName";
-      updateStore.storeName = "storeName";
-      updateStore.owner = "owner";
-      updateStore.partitionNum = 20;
-      updateStore.currentVersion = 1;
-      updateStore.enableReads = true;
-      updateStore.enableWrites = true;
-      updateStore.replicateAllConfigs = true;
-      updateStore.updatedConfigsList = Collections.emptyList();
-      // Purposely set to true. This field doesn't exist in v74, will be dropped during serialization
-      // Default value of this field is False.
-      updateStore.separateRealTimeTopicEnabled = true;
-      AdminOperation adminMessage = new AdminOperation();
-      adminMessage.operationType = AdminMessageType.UPDATE_STORE.getValue();
-      adminMessage.payloadUnion = updateStore;
-      adminMessage.executionId = 1;
-
-      doCallRealMethod().when(adminOperationSerializer).serialize(any(), anyInt());
-      doCallRealMethod().when(adminOperationSerializer).deserialize(any(), anyInt());
-
-      // Serialize the AdminOperation object with writer schema id v74
-      byte[] serializedBytes = adminOperationSerializer.serialize(adminMessage, 74);
-
-      // Deserialize the serialized bytes back into an AdminOperation object
-      // TODO: test that all the new fields only have default values, after the final deserialization.
-      AdminOperation deserializedOperation = adminOperationSerializer.deserialize(ByteBuffer.wrap(serializedBytes), 74);
-      UpdateStore deserializedOperationPayloadUnion = (UpdateStore) deserializedOperation.getPayloadUnion();
-      assertEquals(deserializedOperationPayloadUnion.clusterName.toString(), "clusterName");
-      assertEquals(deserializedOperationPayloadUnion.storeName.toString(), "storeName");
-      assertEquals(deserializedOperationPayloadUnion.owner.toString(), "owner");
-      assertEquals(deserializedOperationPayloadUnion.partitionNum, 20);
-      assertEquals(deserializedOperationPayloadUnion.currentVersion, 1);
-      assertTrue(deserializedOperationPayloadUnion.enableReads);
-      assertTrue(deserializedOperationPayloadUnion.enableWrites);
-      assertTrue(deserializedOperationPayloadUnion.replicateAllConfigs);
-      assertEquals(deserializedOperationPayloadUnion.updatedConfigsList, Collections.emptyList());
-      // The field separateRealTimeTopicEnabled should be set to false (default value) after deserialization
-      assertFalse(deserializedOperationPayloadUnion.separateRealTimeTopicEnabled);
-    }
+    // Deserialize the serialized bytes back into an AdminOperation object
+    // TODO: test that all the new fields only have default values, after the final deserialization.
+    AdminOperation deserializedOperation = adminOperationSerializer.deserialize(ByteBuffer.wrap(serializedBytes), 74);
+    UpdateStore deserializedOperationPayloadUnion = (UpdateStore) deserializedOperation.getPayloadUnion();
+    assertEquals(deserializedOperationPayloadUnion.clusterName.toString(), "clusterName");
+    assertEquals(deserializedOperationPayloadUnion.storeName.toString(), "storeName");
+    assertEquals(deserializedOperationPayloadUnion.owner.toString(), "owner");
+    assertEquals(deserializedOperationPayloadUnion.partitionNum, 20);
+    assertEquals(deserializedOperationPayloadUnion.currentVersion, 1);
+    assertTrue(deserializedOperationPayloadUnion.enableReads);
+    assertTrue(deserializedOperationPayloadUnion.enableWrites);
+    assertTrue(deserializedOperationPayloadUnion.replicateAllConfigs);
+    assertEquals(deserializedOperationPayloadUnion.updatedConfigsList, Collections.emptyList());
+    // The field separateRealTimeTopicEnabled should be set to false (default value) after deserialization
+    assertFalse(deserializedOperationPayloadUnion.separateRealTimeTopicEnabled);
   }
 }
