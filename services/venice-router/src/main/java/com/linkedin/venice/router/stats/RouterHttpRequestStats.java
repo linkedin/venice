8c8eb8871c8f2413c6d60f7fc65056f8fa16fc14
Sourav Maji <tester@linkedin.com>
2025-02-25 16:02:08 -0800

    [router] Fix in-flight request check in router shutdown

--- services/venice-router/src/main/java/com/linkedin/venice/router/stats/RouterHttpRequestStats.java
+++ services/venice-router/src/main/java/com/linkedin/venice/router/stats/RouterHttpRequestStats.java
@@ -74,7 +74,7 @@ public class RouterHttpRequestStats extends AbstractVeniceHttpStats {
 
   private final static Sensor totalInflightRequestSensor = localMetricRepo.sensor("total_inflight_request");
   static {
-    totalInflightRequestSensor.add("total_inflight_request_count", new Total());
+    totalInflightRequestSensor.add("total_inflight_request_count", new Gauge());
   }
 
   /** metrics to track incoming requests */
