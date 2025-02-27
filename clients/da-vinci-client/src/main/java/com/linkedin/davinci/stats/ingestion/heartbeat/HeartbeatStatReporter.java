4e7f05b87f340f21dbf6274b3a6bf235ae8f2cfc
Jialin Liu <jlliu@linkedin.com>
2025-02-27 12:55:36 -0800

    [server] Do not register seprate RT region heartbeat metrics for follower

--- clients/da-vinci-client/src/main/java/com/linkedin/davinci/stats/ingestion/heartbeat/HeartbeatStatReporter.java
+++ clients/da-vinci-client/src/main/java/com/linkedin/davinci/stats/ingestion/heartbeat/HeartbeatStatReporter.java
@@ -3,6 +3,7 @@ package com.linkedin.davinci.stats.ingestion.heartbeat;
 import static com.linkedin.venice.stats.StatsErrorCode.NULL_INGESTION_STATS;
 
 import com.linkedin.davinci.stats.AbstractVeniceStatsReporter;
+import com.linkedin.venice.utils.Utils;
 import io.tehuti.metrics.MetricsRepository;
 import io.tehuti.metrics.stats.AsyncGauge;
 import java.util.Set;
@@ -29,7 +30,18 @@ public class HeartbeatStatReporter extends AbstractVeniceStatsReporter<Heartbeat
         if (getStats() == null) {
           return NULL_INGESTION_STATS.code;
         }
+        return getStats().getReadyToServeLeaderLag(region).getAvg();
+      }, LEADER_METRIC_PREFIX + region + AVG));
 
+      // Do not register follower heartbeat metrics for separate RT region.
+      if (Utils.isSeparateTopicRegion(region)) {
+        continue;
+      }
+
+      registerSensor(new AsyncGauge((ignored, ignored2) -> {
+        if (getStats() == null) {
+          return NULL_INGESTION_STATS.code;
+        }
         return getStats().getReadyToServeFollowerLag(region).getMax();
       }, FOLLOWER_METRIC_PREFIX + region + MAX));
 
@@ -37,15 +49,6 @@ public class HeartbeatStatReporter extends AbstractVeniceStatsReporter<Heartbeat
         if (getStats() == null) {
           return NULL_INGESTION_STATS.code;
         }
-
-        return getStats().getReadyToServeLeaderLag(region).getAvg();
-      }, LEADER_METRIC_PREFIX + region + AVG));
-
-      registerSensor(new AsyncGauge((ignored, ignored2) -> {
-        if (getStats() == null) {
-          return NULL_INGESTION_STATS.code;
-        }
-
         return getStats().getReadyToServeFollowerLag(region).getAvg();
       }, FOLLOWER_METRIC_PREFIX + region + AVG));
 
@@ -53,7 +56,6 @@ public class HeartbeatStatReporter extends AbstractVeniceStatsReporter<Heartbeat
         if (getStats() == null) {
           return NULL_INGESTION_STATS.code;
         }
-
         return getStats().getCatchingUpFollowerLag(region).getMax();
       }, CATCHUP_UP_FOLLOWER_METRIC_PREFIX + region + MAX));
 
@@ -61,7 +63,6 @@ public class HeartbeatStatReporter extends AbstractVeniceStatsReporter<Heartbeat
         if (getStats() == null) {
           return NULL_INGESTION_STATS.code;
         }
-
         return getStats().getCatchingUpFollowerLag(region).getAvg();
       }, CATCHUP_UP_FOLLOWER_METRIC_PREFIX + region + AVG));
     }
