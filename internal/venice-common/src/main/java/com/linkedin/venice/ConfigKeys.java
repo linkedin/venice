ebf38929c81320ce93eecd0d9be484541f024c1f
Min Huang <mnhuang@linkedin.com>
2025-02-27 02:10:08 -0800

    [server][da-vinci] Centralized store ingestion task clean up service
    Currently both state transition threads and store ingestion threads
    can close store ingestion tasks; as a result, race conditions could
    happen in the below order:
    SIT thread decides to close itself
    -> ST thread starts subscribing to new partitions
    -> new partition subscription cleans and ST returns successfuly
    -> SIT finishes closing itself
    
    This PR fixes the issue by introducing a centralized clean up
    service inside KafkaStoreIngestionService which reuses the topic
    level lock manager in it. Besides, SIT will not be able to close
    itself anymore (unless it's isolated ingestion process); ST threads
    and the clean-up service will control the life cycle of SIT instances.
    
    New config to enable clean-up version: "server.idle.ingestion.task.cleanup.interval.in.seconds"
    -1: feature disabled.
    non-negative number: clean-up service enabled, config value is the clean-up
                         delay interval.

--- internal/venice-common/src/main/java/com/linkedin/venice/ConfigKeys.java
+++ internal/venice-common/src/main/java/com/linkedin/venice/ConfigKeys.java
@@ -2560,4 +2560,19 @@ public class ConfigKeys {
    * controls the TTL of the cache per entry.
    */
   public static final String ACL_IN_MEMORY_CACHE_TTL_MS = "acl.in.memory.cache.ttl.ms";
+
+  /**
+   * The interval for cleaning up the idle store ingestion tasks in a centralized way inside KafkaStoreIngestionService.
+   * If config value is -1, it means the new clean up service is disabled and the old mechanism is still in place.
+   * If config value is non-negative, it means the new clean up service is enabled and the value is the clean up
+   * schedule interval in seconds.
+   *
+   * Once enabled, Store ingestion tasks will not try to close themselves when they are idle; instead, a service
+   * is started inside KafkaStoreIngestionService to clean up idle tasks. This is completely eliminate the race
+   * conditions between store ingestion task thread and Helix state transition threads when managing the life cycle
+   * of a store ingestion task.
+   * TODO: Deprecate this config after new clean up service is fully rolled out and stable.
+   */
+  public static final String SERVER_IDLE_INGESTION_TASK_CLEANUP_INTERVAL_IN_SECONDS =
+      "server.idle.ingestion.task.cleanup.interval.in.seconds";
 }
