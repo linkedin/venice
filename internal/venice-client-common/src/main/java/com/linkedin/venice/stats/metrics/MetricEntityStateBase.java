120a6fda39ffae5393f4c01dab91190efcdccfed
Manoj Nagarajan <mnagaraj@mnagaraj-mn5836.linkedin.biz>
2025-02-28 12:32:30 -0800

    some minor cleanup and some javadoc

--- internal/venice-client-common/src/main/java/com/linkedin/venice/stats/metrics/MetricEntityStateBase.java
+++ internal/venice-client-common/src/main/java/com/linkedin/venice/stats/metrics/MetricEntityStateBase.java
@@ -4,11 +4,17 @@ import com.linkedin.venice.stats.VeniceOpenTelemetryMetricsRepository;
 import com.linkedin.venice.stats.dimensions.VeniceMetricsDimensions;
 import io.opentelemetry.api.common.Attributes;
 import io.tehuti.metrics.MeasurableStat;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import javax.annotation.Nonnull;
 
 
+/**
+ * This version of {@link MetricEntityState} is used when the metric entity has no dynamic dimensions.
+ * The base {@link Attributes} that are common for all invocation of this instance are passed in the
+ * constructor and used during every record() call.
+ */
 public class MetricEntityStateBase extends MetricEntityState {
   private final Attributes attributes;
 
@@ -17,11 +23,7 @@ public class MetricEntityStateBase extends MetricEntityState {
       VeniceOpenTelemetryMetricsRepository otelRepository,
       @Nonnull Map<VeniceMetricsDimensions, String> baseDimensionsMap,
       @Nonnull Attributes baseAttributes) {
-    super(metricEntity, otelRepository);
-    validateRequiredDimensions(metricEntity, baseDimensionsMap);
-    // directly passing in the Attributes as multiple MetricEntityState can reuse the same base dimensions object.
-    // If we want to fully abstract the Attribute creation inside these classes, we can create it here instead
-    this.attributes = baseAttributes;
+    this(metricEntity, otelRepository, null, null, Collections.EMPTY_LIST, baseDimensionsMap, baseAttributes);
   }
 
   public MetricEntityStateBase(
@@ -34,8 +36,8 @@ public class MetricEntityStateBase extends MetricEntityState {
       @Nonnull Attributes baseAttributes) {
     super(metricEntity, otelRepository, registerTehutiSensorFn, tehutiMetricNameEnum, tehutiMetricStats);
     validateRequiredDimensions(metricEntity, baseDimensionsMap);
-    // directly passing in the Attributes as multiple MetricEntityState can reuse the same base dimensions
-    // if we want to fully abstract the Attribute creation inside these classes, we can create it here instead
+    // directly passing in the Attributes as multiple MetricEntityState can reuse the same base attributes object.
+    // If we want to fully abstract the Attribute creation inside these classes, we can create it here instead
     this.attributes = baseAttributes;
   }
 
