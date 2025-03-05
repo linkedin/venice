4f48f76df6682f702d9c69519e7157fbdcf020af
Sushant Mane <sumane@linkedin.com>
2025-03-05 00:49:11 -0800

    Revert some changes

--- services/venice-controller/src/main/java/com/linkedin/venice/controller/VeniceHelixAdmin.java
+++ services/venice-controller/src/main/java/com/linkedin/venice/controller/VeniceHelixAdmin.java
@@ -1056,10 +1056,6 @@ public class VeniceHelixAdmin implements Admin, StoreCleaner {
       ReadWriteSchemaRepository schemaRepo = clusterResources.getSchemaRepository();
       schemaRepo.initKeySchema(storeName, keySchema);
       schemaRepo.addValueSchema(storeName, valueSchema, HelixReadOnlySchemaRepository.VALUE_SCHEMA_STARTING_ID);
-      VeniceSystemStoreType systemStoreType = VeniceSystemStoreType.getSystemStoreType(storeName);
-      if (systemStoreType != null && systemStoreType.equals(VeniceSystemStoreType.META_STORE)) {
-        setUpMetaStoreAndMayProduceSnapshot(clusterName, systemStoreType.extractRegularStoreName(storeName));
-      }
       LOGGER.info(
           "Completed creating Store {} in cluster {} with owner {} and largestUsedVersionNumber {}",
           storeName,
@@ -2595,7 +2591,6 @@ public class VeniceHelixAdmin implements Admin, StoreCleaner {
               version.getCompressionStrategy(),
               Optional.ofNullable(compressionDictBuffer),
               Collections.emptyMap());
-          veniceWriter.flush();
         }
       }
     }
@@ -3010,7 +3005,6 @@ public class VeniceHelixAdmin implements Admin, StoreCleaner {
                       -1L, // -1 indicates rewinding from the beginning of the source topic
                       new HashMap<>());
                 }
-                veniceWriter.flush();
               } finally {
                 if (veniceWriter != null) {
                   veniceWriter.close();
