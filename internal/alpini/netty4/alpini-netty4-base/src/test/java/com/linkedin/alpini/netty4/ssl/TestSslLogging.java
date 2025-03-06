f0447fe91635e5824d73d320ad5b67591c6a6e15
Min Huang <mnhuang@linkedin.com>
2025-03-06 15:52:32 -0800

    Added appender into the test

--- internal/alpini/netty4/alpini-netty4-base/src/test/java/com/linkedin/alpini/netty4/ssl/TestSslLogging.java
+++ internal/alpini/netty4/alpini-netty4-base/src/test/java/com/linkedin/alpini/netty4/ssl/TestSslLogging.java
@@ -1,19 +1,71 @@
 package com.linkedin.alpini.netty4.ssl;
 
-import org.apache.logging.log4j.Level;
+import static org.testng.AssertJUnit.assertEquals;
+
+import com.linkedin.alpini.netty4.TestLogAppender;
+import io.netty.util.internal.logging.InternalLoggerFactory;
+import io.netty.util.internal.logging.Log4J2LoggerFactory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.config.Configuration;
+import org.apache.logging.log4j.core.config.LoggerConfig;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 
+@Test(groups = "unit")
 public class TestSslLogging {
+  private static TestLogAppender appender;
+  private static Logger logger;
+
+  @BeforeClass
+  public static void setup() {
+    InternalLoggerFactory.setDefaultFactory(Log4J2LoggerFactory.INSTANCE);
+
+    // Obtain the root logger context and configuration
+    LoggerContext context = (LoggerContext) LogManager.getContext(false);
+    Configuration config = context.getConfiguration();
+
+    // Create and add the custom appender
+    appender = TestLogAppender.createAppender("TestLogAppender", null, null, null);
+    appender.start();
+    config.addAppender(appender);
+
+    // Add the appender to the logger config
+    LoggerConfig loggerConfig = config.getRootLogger();
+    loggerConfig.addAppender(appender, null, null);
+    context.updateLoggers(); // Make sure to update loggers to apply the new configuration
+
+    // Get a logger instance
+    logger = LogManager.getLogger(SslInitializer.class);
+  }
+
+  @AfterClass
+  public static void teardown() {
+    // Remove appender and stop it
+    LoggerContext context = (LoggerContext) LogManager.getContext(false);
+    Configuration config = context.getConfiguration();
+    LoggerConfig loggerConfig = config.getRootLogger();
+    loggerConfig.removeAppender(appender.getName());
+    appender.stop();
+  }
+
   @Test
   public void testLoggingNullThrowable() {
-    // This test is to make sure that the code does not throw an exception when the Throwable is null
-    final Logger LOG = LogManager.getLogger(SslInitializer.class);
+    // This test ensures that the code does not throw an exception when the Throwable is null
     Throwable nullThrowable = null;
-    LOG.log(Level.ERROR, "This is a ERROR level test message", nullThrowable);
-    LOG.log(Level.INFO, "This is a INFO level test message", nullThrowable);
-    LOG.log(Level.WARN, "This is a WARN level test message", nullThrowable);
+
+    // Log the messages
+    logger.log(org.apache.logging.log4j.Level.ERROR, "This is an ERROR level test message", nullThrowable);
+    logger.log(org.apache.logging.log4j.Level.INFO, "This is an INFO level test message", nullThrowable);
+    logger.log(org.apache.logging.log4j.Level.WARN, "This is a WARN level test message", nullThrowable);
+
+    // Verify that messages were logged correctly
+    assertEquals("Expected 3 log events, but found: " + appender.size(), 3, appender.size());
+    assertEquals("This is an ERROR level test message", appender.getMessage(0));
+    assertEquals("This is an INFO level test message", appender.getMessage(1));
+    assertEquals("This is a WARN level test message", appender.getMessage(2));
   }
 }
