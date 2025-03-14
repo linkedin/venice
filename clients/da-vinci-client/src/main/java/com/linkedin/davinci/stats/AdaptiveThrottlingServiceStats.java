34dcc521c35cd981289a23391c1200eda7c2b753
Sourav Maji <tester@linkedin.com>
2025-03-14 11:09:37 -0700

    fix sensor name

--- clients/da-vinci-client/src/main/java/com/linkedin/davinci/stats/AdaptiveThrottlingServiceStats.java
+++ clients/da-vinci-client/src/main/java/com/linkedin/davinci/stats/AdaptiveThrottlingServiceStats.java
@@ -10,7 +10,7 @@ import java.util.Map;
 
 
 public class AdaptiveThrottlingServiceStats extends AbstractVeniceStats {
-  private static final String ADAPTIVE_THROTTLING_SERVICE_SUFFIX = "-AdaptiveThrottlingService";
+  private static final String ADAPTIVE_THROTTLING_SERVICE_SUFFIX = "AdaptiveThrottlingService";
   private final Map<String, Sensor> sensors = new HashMap<>();
 
   public AdaptiveThrottlingServiceStats(MetricsRepository metricsRepository) {
