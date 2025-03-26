0aaa73d27475806240e2549c149317e541c1b54a
Whitney Deng <whdeng@linkedin.com>
2025-03-25 17:24:47 -0700

    nit: comment spelling

--- internal/venice-client-common/src/main/java/com/linkedin/venice/controllerapi/RepushJobResponse.java
+++ internal/venice-client-common/src/main/java/com/linkedin/venice/controllerapi/RepushJobResponse.java
@@ -8,7 +8,7 @@ public class RepushJobResponse extends ControllerResponse {
   public static final String DEFAULT_EXECUTION_ID = "-1";
 
   public RepushJobResponse() {
-    // TODO: ControlleResponse types need to have a default constructor which takes no arguments,
+    // TODO: ControllerResponse types need to have a default constructor which takes no arguments,
     // we either need to refactor that or make it so this class can populate executionId in a different way
     this.executionId = DEFAULT_EXECUTION_ID;
   }
