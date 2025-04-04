0a01058b3264ad918925e0410c8260643610c313
Min Huang <mnhuang@linkedin.com>
2025-04-04 09:54:28 -0700

    [server][da-vinci] Added more diagnostics logs in TopicExistenceChecker
    There are a few cases that TopicExistenceChecker will decide a topic
    shouldn't exist and ConsumerSubscriptionCleaner will then fail the
    ingestion task:
    1. Version doesn't exist in store ZK node anymore for the version topic
    2. Store is no longer hybrid for a real-time topic
    3. Store not found in ZK for the topics
    
    However, ConsumerSubscriptionCleaner failed a push whose topics still
    exists. Added logs to understand which case it is if it happens again.

--- clients/da-vinci-client/src/main/java/com/linkedin/davinci/kafka/consumer/MetadataRepoBasedTopicExistingCheckerImpl.java
+++ clients/da-vinci-client/src/main/java/com/linkedin/davinci/kafka/consumer/MetadataRepoBasedTopicExistingCheckerImpl.java
@@ -10,7 +10,7 @@ import org.apache.logging.log4j.Logger;
 
 public class MetadataRepoBasedTopicExistingCheckerImpl implements TopicExistenceChecker {
   private final ReadOnlyStoreRepository readOnlyStoreRepository;
-  private final Logger logger = LogManager.getLogger(MetadataRepoBasedTopicExistingCheckerImpl.class);
+  private static final Logger LOGGER = LogManager.getLogger(MetadataRepoBasedTopicExistingCheckerImpl.class);
 
   public MetadataRepoBasedTopicExistingCheckerImpl(ReadOnlyStoreRepository readOnlyStoreRepository) {
     this.readOnlyStoreRepository = readOnlyStoreRepository;
@@ -26,17 +26,20 @@ public class MetadataRepoBasedTopicExistingCheckerImpl implements TopicExistence
       if (Version.isVersionTopicOrStreamReprocessingTopic(topic)) {
         int version = Version.parseVersionFromKafkaTopicName(topic);
         if (store.getVersion(version) == null) {
+          LOGGER.warn("Version {} not found for topic: {}", version, topic);
           isExistingTopic = false;
         }
       } else if (Version.isRealTimeTopic(topic)) {
         if (!store.isHybrid()) {
+          LOGGER.warn("Store {} is not hybrid currently, but found real-time topic {}", storeName, topic);
           isExistingTopic = false;
         }
       }
     } catch (VeniceNoStoreException e) {
+      LOGGER.warn("Store not found for topic: {}", topic);
       isExistingTopic = false;
     } catch (Exception e) {
-      logger.error("Exception thrown in checkTopicExists: ", e);
+      LOGGER.error("Exception thrown in checkTopicExists: ", e);
     }
     return isExistingTopic;
   }
