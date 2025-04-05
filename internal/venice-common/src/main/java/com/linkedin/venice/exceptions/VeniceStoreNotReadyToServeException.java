8cdfbf42df670c8cb8bc5fca50ec071b176fbaf2
Whitney Deng <whdeng@linkedin.com>
2025-04-04 17:02:56 -0700

    update VeniceStoreNotReadyToServeException message

--- internal/venice-common/src/main/java/com/linkedin/venice/exceptions/VeniceStoreNotReadyToServeException.java
+++ internal/venice-common/src/main/java/com/linkedin/venice/exceptions/VeniceStoreNotReadyToServeException.java
@@ -11,13 +11,13 @@ public class VeniceStoreNotReadyToServeException extends VeniceException {
   private final int storeVersion;
 
   public VeniceStoreNotReadyToServeException(String storeName) {
-    super("No version of of store " + storeName + " ready to serve");
+    super("No version of store " + storeName + " ready to serve. Try again later.");
     this.storeName = storeName;
     this.storeVersion = Store.NON_EXISTING_VERSION;
   }
 
   public VeniceStoreNotReadyToServeException(String storeName, int storeVersion) {
-    super("version " + storeVersion + " of store " + storeName + "not ready to serve");
+    super("Version " + storeVersion + " of store " + storeName + " not ready to serve. Try again later.");
     this.storeName = storeName;
     this.storeVersion = storeVersion;
   }
