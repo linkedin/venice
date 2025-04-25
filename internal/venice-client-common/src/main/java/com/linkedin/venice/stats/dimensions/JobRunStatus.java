c2e2b7cdd08a42dd06f871fb5d7df3422f53800f
Whitney Deng <whdeng@linkedin.com>
2025-04-25 15:47:09 -0700

    add JOB_RUN_STATUS dimension

---
+++ internal/venice-client-common/src/main/java/com/linkedin/venice/stats/dimensions/JobRunStatus.java
@@ -0,0 +1,25 @@
+package com.linkedin.venice.stats.dimensions;
+
+public enum JobRunStatus implements VeniceDimensionInterface {
+  FAILED, SUCCESS;
+
+  private final String status;
+
+  JobRunStatus() {
+    this.status = name().toLowerCase();
+  }
+
+  /**
+   * All the instances of this Enum should have the same dimension name.
+   * Refer {@link VeniceDimensionInterface#getDimensionName()} for more details.
+   */
+  @Override
+  public VeniceMetricsDimensions getDimensionName() {
+    return VeniceMetricsDimensions.JOB_EXECUTION_STATUS;
+  }
+
+  @Override
+  public String getDimensionValue() {
+    return this.status;
+  }
+}
