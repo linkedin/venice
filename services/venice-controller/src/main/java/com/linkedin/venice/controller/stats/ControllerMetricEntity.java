d415a71103723d941f63113dea45b93f49f01674
Whitney Deng <whdeng@linkedin.com>
2025-04-25 18:22:56 -0700

    add VENICE_CONTROLLER_ENDPOINT dimension

--- services/venice-controller/src/main/java/com/linkedin/venice/controller/stats/ControllerMetricEntity.java
+++ services/venice-controller/src/main/java/com/linkedin/venice/controller/stats/ControllerMetricEntity.java
@@ -1,5 +1,13 @@
 package com.linkedin.venice.controller.stats;
 
+import static com.linkedin.venice.stats.dimensions.VeniceMetricsDimensions.HTTP_RESPONSE_STATUS_CODE;
+import static com.linkedin.venice.stats.dimensions.VeniceMetricsDimensions.HTTP_RESPONSE_STATUS_CODE_CATEGORY;
+import static com.linkedin.venice.stats.dimensions.VeniceMetricsDimensions.VENICE_CLUSTER_NAME;
+import static com.linkedin.venice.stats.dimensions.VeniceMetricsDimensions.VENICE_CONTROLLER_ENDPOINT;
+import static com.linkedin.venice.stats.dimensions.VeniceMetricsDimensions.VENICE_RESPONSE_STATUS_CODE_CATEGORY;
+import static com.linkedin.venice.stats.dimensions.VeniceMetricsDimensions.VENICE_STORE_NAME;
+import static com.linkedin.venice.utils.Utils.setOf;
+
 import com.linkedin.venice.stats.dimensions.VeniceMetricsDimensions;
 import com.linkedin.venice.stats.metrics.MetricEntity;
 import com.linkedin.venice.stats.metrics.MetricType;
@@ -8,8 +16,17 @@ import java.util.Set;
 
 
 public enum ControllerMetricEntity {
+  CALL_COUNT(
+      MetricType.COUNTER, MetricUnit.NUMBER, "Count of all requests during response handling along with response codes",
+      setOf(
+          VENICE_STORE_NAME,
+          VENICE_CLUSTER_NAME,
+          VENICE_CONTROLLER_ENDPOINT,
+          HTTP_RESPONSE_STATUS_CODE,
+          HTTP_RESPONSE_STATUS_CODE_CATEGORY,
+          VENICE_RESPONSE_STATUS_CODE_CATEGORY)
+  );
 
-  ;
   private final MetricEntity metricEntity;
 
   ControllerMetricEntity(
