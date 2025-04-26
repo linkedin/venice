c68675a3e41bf6f0ee5d0b2e59fd36cca50a690a
Sourav Maji <tester@linkedin.com>
2025-04-26 14:11:15 -0700

    [controller] RIP parent VT

--- services/venice-controller/src/main/java/com/linkedin/venice/controller/VeniceHelixAdmin.java
+++ services/venice-controller/src/main/java/com/linkedin/venice/controller/VeniceHelixAdmin.java
@@ -3003,6 +3003,7 @@ public class VeniceHelixAdmin implements Admin, StoreCleaner {
             long createBatchTopicStartTime = System.currentTimeMillis();
             topicToCreationTime.computeIfAbsent(version.kafkaTopicName(), topic -> System.currentTimeMillis());
             if (!isParent()) {
+            topicToCreationTime.computeIfAbsent(version.kafkaTopicName(), topic -> System.currentTimeMillis());
             createBatchTopics(
                 version,
                 pushType,
