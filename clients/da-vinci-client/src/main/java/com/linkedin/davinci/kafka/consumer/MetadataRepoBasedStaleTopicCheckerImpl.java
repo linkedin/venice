48a0bf6a0eced10603b7ec32d720544a5f4198e2
Minh Nguyen <minnguyen@linkedin.com>
2025-04-30 17:38:18 -0700

    Check the hybrid versions for the non hybrid store and using cache

--- clients/da-vinci-client/src/main/java/com/linkedin/davinci/kafka/consumer/MetadataRepoBasedStaleTopicCheckerImpl.java
+++ clients/da-vinci-client/src/main/java/com/linkedin/davinci/kafka/consumer/MetadataRepoBasedStaleTopicCheckerImpl.java
@@ -1,16 +1,22 @@
 package com.linkedin.davinci.kafka.consumer;
 
+import com.linkedin.alpini.base.misc.Pair;
 import com.linkedin.venice.exceptions.VeniceNoStoreException;
 import com.linkedin.venice.meta.ReadOnlyStoreRepository;
 import com.linkedin.venice.meta.Store;
 import com.linkedin.venice.meta.Version;
+import com.linkedin.venice.utils.LatencyUtils;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 
 public class MetadataRepoBasedStaleTopicCheckerImpl implements StaleTopicChecker {
-  private final ReadOnlyStoreRepository readOnlyStoreRepository;
   private static final Logger LOGGER = LogManager.getLogger(MetadataRepoBasedStaleTopicCheckerImpl.class);
+  static final long STALE_TOPIC_CHECK_INTERVAL_MS = TimeUnit.MINUTES.toMillis(30);
+  private final ReadOnlyStoreRepository readOnlyStoreRepository;
+  private final ConcurrentHashMap<Store, Pair<Long, Boolean>> hybridVersionCheckCache = new ConcurrentHashMap<>();
 
   public MetadataRepoBasedStaleTopicCheckerImpl(ReadOnlyStoreRepository readOnlyStoreRepository) {
     this.readOnlyStoreRepository = readOnlyStoreRepository;
@@ -28,10 +34,7 @@ public class MetadataRepoBasedStaleTopicCheckerImpl implements StaleTopicChecker
           return false;
         }
       } else if (Version.isRealTimeTopic(topic)) {
-        if (!store.isHybrid()) {
-          LOGGER.warn("Store {} is not hybrid currently, but found real-time topic {}", storeName, topic);
-          return false;
-        }
+        return shouldRealTimeTopicExist(store, topic);
       }
     } catch (VeniceNoStoreException e) {
       LOGGER.warn("Store not found for topic: {}", topic);
@@ -40,5 +43,63 @@ public class MetadataRepoBasedStaleTopicCheckerImpl implements StaleTopicChecker
       LOGGER.error("Exception thrown in checkTopicExists; unable to decide if topic {} should exist ", topic, e);
     }
     return true;
+  }
+
+  /**
+   * Check if the topic should exist based on the store's hybrid version.
+   * If the store is hybrid, the topic should exist.
+   * If the store is not hybrid, check if it has a hybrid version.
+   */
+  private boolean shouldRealTimeTopicExist(Store store, String topic) {
+    if (store.isHybrid()) {
+      return true;
+    }
+
+    // Default value - always check whether the store contains hybrid version
+    boolean containsHybridVersion = false;
+    boolean shouldRecheck = true;
+
+    Pair<Long, Boolean> cachedInfo = hybridVersionCheckCache.get(store);
+
+    if (cachedInfo != null) {
+      long lastCheckTime = cachedInfo.getFirst();
+      boolean hasHybridVersion = cachedInfo.getSecond();
+      if (hasHybridVersion) {
+        return true;
+      }
+      // If the store did not have hybrid version in previous check and the last check time is longer than the stale
+      // topic check interval, we should recheck the store to see if it has hybrid version now.
+      shouldRecheck = LatencyUtils.getElapsedTimeFromMsToMs(lastCheckTime) >= STALE_TOPIC_CHECK_INTERVAL_MS;
+    }
+
+    if (shouldRecheck) {
+      containsHybridVersion = Version.containsHybridVersion(store.getVersions());
+      hybridVersionCheckCache.put(store, new Pair<>(System.currentTimeMillis(), containsHybridVersion));
+    }
+
+    if (!containsHybridVersion) {
+      LOGGER.info(
+          "Store {} is not hybrid and has no hybrid version, so topic {} should not exist",
+          store.getName(),
+          topic);
+      return false;
+    }
+
+    return true;
+  }
+
+  /**
+   * This method is used for testing purposes only.
+   * It allows to set the hybrid version check cache directly.
+   */
+  protected void setHybridVersionCheckCache(Store store, Pair<Long, Boolean> hybridVersionCheckCache) {
+    this.hybridVersionCheckCache.put(store, hybridVersionCheckCache);
+  }
+
+  /**
+   * This method is used for testing purposes only.
+   */
+  protected Pair<Long, Boolean> getHybridVersionCheckCache(Store store) {
+    return this.hybridVersionCheckCache.get(store);
   }
 }
--- clients/da-vinci-client/src/test/java/com/linkedin/davinci/kafka/consumer/StaleTopicCheckerTest.java
+++ clients/da-vinci-client/src/test/java/com/linkedin/davinci/kafka/consumer/StaleTopicCheckerTest.java
@@ -4,11 +4,16 @@ import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 
+import com.linkedin.alpini.base.misc.Pair;
 import com.linkedin.venice.exceptions.VeniceNoStoreException;
 import com.linkedin.venice.meta.ReadOnlyStoreRepository;
 import com.linkedin.venice.meta.Store;
 import com.linkedin.venice.meta.Version;
 import com.linkedin.venice.meta.VersionImpl;
+import java.util.ArrayList;
+import java.util.List;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -37,5 +42,48 @@ public class StaleTopicCheckerTest {
     Assert.assertTrue(staleTopicChecker.shouldTopicExist(exitingTopic3));
     Assert.assertFalse(staleTopicChecker.shouldTopicExist(nontExitingTopic1));
     Assert.assertFalse(staleTopicChecker.shouldTopicExist(nontExitingTopic2));
+  }
+
+  @Test
+  public void testRealtimeTopicChecker() {
+    String realtimeTopic = "storeA_rt";
+    ReadOnlyStoreRepository repository = mock(ReadOnlyStoreRepository.class);
+    Store store = mock(Store.class);
+    doReturn(store).when(repository).getStoreOrThrow("storeA");
+    MetadataRepoBasedStaleTopicCheckerImpl staleTopicChecker = new MetadataRepoBasedStaleTopicCheckerImpl(repository);
+
+    try (MockedStatic<Version> versionMockedStatic = Mockito.mockStatic(Version.class)) {
+      versionMockedStatic.when(() -> Version.isRealTimeTopic(realtimeTopic)).thenReturn(true);
+      versionMockedStatic.when(() -> Version.parseStoreFromKafkaTopicName(realtimeTopic)).thenReturn("storeA");
+
+      // When store is hybrid, the topic should exist
+      doReturn(true).when(store).isHybrid();
+      Assert.assertTrue(staleTopicChecker.shouldTopicExist(realtimeTopic));
+
+      // When store is not hybrid, the topic depends on the hybrid version
+      doReturn(false).when(store).isHybrid();
+      List<Version> versions = new ArrayList<>();
+      Version version = mock(Version.class);
+      versions.add(version);
+      doReturn(versions).when(store).getVersions();
+
+      // When the cache map is empty, it should check the hybrid version
+      versionMockedStatic.when(() -> Version.containsHybridVersion(versions)).thenReturn(true);
+      Assert.assertTrue(staleTopicChecker.shouldTopicExist(realtimeTopic));
+
+      // Set the cache map for the store to NOT contain hybrid version, but duration is less than the check interval
+      long lastCheckTime = System.currentTimeMillis();
+      staleTopicChecker.setHybridVersionCheckCache(store, new Pair<>(lastCheckTime, false));
+      versionMockedStatic.when(() -> Version.containsHybridVersion(versions)).thenReturn(true);
+      Assert.assertFalse(staleTopicChecker.shouldTopicExist(realtimeTopic));
+      Assert.assertFalse(staleTopicChecker.getHybridVersionCheckCache(store).getSecond());
+
+      // Set the cache map for the store to NOT contain hybrid version, but duration is more than the check interval
+      lastCheckTime = System.currentTimeMillis() - MetadataRepoBasedStaleTopicCheckerImpl.STALE_TOPIC_CHECK_INTERVAL_MS;
+      staleTopicChecker.setHybridVersionCheckCache(store, new Pair<>(lastCheckTime, false));
+      versionMockedStatic.when(() -> Version.containsHybridVersion(versions)).thenReturn(true);
+      Assert.assertTrue(staleTopicChecker.shouldTopicExist(realtimeTopic));
+      Assert.assertTrue(staleTopicChecker.getHybridVersionCheckCache(store).getSecond());
+    }
   }
 }
