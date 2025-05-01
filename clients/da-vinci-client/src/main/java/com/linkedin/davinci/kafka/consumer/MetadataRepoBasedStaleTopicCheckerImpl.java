c3ac9b41bfb71fe74fc19ee9ac57dcc1028bee4a
Minh Nguyen <minnguyen@linkedin.com>
2025-05-01 16:21:50 -0700

    Clean up the code

--- clients/da-vinci-client/src/main/java/com/linkedin/davinci/kafka/consumer/MetadataRepoBasedStaleTopicCheckerImpl.java
+++ clients/da-vinci-client/src/main/java/com/linkedin/davinci/kafka/consumer/MetadataRepoBasedStaleTopicCheckerImpl.java
@@ -1,11 +1,9 @@
 package com.linkedin.davinci.kafka.consumer;
 
-import com.linkedin.alpini.base.misc.Pair;
 import com.linkedin.venice.exceptions.VeniceNoStoreException;
 import com.linkedin.venice.meta.ReadOnlyStoreRepository;
 import com.linkedin.venice.meta.Store;
 import com.linkedin.venice.meta.Version;
-import java.util.concurrent.ConcurrentHashMap;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -13,7 +11,6 @@ import org.apache.logging.log4j.Logger;
 public class MetadataRepoBasedStaleTopicCheckerImpl implements StaleTopicChecker {
   private static final Logger LOGGER = LogManager.getLogger(MetadataRepoBasedStaleTopicCheckerImpl.class);
   private final ReadOnlyStoreRepository readOnlyStoreRepository;
-  private final ConcurrentHashMap<Store, Pair<Long, Boolean>> hybridVersionCheckCache = new ConcurrentHashMap<>();
 
   public MetadataRepoBasedStaleTopicCheckerImpl(ReadOnlyStoreRepository readOnlyStoreRepository) {
     this.readOnlyStoreRepository = readOnlyStoreRepository;
