8da5c6bd93d8d097e2a4d8d716a3543d80f37c06
Sourav Maji <smaji@linkedin.com>
2025-05-02 10:11:13 -0700

    [server] Report ingestion error to controller for batch stores (#1704)
    During ingestion, if after EOP server or DVC instance hits any error, it will not be reported.  as an error to the controller. The controller then waits for 24hrs timeout limit to mark the push   as failed blocking further pushes.
    
    This PR reports error for such failed pushes even if EOP is received for batch stores.

--- clients/da-vinci-client/src/test/java/com/linkedin/davinci/kafka/consumer/IngestionNotificationDispatcherTest.java
+++ clients/da-vinci-client/src/test/java/com/linkedin/davinci/kafka/consumer/IngestionNotificationDispatcherTest.java
@@ -7,7 +7,7 @@ import com.linkedin.venice.exceptions.VeniceException;
 import com.linkedin.venice.exceptions.VeniceIngestionTaskKilledException;
 import com.linkedin.venice.utils.Utils;
 import java.util.ArrayDeque;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.Queue;
 import org.mockito.Mockito;
 import org.testng.annotations.Test;
@@ -34,7 +34,7 @@ public class IngestionNotificationDispatcherTest {
     Mockito.doReturn(true).when(psc).isHybrid();
     Mockito.doReturn(true).when(psc).isEndOfPushReceived();
     Mockito.doReturn(false).when(psc).isErrorReported();
-    dispatcher.reportKilled(Arrays.asList(new PartitionConsumptionState[] { psc }), error);
+    dispatcher.reportKilled(Collections.singletonList(psc), error);
     // Should report kill for a hybrid partition
     Mockito.verify(mockNotifier, Mockito.times(1)).error(topic, partitionId, error.getMessage(), error);
 
@@ -42,14 +42,14 @@ public class IngestionNotificationDispatcherTest {
     Mockito.reset(mockNotifier);
     Mockito.doReturn(false).when(psc).isHybrid();
     Mockito.doReturn(true).when(psc).isCompletionReported();
-    dispatcher.reportKilled(Arrays.asList(new PartitionConsumptionState[] { psc }), error);
+    dispatcher.reportKilled(Collections.singletonList(psc), error);
     // Should not report error for a completed batch partition
     Mockito.verify(mockNotifier, Mockito.never()).error(topic, partitionId, error.getMessage(), error);
 
     // Error has already been reported
     Mockito.reset(mockNotifier);
     Mockito.doReturn(true).when(psc).isErrorReported();
-    dispatcher.reportKilled(Arrays.asList(new PartitionConsumptionState[] { psc }), error);
+    dispatcher.reportKilled(Collections.singletonList(psc), error);
     Mockito.verify(mockNotifier, Mockito.never()).error(topic, partitionId, error.getMessage(), error);
   }
 
@@ -67,9 +67,24 @@ public class IngestionNotificationDispatcherTest {
     Mockito.doReturn(true).when(pcs).isEndOfPushReceived();
     Mockito.doReturn(false).when(pcs).isErrorReported();
     Mockito.doReturn(false).when(pcs).isComplete();
-    dispatcher.reportError(
-        Arrays.asList(new PartitionConsumptionState[] { pcs }),
-        "fake ingestion error",
-        mock(VeniceException.class));
+    dispatcher.reportError(Collections.singletonList(pcs), "fake ingestion error", mock(VeniceException.class));
+  }
+
+  @Test
+  public void testReportErrorOnBatchError() {
+    String topic = Utils.getUniqueString("test_v1");
+    int partitionId = 1;
+    VeniceNotifier mockNotifier = mock(VeniceNotifier.class);
+    Queue<VeniceNotifier> notifiers = new ArrayDeque<>();
+    notifiers.add(mockNotifier);
+    IngestionNotificationDispatcher dispatcher = new IngestionNotificationDispatcher(notifiers, topic, () -> true);
+    PartitionConsumptionState pcs = mock(PartitionConsumptionState.class);
+    Mockito.doReturn(partitionId).when(pcs).getPartition();
+    Mockito.doReturn(false).when(pcs).isHybrid();
+    Mockito.doReturn(true).when(pcs).isEndOfPushReceived();
+    Mockito.doReturn(false).when(pcs).isErrorReported();
+    Mockito.doReturn(true).when(pcs).isComplete();
+    Mockito.doReturn(false).when(pcs).isCurrentVersion();
+    dispatcher.reportError(Collections.singletonList(pcs), "fake ingestion error", mock(VeniceException.class));
   }
 }
