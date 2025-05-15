b5426d19b9e354266115c0a61f90f87ab421ba35
Ali Poursamadi <apoursamady@gmail.com>
2025-05-14 18:34:51 -0700

    RawPubSubInput components

---
+++ clients/venice-push-job/src/main/java/com/linkedin/venice/spark/input/table/rawPubSub/VeniceRawPubsubInputScanBuilder.java
@@ -0,0 +1,19 @@
+package com.linkedin.venice.spark.input.table.rawPubSub;
+
+import com.linkedin.venice.utils.VeniceProperties;
+import org.apache.spark.sql.connector.read.Scan;
+import org.apache.spark.sql.connector.read.ScanBuilder;
+
+
+public class VeniceRawPubsubInputScanBuilder implements ScanBuilder {
+  private final VeniceProperties jobConfig;
+
+  public VeniceRawPubsubInputScanBuilder(VeniceProperties properties) {
+    this.jobConfig = properties;
+  }
+
+  @Override
+  public Scan build() {
+    return new VeniceRawPubsubInputScan(jobConfig);
+  }
+}
