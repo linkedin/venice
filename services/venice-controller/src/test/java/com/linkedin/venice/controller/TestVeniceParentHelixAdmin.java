b5b3c902cf7344816d4816c6454df1227f78b07a
Michelle Kwong <mkwong+1@linkedin.com>
2025-05-15 14:00:39 -0700

    catch timeout exception when deleting store & cleanup acls

--- services/venice-controller/src/test/java/com/linkedin/venice/controller/TestVeniceParentHelixAdmin.java
+++ services/venice-controller/src/test/java/com/linkedin/venice/controller/TestVeniceParentHelixAdmin.java
@@ -52,6 +52,7 @@ import com.linkedin.venice.controllerapi.JobStatusQueryResponse;
 import com.linkedin.venice.controllerapi.MultiStoreStatusResponse;
 import com.linkedin.venice.controllerapi.StoreResponse;
 import com.linkedin.venice.controllerapi.UpdateStoreQueryParams;
+import com.linkedin.venice.exceptions.AdminMessageConsumptionTimeoutException;
 import com.linkedin.venice.exceptions.ConfigurationException;
 import com.linkedin.venice.exceptions.ErrorType;
 import com.linkedin.venice.exceptions.VeniceException;
@@ -3252,6 +3253,23 @@ public class TestVeniceParentHelixAdmin extends AbstractTestVeniceParentHelixAdm
 
     adminSpy.rollForwardToFutureVersion(clusterName, storeName, null);
     verify(adminSpy, times(5)).getCurrentVersionsForMultiColos(clusterName, storeName);
+  }
+
+  @Test
+  public void testDeleteStoreAdminMessageTimeout() {
+    VeniceParentHelixAdmin adminSpy = spy(parentAdmin);
+
+    String storeName = "test-testReCreateStore";
+    String owner = "unittest";
+    Store store = TestUtils.createTestStore(storeName, owner, System.currentTimeMillis());
+    doReturn(store).when(internalAdmin).getStore(eq(clusterName), eq(storeName));
+    doReturn(store).when(internalAdmin).checkPreConditionForDeletion(eq(clusterName), eq(storeName));
+
+    AdminMessageConsumptionTimeoutException exception =
+        new AdminMessageConsumptionTimeoutException("test msg", new Exception());
+    doThrow(exception).when(adminSpy).sendAdminMessageAndWaitForConsumed(any(), any(), any());
+    adminSpy.deleteStore(clusterName, storeName, false, 0, true);
+    verify(adminSpy, times(1)).deleteAclsForStore(store, storeName);
   }
 
   private Store setupForStoreViewConfigUpdateTest(String storeName) {
