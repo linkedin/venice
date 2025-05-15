c79d42ed2e713c1f914688f65687e8f3794b241d
Ali Poursamadi <apoursamady@gmail.com>
2025-05-14 18:51:35 -0700

    basic input partition class.

---
+++ clients/venice-push-job/src/main/java/com/linkedin/venice/spark/input/table/rawPubSub/VeniceBasicPubsubInputPartition.java
@@ -0,0 +1,37 @@
+package com.linkedin.venice.spark.input.table.rawPubSub;
+
+import org.apache.spark.sql.connector.read.InputPartition;
+
+/*
+Basic input partition is a 1 to one mapping between a partition in the topic and a partition in the spark task
+that populates the data into the spark dataframe.
+  */
+
+
+public class VeniceBasicPubsubInputPartition implements InputPartition {
+  //
+  private static final long serialVersionUID = 1L;
+
+  private final String region;
+  private final String TopicName;
+  private final int partitionNumber;
+
+  public VeniceBasicPubsubInputPartition(String region, String topicName, int partitionNumber) {
+    this.region = region;
+    this.TopicName = topicName;
+    this.partitionNumber = partitionNumber;
+  }
+
+  public String getRegion() {
+    return region;
+  }
+
+  public String getTopicName() {
+    return TopicName;
+  }
+
+  public int getPartitionNumber() {
+    return partitionNumber;
+  }
+
+}
