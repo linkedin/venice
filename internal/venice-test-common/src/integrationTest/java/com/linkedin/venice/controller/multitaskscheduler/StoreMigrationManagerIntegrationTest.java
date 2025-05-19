862fff238288194a96ab5257b943c23f6dec44b8
Ran Wang <rwang2+1@linkedin.com>
2025-05-18 23:21:06 -0700

    [Controller]MultiTaskSchedulerService for Store Migration, StoreMigrationManager, StoreMigrationTask and MigrationRecord are implemented (#1723)
    Developed the StoreMigrationManager and store migration task framework, along with the MultiTaskSchedulerService, which can be loaded with various types of task managers

---
+++ internal/venice-test-common/src/integrationTest/java/com/linkedin/venice/controller/multitaskscheduler/StoreMigrationManagerIntegrationTest.java
@@ -0,0 +1,82 @@
+package com.linkedin.venice.controller.multitaskscheduler;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertNull;
+import static org.testng.Assert.assertTrue;
+
+import com.linkedin.venice.utils.TestUtils;
+import com.linkedin.venice.utils.Time;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+
+public class StoreMigrationManagerIntegrationTest {
+  private StoreMigrationManager storeMigrationManager;
+  private static final long TIMEOUT = 10 * Time.MS_PER_SECOND;
+
+  @BeforeMethod
+  public void setUp() {
+    storeMigrationManager = StoreMigrationManager.createStoreMigrationManager(2, 3);
+  }
+
+  @AfterMethod
+  public void tearDown() {
+    storeMigrationManager.shutdown();
+  }
+
+  @Test(timeOut = TIMEOUT)
+  public void testScheduleMigrationWithRealExecutor() throws InterruptedException {
+    String storeName = "TestStore";
+    String sourceCluster = "SourceCluster";
+    String destinationCluster = "DestinationCluster";
+    int initialStep = 0;
+
+    // Schedule the migration
+    storeMigrationManager.scheduleMigration(storeName, sourceCluster, destinationCluster, initialStep, 1);
+    MigrationRecord migrationRecord = storeMigrationManager.getMigrationRecord(storeName);
+    migrationRecord.setStoreMigrationStartTime(Instant.now().minus(25, ChronoUnit.HOURS));
+    // Allow time for the scheduled tasks to execute until getIsAborted is set to true
+    TestUtils.waitForNonDeterministicAssertion(3, TimeUnit.SECONDS, () -> {
+
+      assertTrue(migrationRecord.getIsAborted(), "Migration record should be aborted.");
+    });
+    // Verify that the migration record has been removed, indicating task is aborted
+    assertEquals(migrationRecord.getCurrentStep(), 2);
+    assertNull(
+        storeMigrationManager.getMigrationRecord(storeName),
+        "Migration record should be cleaned up from migrationRecords after abort migration.");
+
+    // Schedule the migration start from step 3
+    int stepUpdateClusterDiscovery = 3;
+    ScheduledFuture<?> future2 = storeMigrationManager
+        .scheduleMigration(storeName, sourceCluster, destinationCluster, stepUpdateClusterDiscovery);
+    MigrationRecord recordFromUpdateClusterDiscovery = storeMigrationManager.getMigrationRecord(storeName);
+    try {
+      future2.get();
+    } catch (InterruptedException interruptedException) {
+      Thread.currentThread().interrupt();
+    } catch (ExecutionException executionException) {
+      // Handle the exception
+      System.err.println("Error occurred while waiting for the future: " + executionException.getCause());
+    }
+    // Allow time for the sequentially scheduled tasks to execute
+    TestUtils.waitForNonDeterministicAssertion(3, TimeUnit.SECONDS, () -> {
+      assertEquals(
+          recordFromUpdateClusterDiscovery.getCurrentStep(),
+          6,
+          "Migration record should be updated to 6 as marked as completed.");
+    });
+    // Verify that the migration record is not aborted
+    assertFalse(recordFromUpdateClusterDiscovery.getIsAborted());
+    assertNull(
+        storeMigrationManager.getMigrationRecord(storeName),
+        "Migration record should be cleaned up from migrationRecords after execution.");
+  }
+}
---
+++ services/venice-controller/src/main/java/com/linkedin/venice/controller/multitaskscheduler/StoreMigrationManager.java
@@ -0,0 +1,86 @@
+package com.linkedin.venice.controller.multitaskscheduler;
+
+import com.linkedin.venice.utils.DaemonThreadFactory;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+public class StoreMigrationManager extends ScheduledTaskManager {
+  // TODO: MigrationRecord will be persisted to ZooKeeper, and cleaned up when the migration is complete, in a future
+  // pull request.
+  private Map<String, MigrationRecord> migrationRecords;
+  private final int maxRetryAttempts;
+  private static final Logger LOGGER = LogManager.getLogger(StoreMigrationManager.class);
+
+  public MigrationRecord getMigrationRecord(String storeName) {
+    return migrationRecords.get(storeName);
+  }
+
+  @Override
+  protected ScheduledExecutorService createExecutorService(int threadPoolSize) {
+    return Executors
+        .newScheduledThreadPool(threadPoolSize, new DaemonThreadFactory("`MultiTaskScheduler-StoreMigration"));
+  }
+
+  @Override
+  protected Logger getLogger() {
+    return LOGGER;
+  }
+
+  public static StoreMigrationManager createStoreMigrationManager(int threadPoolSize, int maxRetryAttempts) {
+    StoreMigrationManager manager = new StoreMigrationManager(threadPoolSize, maxRetryAttempts);
+    LOGGER.info("StoreMigrationManager initialized with thread pool size: {}", threadPoolSize);
+    LOGGER.info("Maximum retry attempts set to: {}", maxRetryAttempts);
+    return manager;
+  }
+
+  /**
+   * Constructor to initialize the StoreMigrationManager with the specified thread pool size and max retry attempts.
+   * Only used for testing purpose, please use createStoreMigrationManager instead.
+   * @param threadPoolSize
+   * @param maxRetryAttempts
+   */
+  @Deprecated
+  public StoreMigrationManager(int threadPoolSize, int maxRetryAttempts) {
+    super(threadPoolSize);
+    this.maxRetryAttempts = maxRetryAttempts;
+    this.migrationRecords = new ConcurrentHashMap<>();
+  }
+
+  public ScheduledFuture<?> scheduleMigration(
+      String storeName,
+      String sourceCluster,
+      String destinationCluster,
+      int currentStep) {
+    return scheduleMigration(storeName, sourceCluster, destinationCluster, currentStep, 0);
+  }
+
+  public ScheduledFuture<?> scheduleMigration(
+      String storeName,
+      String sourceCluster,
+      String destinationCluster,
+      int currentStep,
+      int delayInSeconds) {
+    MigrationRecord record =
+        new MigrationRecord.Builder(storeName, sourceCluster, destinationCluster).currentStep(currentStep).build();
+    migrationRecords.put(storeName, record);
+    return scheduleNextStep(record, delayInSeconds);
+  }
+
+  public ScheduledFuture<?> scheduleNextStep(MigrationRecord record, int delayInSeconds) {
+    return super.scheduleNextStep(new StoreMigrationTask(record, this), delayInSeconds);
+  }
+
+  public void cleanupMigrationRecord(String storeName) {
+    migrationRecords.remove(storeName);
+  }
+
+  public int getMaxRetryAttempts() {
+    return maxRetryAttempts;
+  }
+}
