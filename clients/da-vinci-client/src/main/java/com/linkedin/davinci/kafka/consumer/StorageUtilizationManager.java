1bb2fe8cfae013e9dcb453fe2d0355cbdaa47a56
Zac Policzer <zacattackftw@gmail.com>
2025-06-05 08:31:29 -0700

    [server] Don't pause ingestion of real time data when quota is exceeded (#1816)
    * [server] Don't pause ingestion of real time or VT data when quota is exceeded
    
    ---------
    
    Co-authored-by: Zac Policzer <tester@linkedin.com>

--- clients/da-vinci-client/src/main/java/com/linkedin/davinci/kafka/consumer/StorageUtilizationManager.java
+++ clients/da-vinci-client/src/main/java/com/linkedin/davinci/kafka/consumer/StorageUtilizationManager.java
@@ -282,10 +282,20 @@ public class StorageUtilizationManager implements StoreDataChangedListener {
        * which will break the production.
        */
       for (String consumingTopic: getConsumingTopics(pcs)) {
-        pausePartition(partition, consumingTopic);
+        boolean isPartitionPaused = false;
+        if (Version.isIncrementalPushTopic(consumingTopic)) {
+          // We do not pause consumption from real time topics. The intent of this is that we still should drain
+          // the messages which have been submitted to the queue as best as we are able to. Ideally our mechanisms
+          // which kill the producer upstream have kicked in within a timely manner. We don't give the same preferential
+          // treatment to incremental push jobs as those have a lower priority and if we pause ingestion the job will
+          // at some point terminate.
+          pausePartition(partition, consumingTopic);
+          isPartitionPaused = true;
+        }
         String msgIdentifier = consumingTopic + "_" + partition + "_quota_exceeded";
         // Log quota exceeded info only once a minute per partition.
-        boolean shouldLogQuotaExceeded = !REDUNDANT_LOGGING_FILTER.isRedundantException(msgIdentifier);
+        boolean shouldLogQuotaExceeded =
+            !REDUNDANT_LOGGING_FILTER.isRedundantException(msgIdentifier) && isPartitionPaused;
         if (shouldLogQuotaExceeded) {
           LOGGER.info(
               "Quota exceeded for store version {} partition {}, paused this partition. Partition disk usage: {} >= partition quota: {}",
