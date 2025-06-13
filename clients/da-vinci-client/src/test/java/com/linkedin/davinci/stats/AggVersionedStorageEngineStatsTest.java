476b91f571a94e939a3da2315b717f97939e183f
Sourav Maji <tester@linkedin.com>
2025-06-13 14:50:12 -0700

    add tests

--- clients/da-vinci-client/src/test/java/com/linkedin/davinci/stats/AggVersionedStorageEngineStatsTest.java
+++ clients/da-vinci-client/src/test/java/com/linkedin/davinci/stats/AggVersionedStorageEngineStatsTest.java
@@ -1,14 +1,14 @@
 package com.linkedin.davinci.stats;
 
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 
 import com.linkedin.venice.meta.ReadOnlyStoreRepository;
 import com.linkedin.venice.meta.Store;
+import io.tehuti.Metric;
 import io.tehuti.metrics.MetricsRepository;
-import io.tehuti.metrics.Sensor;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 
@@ -16,18 +16,16 @@ public class AggVersionedStorageEngineStatsTest {
   @Test
   public void testVersionedStats() {
     String storeName = "testStore";
-    MetricsRepository metricsRepository = mock(MetricsRepository.class);
-    doReturn(mock(Sensor.class)).when(metricsRepository).sensor(anyString(), any());
+    MetricsRepository metricsRepository = new MetricsRepository();
     ReadOnlyStoreRepository metadataRepository = mock(ReadOnlyStoreRepository.class);
     doReturn(mock(Store.class)).when(metadataRepository).getStoreOrThrow(anyString());
     AggVersionedStorageEngineStats stats =
         new AggVersionedStorageEngineStats(metricsRepository, metadataRepository, false);
     stats.addStore(storeName);
-    AggVersionedStorageEngineStats.StorageEngineStatsReporter reporter =
-        new AggVersionedStorageEngineStats.StorageEngineStatsReporter(metricsRepository, storeName, "cluster1");
-    reporter.registerStats();
     stats.getStats(storeName, 1).getKeyCountEstimate();
     stats.getStats(storeName, 1).getDuplicateKeyCountEstimate();
-
+    metricsRepository.getMetric(".testStore_future--rocksdb_duplicate_key_count_estimate.Gauge");
+    Metric metric = metricsRepository.getMetric(".testStore_total--rocksdb_key_count_estimate.Gauge");
+    Assert.assertEquals(metric.value(), 0.0);
   }
 }
