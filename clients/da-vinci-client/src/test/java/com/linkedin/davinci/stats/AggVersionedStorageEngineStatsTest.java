efa2804411c0440283ffdafd1c58847e3e27e67e
Sourav Maji <tester@linkedin.com>
2025-06-12 22:33:18 -0700

    add tests

---
+++ clients/da-vinci-client/src/test/java/com/linkedin/davinci/stats/AggVersionedStorageEngineStatsTest.java
@@ -0,0 +1,31 @@
+package com.linkedin.davinci.stats;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+
+import com.linkedin.venice.meta.ReadOnlyStoreRepository;
+import com.linkedin.venice.meta.Store;
+import io.tehuti.metrics.MetricsRepository;
+import io.tehuti.metrics.Sensor;
+import org.testng.annotations.Test;
+
+
+public class AggVersionedStorageEngineStatsTest {
+  @Test
+  public void testVersionedStatss() {
+    String storeName = "testStore";
+    MetricsRepository metricsRepository = mock(MetricsRepository.class);
+    doReturn(mock(Sensor.class)).when(metricsRepository).sensor(anyString(), any());
+    ReadOnlyStoreRepository metadataRepository = mock(ReadOnlyStoreRepository.class);
+    doReturn(mock(Store.class)).when(metadataRepository).getStoreOrThrow(anyString());
+    AggVersionedStorageEngineStats stats =
+        new AggVersionedStorageEngineStats(metricsRepository, metadataRepository, false);
+    stats.addStore(storeName);
+
+    stats.getStats(storeName, 1).getKeyCountEstimate();
+    stats.getStats(storeName, 1).getDuplicateKeyCountEstimate();
+
+  }
+}
