e451b79969b99fa9defedc7381cc509a51d3e056
gabrieldrouin <info@gabrieldrouin.com>
2025-06-13 19:11:03 -0400

    revert test intervalinms value

--- internal/venice-common/src/test/java/com/linkedin/venice/helix/HelixReadOnlySchemaRepositoryTest.java
+++ internal/venice-common/src/test/java/com/linkedin/venice/helix/HelixReadOnlySchemaRepositoryTest.java
@@ -185,7 +185,7 @@ public class HelixReadOnlySchemaRepositoryTest {
     ZkClient zkClient = mock(ZkClient.class);
     HelixSchemaAccessor accessor = mock(HelixSchemaAccessor.class);
     HelixReadOnlySchemaRepository schemaRepository =
-        new HelixReadOnlySchemaRepository(storeRepository, zkClient, accessor, 10, 10000);
+        new HelixReadOnlySchemaRepository(storeRepository, zkClient, accessor, 10, 100);
     String storeName = "store";
     Store store = mock(Store.class);
     when(store.getName()).thenReturn(storeName);
