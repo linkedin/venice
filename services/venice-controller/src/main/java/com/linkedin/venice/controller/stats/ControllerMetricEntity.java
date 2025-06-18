a0a177910800b1129f3246e809e6fb6c6d80ea8b
Whitney Deng <whdeng@linkedin.com>
2025-06-17 18:02:23 -0700

    correct metric emmited for when store is repushed + cont'd refactor for `VeniceExecutionStatus`

--- services/venice-controller/src/main/java/com/linkedin/venice/controller/stats/ControllerMetricEntity.java
+++ services/venice-controller/src/main/java/com/linkedin/venice/controller/stats/ControllerMetricEntity.java
@@ -2,7 +2,7 @@ package com.linkedin.venice.controller.stats;
 
 import static com.linkedin.venice.stats.dimensions.VeniceMetricsDimensions.REPUSH_STORE_TRIGGER_SOURCE;
 import static com.linkedin.venice.stats.dimensions.VeniceMetricsDimensions.VENICE_CLUSTER_NAME;
-import static com.linkedin.venice.stats.dimensions.VeniceMetricsDimensions.VENICE_RESPONSE_STATUS_CODE_CATEGORY;
+import static com.linkedin.venice.stats.dimensions.VeniceMetricsDimensions.VENICE_EXECUTION_STATUS;
 import static com.linkedin.venice.stats.dimensions.VeniceMetricsDimensions.VENICE_STORE_NAME;
 import static com.linkedin.venice.stats.dimensions.VeniceMetricsDimensions.VENICE_STORE_VERSION;
 import static com.linkedin.venice.utils.Utils.setOf;
@@ -17,7 +17,7 @@ import java.util.Set;
 public enum ControllerMetricEntity {
   REPUSH_STORE_ENDPOINT_CALL_COUNT(
       MetricType.COUNTER, MetricUnit.NUMBER, "Count of all calls to a controller endpoint",
-      setOf(VENICE_STORE_NAME, VENICE_CLUSTER_NAME, REPUSH_STORE_TRIGGER_SOURCE, VENICE_RESPONSE_STATUS_CODE_CATEGORY)
+      setOf(VENICE_STORE_NAME, VENICE_CLUSTER_NAME, REPUSH_STORE_TRIGGER_SOURCE, VENICE_EXECUTION_STATUS)
   ),
   STORE_NOMINATED_FOR_SCHEDULED_COMPACTION(
       MetricType.GAUGE, MetricUnit.NUMBER,
