15cca0bcaf2a22605814e7b4cd206f2ccee5d081
Hao Xu <xhao@linkedin.com>
2025-07-14 15:05:51 -0700

    Avoid API gap for dependency.

--- internal/venice-common/src/main/java/com/linkedin/venice/controllerapi/D2ControllerClientFactory.java
+++ internal/venice-common/src/main/java/com/linkedin/venice/controllerapi/D2ControllerClientFactory.java
@@ -4,15 +4,29 @@ import com.linkedin.d2.balancer.D2Client;
 import com.linkedin.venice.exceptions.ErrorType;
 import com.linkedin.venice.exceptions.VeniceException;
 import com.linkedin.venice.exceptions.VeniceNoStoreException;
+import com.linkedin.venice.security.SSLFactory;
 import com.linkedin.venice.utils.SharedObjectFactory;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 import java.util.function.Supplier;
 
 
 public class D2ControllerClientFactory {
   private static final SharedObjectFactory<D2ControllerClient> SHARED_OBJECT_FACTORY = new SharedObjectFactory<>();
   private static final Map<ControllerClient, String> CONTROLLER_CLIENT_TO_IDENTIFIER_MAP = new HashMap<>();
+
+  @Deprecated
+  public static D2ControllerClient getControllerClient(
+      String d2ServiceName,
+      String clusterName,
+      String d2ZkHost,
+      Optional<SSLFactory> sslFactory) {
+    final String clientIdentifier = clusterName + d2ServiceName + d2ZkHost;
+    return createIfAbsent(
+        clientIdentifier,
+        () -> new D2ControllerClient(d2ServiceName, clusterName, d2ZkHost, sslFactory));
+  }
 
   public static D2ControllerClient getControllerClient(String d2ServiceName, String clusterName, D2Client d2Client) {
     final String clientIdentifier = clusterName + d2ServiceName + d2Client.hashCode();
@@ -36,6 +50,19 @@ public class D2ControllerClientFactory {
         D2ControllerClient.discoverCluster(d2Client, d2ServiceName, storeName, retryAttempts);
     checkDiscoveryResponse(storeName, discoveryResponse);
     return getControllerClient(d2ServiceName, discoveryResponse.getCluster(), d2Client);
+  }
+
+  @Deprecated
+  public static D2ControllerClient discoverAndConstructControllerClient(
+      String storeName,
+      String d2ServiceName,
+      String d2ZkHost,
+      Optional<SSLFactory> sslFactory,
+      int retryAttempts) {
+    D2ServiceDiscoveryResponse discoveryResponse =
+        D2ControllerClient.discoverCluster(d2ZkHost, d2ServiceName, storeName, retryAttempts, sslFactory);
+    checkDiscoveryResponse(storeName, discoveryResponse);
+    return getControllerClient(d2ServiceName, discoveryResponse.getCluster(), d2ZkHost, sslFactory);
   }
 
   private static D2ControllerClient createIfAbsent(
