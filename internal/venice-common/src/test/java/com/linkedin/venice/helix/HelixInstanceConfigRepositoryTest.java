87604d84969e9051c82df09266c6ef0d7466d904
Nisarg Thakkar <nisargtha@gmail.com>
2025-08-11 13:03:26 -0700

    [router][server] Fix NPE error in HelixInstanceConfigRepository (#2014)
    If a storage cluster's Helix configuration changes from topology-unaware to topology-aware, "HelixInstanceConfigRepository" runs into an "NullPointerException". This change prevents that scenario

--- internal/venice-common/src/test/java/com/linkedin/venice/helix/HelixInstanceConfigRepositoryTest.java
+++ internal/venice-common/src/test/java/com/linkedin/venice/helix/HelixInstanceConfigRepositoryTest.java
@@ -39,7 +39,8 @@ public class HelixInstanceConfigRepositoryTest {
     return helixManager;
   }
 
-  private List<InstanceConfig> mockInstanceConfigs(Map<String, InstanceVirtualZone> instanceGroupMapping) {
+  private List<InstanceConfig> mockInstanceConfigsWithVirtualGrouping(
+      Map<String, InstanceVirtualZone> instanceGroupMapping) {
     List<InstanceConfig> instanceConfigs = new ArrayList<>();
     instanceGroupMapping.forEach((instanceId, zoneAndGroup) -> {
       ZNRecord record = new ZNRecord(instanceId);
@@ -47,6 +48,19 @@ public class HelixInstanceConfigRepositoryTest {
           InstanceConfig.InstanceConfigProperty.DOMAIN.name(),
           VENICE_FAULT_ZONE_TYPE_GROUP + "=" + zoneAndGroup.virtualGroup + "," + VENICE_FAULT_ZONE_TYPE_ZONE + "="
               + zoneAndGroup.virtualZone);
+      InstanceConfig instanceConfig = new InstanceConfig(record);
+      instanceConfigs.add(instanceConfig);
+    });
+    return instanceConfigs;
+  }
+
+  private List<InstanceConfig> mockInstanceConfigs(Map<String, String> instanceGroupMapping) {
+    List<InstanceConfig> instanceConfigs = new ArrayList<>();
+    instanceGroupMapping.forEach((instanceId, group) -> {
+      ZNRecord record = new ZNRecord(instanceId);
+      record.setSimpleField(
+          InstanceConfig.InstanceConfigProperty.DOMAIN.name(),
+          VENICE_FAULT_ZONE_TYPE_GROUP + "=" + group);
       InstanceConfig instanceConfig = new InstanceConfig(record);
       instanceConfigs.add(instanceConfig);
     });
@@ -78,7 +92,7 @@ public class HelixInstanceConfigRepositoryTest {
     instanceGroupMapping.put(nodeId5, new InstanceVirtualZone(group1, zone1));
     instanceGroupMapping.put(nodeId6, new InstanceVirtualZone(group3, zone2));
 
-    List<InstanceConfig> instanceConfigs1 = mockInstanceConfigs(instanceGroupMapping);
+    List<InstanceConfig> instanceConfigs1 = mockInstanceConfigsWithVirtualGrouping(instanceGroupMapping);
 
     Collections.shuffle(instanceConfigs1);
 
@@ -113,6 +127,71 @@ public class HelixInstanceConfigRepositoryTest {
     Assert.assertEquals(repo1.getInstanceGroupId(nodeId4), 1);
     Assert.assertEquals(repo1.getInstanceGroupId(nodeId5), 0);
     Assert.assertEquals(repo1.getInstanceGroupId(nodeId6), 1);
+    Assert.assertEquals(repo1.getInstanceGroupId(unknownNodeId), 0);
+
+    PropertyKey.Builder builder = new PropertyKey.Builder(clusterName);
+
+    repo1.clear();
+    verify(manager, times(1)).removeListener(builder.clusterConfig(), repo1);
+    verify(manager, times(1)).removeListener(builder.instanceConfigs(), repo1);
+  }
+
+  @Test
+  public void testTopologyAwareSwitch() {
+    String nodeId1 = "node_id_1_port";
+    String nodeId2 = "node_id_2_port";
+    String nodeId3 = "node_id_3_port";
+    String nodeId4 = "node_id_4_port";
+    String nodeId5 = "node_id_5_port";
+    String nodeId6 = "node_id_6_port";
+    String unknownNodeId = "unknown_node_id_port";
+
+    String group1 = "group_1";
+    String group2 = "group_2";
+    String group3 = "group_3";
+
+    Map<String, String> instanceGroupMapping = new TreeMap<>();
+    instanceGroupMapping.put(nodeId1, group1);
+    instanceGroupMapping.put(nodeId2, group2);
+    instanceGroupMapping.put(nodeId3, group3);
+    instanceGroupMapping.put(nodeId4, group2);
+    instanceGroupMapping.put(nodeId5, group1);
+    instanceGroupMapping.put(nodeId6, group3);
+    List<InstanceConfig> instanceConfigs1 = mockInstanceConfigs(instanceGroupMapping);
+
+    Collections.shuffle(instanceConfigs1);
+
+    SafeHelixManager manager = getMockHelixManager();
+    String clusterName = manager.getClusterName();
+    ClusterConfig clusterConfig = manager.getConfigAccessor().getClusterConfig(clusterName);
+
+    doReturn(null).when(clusterConfig).getFaultZoneType();
+    Mockito.clearInvocations(manager);
+
+    HelixInstanceConfigRepository repo1 = new HelixInstanceConfigRepository(manager);
+    repo1.refresh();
+    repo1.onInstanceConfigChange(instanceConfigs1, null);
+    Assert.assertEquals(repo1.getGroupCount(), 1);
+    Assert.assertEquals(repo1.getInstanceGroupId(nodeId1), 0);
+    Assert.assertEquals(repo1.getInstanceGroupId(nodeId2), 0);
+    Assert.assertEquals(repo1.getInstanceGroupId(nodeId3), 0);
+    Assert.assertEquals(repo1.getInstanceGroupId(nodeId4), 0);
+    Assert.assertEquals(repo1.getInstanceGroupId(nodeId5), 0);
+    Assert.assertEquals(repo1.getInstanceGroupId(nodeId6), 0);
+    Assert.assertEquals(repo1.getInstanceGroupId(unknownNodeId), 0);
+
+    // Cluster becomes TOPLOGY_AWARE
+    doReturn(VENICE_FAULT_ZONE_TYPE_GROUP).when(clusterConfig).getFaultZoneType();
+    Mockito.clearInvocations(manager);
+
+    repo1.onClusterConfigChange(clusterConfig, null);
+    Assert.assertEquals(repo1.getGroupCount(), 3);
+    Assert.assertEquals(repo1.getInstanceGroupId(nodeId1), 0);
+    Assert.assertEquals(repo1.getInstanceGroupId(nodeId2), 1);
+    Assert.assertEquals(repo1.getInstanceGroupId(nodeId3), 2);
+    Assert.assertEquals(repo1.getInstanceGroupId(nodeId4), 1);
+    Assert.assertEquals(repo1.getInstanceGroupId(nodeId5), 0);
+    Assert.assertEquals(repo1.getInstanceGroupId(nodeId6), 2);
     Assert.assertEquals(repo1.getInstanceGroupId(unknownNodeId), 0);
 
     PropertyKey.Builder builder = new PropertyKey.Builder(clusterName);
