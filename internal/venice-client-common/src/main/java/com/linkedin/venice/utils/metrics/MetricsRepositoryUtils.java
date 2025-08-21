0a40b26d884ced9b37ccf90885a4b3fc8cf7b658
nvijayak <nvijayakumar@linkedin.com>
2025-08-21 15:56:40 -0700

    Add  tests to verify `call_size` metrics are correctly recorded in otel

--- internal/venice-client-common/src/main/java/com/linkedin/venice/utils/metrics/MetricsRepositoryUtils.java
+++ internal/venice-client-common/src/main/java/com/linkedin/venice/utils/metrics/MetricsRepositoryUtils.java
@@ -77,4 +77,5 @@ public class MetricsRepositoryUtils {
             .setMaxMetricsMeasurementTimeoutInMs(maxMetricsMeasurementTimeoutMs)
             .build());
   }
+
 }
--- services/venice-router/src/test/java/com/linkedin/venice/router/stats/RouterHttpRequestStatsTest.java
+++ services/venice-router/src/test/java/com/linkedin/venice/router/stats/RouterHttpRequestStatsTest.java
@@ -1,13 +1,15 @@
 package com.linkedin.venice.router.stats;
 
+import static com.linkedin.venice.read.RequestType.*;
 import static com.linkedin.venice.router.stats.RouterHttpRequestStats.RouterTehutiMetricNameEnum.HEALTHY_REQUEST;
 import static com.linkedin.venice.router.stats.RouterHttpRequestStats.RouterTehutiMetricNameEnum.KEY_SIZE_IN_BYTE;
 import static com.linkedin.venice.router.stats.RouterHttpRequestStats.RouterTehutiMetricNameEnum.REQUEST_SIZE;
 import static com.linkedin.venice.router.stats.RouterHttpRequestStats.RouterTehutiMetricNameEnum.RESPONSE_SIZE;
+import static com.linkedin.venice.stats.VeniceMetricsRepository.*;
 import static com.linkedin.venice.stats.VeniceOpenTelemetryMetricNamingFormat.PASCAL_CASE;
-import static com.linkedin.venice.stats.dimensions.VeniceMetricsDimensions.VENICE_CLUSTER_NAME;
-import static com.linkedin.venice.stats.dimensions.VeniceMetricsDimensions.VENICE_REQUEST_METHOD;
-import static com.linkedin.venice.stats.dimensions.VeniceMetricsDimensions.VENICE_STORE_NAME;
+import static com.linkedin.venice.stats.dimensions.VeniceMetricsDimensions.*;
+import static com.linkedin.venice.utils.OpenTelemetryDataPointTestUtils.validateExponentialHistogramPointData;
+import static com.linkedin.venice.utils.metrics.MetricsRepositoryUtils.*;
 import static org.mockito.Mockito.mock;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertFalse;
@@ -17,8 +19,12 @@ import static org.testng.Assert.assertTrue;
 
 import com.linkedin.alpini.router.monitoring.ScatterGatherStats;
 import com.linkedin.venice.read.RequestType;
+import com.linkedin.venice.router.RouterServer;
+import com.linkedin.venice.stats.VeniceMetricsConfig;
+import com.linkedin.venice.stats.VeniceMetricsRepository;
 import com.linkedin.venice.stats.VeniceOpenTelemetryMetricNamingFormat;
 import com.linkedin.venice.stats.VeniceOpenTelemetryMetricsRepository;
+import com.linkedin.venice.stats.dimensions.MessageType;
 import com.linkedin.venice.stats.dimensions.VeniceMetricsDimensions;
 import com.linkedin.venice.stats.metrics.MetricEntity;
 import com.linkedin.venice.stats.metrics.MetricType;
@@ -29,6 +35,10 @@ import com.linkedin.venice.utils.Utils;
 import com.linkedin.venice.utils.metrics.MetricsRepositoryUtils;
 import io.netty.handler.codec.http.HttpResponseStatus;
 import io.opentelemetry.api.common.Attributes;
+import io.opentelemetry.api.common.AttributesBuilder;
+import io.opentelemetry.sdk.metrics.data.MetricData;
+import io.opentelemetry.sdk.metrics.export.MetricReader;
+import io.opentelemetry.sdk.testing.exporter.InMemoryMetricReader;
 import io.tehuti.metrics.MetricsRepository;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -166,5 +176,87 @@ public class RouterHttpRequestStatsTest {
     routerHttpRequestStats.recordResponseSize(1024.0);
     String responseSizeMetricName = "." + storeName + "--" + RESPONSE_SIZE.getMetricName() + ".Avg";
     assertNotNull(metricsRepository.getMetric(responseSizeMetricName));
+  }
+
+  @Test
+  public void testEmitCallSizeMetrics() {
+    String storeName = "test-store";
+    String clusterName = "test-cluster";
+    InMemoryMetricReader inMemoryMetricReader = InMemoryMetricReader.create();
+    VeniceMetricsRepository metricsRepository =
+        getVeniceMetricsRepositoryForRouter(RouterServer.ROUTER_SERVICE_METRIC_ENTITIES, true, inMemoryMetricReader);
+    RouterHttpRequestStats routerHttpRequestStats = new RouterHttpRequestStats(
+        metricsRepository,
+        storeName,
+        clusterName,
+        RequestType.SINGLE_GET,
+        mock(ScatterGatherStats.class),
+        false, // keyValueProfilingEnabled = true
+        null);
+
+    int requestSize = 512;
+
+    // Record call size metrics
+    routerHttpRequestStats.recordRequestSize(requestSize);
+
+    // validate request size metrics
+    validateOtelMetrics(
+        inMemoryMetricReader,
+        storeName,
+        clusterName,
+        RequestType.SINGLE_GET,
+        MessageType.REQUEST,
+        "",
+        512.0,
+        "call_size");
+
+  }
+
+  private void validateOtelMetrics(
+      InMemoryMetricReader inMemoryMetricReader,
+      String storeName,
+      String clusterName,
+      RequestType requestType,
+      MessageType messageType,
+      String otelPrefix,
+      double expectedDataSize,
+      String expectedMetricName) {
+    Attributes attributes = getAttributes(storeName, clusterName, requestType, messageType);
+    Collection<MetricData> metricsData = inMemoryMetricReader.collectAllMetrics();
+    assertEquals(
+        1,
+        metricsData.size(),
+        String.format("There should be exactly %d metric data points collected for the test case", metricsData.size()));
+    validateExponentialHistogramPointData(
+        inMemoryMetricReader,
+        expectedDataSize,
+        expectedDataSize,
+        1,
+        expectedDataSize,
+        attributes,
+        expectedMetricName,
+        otelPrefix);
+
+  }
+
+  private Attributes getAttributes(String storeName, String clusterName, RequestType requestType, MessageType type) {
+    AttributesBuilder builder = Attributes.builder()
+        .put(VENICE_STORE_NAME.getDimensionNameInDefaultFormat(), storeName)
+        .put(VENICE_CLUSTER_NAME.getDimensionNameInDefaultFormat(), clusterName)
+        .put(VENICE_REQUEST_METHOD.getDimensionNameInDefaultFormat(), requestType.name().toLowerCase())
+        .put(VENICE_MESSAGE_TYPE.getDimensionNameInDefaultFormat(), type.getDimensionValue());
+    return builder.build();
+
+  }
+
+  public static VeniceMetricsRepository getVeniceMetricsRepositoryForRouter(
+      Collection<MetricEntity> metricEntities,
+      boolean emitOtelMetrics,
+      MetricReader additionalMetricReader) {
+    return new VeniceMetricsRepository(
+        new VeniceMetricsConfig.Builder().setMetricEntities(metricEntities)
+            .setEmitOtelMetrics(emitOtelMetrics)
+            .setOtelAdditionalMetricsReader(additionalMetricReader)
+            .build());
   }
 }
