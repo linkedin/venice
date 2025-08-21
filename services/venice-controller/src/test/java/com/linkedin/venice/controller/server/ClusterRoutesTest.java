6466da61167a06d6c0c6f15643c520a4fba9df44
Minh Nguyen <minnguyen@linkedin.com>
2025-08-21 11:13:32 -0700

    Add unittest

---
+++ services/venice-controller/src/test/java/com/linkedin/venice/controller/server/ClusterRoutesTest.java
@@ -0,0 +1,58 @@
+package com.linkedin.venice.controller.server;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.testng.Assert.assertFalse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.linkedin.venice.controller.Admin;
+import com.linkedin.venice.controller.VeniceParentHelixAdmin;
+import com.linkedin.venice.controllerapi.ControllerApiConstants;
+import com.linkedin.venice.controllerapi.ControllerResponse;
+import com.linkedin.venice.utils.ObjectMapperFactory;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import spark.QueryParamsMap;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+
+public class ClusterRoutesTest {
+  private static final ObjectMapper OBJECT_MAPPER = ObjectMapperFactory.getInstance();
+  private static final String TEST_CLUSTER = "test_cluster";
+  private Admin mockAdmin;
+
+  @BeforeMethod(alwaysRun = true)
+  public void setUp() {
+    mockAdmin = mock(VeniceParentHelixAdmin.class);
+  }
+
+  @Test
+  public void testUpdateDarkClusterConfig() throws Exception {
+    doReturn(true).when(mockAdmin).isLeaderControllerFor(anyString());
+
+    Request request = mock(Request.class);
+
+    QueryParamsMap queryParamsMap = mock(QueryParamsMap.class);
+    when(request.queryMap()).thenReturn(queryParamsMap);
+
+    Map<String, String[]> queryMapData = new HashMap<>();
+    queryMapData.put(ControllerApiConstants.CLUSTER, new String[] { TEST_CLUSTER });
+    queryMapData.put(ControllerApiConstants.DARK_CLUSTER_TARGET_STORES, new String[] { "store1,store2" });
+
+    when(queryParamsMap.toMap()).thenReturn(queryMapData);
+    when(request.queryParams(ControllerApiConstants.CLUSTER)).thenReturn(TEST_CLUSTER);
+    Route updateDarkClusterConfigRoute = new ClusterRoutes(false, Optional.empty()).updateDarkClusterConfig(mockAdmin);
+
+    ControllerResponse response = OBJECT_MAPPER.readValue(
+        updateDarkClusterConfigRoute.handle(request, mock(Response.class)).toString(),
+        ControllerResponse.class);
+    assertFalse(response.isError());
+  }
+}
