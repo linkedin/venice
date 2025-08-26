7ff9a0faf31d8575d5bb41eeaa33ee4ace73a259
Minh Nguyen <minnguyen@linkedin.com>
2025-08-25 19:02:53 -0700

    Fix unittest

--- internal/venice-common/src/test/java/com/linkedin/venice/meta/TestDarkClusterConfig.java
+++ internal/venice-common/src/test/java/com/linkedin/venice/meta/TestDarkClusterConfig.java
@@ -1,5 +1,6 @@
 package com.linkedin.venice.meta;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.linkedin.venice.ConfigKeys;
 import com.linkedin.venice.utils.ObjectMapperFactory;
@@ -13,22 +14,29 @@ import org.testng.annotations.Test;
 
 public class TestDarkClusterConfig {
   private static final ObjectMapper OBJECT_MAPPER = ObjectMapperFactory.getInstance();
-  private static final String SERIALIZED_CONFIG =
-      String.format("{\"%s\":[\"store1\",\"store2\"]}", ConfigKeys.STORES_TO_REPLICATE);
+  private static final String SERIALIZED_CONFIG = String.format(
+      "{\"%s\":true,\"%s\":[\"store1\",\"store2\"]}",
+      ConfigKeys.IS_DARK_CLUSTER,
+      ConfigKeys.STORES_TO_REPLICATE);
 
   @Test
   public void deserializesAsJson() throws IOException {
     DarkClusterConfig config = OBJECT_MAPPER.readValue(SERIALIZED_CONFIG, DarkClusterConfig.class);
     Set<String> expectedStores = new HashSet<>(Arrays.asList("store1", "store2"));
     Assert.assertEquals(config.getStoresToReplicate(), expectedStores);
+    Assert.assertTrue(config.getIsDarkCluster());
   }
 
   @Test
   public void serializesAsJson() throws IOException {
     DarkClusterConfig config = new DarkClusterConfig();
+    config.setIsDarkCluster(true);
     config.setStoresToReplicate(Arrays.asList("store1", "store2"));
 
     String serializedTestObj = OBJECT_MAPPER.writerWithDefaultPrettyPrinter().writeValueAsString(config);
-    Assert.assertEquals(OBJECT_MAPPER.readTree(serializedTestObj), OBJECT_MAPPER.readTree(SERIALIZED_CONFIG));
+    JsonNode expected = OBJECT_MAPPER.readTree(SERIALIZED_CONFIG);
+    JsonNode actual = OBJECT_MAPPER.readTree(serializedTestObj);
+
+    Assert.assertEquals(actual, expected);
   }
 }
