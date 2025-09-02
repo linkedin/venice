f363ad41540bb8dcb9e62f73c0d1c98135ce813e
Gaojie Liu <goliu@linkedin.com>
2025-09-02 14:05:19 -0700

    Reenabled disabled test

--- clients/da-vinci-client/src/test/java/com/linkedin/davinci/store/rocksdb/RocksDBStoragePartitionTest.java
+++ clients/da-vinci-client/src/test/java/com/linkedin/davinci/store/rocksdb/RocksDBStoragePartitionTest.java
@@ -255,7 +255,7 @@ public class RocksDBStoragePartitionTest {
    * This test takes way too long... both per-permutation and because there are so many permutations.
    * TODO: Speed it up, or else refactor the permutations into several subclasses so they can run concurrently.
    */
-  @Test(dataProvider = "Six-True-and-False", dataProviderClass = DataProviderUtils.class, enabled = false)
+  @Test(dataProvider = "Six-True-and-False", dataProviderClass = DataProviderUtils.class)
   public void testIngestion(
       boolean sorted,
       boolean interrupted,
