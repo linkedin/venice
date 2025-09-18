ffdfe6846417dcc2d1d32a5afba9e40a93f1aec5
Lei Lu <lelu@linkedin.com>
2025-09-18 11:14:23 -0700

    [client] ClientStats metrics OTEL migration (#2102)
    * [client] ClientStats metrics OTEL migration
    
    - This PR migrates the following metrics to OTel:
    request_serialization_time -> REQUEST_SERIALIZATION_TIME
    request_submission_to_response_handling_time -> CALL_SUBMISSION_TO_HANDLING_TIME
    response_deserialization_time -> RESPONSE_DESERIALIZATION_TIME
    response_decompression_time -> RESPONSE_DECOMPRESSION_TIME
    response_ttfr, response_tt50pr, response_tt90pr -> RESPONSE_BATCH_STREAM_PROGRESS_TIME
    
    - Two tehuti metrics are removed:
    response_tt95pr
    response_tt99pr

--- clients/venice-thin-client/src/test/java/com/linkedin/venice/client/stats/ClientStatsTest.java
+++ clients/venice-thin-client/src/test/java/com/linkedin/venice/client/stats/ClientStatsTest.java
@@ -13,17 +13,209 @@ import static com.linkedin.venice.utils.OpenTelemetryDataPointTestUtils.validate
 import static org.testng.Assert.assertEquals;
 
 import com.linkedin.venice.client.store.ClientConfig;
+import com.linkedin.venice.read.RequestType;
 import com.linkedin.venice.stats.VeniceMetricsRepository;
+import com.linkedin.venice.stats.dimensions.StreamProgress;
+import com.linkedin.venice.stats.dimensions.VeniceMetricsDimensions;
 import com.linkedin.venice.stats.dimensions.VeniceResponseStatusCategory;
 import io.opentelemetry.api.common.Attributes;
 import io.opentelemetry.sdk.testing.exporter.InMemoryMetricReader;
 import io.tehuti.Metric;
 import java.util.Map;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 
 public class ClientStatsTest {
+  private static final String STORE = "test_store";
+  private static final RequestType REQUEST_TYPE = RequestType.MULTI_GET_STREAMING;
+  private InMemoryMetricReader inMemoryMetricReader;
+  private VeniceMetricsRepository metricsRepository;
+  private ClientStats clientStats;
+
+  @BeforeMethod
+  public void setUp() {
+    inMemoryMetricReader = InMemoryMetricReader.create();
+    metricsRepository = VeniceMetricsRepository
+        .getVeniceMetricsRepository(THIN_CLIENT, BasicClientStats.CLIENT_METRIC_ENTITIES, true, inMemoryMetricReader);
+    clientStats = ClientStats.getClientStats(metricsRepository, STORE, REQUEST_TYPE, null, THIN_CLIENT);
+  }
+
+  @AfterMethod
+  public void tearDown() {
+    if (metricsRepository != null) {
+      metricsRepository.close();
+    }
+  }
+
+  private Attributes baseAttributes() {
+    return Attributes.builder()
+        .put(
+            metricsRepository.getOpenTelemetryMetricsRepository()
+                .getDimensionName(VeniceMetricsDimensions.VENICE_STORE_NAME),
+            STORE)
+        .put(
+            metricsRepository.getOpenTelemetryMetricsRepository()
+                .getDimensionName(VeniceMetricsDimensions.VENICE_REQUEST_METHOD),
+            REQUEST_TYPE.getDimensionValue())
+        .build();
+  }
+
+  @Test
+  public void testRequestSerializationTimeOtel() {
+    clientStats.recordRequestSerializationTime(10);
+    clientStats.recordRequestSerializationTime(20);
+    clientStats.recordRequestSerializationTime(30);
+
+    validateExponentialHistogramPointData(
+        inMemoryMetricReader,
+        10,
+        30,
+        3,
+        60,
+        baseAttributes(),
+        // entity name is enum name lowercased
+        ClientMetricEntity.REQUEST_SERIALIZATION_TIME.getMetricEntity().getMetricName(),
+        THIN_CLIENT.getMetricsPrefix());
+  }
+
+  @Test
+  public void testResponseDecompressionTimeOtel() {
+    clientStats.recordResponseDecompressionTime(7);
+    clientStats.recordResponseDecompressionTime(13);
+
+    validateExponentialHistogramPointData(
+        inMemoryMetricReader,
+        7,
+        13,
+        2,
+        20,
+        baseAttributes(),
+        ClientMetricEntity.RESPONSE_DECOMPRESSION_TIME.getMetricEntity().getMetricName(),
+        THIN_CLIENT.getMetricsPrefix());
+  }
+
+  @Test
+  public void testResponseDeserializationTimeOtel() {
+    clientStats.recordResponseDeserializationTime(5);
+    clientStats.recordResponseDeserializationTime(15);
+    clientStats.recordResponseDeserializationTime(25);
+
+    validateExponentialHistogramPointData(
+        inMemoryMetricReader,
+        5,
+        25,
+        3,
+        45,
+        baseAttributes(),
+        ClientMetricEntity.RESPONSE_DESERIALIZATION_TIME.getMetricEntity().getMetricName(),
+        THIN_CLIENT.getMetricsPrefix());
+  }
+
+  @Test
+  public void testCallSubmissionToHandlingTimeOtel() {
+    clientStats.recordRequestSubmissionToResponseHandlingTime(3);
+    clientStats.recordRequestSubmissionToResponseHandlingTime(9);
+
+    validateExponentialHistogramPointData(
+        inMemoryMetricReader,
+        3,
+        9,
+        2,
+        12,
+        baseAttributes(),
+        ClientMetricEntity.CALL_SUBMISSION_TO_HANDLING_TIME.getMetricEntity().getMetricName(),
+        THIN_CLIENT.getMetricsPrefix());
+  }
+
+  @Test
+  public void testResponseBatchStreamProgressTimeOtel() {
+    // TTFR
+    clientStats.recordStreamingResponseTimeToReceiveFirstRecord(11);
+    // TT50PR
+    clientStats.recordStreamingResponseTimeToReceive50PctRecord(22);
+    // TT90PR
+    clientStats.recordStreamingResponseTimeToReceive90PctRecord(33);
+
+    // base attributes + delivery.progress for FIRST
+    Attributes ttfrAttrs = Attributes.builder()
+        .put(
+            metricsRepository.getOpenTelemetryMetricsRepository()
+                .getDimensionName(VeniceMetricsDimensions.VENICE_STORE_NAME),
+            STORE)
+        .put(
+            metricsRepository.getOpenTelemetryMetricsRepository()
+                .getDimensionName(VeniceMetricsDimensions.VENICE_REQUEST_METHOD),
+            REQUEST_TYPE.getDimensionValue())
+        .put(
+            metricsRepository.getOpenTelemetryMetricsRepository()
+                .getDimensionName(VeniceMetricsDimensions.VENICE_STREAM_PROGRESS),
+            StreamProgress.FIRST.getDimensionValue())
+        .build();
+
+    Attributes tt50Attrs = Attributes.builder()
+        .put(
+            metricsRepository.getOpenTelemetryMetricsRepository()
+                .getDimensionName(VeniceMetricsDimensions.VENICE_STORE_NAME),
+            STORE)
+        .put(
+            metricsRepository.getOpenTelemetryMetricsRepository()
+                .getDimensionName(VeniceMetricsDimensions.VENICE_REQUEST_METHOD),
+            REQUEST_TYPE.getDimensionValue())
+        .put(
+            metricsRepository.getOpenTelemetryMetricsRepository()
+                .getDimensionName(VeniceMetricsDimensions.VENICE_STREAM_PROGRESS),
+            StreamProgress.PCT_50.getDimensionValue())
+        .build();
+
+    Attributes tt90Attrs = Attributes.builder()
+        .put(
+            metricsRepository.getOpenTelemetryMetricsRepository()
+                .getDimensionName(VeniceMetricsDimensions.VENICE_STORE_NAME),
+            STORE)
+        .put(
+            metricsRepository.getOpenTelemetryMetricsRepository()
+                .getDimensionName(VeniceMetricsDimensions.VENICE_REQUEST_METHOD),
+            REQUEST_TYPE.getDimensionValue())
+        .put(
+            metricsRepository.getOpenTelemetryMetricsRepository()
+                .getDimensionName(VeniceMetricsDimensions.VENICE_STREAM_PROGRESS),
+            StreamProgress.PCT_90.getDimensionValue())
+        .build();
+
+    String metricName = ClientMetricEntity.RESPONSE_BATCH_STREAM_PROGRESS_TIME.getMetricEntity().getMetricName();
+
+    validateExponentialHistogramPointData(
+        inMemoryMetricReader,
+        11,
+        11,
+        1,
+        11,
+        ttfrAttrs,
+        metricName,
+        THIN_CLIENT.getMetricsPrefix());
+    validateExponentialHistogramPointData(
+        inMemoryMetricReader,
+        22,
+        22,
+        1,
+        22,
+        tt50Attrs,
+        metricName,
+        THIN_CLIENT.getMetricsPrefix());
+    validateExponentialHistogramPointData(
+        inMemoryMetricReader,
+        33,
+        33,
+        1,
+        33,
+        tt90Attrs,
+        metricName,
+        THIN_CLIENT.getMetricsPrefix());
+  }
+
   @Test
   public void testAppTimedOutRequestResultRatio() {
     InMemoryMetricReader inMemoryMetricReader = InMemoryMetricReader.create();
--- clients/venice-thin-client/src/test/java/com/linkedin/venice/client/store/streaming/StreamingResponseTrackerTest.java
+++ clients/venice-thin-client/src/test/java/com/linkedin/venice/client/store/streaming/StreamingResponseTrackerTest.java
@@ -19,19 +19,19 @@ public class StreamingResponseTrackerTest {
     StreamingResponseTracker streamingResponseTracker =
         new StreamingResponseTracker(mockClientStats, keyCount, startTimeNs);
     streamingResponseTracker.recordReceived();
-    verifyRecordStreamingResponseTime(mockClientStats, true, false, false, false, false);
+    verifyRecordStreamingResponseTime(mockClientStats, true, false, false);
     for (int i = 0; i < 48; i++) {
       streamingResponseTracker.recordReceived();
     }
-    verifyRecordStreamingResponseTime(mockClientStats, true, false, false, false, false);
+    verifyRecordStreamingResponseTime(mockClientStats, true, false, false);
     streamingResponseTracker.recordReceived();
-    verifyRecordStreamingResponseTime(mockClientStats, true, true, false, false, false);
+    verifyRecordStreamingResponseTime(mockClientStats, true, true, false);
     for (int i = 0; i < 48; i++) {
       streamingResponseTracker.recordReceived();
     }
-    verifyRecordStreamingResponseTime(mockClientStats, true, true, true, true, false);
+    verifyRecordStreamingResponseTime(mockClientStats, true, true, true);
     streamingResponseTracker.recordReceived();
-    verifyRecordStreamingResponseTime(mockClientStats, true, true, true, true, true);
+    verifyRecordStreamingResponseTime(mockClientStats, true, true, true);
   }
 
   @Test
@@ -43,18 +43,16 @@ public class StreamingResponseTrackerTest {
         new StreamingResponseTracker(mockClientStats, keyCount, startTimeNs);
     streamingResponseTracker.recordReceived();
     // The key count ranges are calculated using integers (floor) so 2*99/100 = 1
-    verifyRecordStreamingResponseTime(mockClientStats, true, true, true, true, true);
+    verifyRecordStreamingResponseTime(mockClientStats, true, true, true);
     streamingResponseTracker.recordReceived();
-    verifyRecordStreamingResponseTime(mockClientStats, true, true, true, true, true);
+    verifyRecordStreamingResponseTime(mockClientStats, true, true, true);
   }
 
   private void verifyRecordStreamingResponseTime(
       ClientStats mockClientStats,
       boolean ttfr,
       boolean tt50pr,
-      boolean tt90pr,
-      boolean tt95pr,
-      boolean tt99pr) {
+      boolean tt90pr) {
     if (ttfr) {
       verify(mockClientStats, times(1)).recordStreamingResponseTimeToReceiveFirstRecord(anyDouble());
     } else {
@@ -69,16 +67,6 @@ public class StreamingResponseTrackerTest {
       verify(mockClientStats, times(1)).recordStreamingResponseTimeToReceive90PctRecord(anyDouble());
     } else {
       verify(mockClientStats, never()).recordStreamingResponseTimeToReceive90PctRecord(anyDouble());
-    }
-    if (tt95pr) {
-      verify(mockClientStats, times(1)).recordStreamingResponseTimeToReceive95PctRecord(anyDouble());
-    } else {
-      verify(mockClientStats, never()).recordStreamingResponseTimeToReceive95PctRecord(anyDouble());
-    }
-    if (tt99pr) {
-      verify(mockClientStats, times(1)).recordStreamingResponseTimeToReceive99PctRecord(anyDouble());
-    } else {
-      verify(mockClientStats, never()).recordStreamingResponseTimeToReceive99PctRecord(anyDouble());
     }
   }
 }
