2635dcd5ed1a8ab8ddbcb13b7172b5d76d600824
Koorous Vargha <kvargha@linkedin.com>
2025-10-06 11:57:32 -0700

    Fix writer value

--- clients/da-vinci-client/src/main/java/com/linkedin/davinci/client/DaVinciRecordTransformerUtility.java
+++ clients/da-vinci-client/src/main/java/com/linkedin/davinci/client/DaVinciRecordTransformerUtility.java
@@ -1,5 +1,7 @@
 package com.linkedin.davinci.client;
 
+import static com.linkedin.davinci.client.DaVinciRecordTransformerRecordMetadata.SENTINEL_WRITER_SCHEMA_ID;
+
 import com.linkedin.davinci.store.AbstractStorageIterator;
 import com.linkedin.davinci.store.StorageEngine;
 import com.linkedin.davinci.store.StoragePartitionAdjustmentTrigger;
@@ -213,7 +215,7 @@ public class DaVinciRecordTransformerUtility<K, O> {
                        * We can technically supply the writer schema id, but that would require pulling some logic
                        * out of lazyValue, which could add latency. Revisit if it is actually needed.
                        */
-                      0,
+                      SENTINEL_WRITER_SCHEMA_ID,
                       PubSubSymbolicPosition.EARLIEST,
                       keyBytes.length + valueBytes.length,
                       null)
