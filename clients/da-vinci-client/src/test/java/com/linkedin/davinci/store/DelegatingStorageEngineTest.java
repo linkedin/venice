75e34ff05d0a34e19152cdfc4f140b47cd1ec916
Jialin Liu <jlliu@linkedin.com>
2025-10-10 14:43:51 -0700

    fix unit test coverage

--- clients/da-vinci-client/src/test/java/com/linkedin/davinci/store/DelegatingStorageEngineTest.java
+++ clients/da-vinci-client/src/test/java/com/linkedin/davinci/store/DelegatingStorageEngineTest.java
@@ -6,13 +6,14 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 import com.linkedin.davinci.callback.BytesStreamingCallback;
+import com.linkedin.davinci.compression.KeyUrnCompressor;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 
 public class DelegatingStorageEngineTest {
   @Test
-  public void testKeyUrnCompressorInitialization() {
+  public void testKeyUrnCompressorFunctionInitialization() {
     DelegatingStorageEngine delegate = mock(DelegatingStorageEngine.class);
     DelegatingStorageEngine storageEngine = new DelegatingStorageEngine(delegate);
     Assert.assertNotNull(storageEngine.getKeyDictCompressionFunction());
@@ -23,5 +24,16 @@ public class DelegatingStorageEngineTest {
     BytesStreamingCallback bytesStreamingCallback = mock(BytesStreamingCallback.class);
     storageEngine.getByKeyPrefix(0, key, bytesStreamingCallback);
     verify(delegate, times(1)).getByKeyPrefix(eq(0), eq(key), eq(bytesStreamingCallback));
+  }
+
+  @Test
+  public void testKeyUrnCompressorFunctionUpdate() {
+    DelegatingStorageEngine delegate = mock(DelegatingStorageEngine.class);
+    DelegatingStorageEngine storageEngine = new DelegatingStorageEngine(delegate);
+    Assert.assertNotNull(storageEngine.getKeyDictCompressionFunction());
+    Assert.assertFalse(storageEngine.isKeyUrnCompressionEnabled(0));
+    storageEngine.setKeyDictCompressionFunction(ignored -> mock(KeyUrnCompressor.class));
+    Assert.assertTrue(storageEngine.isKeyUrnCompressionEnabled(0));
+    Assert.assertThrows(() -> storageEngine.setKeyDictCompressionFunction(null));
   }
 }
