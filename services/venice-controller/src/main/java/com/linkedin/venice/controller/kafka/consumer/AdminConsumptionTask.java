a88f6492ce9b94944ada7b05b6d8f6a4486fec83
Minh Nguyen <minnguyen@linkedin.com>
2025-10-14 13:34:34 -0700

    Add admin_operation schema system store

--- services/venice-controller/src/main/java/com/linkedin/venice/controller/kafka/consumer/AdminConsumptionTask.java
+++ services/venice-controller/src/main/java/com/linkedin/venice/controller/kafka/consumer/AdminConsumptionTask.java
@@ -902,6 +902,7 @@ public class AdminConsumptionTask implements Runnable, Closeable {
     if (MessageType.PUT == messageType) {
       Put put = (Put) kafkaValue.payloadUnion;
       try {
+        AdminOperationSerializer.downloadAndAddSchemaIfNecessary(admin, put.schemaId);
         adminOperation = deserializer.deserialize(put.putValue, put.schemaId);
         executionIdFromPayload = adminOperation.executionId;
       } catch (Exception e) {
