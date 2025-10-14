0fae4e4738fae64a1652e2844c440afff29d02d8
Jialin Liu <jlliu@linkedin.com>
2025-10-13 17:31:49 -0700

    [server][dvc] Implement streaming checksum validation for blob receiver

--- clients/da-vinci-client/src/test/java/com/linkedin/davinci/blobtransfer/TestP2PFileTransferClientHandler.java
+++ clients/da-vinci-client/src/test/java/com/linkedin/davinci/blobtransfer/TestP2PFileTransferClientHandler.java
@@ -42,7 +42,6 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import org.mockito.Mockito;
@@ -70,7 +69,6 @@ public class TestP2PFileTransferClientHandler {
     baseDir = Files.createTempDirectory("tmp");
     inputStreamFuture = new CompletableFuture<>();
     storageMetadataService = Mockito.mock(StorageMetadataService.class);
-    checksumValidationExecutorService = Executors.newSingleThreadExecutor();
 
     clientFileHandler = Mockito.spy(
         new P2PFileTransferClientHandler(
@@ -79,8 +77,7 @@ public class TestP2PFileTransferClientHandler {
             TEST_STORE,
             TEST_VERSION,
             TEST_PARTITION,
-            BlobTransferUtils.BlobTransferTableFormat.BLOCK_BASED_TABLE,
-            checksumValidationExecutorService));
+            BlobTransferUtils.BlobTransferTableFormat.BLOCK_BASED_TABLE));
 
     clientMetadataHandler = Mockito.spy(
         new P2PMetadataTransferHandler(
@@ -175,11 +172,6 @@ public class TestP2PFileTransferClientHandler {
 
     ch.writeInbound(response);
     ch.writeInbound(chunk1);
-
-    // Write BLOB_TRANSFER_COMPLETED as exception is validated there.
-    DefaultHttpResponse endOfTransfer = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK);
-    endOfTransfer.headers().add(BLOB_TRANSFER_STATUS, BLOB_TRANSFER_COMPLETED);
-    ch.writeInbound(endOfTransfer);
     try {
       inputStreamFuture.toCompletableFuture().get(1, TimeUnit.MINUTES);
       Assert.fail("Expected exception not thrown");
