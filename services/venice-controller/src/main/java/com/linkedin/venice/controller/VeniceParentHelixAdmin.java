6d270cfff065d97453d499d1618f31428ba32a09
Arjun <abora@linkedin.com>
2025-10-21 11:53:32 -0700

    .

--- services/venice-controller/src/main/java/com/linkedin/venice/controller/VeniceParentHelixAdmin.java
+++ services/venice-controller/src/main/java/com/linkedin/venice/controller/VeniceParentHelixAdmin.java
@@ -4843,8 +4843,12 @@ public class VeniceParentHelixAdmin implements Admin {
    * @see Admin#skipAdminMessage(String, long, boolean, long)
    */
   @Override
-  public void skipAdminMessage(String clusterName, long offset, boolean skipDIV, long executionId) {
-    getVeniceHelixAdmin().skipAdminMessage(clusterName, offset, skipDIV, executionId);
+  public void skipAdminMessage(
+      String clusterName,
+      String typeIdAndBase64PositionBytes,
+      boolean skipDIV,
+      long executionId) {
+    getVeniceHelixAdmin().skipAdminMessage(clusterName, typeIdAndBase64PositionBytes, skipDIV, executionId);
   }
 
   /**
