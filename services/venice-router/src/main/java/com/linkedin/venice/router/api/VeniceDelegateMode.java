c2eda7529404d0d4a11434ad7d83b347cbbce538
Manoj Nagarajan <manojkumar89@yahoo.co.in>
2025-11-03 07:59:37 -0800

    Revert "[router] Add more aggregation to pending_request_count and add more metrics for host selection (#2258)"
    This reverts commit 55e649cb5707620d23ab674d84740c25407825c0.

--- services/venice-router/src/main/java/com/linkedin/venice/router/api/VeniceDelegateMode.java
+++ services/venice-router/src/main/java/com/linkedin/venice/router/api/VeniceDelegateMode.java
@@ -356,7 +356,6 @@ public class VeniceDelegateMode extends ScatterGatherMode {
     H minHost = null;
     for (H h: hosts) {
       Instance node = (Instance) h;
-      routeHttpRequestStats.recordHostConsidered(node.getNodeId());
       if (!path.canRequestStorageNode(node.getNodeId()))
         continue;
       long pendingRequestCount = routeHttpRequestStats.getPendingRequestCount(node.getNodeId());
@@ -382,7 +381,6 @@ public class VeniceDelegateMode extends ScatterGatherMode {
       }
     }
     H finalHost = minHost;
-    routeHttpRequestStats.recordHostSelected(((Instance) finalHost).getNodeId());
     hosts.removeIf(aHost -> !aHost.equals(finalHost));
     host = finalHost;
     return host;
