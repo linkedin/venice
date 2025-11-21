537b4989698c5847f42988f3d12cd53737c9b534
Manoj Nagarajan <manojkumar89@yahoo.co.in>
2025-11-21 00:24:41 -0800

    [fc] Fix a minor race condition while checking replica groupd id (#2307)
    move containsKey() and then get() to get() and a null check
    
    (cherry picked from commit 0ed36c0e67d0eabc6451771d656ed72bcea18db5)

--- clients/venice-client/src/main/java/com/linkedin/venice/fastclient/meta/HelixGroupRoutingStrategy.java
+++ clients/venice-client/src/main/java/com/linkedin/venice/fastclient/meta/HelixGroupRoutingStrategy.java
@@ -48,8 +48,11 @@ public class HelixGroupRoutingStrategy extends AbstractClientRoutingStrategy {
     for (int i = 0; i < groupCnt; i++) {
       int tmpGroupId = groupIds.get((i + groupId) % groupCnt);
       for (String replica: replicas) {
-        if (instanceToGroupIdMapping.containsKey(replica) && instanceToGroupIdMapping.get(replica) == tmpGroupId
-            && instanceHealthMonitor.isRequestAllowed(replica)) {
+        Integer replicaGroupId = instanceToGroupIdMapping.get(replica);
+        if (replicaGroupId == null) {
+          continue;
+        }
+        if (replicaGroupId == tmpGroupId && instanceHealthMonitor.isRequestAllowed(replica)) {
           return replica;
         }
       }
