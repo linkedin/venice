d3a410b46f4a2e6895afdf9e03a01431aad6daa5
Jialin Liu <jlliu@linkedin.com>
2025-11-21 01:29:56 -0800

    add unit test

---
+++ clients/da-vinci-client/src/test/java/com/linkedin/davinci/kafka/consumer/AutoResubscribeTest.java
@@ -0,0 +1,58 @@
+package com.linkedin.davinci.kafka.consumer;
+
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doCallRealMethod;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import com.linkedin.davinci.config.VeniceServerConfig;
+import com.linkedin.venice.utils.concurrent.VeniceConcurrentHashMap;
+import java.util.Map;
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.TimeUnit;
+import org.testng.annotations.Test;
+
+
+public class AutoResubscribeTest {
+  @Test
+  public void testHandleAutoResubscribe() throws InterruptedException {
+    StoreIngestionTask storeIngestionTask = mock(StoreIngestionTask.class);
+    doCallRealMethod().when(storeIngestionTask).maybeProcessResubscribeRequest();
+    VeniceServerConfig serverConfig = mock(VeniceServerConfig.class);
+    doReturn(serverConfig).when(storeIngestionTask).getServerConfig();
+    PriorityBlockingQueue<Integer> resubscribeQueue = new PriorityBlockingQueue<>();
+    doReturn(resubscribeQueue).when(storeIngestionTask).getResubscribeRequestQueue();
+    Map<Integer, Long> resubscribeRequestTimestamp = new VeniceConcurrentHashMap<>();
+    doReturn(resubscribeRequestTimestamp).when(storeIngestionTask).getPartitionToPreviousResubscribeTimeMap();
+    Map<Integer, PartitionConsumptionState> partitionConsumptionStateMap = new VeniceConcurrentHashMap<>();
+    doReturn(partitionConsumptionStateMap).when(storeIngestionTask).getPartitionConsumptionStateMap();
+    doReturn(300).when(serverConfig).getLagBasedReplicaAutoResubscribeIntervalInSeconds();
+
+    doReturn(2).when(serverConfig).getLagBasedReplicaAutoResubscribeMaxReplicaCount();
+
+    PartitionConsumptionState pcs1 = mock(PartitionConsumptionState.class);
+    PartitionConsumptionState pcs3 = mock(PartitionConsumptionState.class);
+    PartitionConsumptionState pcs4 = mock(PartitionConsumptionState.class);
+    partitionConsumptionStateMap.put(1, pcs1);
+    partitionConsumptionStateMap.put(3, pcs3);
+    partitionConsumptionStateMap.put(4, pcs4);
+    resubscribeRequestTimestamp.put(0, System.currentTimeMillis());
+    resubscribeRequestTimestamp.put(1, System.currentTimeMillis() - TimeUnit.SECONDS.toMillis(500));
+    resubscribeRequestTimestamp.put(2, System.currentTimeMillis() - TimeUnit.SECONDS.toMillis(500));
+    resubscribeRequestTimestamp.put(4, System.currentTimeMillis() - TimeUnit.SECONDS.toMillis(500));
+    resubscribeQueue.put(0);
+    resubscribeQueue.put(1);
+    resubscribeQueue.put(2);
+    resubscribeQueue.put(3);
+    resubscribeQueue.put(4);
+
+    storeIngestionTask.maybeProcessResubscribeRequest();
+    verify(storeIngestionTask, times(1)).resubscribe(eq(pcs1));
+    verify(storeIngestionTask, times(1)).resubscribe(eq(pcs3));
+    verify(storeIngestionTask, never()).resubscribe(eq(pcs4));
+
+  }
+}
