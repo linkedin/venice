1dca6dd9645590ce7f1b6b6ad970a893ab0b39d9
Sourav Maji <smaji@linkedin.com>
2025-11-26 12:11:47 -0800

    addressed comments

--- clients/da-vinci-client/src/main/java/com/linkedin/davinci/consumer/VeniceChangelogConsumerDaVinciRecordTransformerImpl.java
+++ clients/da-vinci-client/src/main/java/com/linkedin/davinci/consumer/VeniceChangelogConsumerDaVinciRecordTransformerImpl.java
@@ -441,9 +441,6 @@ public class VeniceChangelogConsumerDaVinciRecordTransformerImpl<K, V>
 
   @Override
   public boolean isCaughtUp() {
-    if (!changelogClientConfig.isStateful()) {
-      throw new VeniceClientException("isCaughtUp is not supported for non-stateful changelog consumer");
-    }
     return isCaughtUp.get();
   }
 
