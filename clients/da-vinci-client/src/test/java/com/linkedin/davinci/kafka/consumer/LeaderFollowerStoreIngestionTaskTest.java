d729032ab98d210d1f0ef9198bf06ef4a013951e
Kai-Sern Lim <kailim@linkedin.com>
2025-12-08 22:37:19 -0800

    Added support for `Delete` in `shouldSyncOffsetFromSnapshot()`. 🏈

--- clients/da-vinci-client/src/test/java/com/linkedin/davinci/kafka/consumer/LeaderFollowerStoreIngestionTaskTest.java
+++ clients/da-vinci-client/src/test/java/com/linkedin/davinci/kafka/consumer/LeaderFollowerStoreIngestionTaskTest.java
@@ -762,6 +762,8 @@ public class LeaderFollowerStoreIngestionTaskTest {
     // Test case 4: When syncBytesInterval is 0 (disabled)
     doReturn(0L).when(mockIngestionTask).getSyncBytesInterval(any());
     assertFalse(mockIngestionTask.shouldSyncOffsetFromSnapshot(regularMessage, mockPartitionConsumptionState));
+
+    final DefaultPubSubMessage nonPutMessage = getMockMessage(4).getMessage();
   }
 
   @Test
