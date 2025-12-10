e1dfe01d1a855a9bb2a7d6f088d8138593ff2b50
Sourav Maji <smaji@linkedin.com>
2025-12-09 22:36:06 -0800

    [controller] Retry topic check during store migration

--- services/venice-controller/src/main/java/com/linkedin/venice/controller/VeniceHelixAdmin.java
+++ services/venice-controller/src/main/java/com/linkedin/venice/controller/VeniceHelixAdmin.java
@@ -3654,8 +3654,7 @@ public class VeniceHelixAdmin implements Admin, StoreCleaner {
       PubSubTopic versionTopic = pubSubTopicRepository.getTopic(Version.composeKafkaTopic(storeName, versionNumber));
       String migrationSourceCluster = storeConfig.getMigrationSrcCluster();
       StoreInfo storeInfo = getStoreInfo(storeName, migrationSourceCluster);
-      int currentVersion = storeInfo.getCurrentVersion();
-      if (currentVersion < versionNumber) {
+      if (storeInfo.getLargestUsedVersionNumber() <= versionNumber) {
         LOGGER.info(
             "Dont skip version {} being pushed during migration, the source cluster may not have processed it.",
             versionNumber);
