1881eb44463669422361f1b16e04d5a915a1011c
Minh Nguyen <minnguyen@linkedin.com>
2026-01-02 13:10:42 -0800

    Add param to optional args and bump storemetavalue to v38

--- internal/venice-common/src/main/java/com/linkedin/venice/serialization/avro/AvroProtocolDefinition.java
+++ internal/venice-common/src/main/java/com/linkedin/venice/serialization/avro/AvroProtocolDefinition.java
@@ -148,7 +148,7 @@ public enum AvroProtocolDefinition {
   /**
    * Value schema for metadata system store.
    */
-  METADATA_SYSTEM_SCHEMA_STORE(37, StoreMetaValue.class),
+  METADATA_SYSTEM_SCHEMA_STORE(38, StoreMetaValue.class),
 
   /*
     Value Schema for Parent Controller Metadata system store
