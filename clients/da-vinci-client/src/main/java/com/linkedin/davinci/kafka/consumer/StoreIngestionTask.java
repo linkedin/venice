3974f92bd15388ffbf3b173802a9818fa41be5e3
Sushant Mane <sushantmane7@gmail.com>
2026-02-07 04:51:42 -0800

    Update DIV skip comment to mention DoL stamps
    The comment was only mentioning heartbeat records but the code also
    skips validation for DoL stamp records. Updated for clarity.

--- clients/da-vinci-client/src/main/java/com/linkedin/davinci/kafka/consumer/StoreIngestionTask.java
+++ clients/da-vinci-client/src/main/java/com/linkedin/davinci/kafka/consumer/StoreIngestionTask.java
@@ -3727,7 +3727,7 @@ public abstract class StoreIngestionTask implements Runnable, Closeable {
     KafkaKey key = consumerRecord.getKey();
     if (key.isControlMessage() && (Arrays.equals(KafkaKey.HEART_BEAT.getKey(), key.getKey())
         || Arrays.equals(KafkaKey.DOL_STAMP.getKey(), key.getKey()))) {
-      return; // Skip validation for ingestion heartbeat records.
+      return; // Skip validation for ingestion heartbeat and DoL stamp records.
     } else if (isGlobalRtDivEnabled() && isRecordSelfProduced(consumerRecord)) {
       // Skip validation for self-produced records. If there were any issues, the followers would've reported it already
       // e.g. Leader->Follower, resubscribe to local VT, consume messages produced by itself (when it was leader)
