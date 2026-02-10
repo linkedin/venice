3d4db25a6c1af120cb25796ccd076c7724258189
Jingyan Li <jingyli@jingyli-mn0199.linkedin.biz>
2026-02-10 14:39:27 -0800

    Add tests

--- clients/da-vinci-client/src/test/java/com/linkedin/davinci/helix/HelixParticipationServiceTest.java
+++ clients/da-vinci-client/src/test/java/com/linkedin/davinci/helix/HelixParticipationServiceTest.java
@@ -1,12 +1,15 @@
 package com.linkedin.davinci.helix;
 
 import static org.mockito.Mockito.doCallRealMethod;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.testng.Assert.expectThrows;
 
 import com.linkedin.davinci.config.VeniceServerConfig;
 import com.linkedin.davinci.storage.StorageService;
+import com.linkedin.venice.exceptions.VeniceException;
 import com.linkedin.venice.helix.HelixPartitionStatusAccessor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -17,7 +20,7 @@ public class HelixParticipationServiceTest {
   private static final Logger LOGGER = LogManager.getLogger(HelixParticipationServiceTest.class);
 
   @Test
-  public void testRestAllInstanceCVStates() {
+  public void testResetAllInstanceCVStates() {
     HelixPartitionStatusAccessor mockAccessor = mock(HelixPartitionStatusAccessor.class);
     StorageService mockStorageService = mock(StorageService.class);
 
@@ -25,6 +28,24 @@ public class HelixParticipationServiceTest {
     HelixParticipationService.resetAllInstanceCVStates(mockAccessor, mockStorageService, LOGGER);
 
     // Verify that the bulk delete API is called instead of per-partition deletion
+    verify(mockAccessor).deleteAllCustomizedStates();
+  }
+
+  @Test
+  public void testResetAllInstanceCVStatesWithException() {
+    HelixPartitionStatusAccessor mockAccessor = mock(HelixPartitionStatusAccessor.class);
+    StorageService mockStorageService = mock(StorageService.class);
+
+    // Make the delete operation throw an exception
+    VeniceException testException = new VeniceException("Test exception during CV cleanup");
+    doThrow(testException).when(mockAccessor).deleteAllCustomizedStates();
+
+    // Call the reset method and expect it to throw the exception
+    expectThrows(
+        VeniceException.class,
+        () -> HelixParticipationService.resetAllInstanceCVStates(mockAccessor, mockStorageService, LOGGER));
+
+    // Verify that the bulk delete API was attempted
     verify(mockAccessor).deleteAllCustomizedStates();
   }
 
