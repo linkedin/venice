4dcda1b35fae86ff752167f4f6dd3686bc8cdd98
Kai-Sern Lim <kailim@linkedin.com>
2026-02-23 23:30:40 -0800

    fixed if branch

--- clients/da-vinci-client/src/main/java/com/linkedin/davinci/kafka/consumer/LeaderFollowerStoreIngestionTask.java
+++ clients/da-vinci-client/src/main/java/com/linkedin/davinci/kafka/consumer/LeaderFollowerStoreIngestionTask.java
@@ -2833,7 +2833,9 @@ public class LeaderFollowerStoreIngestionTask extends StoreIngestionTask {
       }
     } else if (isNonSegmentControlMessage(consumerRecord, null)) {
       return true; // sync when processing most control messages
-    } else if (pcs == null) {
+    }
+
+    if (pcs == null) {
       LOGGER.warn("event=globalRtDiv No PCS found for: {} Will not sync VT DIV", consumerRecord.getTopicPartition());
       return false;
     }
