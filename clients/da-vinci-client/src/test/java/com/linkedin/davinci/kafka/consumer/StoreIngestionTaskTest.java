c2789dd48d9dc8851c0e87c51cc9565b6c106e0c
Sushant Mane <sumane@linkedin.com>
2026-03-17 03:47:36 -0700

    Fix testOffsetSyncBeforeGracefulShutDown: re-enable checkpoint for this specific test

--- clients/da-vinci-client/src/test/java/com/linkedin/davinci/kafka/consumer/StoreIngestionTaskTest.java
+++ clients/da-vinci-client/src/test/java/com/linkedin/davinci/kafka/consumer/StoreIngestionTaskTest.java
@@ -4883,7 +4883,9 @@ public abstract class StoreIngestionTaskTest {
     }).setStoreVersionConfigOverride(configOverride -> {
       // set very high threshold so offsetRecord isn't be synced during regular consumption
       doReturn(100_000L).when(configOverride).getDatabaseSyncBytesIntervalForTransactionalMode();
-    });
+    })
+        .setExtraServerProperties(
+            Collections.singletonMap(SERVER_INGESTION_CHECKPOINT_DURING_GRACEFUL_SHUTDOWN_ENABLED, true));
     runTest(testConfig);
     Assert.assertEquals(mockNotifierError.size(), 0);
   }
