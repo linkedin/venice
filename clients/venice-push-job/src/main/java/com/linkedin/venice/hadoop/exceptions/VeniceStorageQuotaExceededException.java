ddce90341a1689734d74098124b31026c9851467
Ali Poursamadi <apoursamadi@linkedin.com>
2026-07-14 22:10:29 -0700

    [vpj]: Add Spark pre-write storage-quota check with mid-push quota refresh

---
+++ clients/venice-push-job/src/main/java/com/linkedin/venice/hadoop/exceptions/VeniceStorageQuotaExceededException.java
@@ -0,0 +1,18 @@
+package com.linkedin.venice.hadoop.exceptions;
+
+import com.linkedin.venice.exceptions.VeniceException;
+
+
+/**
+ * Thrown when the input data size exceeds the store's storage quota. Raised by the Spark data writer's
+ * pre-write (intermediary) quota check to fail the push before writing the data to Kafka.
+ */
+public class VeniceStorageQuotaExceededException extends VeniceException {
+  public VeniceStorageQuotaExceededException(String message) {
+    super(message);
+  }
+
+  public VeniceStorageQuotaExceededException(String message, Throwable throwable) {
+    super(message, throwable);
+  }
+}
